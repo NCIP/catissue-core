0d46eeac5d6736c471788eb11fe8728e2a0fac19
Gautam <Gautam>
2005-07-19 05:48:42 +0000

    User Action
    SVN-Revision: 30

--- WEB-INF/src/edu/wustl/catissuecore/action/UserAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/UserAction.java
@@ -11,8 +11,6 @@
 package edu.wustl.catissuecore.action;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.ListIterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -23,12 +21,6 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import edu.wustl.catissuecore.dao.AbstractBizLogic;
-import edu.wustl.catissuecore.dao.BizLogicFactory;
-import edu.wustl.catissuecore.domain.ActivityStatus;
-import edu.wustl.catissuecore.domain.Department;
-import edu.wustl.catissuecore.domain.Institute;
-import edu.wustl.catissuecore.domain.Role;
 import edu.wustl.catissuecore.util.global.Constants;
 
 /**
@@ -59,74 +51,77 @@ public class UserAction extends Action
         request.setAttribute(Constants.COUNTRYLIST, Constants.COUNTRYARRAY);
 
         //Sets the cancerResearchGroupList attribute to be used in the Add/Edit User Page.
-        request.setAttribute(Constants.CANCER_RESEARCH_GROUP_LIST, Constants.COUNTRYARRAY);
+        request.setAttribute(Constants.CANCER_RESEARCH_GROUP_LIST, Constants.CANCER_RESEARCH_GROUP_VALUES);
+        
+        //Sets the pageOf attribute (for Add,Edit or Query Interface)
+        String pageName  = request.getParameter(Constants.PAGEOF);
+        request.setAttribute(Constants.PAGEOF,pageName);
 
-        try
-        {
-            AbstractBizLogic dao = BizLogicFactory.getDAO(Constants.USER_FORM_ID);
-            ListIterator iterator = null;
-            int i;
+//        try
+//        {
+//            AbstractBizLogic dao = BizLogicFactory.getDAO(Constants.USER_FORM_ID);
+//            ListIterator iterator = null;
+//            int i;
 //            if (operation.equals(Constants.EDIT))
 //            {
-                //Sets the roleList attribute to be used in the Add/Edit User Page.
-                List roleList = dao.retrieve(Role.class.getName());
-                String[] roleArray = new String[roleList.size()];
-                iterator = roleList.listIterator();
-                i = 0;
-                while (iterator.hasNext())
-                {
-                    Role role = (Role) iterator.next();
-                    roleArray[i] = role.getName();
-                    i++;
-                }
-                request.setAttribute(Constants.ROLELIST, roleArray);
+//                //Sets the roleList attribute to be used in the Add/Edit User Page.
+//                List roleList = dao.retrieve(Role.class.getName());
+//                String[] roleArray = new String[roleList.size()];
+//                iterator = roleList.listIterator();
+//                i = 0;
+//                while (iterator.hasNext())
+//                {
+//                    Role role = (Role) iterator.next();
+//                    roleArray[i] = role.getName();
+//                    i++;
+//                }
+                request.setAttribute(Constants.ROLELIST, Constants.ROLE_VALUES);
 //            }
 
-            //Sets the instituteList attribute to be used in the Add/Edit User Page.
-            List instituteList = dao.retrieve(Institute.class.getName());
-            String[] instituteArray = new String[instituteList.size()];
-            iterator = instituteList.listIterator();
-            i = 0;
-            while (iterator.hasNext())
-            {
-                Institute institute = (Institute) iterator.next();
-                instituteArray[i] = institute.getName();
-                i++;
-            }
-            request.setAttribute(Constants.INSTITUTELIST, instituteArray);
+//            //Sets the instituteList attribute to be used in the Add/Edit User Page.
+//            List instituteList = dao.retrieve(Institution.class.getName());
+//            String[] instituteArray = new String[instituteList.size()];
+//            iterator = instituteList.listIterator();
+//            i = 0;
+//            while (iterator.hasNext())
+//            {
+//                Institution institute = (Institution) iterator.next();
+//                instituteArray[i] = institute.getName();
+//                i++;
+//            }
+            request.setAttribute(Constants.INSTITUTELIST, Constants.INSTITUTE_VALUES);
 
-            //Sets the departmentList attribute to be used in the Add/Edit User Page.
-            List departmentList = dao.retrieve(Department.class.getName());
-            String[] departmentArray = new String[departmentList.size()];
-            iterator = departmentList.listIterator();
-            i = 0;
-            while (iterator.hasNext())
-            {
-                Department department = (Department) iterator.next();
-                departmentArray[i] = department.getName();
-                i++;
-            }
-            request.setAttribute(Constants.DEPARTMENTLIST, departmentArray);
+//            //Sets the departmentList attribute to be used in the Add/Edit User Page.
+//            List departmentList = dao.retrieve(Department.class.getName());
+//            String[] departmentArray = new String[departmentList.size()];
+//            iterator = departmentList.listIterator();
+//            i = 0;
+//            while (iterator.hasNext())
+//            {
+//                Department department = (Department) iterator.next();
+//                departmentArray[i] = department.getName();
+//                i++;
+//            }
+            request.setAttribute(Constants.DEPARTMENTLIST, Constants.DEPARTMENT_VALUES);
 
-            departmentList = dao.retrieve(ActivityStatus.class.getName());
-            String[] activityStatusArray = new String[departmentList.size()];
-            iterator = departmentList.listIterator();
-            i = 0;
-            while (iterator.hasNext())
-            {
-                ActivityStatus activityStatus = (ActivityStatus) iterator
-                        .next();
-                activityStatusArray[i] = activityStatus.getStatus();
-                i++;
-            }
+//            departmentList = dao.retrieve(ActivityStatus.class.getName());
+//            String[] activityStatusArray = new String[departmentList.size()];
+//            iterator = departmentList.listIterator();
+//            i = 0;
+//            while (iterator.hasNext())
+//            {
+//                ActivityStatus activityStatus = (ActivityStatus) iterator
+//                        .next();
+//                activityStatusArray[i] = activityStatus.getStatus();
+//                i++;
+//            }
             request.setAttribute(Constants.ACTIVITYSTATUSLIST,
-                    activityStatusArray);
-        }
-        catch (Exception exc)
-        {
-            exc.printStackTrace();
+                    Constants.ACTIVITY_STATUS_VALUES);
+//        }
+//        catch (Exception exc)
+//        {
 //            Logger.out.error(exc.getMessage());
-        }
+//        }
 
         return mapping.findForward(Constants.SUCCESS);
     }
