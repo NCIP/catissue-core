cbc352a5b00b325073dfebfac0e89befe9f103a7
Aarti <Aarti>
2005-07-27 05:50:31 +0000

    user not authenticated exception
    SVN-Revision: 193

---
+++ WEB-INF/src/edu/wustl/catissuecore/exception/UserNotAuthenticatedException.java
@@ -0,0 +1,25 @@
+/**
+ *<p>Title: </p>
+ *<p>Description:  </p>
+ *<p>Copyright: (c) Washington University, School of Medicine 2004</p>
+ *<p>Company: Washington University, School of Medicine, St. Louis.</p>
+ *@author Aarti Sharma
+ *@version 1.0
+ */ 
+package edu.wustl.catissuecore.exception;
+
+
+/**
+ *<p>Title: </p>
+ *<p>Description:  </p>
+ *<p>Copyright: (c) Washington University, School of Medicine 2005</p>
+ *<p>Company: Washington University, School of Medicine, St. Louis.</p>
+ *@author Aarti Sharma
+ *@version 1.0
+ */
+
+public class UserNotAuthenticatedException extends Exception
+{
+
+    
+}
