148aba3e15ddbed107461eca235359511600a9a0
Aarti <Aarti>
2005-07-30 12:40:35 +0000

    name value bean added for dropdowns
    SVN-Revision: 259

---
+++ WEB-INF/src/edu/wustl/common/beans/NameValueBean.java
@@ -0,0 +1,74 @@
+/**
+ *<p>Title: </p>
+ *<p>Description:  </p>
+ *<p>Copyright: (c) Washington University, School of Medicine 2004</p>
+ *<p>Company: Washington University, School of Medicine, St. Louis.</p>
+ *@author Aarti Sharma
+ *@version 1.0
+ */ 
+package edu.wustl.common.beans;
+
+
+/**
+ *<p>Title: </p>
+ *<p>Description:  </p>
+ *<p>Copyright: (c) Washington University, School of Medicine 2005</p>
+ *<p>Company: Washington University, School of Medicine, St. Louis.</p>
+ *@author Aarti Sharma
+ *@version 1.0
+ */
+
+public class NameValueBean
+{
+    private String name=new String();
+    private String value=new String();
+    
+    public NameValueBean()
+    {
+        
+    }
+    
+    public NameValueBean(String name, String value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+    
+    /**
+     * @return Returns the name.
+     */
+    public String getName()
+    {
+        return name;
+    }
+    /**
+     * @param name The name to set.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    /**
+     * @return Returns the value.
+     */
+    public String getValue()
+    {
+        return value;
+    }
+    /**
+     * @param value The value to set.
+     */
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    public String toString()
+    {
+        return new String("name:"+ name +" value:"+value);
+    }
+}
