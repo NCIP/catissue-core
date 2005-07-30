fdb1326473cb388e909d55f4520a477395f3a703
Kapil <Kapil>
2005-07-30 05:39:55 +0000

    Collection Protocol Chages
    SVN-Revision: 247

--- WEB-INF/src/edu/wustl/catissuecore/actionForm/CollectionProtocolForm.java
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/CollectionProtocolForm.java
@@ -12,16 +12,19 @@
 package edu.wustl.catissuecore.actionForm;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Collection;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.domain.AbstractDomainObject;
 //import edu.wustl.catissuecore.domain.User;
 import edu.wustl.catissuecore.domain.CollectionProtocol;
+import edu.wustl.catissuecore.util.global.ApplicationProperties;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Validator;
 import edu.wustl.common.util.logger.Logger;
@@ -32,7 +35,8 @@ import edu.wustl.common.util.logger.Logger;
  * 
  * @author gautam_shetty
  */
-public class CollectionProtocolForm extends AbstractActionForm {
+public class CollectionProtocolForm extends AbstractActionForm
+{
 	/**
 	 * identifier is a unique id assigned to each User.
 	 */
@@ -41,7 +45,7 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	/**
 	 * Represents the operation(Add/Edit) to be performed.
 	 */
-	private String operation ;
+	private String operation;
 
 	private String activityStatus;
 
@@ -49,30 +53,20 @@ public class CollectionProtocolForm extends AbstractActionForm {
 
 	private long protocolCoordinatorIds[];
 
-	private String irbid;
+	private String irbID;
 
-	private String descriptionurl;
+	private String descriptionURL;
 
 	private String title;
 
-	private String shorttitle;
+	private String shortTitle;
 
-//	private java.util.Date startDate;
 	private String startDate;
-	
-//	private java.util.Date endDate;
-	
-	private String endDate;
-	
-	private String enrollment;
 
-	/*
-	 * private String clinicalStatus; private String studycalendartitle; private
-	 * String enrollment; private String pathologyStatus;
-	 * 
-	 * private String tissueSide; private String tissueSite; private String
-	 * specimenType; private String specimenSubType;
-	 */
+	private String endDate;
+
+	private int enrollment;
+
 	/**
 	 * Map to handle values of all the CollectionProtocol Events
 	 */
@@ -80,13 +74,11 @@ public class CollectionProtocolForm extends AbstractActionForm {
 
 	/**
 	 * Associates the specified object with the specified key in the map.
-	 * 
-	 * @param key
-	 *            the key to which the object is mapped.
-	 * @param value
-	 *            the object which is mapped.
+	 * @param key the key to which the object is mapped.
+	 * @param value the object which is mapped.
 	 */
-	public void setValue(String key, Object value) {
+	public void setValue(String key, Object value)
+	{
 		values.put(key, value);
 	}
 
@@ -97,14 +89,16 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 *            the required key.
 	 * @return the object to which this map maps the specified key.
 	 */
-	public Object getValue(String key) {
+	public Object getValue(String key)
+	{
 		return values.get(key);
 	}
 
 	/**
 	 * @return Returns the values.
 	 */
-	public Collection getAllValues() {
+	public Collection getAllValues()
+	{
 		return values.values();
 	}
 
@@ -112,62 +106,41 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param values
 	 *            The values to set.
 	 */
-	public void setValues(Map values) {
+	public void setValues(Map values)
+	{
 		this.values = values;
 	}
 
 	/**
 	 * No argument constructor for CollectionProtocolForm class.
 	 */
-	public CollectionProtocolForm() {
+	public CollectionProtocolForm()
+	{
 		reset();
-	}
-
-	/**
-	 * Copies the data from an AbstractDomain object to a CollectionProtocolForm
-	 * object.
-	 * 
-	 * @param abstractDomain
-	 *            An AbstractDomain object.
-	 */
-	public void setAllValues(AbstractDomainObject abstractDomain) {
-		try {
-			CollectionProtocol collectionProtocol = (CollectionProtocol) abstractDomain;
-			this.systemIdentifier =  collectionProtocol.getSystemIdentifier().longValue() ;
-			this.activityStatus = collectionProtocol.getActivityStatus();
-			this.principalInvestigatorId = collectionProtocol.getPrincipalInvestigator().getSystemIdentifier().longValue() ;
-			this.title = collectionProtocol.getTitle();
-			this.shorttitle = collectionProtocol.getShortTitle() ;
-			this.startDate  = collectionProtocol.getStartDate().toString();
-			this.endDate = collectionProtocol.getEndDate().toString();
-			
-			
-			
-		} catch (Exception excp) {
-			excp.printStackTrace();
-			Logger.out.error(excp.getMessage());
-		}
 	}
 
 	/**
 	 * @return Returns the descriptionurl.
 	 */
-	public String getDescriptionurl() {
-		return descriptionurl;
+	public String getDescriptionURL()
+	{
+		return descriptionURL;
 	}
 
 	/**
 	 * @param descriptionurl
 	 *            The descriptionurl to set.
 	 */
-	public void setDescriptionurl(String descriptionurl) {
-		this.descriptionurl = descriptionurl;
+	public void setDescriptionURL(String descriptionurl)
+	{
+		this.descriptionURL = descriptionurl;
 	}
 
 	/**
 	 * @return Returns the endDate.
 	 */
-	public String getEndDate() {
+	public String getEndDate()
+	{
 		return endDate;
 	}
 
@@ -175,29 +148,33 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param endDate
 	 *            The endDate to set.
 	 */
-	public void setEndDate(String endDate) {
+	public void setEndDate(String endDate)
+	{
 		this.endDate = endDate;
 	}
 
 	/**
 	 * @return Returns the irbid.
 	 */
-	public String getIrbid() {
-		return irbid;
+	public String getIrbID()
+	{
+		return irbID;
 	}
 
 	/**
 	 * @param irbid
 	 *            The irbid to set.
 	 */
-	public void setIrbid(String irbid) {
-		this.irbid = irbid;
+	public void setIrbID(String irbid)
+	{
+		this.irbID = irbid;
 	}
 
 	/**
 	 * @return Returns the participants.
 	 */
-	public String getEnrollment() {
+	public int getEnrollment()
+	{
 		return enrollment;
 	}
 
@@ -205,14 +182,16 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param participants
 	 *            The participants to set.
 	 */
-	public void setEnrollment(String participants) {
+	public void setEnrollment(int participants)
+	{
 		this.enrollment = participants;
 	}
 
 	/**
 	 * @return Returns the principalinvestigatorid.
 	 */
-	public long getPrincipalInvestigatorId() {
+	public long getPrincipalInvestigatorId()
+	{
 		return principalInvestigatorId;
 	}
 
@@ -220,14 +199,16 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param principalinvestigator
 	 *            The principalinvestigator to set.
 	 */
-	public void setPrincipalInvestigatorId(long principalInvestigatorId) {
+	public void setPrincipalInvestigatorId(long principalInvestigatorId)
+	{
 		this.principalInvestigatorId = principalInvestigatorId;
 	}
 
 	/**
 	 * @return Returns the protocolcoordinator ids.
 	 */
-	public long[] getProtocolCoordinatorIds() {
+	public long[] getProtocolCoordinatorIds()
+	{
 		return protocolCoordinatorIds;
 	}
 
@@ -235,29 +216,33 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param protocolcoordinators
 	 *            The protocolcoordinators to set.
 	 */
-	public void setProtocolcoordinators(long[] protocolCoordinatorIds) {
+	public void setProtocolcoordinators(long[] protocolCoordinatorIds)
+	{
 		this.protocolCoordinatorIds = protocolCoordinatorIds;
 	}
 
 	/**
 	 * @return Returns the shorttitle.
 	 */
-	public String getShorttitle() {
-		return shorttitle;
+	public String getShortTitle()
+	{
+		return shortTitle;
 	}
 
 	/**
 	 * @param shorttitle
 	 *            The shorttitle to set.
 	 */
-	public void setShorttitle(String shorttitle) {
-		this.shorttitle = shorttitle;
+	public void setShortTitle(String shorttitle)
+	{
+		this.shortTitle = shorttitle;
 	}
 
 	/**
 	 * @return Returns the startDate.
 	 */
-	public String getStartDate() {
+	public String getStartDate()
+	{
 		return startDate;
 	}
 
@@ -265,14 +250,16 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param startDate
 	 *            The startDate to set.
 	 */
-	public void setStartDate(String startDate) {
+	public void setStartDate(String startDate)
+	{
 		this.startDate = startDate;
 	}
 
 	/**
 	 * @return Returns the title.
 	 */
-	public String getTitle() {
+	public String getTitle()
+	{
 		return title;
 	}
 
@@ -280,7 +267,8 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param title
 	 *            The title to set.
 	 */
-	public void setTitle(String title) {
+	public void setTitle(String title)
+	{
 		this.title = title;
 	}
 
@@ -288,37 +276,24 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param operation
 	 *            The operation to set.
 	 */
-	public void setOperation(String operation) {
+	public void setOperation(String operation)
+	{
 		this.operation = operation;
 	}
 
 	/**
 	 * Returns the id assigned to form bean
 	 */
-	public int getFormId() {
-		return Constants.COLLECTIONPROTOCOL_FORM_ID;
-	}
-
-	/**
-	 * Resets the values of all the fields. This method defined in ActionForm is
-	 * overridden in this class.
-	 */
-	public void reset(ActionMapping mapping, HttpServletRequest request) {
-		reset();
-	}
-
-	/**
-	 * Resets the values of all the fields. Is called by the overridden reset
-	 * method defined in ActionForm.
-	 */
-	private void reset() {
-
+	public int getFormId()
+	{
+		return Constants.COLLECTION_PROTOCOL_FORM_ID;
 	}
 
 	/**
 	 * @return Returns the activityStatus.
 	 */
-	public String getActivityStatus() {
+	public String getActivityStatus()
+	{
 		return activityStatus;
 	}
 
@@ -326,7 +301,8 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @param activityStatus
 	 *            The activityStatus to set.
 	 */
-	public void setActivityStatus(String activityStatus) {
+	public void setActivityStatus(String activityStatus)
+	{
 		this.activityStatus = activityStatus;
 	}
 
@@ -336,7 +312,8 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * @return Returns true if operation is equal to "add", else it returns
 	 *         false
 	 */
-	public boolean isAddOperation() {
+	public boolean isAddOperation()
+	{
 		return (getOperation().equals(Constants.ADD));
 	}
 
@@ -345,72 +322,169 @@ public class CollectionProtocolForm extends AbstractActionForm {
 	 * 
 	 * @return Returns the operation.
 	 */
-	public String getOperation() {
+	public String getOperation()
+	{
 		return operation;
-	}
-
-	/**
-	 * Overrides the validate method of ActionForm.
-	 */
-	public ActionErrors validate(ActionMapping mapping,
-			HttpServletRequest request) {
-		ActionErrors errors = null;
-		Validator validator = new Validator();
-		try 
-		{
-			System.out.println("\n------------------------");
-			
-			
-			System.out.println(this.getPrincipalInvestigatorId());
-			if (this.getProtocolCoordinatorIds() != null)
-			{
-				System.out.println(this.getProtocolCoordinatorIds());
-			}
-			System.out.println(this.getTitle());
-			System.out.println(this.getStartDate());
-			java.util.Iterator i = this.values.keySet().iterator() ;
-			
-			while(i.hasNext() )
-			{
-				Object o = i.next();
-				System.out.print(o);
-				System.out.println(" : "+this.getValue(o.toString() )); 
-			}
-			System.out.println("\n===============\n");
-		}
-		catch (Exception excp)
-		{
-			errors = new ActionErrors();
-		}
-		return errors;
 	}
 
 	/**
 	 * @return Returns the values.
 	 */
-	public Map getValues() {
+	public Map getValues()
+	{
 		return values;
 	}
+
 	/**
 	 * @param protocolCoordinatorIds The protocolCoordinatorIds to set.
 	 */
-	public void setProtocolCoordinatorIds(long[] protocolCoordinatorIds) {
+	public void setProtocolCoordinatorIds(long[] protocolCoordinatorIds)
+	{
 		this.protocolCoordinatorIds = protocolCoordinatorIds;
 	}
-	
-	
-	
-	
+
 	/**
 	 * @return Returns the systemIdentifier.
 	 */
-	public long getSystemIdentifier() {
+	public long getSystemIdentifier()
+	{
 		return systemIdentifier;
 	}
+
 	/**
 	 * @param systemIdentifier The systemIdentifier to set.
 	 */
-	public void setSystemIdentifier(long systemIdentifier) {
+	public void setSystemIdentifier(long systemIdentifier)
+	{
 		this.systemIdentifier = systemIdentifier;
+	}
+	/**
+	 * Copies the data from an AbstractDomain object to a CollectionProtocolForm
+	 * object.
+	 * 
+	 * @param abstractDomain
+	 *            An AbstractDomain object.
+	 */
+	public void setAllValues(AbstractDomainObject abstractDomain)
+	{
+		try
+		{
+			CollectionProtocol collectionProtocol = (CollectionProtocol) abstractDomain;
+			this.systemIdentifier = collectionProtocol.getSystemIdentifier().longValue();
+			this.activityStatus = collectionProtocol.getActivityStatus();
+			this.principalInvestigatorId = collectionProtocol.getPrincipalInvestigator()
+					.getSystemIdentifier().longValue();
+			this.title = collectionProtocol.getTitle();
+			this.shortTitle = collectionProtocol.getShortTitle();
+			this.startDate = collectionProtocol.getStartDate().toString();
+			this.endDate = collectionProtocol.getEndDate().toString();
+			this.irbID = collectionProtocol.getIrbIdentifier();
+			this.enrollment = collectionProtocol.getEnrollment().intValue();
+			this.descriptionURL = collectionProtocol.getDescriptionURL();
+		}
+		catch (Exception excp)
+		{
+			excp.printStackTrace();
+			Logger.out.error(excp.getMessage());
+		}
+	}
+	
+	/**
+	 * Resets the values of all the fields. This method defined in ActionForm is
+	 * overridden in this class.
+	 */
+	public void reset(ActionMapping mapping, HttpServletRequest request)
+	{
+		reset();
+	}
+
+	/**
+	 * Resets the values of all the fields. Is called by the overridden reset
+	 * method defined in ActionForm.
+	 */
+	private void reset()
+	{
+		this.systemIdentifier = 0;
+		this.activityStatus = null;
+		this.principalInvestigatorId = 0;
+		this.title = null;
+		this.shortTitle = null;
+		this.startDate = null;
+		this.endDate = null;
+		this.irbID = null;
+		this.enrollment = 0;
+		this.descriptionURL = null;
+		
+		values = new HashMap();
+	}
+	
+	/**
+	 * Overrides the validate method of ActionForm.
+	 */
+	public ActionErrors validate(ActionMapping mapping, HttpServletRequest request)
+	{
+		ActionErrors errors = new ActionErrors();
+		Validator validator = new Validator();
+		try
+		{
+			if (operation.equals(Constants.ADD) || operation.equals(Constants.EDIT))
+            {
+                if(this.principalInvestigatorId == -1)
+				{
+					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.selected",ApplicationProperties.getValue("collectionprotocol.principalinvestigator")));
+				}
+                if (validator.isEmpty(this.title))
+                {
+                	errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.required",ApplicationProperties.getValue("collectionprotocol.protocoltitle")));
+                }
+                
+    			Iterator it = this.values.keySet().iterator();
+    			while (it.hasNext())
+    			{
+    				String key = (String)it.next();
+    				String value = (String)values.get(key);
+    				System.out.println(key+ " : " + value);
+    				
+    				if(key.indexOf("clinicalStatus")!=-1 && value.equals(Constants.SELECT_OPTION))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.selected",ApplicationProperties.getValue("collectionprotocol.clinicalstatus")));
+    				}
+    				
+    				if(key.indexOf("studyCalendarEventPoint")!=-1 && validator.isEmpty(value))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.required",ApplicationProperties.getValue("collectionprotocol.studycalendartitle")));
+    				}
+
+    				if(key.indexOf("specimenClass")!=-1 && value.equals(Constants.SELECT_OPTION))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.selected",ApplicationProperties.getValue("collectionprotocol.specimenclass")));
+    				}
+    				
+    				if(key.indexOf("specimenType")!=-1 && value.equals(Constants.SELECT_OPTION))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.selected",ApplicationProperties.getValue("collectionprotocol.specimetype")));
+    				}
+    				
+    				if(key.indexOf("tissueSite")!=-1 && value.equals(Constants.SELECT_OPTION))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.selected",ApplicationProperties.getValue("collectionprotocol.specimensite")));
+    				}
+    				if(key.indexOf("pathologyStatus")!=-1 && value.equals(Constants.SELECT_OPTION))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.selected",ApplicationProperties.getValue("collectionprotocol.specimenstatus")));
+    				}
+    				if(key.indexOf("quantityIn")!=-1 && validator.isEmpty(value))
+    				{
+    					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.required",ApplicationProperties.getValue("collectionprotocol.quantity")));
+    				}
+    			}
+            }    
+		}
+		catch (Exception excp)
+		{
+			excp.printStackTrace();
+			errors = new ActionErrors();
+		}
+		return errors;
 	}
 }
