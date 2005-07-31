1eb33a8a7c2fbb3e1fc023a0d521a4ffb540245f
Gautam <Gautam>
2005-07-31 10:58:02 +0000

    *** empty log message ***
    SVN-Revision: 269

--- WEB-INF/src/edu/wustl/catissuecore/util/global/Utility.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/global/Utility.java
@@ -1,27 +1,31 @@
-/*
- * Created on Apr 12, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+/**
+ * <p>Title: Utility Class>
+ * <p>Description:  Utility Class contain general methods used through out the application. </p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Gautam Shetty
+ * @version 1.00
  */
+
 package edu.wustl.catissuecore.util.global;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.StringTokenizer;
-
-import edu.wustl.common.util.logger.Logger;
 
 /**
+ * Utility Class contain general methods used through out the application.
  * @author kapil_kaveeshwar
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class Utility
 {
+    /**
+     * Parses the string format of date in the given format and returns the Data object.
+     * @param date the string containing date.
+     * @param pattern the pattern in which the date is present.
+     * @return the string format of date in the given format and returns the Data object.
+     * @throws ParseException
+     */
 	public static Date parseDate(String date,String pattern) throws ParseException
 	{
 		try
@@ -36,37 +40,35 @@ public class Utility
 		}
 	}
 	
+	/**
+	 * Parses the Date in given format and returns the string representation.
+	 * @param date the Date to be parsed.
+	 * @param pattern the pattern of the date.
+	 * @return
+	 */
 	public static String parseDateToString(Date date, String pattern)
 	{
 	    String d = null;
 	    
-	    try
-	    {
-	        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-		    d = dateFormat.format(date);
+	    SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+		d = dateFormat.format(date);
 		    
-	    }
-	    catch(Exception excp)
-	    {
-	        Logger.out.error(excp.getMessage(),excp);
-	    }
 	    return d;
 	}
 	
-	public static void main(String[] args)
-    {
-	    try{
-	      String date = "10-22-2005";  
-//	      String pattern = "yyyy-MM-dd";
-	      String pattern = "MM-dd-yyyy";
-	      Date d = parseDate(date,pattern);
-	      String dd = parseDateToString(d,pattern);
-	      System.out.println("Date........."+d);
-	      System.out.println("String......."+dd);
-	    }
-	    catch(ParseException pexcp)
-	    {
-	        System.out.println("Exception"+pexcp.getMessage());
-	    }
-    }
+//	public static void main(String[] args)
+//    {
+//	    try{
+//	      String date = "2005-10-22";  
+//	      String pattern = "yyyy-MM-dd HH:mm aa";
+//	      Date d = parseDate(date,pattern);
+//	      String dd = parseDateToString(d,pattern);
+//	      System.out.println("Date........."+d);
+//	      System.out.println("String......."+dd);
+//	    }
+//	    catch(ParseException pexcp)
+//	    {
+//	        System.out.println("Exception"+pexcp.getMessage());
+//	    }
+//    }
 }
