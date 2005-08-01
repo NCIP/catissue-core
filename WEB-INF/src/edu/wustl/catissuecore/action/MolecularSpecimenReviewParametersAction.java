13d604e003d09d0a9873065d72571c5573147808
Mandar <Mandar>
2005-08-01 10:10:12 +0000

    Changes made by mandar on 01aug related tomolecular specimenreviewEVENT PARAMETERS FORM
    SVN-Revision: 296

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/MolecularSpecimenReviewParametersAction.java
@@ -0,0 +1,29 @@
+/**
+ * <p>Title: MolecularSpecimenReviewParametersAction Class>
+ * <p>Description:	This class initializes the fields in the MolecularSpecimenReviewParameters Add/Edit webpage.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Mandar Deshmukh
+ * @version 1.00
+ * Created on July 30, 2005
+ */
+package edu.wustl.catissuecore.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * @author mandar_deshmukh
+ *
+ * This class initializes the fields in the MolecularSpecimenReviewParameters Add/Edit webpage.
+ */
+public class MolecularSpecimenReviewParametersAction extends SpecimenEventParametersAction
+{
+	protected void setRequestParameters(HttpServletRequest request)
+	{
+		super.setRequestParameters(request);
+		
+		// class specific code goes here.
+        
+	}
+}
