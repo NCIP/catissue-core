dd56dca2e1cf0fa825f6fa0d972b13de3a4900b4
Mandar <Mandar>
2005-08-05 12:36:23 +0000

    thaw event parameters updated and working
    SVN-Revision: 429

--- WEB-INF/src/edu/wustl/catissuecore/domain/ThawEventParameters.java
+++ WEB-INF/src/edu/wustl/catissuecore/domain/ThawEventParameters.java
@@ -9,6 +9,10 @@
  */
 package edu.wustl.catissuecore.domain;
 
+import edu.wustl.catissuecore.actionForm.AbstractActionForm;
+import edu.wustl.catissuecore.actionForm.ThawEventParametersForm;
+import edu.wustl.common.util.logger.Logger;
+
 /**
  * Attributes associated with a thawing event of a specimen.
  * @hibernate.joined-subclass table="CATISSUE_THAW_EVENT_PARAMETERS"
@@ -18,4 +22,40 @@ package edu.wustl.catissuecore.domain;
 public class ThawEventParameters extends SpecimenEventParameters implements java.io.Serializable
 {
 	private static final long serialVersionUID = 1234567890L;
+	
+	/**
+	 * NOTE: Do not delete this constructor. Hibernet uses this by reflection API.
+	 * */
+	public ThawEventParameters()
+	{
+		
+	}
+
+	/**
+	 *  Parameterised constructor 
+	 * @param abstractForm
+	 */
+	public ThawEventParameters(AbstractActionForm abstractForm)
+	{
+		setAllValues(abstractForm);
+	}
+	
+	/**
+     * This function Copies the data from an ThawEventParameters object.
+     * @param ThawEventParametersForm An ThawEventParametersForm object containing the information about the ThawEventParameters.  
+     * */
+    public void setAllValues(AbstractActionForm abstractForm)
+    {
+        try
+        {
+        	ThawEventParametersForm form = (ThawEventParametersForm) abstractForm;
+
+        	super.setAllValues(form);
+        }
+        catch (Exception excp)
+        {
+            Logger.out.error(excp.getMessage());
+        }
+    }
+	
 }
