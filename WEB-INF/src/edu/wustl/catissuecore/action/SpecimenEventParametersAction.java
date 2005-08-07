8ab35397f3c90a7abeb42b1be095867bda9d8b5f
Kapil <Kapil>
2005-08-07 06:23:40 +0000

    User from DB
    SVN-Revision: 446

--- WEB-INF/src/edu/wustl/catissuecore/action/SpecimenEventParametersAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/SpecimenEventParametersAction.java
@@ -11,7 +11,6 @@
 package edu.wustl.catissuecore.action;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.servlet.ServletException;
@@ -26,7 +25,6 @@ import org.apache.struts.action.ActionMapping;
 import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
 import edu.wustl.catissuecore.bizlogic.UserBizLogic;
 import edu.wustl.catissuecore.util.global.Constants;
-import edu.wustl.common.beans.NameValueBean;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -52,13 +50,13 @@ public class SpecimenEventParametersAction extends Action
        try
        {
         	
-//        	UserBizLogic userBizLogic = (UserBizLogic)BizLogicFactory.getBizLogic(Constants.USER_FORM_ID);
-//        	Collection coll =  userBizLogic.getUsers(Constants.ACTIVITY_STATUS_ACTIVE);
-			Collection coll =  new ArrayList();
-			NameValueBean aNameValueBean = new NameValueBean();
-			aNameValueBean.setName(Constants.SELECT_OPTION);
-			aNameValueBean.setValue("-1");
-			coll.add(aNameValueBean);
+        	UserBizLogic userBizLogic = (UserBizLogic)BizLogicFactory.getBizLogic(Constants.USER_FORM_ID);
+        	Collection coll =  userBizLogic.getUsers(Constants.ACTIVITY_STATUS_ACTIVE);
+//			Collection coll =  new ArrayList();
+//			NameValueBean aNameValueBean = new NameValueBean();
+//			aNameValueBean.setName(Constants.SELECT_OPTION);
+//			aNameValueBean.setValue("-1");
+//			coll.add(aNameValueBean);
 		
         	request.setAttribute(Constants.USERLIST, coll);
             
