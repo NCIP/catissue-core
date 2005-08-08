dde65f899426ad7f67652557020023a28a0d0817
Aniruddha <Aniruddha>
2005-08-08 13:52:12 +0000

    New Specimen
    SVN-Revision: 467

--- WEB-INF/src/edu/wustl/catissuecore/action/NewSpecimenAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/NewSpecimenAction.java
@@ -9,6 +9,7 @@
 
 package edu.wustl.catissuecore.action;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,10 +20,10 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import edu.wustl.catissuecore.bizlogic.AbstractBizLogic;
+import edu.wustl.catissuecore.actionForm.NewSpecimenForm;
 import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.bizlogic.NewSpecimenBizLogic;
 import edu.wustl.catissuecore.domain.Biohazard;
-import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
 import edu.wustl.catissuecore.util.global.Constants;
 
 
@@ -49,23 +50,63 @@ public class NewSpecimenAction extends Action
         
         request.setAttribute(Constants.PAGEOF,pageOf);
         
-        AbstractBizLogic dao = BizLogicFactory.getBizLogic(Constants.NEW_SPECIMEN_FORM_ID);
+        NewSpecimenBizLogic dao = (NewSpecimenBizLogic)BizLogicFactory.getBizLogic(Constants.NEW_SPECIMEN_FORM_ID);
+        NewSpecimenForm newForm = (NewSpecimenForm) form;
         
-        String sourceObjectName = SpecimenCollectionGroup.class.getName();
-        String[] displayNameFields = {"systemIdentifier"};
-        String valueField = "systemIdentifier";
+        try
+		{
+            //Sets the Biohazard list of corresponding type.
+//        	HashMap map = new HashMap();
+//        	
+//        	for(int i=0;i<Constants.BIOHAZARD_TYPE_ARRAY.length;i++)
+//        	{
+//	            String sourceObjectName = Biohazard.class.getName();
+//	            String[] displayNameFields = {"name"};
+//	            String valueField = "systemIdentifier";
+//	            String[] whereColumnName = {"type"};
+//	            String[] whereColumnCondition = {"="};
+//	            Object[] whereColumnValue = {Constants.BIOHAZARD_TYPE_ARRAY[i]};
+//	            String joinCondition = Constants.AND_JOIN_CONDITION;
+//	            String separatorBetweenFields = "";
+//				
+//	            List biohazardList = dao.getList(sourceObjectName, displayNameFields, valueField,whereColumnName,
+//	            				whereColumnCondition,whereColumnValue,joinCondition,separatorBetweenFields);
+//	            
+//	            map.put(Constants.BIOHAZARD_TYPE_ARRAY[i],biohazardList);
+//        	}
+//        	
+//        	request.setAttribute(Constants.BIOHAZARD_NAME_LIST, map);
+        	request.setAttribute(Constants.BIOHAZARD_TYPE_LIST, Constants.BIOHAZARD_TYPE_ARRAY);
+        	
+           	List biohazardList = dao.retrieve(Biohazard.class.getName());
+        	Iterator iterator = biohazardList.iterator();
+        	if(biohazardList!=null && biohazardList.size()>0)
+        	{
+	        	String [] bhIdArray =  new String[biohazardList.size()];
+	        	String [] bhTypeArray =  new String[biohazardList.size()];
+	        	String [] bhNameArray =  new String[biohazardList.size()];
+	        	int i=0;
+	        	
+	        	while(iterator.hasNext())
+	        	{
+	        		Biohazard hazard = (Biohazard)iterator.next();
+	        		bhIdArray[i] = String.valueOf(hazard.getSystemIdentifier());
+	        		bhNameArray[i] = hazard.getName();
+	        		bhTypeArray[i] = hazard.getType();
+	        		i++;
+	        	}
+	        	
+	        	request.setAttribute(Constants.BIOHAZARD_NAME_LIST, bhNameArray);
+	        	request.setAttribute(Constants.BIOHAZARD_ID_LIST, bhIdArray);
+	        	request.setAttribute(Constants.BIOHAZARD_TYPES_LIST, bhTypeArray);
+        	}
+		}
+        catch(Exception e)
+		{
+        	e.printStackTrace();
+		}
         
-        List specimenList = dao.getList(sourceObjectName, displayNameFields, valueField);
-        request.setAttribute(Constants.SPECIMENCOLLECTIONLIST, specimenList);
-        
-        sourceObjectName = Biohazard.class.getName();
-        String [] displayNameFields1 = {"name"};
-        valueField = "systemIdentifier";
-        
-        List biohazardList = dao.getList(sourceObjectName, displayNameFields1, valueField);
-        request.setAttribute(Constants.SPECIMENCOLLECTIONLIST, biohazardList);
-        
-        String [] specimenCollectionGroupArray = {"1","2","3","4"};
+        String [] specimenCollectionGroupArray = {Constants.SELECT_OPTION,"1","2","3","4"};
         
         request.setAttribute(Constants.SPECIMEN_COLLECTION_GROUP_LIST,specimenCollectionGroupArray);
         
@@ -78,8 +119,6 @@ public class NewSpecimenAction extends Action
         request.setAttribute(Constants.TISSUE_SIDE_LIST,Constants.TISSUE_SIDE_VALUES);
         
         request.setAttribute(Constants.PATHOLOGICAL_STATUS_LIST, Constants.PATHOLOGICAL_STATUS_VALUES);
-        
-        request.setAttribute(Constants.BIOHAZARD_NAME_LIST, Constants.BIOHAZARD_NAME_VALUES);
         
         request.setAttribute(Constants.BIOHAZARD_TYPE_LIST, Constants.BIOHAZARD_TYPE_ARRAY);
         
