2fe3594755f198483149000f074823765daaacb5
Aarti <Aarti>
2005-08-09 05:51:15 +0000

    Class to hold permission strings of the application
    SVN-Revision: 475

---
+++ WEB-INF/src/edu/wustl/catissuecore/util/Permissions.java
@@ -0,0 +1,21 @@
+
+package edu.wustl.catissuecore.util;
+
+/**
+ * Interface for storing the names of Permissions that are defined
+ * in the authorization database.
+ * 
+ * 
+ * @author Brian Husted
+ */
+public interface Permissions {
+
+	public static final String READ = "READ";
+	public static final String UPDATE = "UPDATE";
+	public static final String DELETE = "DELETE";
+	public static final String CREATE = "CREATE";
+	public static final String EXECUTE = "EXECUTE";
+	public static final String USE = "USE";
+	public static final String ASSIGN_READ = "ASSIGN_READ";
+	public static final String ASSIGN_USE = "ASSIGN_USE";
+}
