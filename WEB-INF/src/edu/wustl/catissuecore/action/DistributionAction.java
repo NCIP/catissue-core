54978651c526c472272c7dd4276b302acb1907aa
Aniruddha <Aniruddha>
2005-08-24 06:29:35 +0000

    Backend connectivity for populating lists
    SVN-Revision: 629

--- WEB-INF/src/edu/wustl/catissuecore/action/DistributionAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/DistributionAction.java
@@ -3,24 +3,27 @@
  * <p>Description:	This class initializes the fields in the  Distribution Add/Edit webpage.</p>
  * Copyright:    Copyright (c) year
  * Company: Washington University, School of Medicine, St. Louis.
- * @author Jyoti Singh
+ * @author Aniruddha Phadnis
  * @version 1.00
  * Created on Aug 10, 2005
  */
 
 package edu.wustl.catissuecore.action;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.bizlogic.DistributionBizLogic;
+import edu.wustl.catissuecore.domain.DistributionProtocol;
+import edu.wustl.catissuecore.domain.Site;
+import edu.wustl.catissuecore.domain.Specimen;
 import edu.wustl.catissuecore.util.global.Constants;
-import edu.wustl.common.beans.NameValueBean;
 
 /**
  * This class initializes the fields in the  Distribution Add/Edit webpage.
- 
+ * @author aniruddha_phadnis
  */
 public class  DistributionAction extends SpecimenEventParametersAction
 {
@@ -28,18 +31,36 @@ public class  DistributionAction extends SpecimenEventParametersAction
 	{
 		super.setRequestParameters(request);
 		
-		
-		request.setAttribute(Constants.FROMSITELIST, Constants.FROMSITEARRAY);
-		request.setAttribute(Constants.TOSITELIST, Constants.TOSITEARRAY);
-		
-		List list = new ArrayList();
-		NameValueBean nameValueBean = new NameValueBean();
-		nameValueBean.setName(Constants.SELECT_OPTION);
-		nameValueBean.setValue("-1");
-		
-		list.add(nameValueBean);
-		
-		
-		request.setAttribute(Constants.ITEMLIST, list);
+		try
+		{
+			DistributionBizLogic dao = (DistributionBizLogic)BizLogicFactory.getBizLogic(Constants.DISTRIBUTION_FORM_ID);
+			
+			//Sets the Site list.
+	        String sourceObjectName = Site.class.getName();
+	        String[] displayNameFields = {"name"};
+	        String valueField = "systemIdentifier";
+	        
+	        List siteList = dao.getList(sourceObjectName, displayNameFields, valueField);
+			 
+			request.setAttribute(Constants.FROMSITELIST, siteList);
+			request.setAttribute(Constants.TOSITELIST, siteList);
+			
+			//Sets the Distribution Protocol Id List.
+	        sourceObjectName = DistributionProtocol.class.getName();
+	        String [] displayNameField = {"systemIdentifier"};
+	        
+	        List protocolList = dao.getList(sourceObjectName, displayNameField, valueField);
+			request.setAttribute(Constants.DISTRIBUTIONPROTOCOLLIST, protocolList);
+			
+			//Sets the Specimen list.
+	        sourceObjectName = Specimen.class.getName();
+	        
+	        List specimenList = dao.getList(sourceObjectName, displayNameField, valueField);
+			request.setAttribute(Constants.ITEMLIST, specimenList);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
