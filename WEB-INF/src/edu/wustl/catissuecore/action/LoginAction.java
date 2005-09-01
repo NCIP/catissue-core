8ad8f431a967e091dc07c2fa49e13607d52dd65b
Mandar <Mandar>
2005-09-01 06:17:46 +0000

    Users with only ActivityStatus as "Active" will be allowed to login. As per bug reported by Poornima
    SVN-Revision: 763

--- WEB-INF/src/edu/wustl/catissuecore/action/LoginAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/LoginAction.java
@@ -2,6 +2,7 @@
 package edu.wustl.catissuecore.action;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +17,13 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.actionForm.LoginForm;
+import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.bizlogic.UserBizLogic;
+import edu.wustl.catissuecore.domain.User;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.security.SecurityManager;
+import edu.wustl.common.security.exceptions.SMException;
+import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -59,32 +65,53 @@ public class LoginAction extends Action
 
         try
         {
+        	boolean validUser = isValidUser(loginName );
+        	
+        	if (validUser)
+        	{
+	            boolean loginOK = SecurityManager.getInstance(LoginAction.class)
+	                    .login(loginName, password);
+	            if (loginOK)
+	            {
+	                Logger.out.info(">>>>>>>>>>>>> SUCESSFUL LOGIN <<<<<<<<< ");
+	                session = request.getSession(true);
+	                session.setAttribute(Constants.USER,loginName);
+	                return mapping.findForward(Constants.SUCCESS);
+	            }
+	            else
+	            {
+	                Logger.out.info("User " + loginName
+	                        + " Invalid user. Sending back to the login Page");
+	                ActionErrors errors = new ActionErrors();
+	                errors.add(ActionErrors.GLOBAL_ERROR, new ActionError(
+	                        "errors.incorrectLoginNamePassword"));
+	
+	                //Report any errors we have discovered
+	                if (!errors.isEmpty())
+	                {
+	                    saveErrors(request, errors);
+	                }
+	                return (mapping.findForward(Constants.FAILURE));
+	            }
+        	} // if valid user
+        	else
+        	{                Logger.out.info("User " + loginName
+                    + " Invalid user. Sending back to the login Page");
+            ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR, new ActionError(
+                    "errors.incorrectLoginNamePassword"));
 
-            boolean loginOK = SecurityManager.getInstance(LoginAction.class)
-                    .login(loginName, password);
-            if (loginOK)
+            //Report any errors we have discovered
+            if (!errors.isEmpty())
             {
-                Logger.out.info(">>>>>>>>>>>>> SUCESSFUL LOGIN <<<<<<<<< ");
-                session = request.getSession(true);
-                session.setAttribute(Constants.USER,loginName);
-                return mapping.findForward(Constants.SUCCESS);
+                saveErrors(request, errors);
             }
-            else
-            {
-                Logger.out.info("User " + loginName
-                        + " Invalid user. Sending back to the login Page");
-                ActionErrors errors = new ActionErrors();
-                errors.add(ActionErrors.GLOBAL_ERROR, new ActionError(
-                        "errors.incorrectLoginNamePassword"));
+           	System.out.println("\n\n\n\n****** Invalid User : " + loginName + "\n\n\n\n\n****");
+            return (mapping.findForward(Constants.FAILURE));
 
-                //Report any errors we have discovered
-                if (!errors.isEmpty())
-                {
-                    saveErrors(request, errors);
-                }
-                return (mapping.findForward(Constants.FAILURE));
-            }
-        }
+
+        	} // invalid user
+       	}
         catch (Exception e)
         {
             Logger.out.info("Exception: " + e.getMessage());
@@ -100,5 +127,72 @@ public class LoginAction extends Action
         }
 
     }
+    public boolean isValidUser(String loginName) throws DAOException
+    {
+    	System.out.println("\n\n\n\n****** User : " + loginName + "\n\n\n\n\n****");
+        gov.nih.nci.security.authorization.domainobjects.User csmUser = null;
+        try
+        {
+            csmUser = SecurityManager.getInstance(UserBizLogic.class).getUser(loginName);
+            
+            if (csmUser == null)
+            	return false;
+            else
+            {
+            	Long uidCSM = csmUser.getUserId();
+//            	System.out.println("\n\n\n\n****** CSMUser : " + csmUser + "\n\n\n\n\n****");
+//            	System.out.println("\n\n\n\n****** CSMUserId : " + csmUser.getUserId() + "\n\n\n\n\n****");
+//            	//---------------------------
+            	
+            	UserBizLogic userBizLogic = (UserBizLogic)BizLogicFactory.getBizLogic(Constants.USER_FORM_ID);
+            	List users = userBizLogic.retrieve(User.class.getName(), "activityStatus",Constants.ACTIVITY_STATUS_ACTIVE);
+            	
 
+				User user = null;
+               
+                if (users != null)
+                {
+                	boolean found=false;
+                    for (int i = 0; i < users.size(); i++)
+                    {
+                        user = (User) users.get(i);
+                        try
+                        {
+                        	Long uidLocal = user.getSystemIdentifier();
+//                        	System.out.println("\n****** LocalUserId : " + uidLocal.toString() + "\n****");
+                        	if (uidLocal.longValue() == (uidCSM.longValue() ))
+                        	{
+                        		found = true;
+                        		break;
+                        	}
+                        		
+                        }
+                        catch (Exception e)
+                        {
+                            Logger.out.debug("Unable to get user : " + e.getMessage());
+                            throw new DAOException(e);
+                        }
+                   } // for
+                   if (found)
+                   	return true;
+                   else
+                   	return false;
+                }
+                else
+                	return false;
+            }
+        }
+        catch (SMException e)
+        {
+//        	System.out.println("\n\n\n\n****** error : " + e + "\n\n\n\n\n****");
+            Logger.out.debug("Unable to get user : " + e.getMessage());
+            return false;
+        }
+        catch (Exception e1)
+        {
+//        	System.out.println("\n\n\n\n****** error : " + e1 + "\n\n\n\n\n****");
+            Logger.out.debug("Unable to get user : " + e1.getMessage());
+            return false;
+        }
+    }
 }
