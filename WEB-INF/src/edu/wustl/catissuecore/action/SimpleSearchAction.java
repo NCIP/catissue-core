ba544a0a04aff5441e741df3cda6295372ee9dce
Gautam <Gautam>
2005-09-06 11:55:27 +0000

    try catch block
    SVN-Revision: 912

--- WEB-INF/src/edu/wustl/catissuecore/action/SimpleSearchAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/SimpleSearchAction.java
@@ -32,6 +32,7 @@ import edu.wustl.catissuecore.query.SimpleConditionsNode;
 import edu.wustl.catissuecore.query.SimpleQuery;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.util.MapDataParser;
+import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -43,75 +44,95 @@ public class SimpleSearchAction extends DispatchAction
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response) throws Exception
     {
-        
         SimpleQueryInterfaceForm simpleQueryInterfaceForm = (SimpleQueryInterfaceForm) form;
-        Map map = simpleQueryInterfaceForm.getValuesMap();
-        
-        MapDataParser parser = new MapDataParser("edu.wustl.catissuecore.query");
-        
-        Collection simpleConditionNodeCollection = parser.generateData(map);
-        
-        Iterator iterator = simpleConditionNodeCollection.iterator();
-        
-        while (iterator.hasNext())
-        {
-            SimpleConditionsNode simpleConditionsNode = (SimpleConditionsNode)iterator.next();
-            String columnName = simpleConditionsNode.getCondition().getDataElement().getField();
-            StringTokenizer stringToken = new StringTokenizer(columnName,".");
-            simpleConditionsNode.getCondition().getDataElement().setTable(stringToken.nextToken());
-            simpleConditionsNode.getCondition().getDataElement().setField(stringToken.nextToken());
-            String fieldType = stringToken.nextToken();
-            String value = simpleConditionsNode.getCondition().getValue();
-            
-            if (fieldType.equals(Constants.FIELD_TYPE_VARCHAR) || fieldType.equals(Constants.FIELD_TYPE_DATE))
-            {
-                value = "'"+value+"'";
-                simpleConditionsNode.getCondition().setValue(value);
-            }
-        }
-        
-        Client.initialize();
-        
-        Query query = QueryFactory.getInstance().newQuery(Query.SIMPLE_QUERY, simpleQueryInterfaceForm.getAliasName());
-        
-        String [] columnNames = query.setViewElements(simpleQueryInterfaceForm.getAliasName());
-        
-//        HttpSession session = request.getSession();
-//        String[] columnList = (String[])request.getAttribute(Constants.SPREADSHEET_COLUMN_LIST);
-//        
-//        if (columnList == null)
-//        {
-//            columnList = Constants.DEFAULT_SPREADSHEET_COLUMNS;
-//        }
-          
-//        query.addElementToView(new DataElement(Query.PARTICIPANT,"IDENTIFIER"));
-//        query.addElementToView(new DataElement(Query.COLLECTION_PROTOCOL,"IDENTIFIER"));
-//        query.addElementToView(new DataElement(Query.COLLECTION_PROTOCOL_EVENT,"IDENTIFIER"));
-//        query.addElementToView(new DataElement(Query.SPECIMEN_COLLECTION_GROUP,"IDENTIFIER"));
-//        query.addElementToView(new DataElement(Query.SPECIMEN,"IDENTIFIER"));
-//        query.addElementToView(new DataElement(Query.SPECIMEN,"TYPE"));
-//        query.addElementToView(new DataElement(Query.DEPARTMENT,"IDENTIFIER"));
-//        query.addElementToView(new DataElement(Query.DEPARTMENT,"NAME"));
-        
-        ((SimpleQuery)query).addConditions(simpleConditionNodeCollection);
-        Logger.out.debug("BEFORE EXECUTE*******************");
-        List list = query.execute();
+
         String target = simpleQueryInterfaceForm.getPageOf();
         
-        if (list.isEmpty())
+        try
         {
-            ActionErrors errors = new ActionErrors();
-            errors.add(ActionErrors.GLOBAL_ERROR,new ActionError("simpleQuery.noRecordsFound"));
-            saveErrors(request,errors);
+            Map map = simpleQueryInterfaceForm.getValuesMap();
             
-            String path = "SimpleQueryInterface.do?pageOf="+simpleQueryInterfaceForm.getPageOf()+"&aliasName="+simpleQueryInterfaceForm.getAliasName();
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
-            requestDispatcher.forward(request,response);
+            MapDataParser parser = new MapDataParser("edu.wustl.catissuecore.query");
+            
+            Collection simpleConditionNodeCollection = parser.generateData(map);
+            
+            Iterator iterator = simpleConditionNodeCollection.iterator();
+            
+            while (iterator.hasNext())
+            {
+                SimpleConditionsNode simpleConditionsNode = (SimpleConditionsNode)iterator.next();
+                String columnName = simpleConditionsNode.getCondition().getDataElement().getField();
+                StringTokenizer stringToken = new StringTokenizer(columnName,".");
+                simpleConditionsNode.getCondition().getDataElement().setTable(stringToken.nextToken());
+                simpleConditionsNode.getCondition().getDataElement().setField(stringToken.nextToken());
+                String fieldType = stringToken.nextToken();
+                String value = simpleConditionsNode.getCondition().getValue();
+                
+                if (fieldType.equals(Constants.FIELD_TYPE_VARCHAR) || fieldType.equals(Constants.FIELD_TYPE_DATE))
+                {
+                    value = "'"+value+"'";
+                    simpleConditionsNode.getCondition().setValue(value);
+                }
+            }
+            
+            Client.initialize();
+            
+            Query query = QueryFactory.getInstance().newQuery(Query.SIMPLE_QUERY, simpleQueryInterfaceForm.getAliasName());
+            
+            String [] columnNames = query.setViewElements(simpleQueryInterfaceForm.getAliasName());
+            
+//            HttpSession session = request.getSession();
+//            String[] columnList = (String[])request.getAttribute(Constants.SPREADSHEET_COLUMN_LIST);
+//            
+//            if (columnList == null)
+//            {
+//                columnList = Constants.DEFAULT_SPREADSHEET_COLUMNS;
+//            }
+              
+//            query.addElementToView(new DataElement(Query.PARTICIPANT,"IDENTIFIER"));
+//            query.addElementToView(new DataElement(Query.COLLECTION_PROTOCOL,"IDENTIFIER"));
+//            query.addElementToView(new DataElement(Query.COLLECTION_PROTOCOL_EVENT,"IDENTIFIER"));
+//            query.addElementToView(new DataElement(Query.SPECIMEN_COLLECTION_GROUP,"IDENTIFIER"));
+//            query.addElementToView(new DataElement(Query.SPECIMEN,"IDENTIFIER"));
+//            query.addElementToView(new DataElement(Query.SPECIMEN,"TYPE"));
+//            query.addElementToView(new DataElement(Query.DEPARTMENT,"IDENTIFIER"));
+//            query.addElementToView(new DataElement(Query.DEPARTMENT,"NAME"));
+            
+            ((SimpleQuery)query).addConditions(simpleConditionNodeCollection);
+            List list = query.execute();
+            
+            if (list.isEmpty())
+            {
+                ActionErrors errors = new ActionErrors();
+                errors.add(ActionErrors.GLOBAL_ERROR,new ActionError("simpleQuery.noRecordsFound"));
+                saveErrors(request,errors);
+                
+                String path = "SimpleQueryInterface.do?pageOf="+simpleQueryInterfaceForm.getPageOf()+"&aliasName="+simpleQueryInterfaceForm.getAliasName();
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+                requestDispatcher.forward(request,response);
+            }
+            else
+            {
+                if (list.size() == 1)
+                {
+                    List rowList = (List)list.get(0);
+                    String action = "SearchObject.do?pageOf="+simpleQueryInterfaceForm.getPageOf() + 
+                    				"&operation=search&systemIdentifier=" + rowList.get(0);
+                    
+                    RequestDispatcher requestDispatcher = request.getRequestDispatcher(action);
+                    requestDispatcher.forward(request,response);
+                }
+                else
+                {
+                    request.setAttribute(Constants.SPREADSHEET_DATA_LIST, list);
+                    request.setAttribute(Constants.SPREADSHEET_COLUMN_LIST, columnNames);
+                }
+            }
         }
-        else
+        catch(DAOException daoExp)
         {
-            request.setAttribute(Constants.SPREADSHEET_DATA_LIST, list);
-            request.setAttribute(Constants.SPREADSHEET_COLUMN_LIST, columnNames);
+            Logger.out.debug(daoExp.getMessage(), daoExp);
+            target = Constants.FAILURE;
         }
         
         return mapping.findForward(target);
