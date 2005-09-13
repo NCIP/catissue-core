eb1435b6f008dc6fb3a1394f327da260b4949137
Gautam <Gautam>
2005-09-13 13:52:09 +0000

    Setting specimen for the Event Parameters
    SVN-Revision: 1057

--- WEB-INF/src/edu/wustl/catissuecore/bizlogic/SpecimenEventParametersBizLogic.java
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/SpecimenEventParametersBizLogic.java
@@ -9,9 +9,11 @@ package edu.wustl.catissuecore.bizlogic;
 import java.util.List;
 
 import edu.wustl.catissuecore.dao.DAO;
+import edu.wustl.catissuecore.domain.Specimen;
 import edu.wustl.catissuecore.domain.SpecimenEventParameters;
 import edu.wustl.catissuecore.domain.User;
 import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.logger.Logger;
 
 
 /**
@@ -32,10 +34,17 @@ public class SpecimenEventParametersBizLogic extends DefaultBizLogic
 		SpecimenEventParameters specimenEventParametersObject = (SpecimenEventParameters)obj;
 
 		List list = dao.retrieve(User.class.getName(), "systemIdentifier", specimenEventParametersObject.getUser().getSystemIdentifier()  );
-		if (list.size() != 0)
+		if (!list.isEmpty())
 		{
 		    User user = (User) list.get(0);
 		    specimenEventParametersObject.setUser(user);
+		}
+		Logger.out.debug("specimen ************************"+"specimenEventParametersObject.getSpecimen().getSystemIdentifier()**************************"+specimenEventParametersObject.getSpecimen().getSystemIdentifier());
+		Specimen specimen = (Specimen) dao.retrieve(Specimen.class.getName(), specimenEventParametersObject.getSpecimen().getSystemIdentifier());
+		
+		if (specimen != null)
+		{
+		    specimenEventParametersObject.setSpecimen(specimen);
 		}
 		
 		dao.insert(specimenEventParametersObject,true);
