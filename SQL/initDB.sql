56391c1b861a9f8b6b6df5194b4eea0c4adf3e17
Kapil <Kapil>
2005-09-15 07:53:33 +0000

    *** empty log message ***
    SVN-Revision: 1107

--- SQL/initDB.sql
+++ SQL/initDB.sql
@@ -284,24 +284,6 @@ insert into CATISSUE_TABLE_RELATION (PARENT_TABLE_ID,CHILD_TABLE_ID) values (38 
 insert into CATISSUE_TABLE_RELATION (PARENT_TABLE_ID,CHILD_TABLE_ID) values (38 , 37);
 
 
-
-commit;
-
-insert into CATISSUE_INSTITUTION VALUES (1,'Washington University');
-
-insert into CATISSUE_DEPARTMENT VALUES (1,'Cardiology');
-insert into CATISSUE_DEPARTMENT VALUES (2,'Pathology');
-
-insert into CATISSUE_CANCER_RESEARCH_GROUP VALUES (1,'Siteman Cancer Center');
-insert into CATISSUE_CANCER_RESEARCH_GROUP VALUES (2,'Washington University');
-
-insert into CATISSUE_ADDRESS values (1,'abc','abc','asd','abc','abc','asdas','asdas');
-insert into CATISSUE_SITE VALUES (1,'SITE1',"LAB","as@b.cn",47,'Active',1);
-insert into CATISSUE_STORAGE_container_capacity VALUES (1,5,5,'abc','abc');
-insert into CATISSUE_STORAGE_TYPE VALUES (1,'Box',50,1);
-insert into CATISSUE_STORAGE_container VALUES (1,'name1',50,1,'abc','Active',1,1,null,1,0,0);
-insert into CATISSUE_STORAGE_CONTAINER values(2,'name2',50,false,'acb','Active',1,null,1,1,0,1)
-
 drop table CATISSUE_RELATED_TABLES_MAP;
 CREATE TABLE CATISSUE_RELATED_TABLES_MAP
 (
