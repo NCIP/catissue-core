9b5e65d566322850a3c05dbbc251f5a139d62e48
Aarti <Aarti>
2005-09-20 13:25:17 +0000

    changes to getCollectionProtocolPGName
    SVN-Revision: 1210

--- WEB-INF/src/edu/wustl/catissuecore/util/global/Constants.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/global/Constants.java
@@ -1141,6 +1141,10 @@ public class Constants
 	
 	public static final String getCollectionProtocolPGName(Long identifier)
 	{
+	    if(identifier == null)
+	    {
+	        return "COLLECTION_PROTOCOL_";
+	    }
 	    return "COLLECTION_PROTOCOL_"+identifier;
 	}
 	
