1d979930bbca227989b9082a55bf6a4e70b9f760
Gautam <Gautam>
2005-09-28 07:42:42 +0000

    Added systemIdentifier,activityStatus,operation,pageOf
    SVN-Revision: 1373

--- WEB-INF/src/edu/wustl/catissuecore/actionForm/AbstractActionForm.java
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/AbstractActionForm.java
@@ -19,6 +19,7 @@ import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.domain.AbstractDomainObject;
 import edu.wustl.catissuecore.util.global.ApplicationProperties;
+import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Validator;
 
 /**
@@ -27,21 +28,126 @@ import edu.wustl.catissuecore.util.global.Validator;
  */
 public abstract class AbstractActionForm extends ActionForm
 {
+    
+    /**
+     * Specifies whether the system identifier is mutable or not.
+     */
+    private boolean mutable = true;
+    
+    /**
+     * System generated unique identifier.
+     * */
+    protected long systemIdentifier;
+    
+    /**
+     * Specifies the page associated with this form bean.
+     */
+    protected String pageOf;
+    
+    /**
+     * A String containing the operation(Add/Edit) to be performed.
+     * */
+    protected String operation;
+    
+    /**
+     * Activity Status.
+     */
+    protected String activityStatus;
 
     /**
-     * Returns the systemIdentifier assigned to Participant.
-     * @return int representing the id assigned to User.
-     * @see #setIdentifier(int)
+     * @return Returns the mutable.
+     */
+    public boolean isMutable()
+    {
+        return mutable;
+    }
+    
+    /**
+     * @param mutable The mutable to set.
+     */
+    public void setMutable(boolean mutable)
+    {
+        this.mutable = mutable;
+    }
+    
+    /**
+     * Returns system generated unique identifier.
+     * @return system generated unique identifier.
+     * @see #setSystemIdentifier(long)
      * */
-    public abstract long getSystemIdentifier();
+	public long getSystemIdentifier()
+	{
+		return systemIdentifier;
+	}
+
+	/**
+     * Sets system generated unique identifier.
+     * @param identifier system generated unique identifier.
+     * @see #getSystemIdentifier()
+     * */
+	public void setSystemIdentifier(long identifier)
+	{
+	    if (isMutable())
+	        this.systemIdentifier = identifier;
+	}
+	
+    /**
+     * Returns the operation(Add/Edit) to be performed.
+     * @return Returns the operation.
+     * @see #setOperation(String)
+     */
+    public String getOperation()
+    {
+        return operation;
+    }
+    
+    /**
+     * Sets the operation to be performed.
+     * @param operation The operation to set.
+     * @see #getOperation()
+     */
+    public void setOperation(String operation)
+    {
+        this.operation = operation;
+    }
+    
+    /**
+     * Returns the specifies the page associated with this form bean.
+     * @return
+     */
+    public String getPageOf()
+    {
+        return pageOf;
+    }
+    
+    /**
+     * Sets the page associated with this form bean.
+     * @param pageOf
+     */
+    public void setPageOf(String pageOf)
+    {
+        this.pageOf = pageOf;
+    }
 
     /**
-     * Sets an id for the Participant.
-     * @param systemIdentifier id to be assigned to the Participant.
-     * @see #getIdentifier()
-     * */
-    public abstract void setSystemIdentifier(long systemIdentifier);
-
+	 * Returns the activity status
+	 * @return the activityStatus.
+	 * @see #setActivityStatus(String)
+	 */
+	public String getActivityStatus()
+	{
+		return activityStatus;
+	}
+	/**
+	 * Sets the activity status.
+	 * @param activityStatus activity status.
+	 * @see #getActivityStatus()
+	 */
+	public void setActivityStatus(String activityStatus)
+	{
+		this.activityStatus = activityStatus;
+	}
+	
     /**
      * Returns the id assigned to form bean.
      * @return the id assigned to form bean
@@ -59,16 +165,6 @@ public abstract class AbstractActionForm extends ActionForm
      * @return true if the operation is Add, else returns false.
      */
     public abstract boolean isAddOperation();
-    
-    /**
-     * @return Returns the activityStatus.
-     */
-    public abstract String getActivityStatus();
-    
-    /**
-     * @param activityStatus The activityStatus to set.
-     */
-    public abstract void setActivityStatus(String activityStatus);
     
     /**
      * Checks the validity of string value of the component and adds an ActionError object in the ActionErrors object.
@@ -118,7 +214,11 @@ public abstract class AbstractActionForm extends ActionForm
      */
     public void reset(ActionMapping mapping, HttpServletRequest request)
     {
+        systemIdentifier = -1;
+        operation = null;
+        activityStatus = Constants.ACTIVITY_STATUS_ACTIVE;
         reset();
     }
+    
     protected abstract void reset();
 }
