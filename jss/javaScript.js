0aecb65253bcceb8162e3c67fb855b52337b5e40
Namita_Shrivastava <Namita_Shrivastava>
2005-09-29 09:03:17 +0000

    delete function is added for delete row
    SVN-Revision: 1392

--- jss/javaScript.js
+++ jss/javaScript.js
@@ -23,3 +23,32 @@ function addDiv(div,adstr)
 	div.innerHTML = div.innerHTML +adstr;
 	alert(div.innerHTML);
 }
+
+function  deleteChecked(subdivtag,action)
+{
+	var r = new Array(); 
+	var element = document.getElementById(subdivtag);
+	var pNode = element.parentNode;
+	//var counts = element.rows.length;
+	var counts = document.forms[0].counter.value;
+	var delCounts = 0;
+	for(i=1;i<=counts;i++)//2
+	{
+		itemCheck="chk_"+i;
+		var chk = document.getElementById(itemCheck);
+		if(document.all[itemCheck].checked==true){
+			var currentRow = chk.parentNode.parentNode;
+			var k = currentRow.rowIndex;
+			pNode.deleteRow(k);
+			delCounts++;
+		}
+	}
+	
+	//document.forms[0].counter.value = element.rows.length;
+	document.forms[0].counter.value = (document.forms[0].counter.value - delCounts);
+	document.forms[0].action = action;
+	document.forms[0].submit();
+		
+		
+}
+		
