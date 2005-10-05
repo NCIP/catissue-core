191aea3f49f05e3556824ec511648808037c96b2
Aarti <Aarti>
2005-10-05 05:58:34 +0000

    Our implementation class for CSM Authorization Manager
    SVN-Revision: 1438

---
+++ WEB-INF/src/edu/wustl/common/security/UserProvisioningManagerImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Created on Oct 4, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package edu.wustl.common.security;
+
+import net.sf.hibernate.SessionFactory;
+import edu.wustl.common.security.dao.AuthorizationDAOImpl;
+import gov.nih.nci.security.dao.AuthorizationDAO;
+import gov.nih.nci.security.system.ApplicationSessionFactory;
+
+
+/**
+ * @author aarti_sharma
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class UserProvisioningManagerImpl extends
+		gov.nih.nci.security.provisioning.UserProvisioningManagerImpl {
+
+	/**
+	 * @param arg0
+	 * @throws Exception
+	 */
+	public UserProvisioningManagerImpl(String arg0) throws Exception {
+		super(arg0);
+		
+	}
+	
+	/**
+	 * @param arg0
+	 * @throws Exception
+	 */
+	public UserProvisioningManagerImpl() throws Exception {
+		super("catissuecore");
+		SessionFactory sf = ApplicationSessionFactory.getSessionFactory("catissuecore");
+		super.setAuthorizationDAO(new AuthorizationDAOImpl(sf,"catissuecore"));
+	}
+	
+	
+
+	
+
+}
