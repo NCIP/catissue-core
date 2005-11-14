605de4df2bc1b66aea189016aa0276c6c3eacc8d
Gautam <Gautam>
2005-11-14 09:04:12 +0000

    Class which contains methods which send email to the users and administrator about user signup, creation and approval.
    SVN-Revision: 1986

---
+++ WEB-INF/src/edu/wustl/catissuecore/util/EmailHandler.java
@@ -0,0 +1,282 @@
+/**
+ * <p>Title: EmailHandler Class>
+ * <p>Description:	EmailHandler is used to send emails during user signup, creation, forgot password.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Gautam Shetty
+ * @version 1.00
+ */
+
+package edu.wustl.catissuecore.util;
+
+import edu.wustl.catissuecore.domain.User;
+import edu.wustl.catissuecore.util.global.ApplicationProperties;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.catissuecore.util.global.SendEmail;
+import edu.wustl.common.security.SecurityManager;
+import edu.wustl.common.security.exceptions.SMException;
+import edu.wustl.common.util.PasswordManager;
+import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.logger.Logger;
+
+/**
+ * EmailHandler is used to send emails during user signup, creation, forgot password.
+ * @author gautam_shetty
+ */
+public class EmailHandler
+{
+    /**
+     * Creates and sends the user registration approval emails to user and the administrator.
+     * @param user The user whose registration is approved.
+     * @param roleOfUser Role of the user.
+     */
+    public void sendApprovalEmail(User user) throws DAOException
+    {
+        String subject = ApplicationProperties
+        					.getValue("userRegistration.approve.subject");
+        
+        String body = "Dear " + user.getLastName() +
+			        " " + user.getFirstName() +
+			        "\n\n"+ ApplicationProperties.getValue("userRegistration.approved.body.start") +
+			        getUserDetailsEmailBody(user); // Get the user details in the body of the email.
+			        
+        //Send login details email to the user.
+        sendLoginDetailsEmail(user, body);
+			        
+		body = body + "\n\n" + ApplicationProperties.getValue("userRegistration.thank.body.end") +
+			        "\n\n" + ApplicationProperties.getValue("email.catissuecore.team");
+        
+		//Send the user registration details email to the administrator.
+        boolean emailStatus = sendEmailToAdministrator(subject, body);
+		
+		if (emailStatus)
+        {
+            Logger.out.info(ApplicationProperties
+                    .getValue("user.approve.email.success")
+                    + user.getLastName() + " " + user.getFirstName());
+        }
+        else
+        {
+            Logger.out.info(ApplicationProperties
+                    .getValue("user.approve.email.failure")
+                    + user.getLastName() + " " + user.getFirstName());
+        }
+    }
+    
+    /**
+     * Returns the users details to be incorporated in the email.
+     * @param user The user object.
+     * @return the users details to be incorporated in the email.
+     */
+    private String getUserDetailsEmailBody(User user)
+    {
+        String userDetailsBody = "\n\n" + ApplicationProperties.getValue("user.loginName")+ Constants.SEPARATOR + user.getLoginName() + 
+						"\n\n" + ApplicationProperties.getValue("user.lastName")+ Constants.SEPARATOR + user.getLastName() +
+						"\n\n" + ApplicationProperties.getValue("user.firstName")+ Constants.SEPARATOR + user.getFirstName() +
+						"\n\n" + ApplicationProperties.getValue("user.street")+ Constants.SEPARATOR + user.getAddress().getStreet() +
+						"\n\n" + ApplicationProperties.getValue("user.city")+ Constants.SEPARATOR + user.getAddress().getCity() +
+						"\n\n" + ApplicationProperties.getValue("user.zipCode")+ Constants.SEPARATOR + user.getAddress().getZipCode() +
+						"\n\n" + ApplicationProperties.getValue("user.state")+ Constants.SEPARATOR + user.getAddress().getState() +
+						"\n\n" + ApplicationProperties.getValue("user.country")+ Constants.SEPARATOR + user.getAddress().getCountry() +
+						"\n\n" + ApplicationProperties.getValue("user.phoneNumber")+ Constants.SEPARATOR + user.getAddress().getPhoneNumber() +
+						"\n\n" + ApplicationProperties.getValue("user.faxNumber")+ Constants.SEPARATOR + user.getAddress().getFaxNumber() +
+						"\n\n" + ApplicationProperties.getValue("user.emailAddress")+ Constants.SEPARATOR + user.getEmailAddress() +
+						"\n\n" + ApplicationProperties.getValue("user.institution")+ Constants.SEPARATOR + user.getInstitution().getName() +
+						"\n\n" + ApplicationProperties.getValue("user.department")+ Constants.SEPARATOR + user.getDepartment().getName() +
+						"\n\n" + ApplicationProperties.getValue("user.cancerResearchGroup")+ Constants.SEPARATOR + user.getCancerResearchGroup().getName();
+        
+        return userDetailsBody;
+    }
+    
+    /**
+     * Creates and sends the user registration rejection emails to user and the administrator.
+     * @param user The user whose registration is rejected.
+     */
+    public void sendRejectionEmail(User user)
+    {
+        String subject = ApplicationProperties.getValue("userRegistration.reject.subject");
+        
+        String body = "Dear " + user.getFirstName()
+        + " " + user.getLastName()
+        + "\n\n"+ ApplicationProperties.getValue("userRegistration.reject.body.start");
+        
+        //Append the comments given by the administrator, if any.
+        if ((user.getComments() != null) 
+                && ("".equals(user.getComments()) == false))
+        {
+            body = body + "\n\n" + ApplicationProperties.getValue("userRegistration.reject.comments")
+            					 + user.getComments();
+        }
+        
+        body = body + "\n\n"+ ApplicationProperties.getValue("userRegistration.thank.body.end")
+        			+ "\n\n" + ApplicationProperties.getValue("email.catissuecore.team");
+        
+        boolean emailStatus = sendEmailToUserAndAdministrator(user.getEmailAddress(), subject, body);
+        
+        if (emailStatus)
+        {
+            Logger.out.info(ApplicationProperties
+                    .getValue("user.reject.email.success")
+                    + user.getLastName() + " " + user.getFirstName());
+        }
+        else
+        {
+            Logger.out.info(ApplicationProperties
+                    .getValue("user.reject.email.success")
+                    + user.getLastName() + " " + user.getFirstName());
+        }
+    }
+    
+    /**
+     * Creates and sends the user signup request received email to the user and the administrator.
+     * @param user The user registered for the membership.
+     */
+    public void sendUserSignUpEmail(User user) 
+    {
+        String subject = ApplicationProperties.getValue("userRegistration.request.subject");
+        
+        String body = "Dear "+ user.getLastName()+" "+ user.getFirstName() + "\n\n" +
+					  ApplicationProperties.getValue("userRegistration.request.body.start") + "\n" +
+					  getUserDetailsEmailBody(user) +
+					  "\n\n\t" + ApplicationProperties.getValue("userRegistration.request.body.end") +
+					  "\n\n" + ApplicationProperties.getValue("email.catissuecore.team");
+        
+        boolean emailStatus = sendEmailToUserAndAdministrator(user.getEmailAddress(), subject, body);
+        
+        if (emailStatus)
+        {
+            Logger.out.info(ApplicationProperties
+                    .getValue("userRegistration.email.success")
+                    + user.getLastName() + " " + user.getFirstName());
+        }
+        else
+        {
+            Logger.out.info(ApplicationProperties
+                    .getValue("userRegistration.email.failure")
+                    + user.getLastName() + " " + user.getFirstName());
+        }
+    }
+    
+    /**
+     * Creates and sends the login details email to the user.
+     * Returns true if the email is successfully sent else returns false.
+     * @param user The user whose login details are to be sent. 
+     * @param userDetailsBody User registration details.
+     * @return true if the email is successfully sent else returns false.
+     * @throws DAOException
+     */
+    public boolean sendLoginDetailsEmail(User user, String userDetailsBody) throws DAOException
+    {
+        boolean emailStatus = false;
+        
+        try
+        {
+            String subject = ApplicationProperties
+								.getValue("loginDetails.email.subject");
+            
+			String body = "Dear " + user.getFirstName()
+			    				  + " " + user.getLastName();
+			
+			if (userDetailsBody != null)
+			{
+			    body = userDetailsBody;
+			}
+			
+			String roleOfUser = SecurityManager.getInstance(EmailHandler.class)
+							.getUserRole(user.getCsmUserId().longValue()).getName();
+			body = body + "\n\n" + ApplicationProperties.getValue("forgotPassword.email.body.start")
+				+ "\n\t "+ ApplicationProperties.getValue("user.loginName")+ Constants.SEPARATOR + user.getLoginName()
+			    + "\n\t "+ ApplicationProperties.getValue("user.password")+ Constants.SEPARATOR + PasswordManager.decode(user.getPassword())
+			    + "\n\t "+ ApplicationProperties.getValue("user.role")+ Constants.SEPARATOR + roleOfUser
+			    + "\n\n" + ApplicationProperties.getValue("email.catissuecore.team");
+			
+			emailStatus = sendEmailToUser(user.getEmailAddress(), subject, body);
+			
+			if (emailStatus)
+			{
+				Logger.out.info(ApplicationProperties
+				    .getValue("user.loginDetails.email.success")
+				    + user.getLastName() + " " + user.getFirstName());
+			}
+			else
+			{
+				Logger.out.info(ApplicationProperties
+				    .getValue("user.loginDetails.email.failure")
+				    + user.getLastName() + " " + user.getFirstName());
+			}
+        }
+        catch(SMException smExp)
+        {
+            throw new DAOException(smExp.getMessage(), smExp);
+        }
+        
+        return emailStatus;
+    }
+    
+    /**
+     * Sends email to the user with the email address passed.
+     * Returns true if the email is successfully sent else returns false.
+     * @param userEmailAddress Email address of the user.
+     * @param subject The subject of the email. 
+     * @param body The body of the email.
+     * @return true if the email is successfully sent else returns false.
+     */
+    private boolean sendEmailToUser(String userEmailAddress, String subject, String body)
+    {
+        String mailServer = ApplicationProperties
+        		.getValue("email.mailServer");
+		String technicalSupportEmailAddress = ApplicationProperties
+		        .getValue("email.technicalSupport.emailAddress");
+		
+		SendEmail email = new SendEmail();
+        boolean emailStatus = email.sendmail(userEmailAddress, technicalSupportEmailAddress,
+				                				mailServer, subject, body);
+        return emailStatus;
+    }
+    
+    /**
+     * Sends email to the administrator and user with the email address passed.
+     * Returns true if the email is successfully sent else returns false.
+     * @param userEmailAddress Email address of the user.
+     * @param subject The subject of the email. 
+     * @param body The body of the email.
+     * @return true if the email is successfully sent else returns false.
+     */
+    private boolean sendEmailToUserAndAdministrator(String userEmailAddress, String subject, String body)
+    {
+        String adminEmailAddress = ApplicationProperties
+        		.getValue("email.administrative.emailAddress");
+        String technicalSupportEmailAddress = ApplicationProperties
+        		.getValue("email.technicalSupport.emailAddress");
+        String mailServer = ApplicationProperties
+                .getValue("email.mailServer");
+         
+        SendEmail email = new SendEmail();
+        boolean emailStatus = email.sendmail(userEmailAddress, adminEmailAddress, 
+                							 null, technicalSupportEmailAddress, mailServer, subject, body);
+        return emailStatus;
+    }
+    
+    /**
+     * Sends email to the adminstrator.
+     * Returns true if the email is successfully sent else returns false.
+     * @param subject The subject of the email. 
+     * @param body The body of the email.
+     * @return true if the email is successfully sent else returns false.
+     */
+    private boolean sendEmailToAdministrator(String subject, String body)
+    {
+        String adminEmailAddress = ApplicationProperties
+        		.getValue("email.administrative.emailAddress");
+        String technicalSupportEmailAddress = ApplicationProperties
+        		.getValue("email.technicalSupport.emailAddress");
+        String mailServer = ApplicationProperties
+                .getValue("email.mailServer");
+         
+        SendEmail email = new SendEmail();
+        boolean emailStatus = email.sendmail(adminEmailAddress, 
+                								technicalSupportEmailAddress, mailServer, subject, body);
+        
+        return emailStatus;
+    }
+}
