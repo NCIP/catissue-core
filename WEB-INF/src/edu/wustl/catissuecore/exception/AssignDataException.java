814bc5a870918e06fb185d0be872d62b9824758a
Kapil <Kapil>
2005-11-16 08:48:02 +0000

    *** empty log message ***
    SVN-Revision: 2018

--- WEB-INF/src/edu/wustl/catissuecore/exception/AssignDataException.java
+++ WEB-INF/src/edu/wustl/catissuecore/exception/AssignDataException.java
@@ -15,12 +15,38 @@ package edu.wustl.catissuecore.exception;
  */
 public class AssignDataException extends Exception
 {
+	private Exception wrapException;
 	public AssignDataException()
 	{
 		
 	}
-	public AssignDataException(Exception e)
+	public AssignDataException(String message)
 	{
-		
+		this(message,null);
 	}
+	
+	public AssignDataException(Exception ex)
+	{
+		this("",ex);
+	}
+	/**
+	 * @param wrapException The wrapException to set.
+	 */
+	public AssignDataException(String message, Exception wrapException)
+	{
+		super(message);
+		this.wrapException = wrapException;
+	}
+	
+	public static void main(String[] args) 
+	{
+	    String str = "%a";
+	    
+	    if(str.endsWith("%"))
+	    {
+	        
+	    }
+	    else
+	    System.out.println("NOT WITH");
+    }
 }
