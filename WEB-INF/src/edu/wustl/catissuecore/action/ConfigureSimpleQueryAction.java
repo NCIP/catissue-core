68f5a332fef3774181cb52aa00c9ec245d549a75
Poornima <Poornima>
2005-11-16 13:18:37 +0000

    Changes made for Exception handling
    SVN-Revision: 2033

--- WEB-INF/src/edu/wustl/catissuecore/action/ConfigureSimpleQueryAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ConfigureSimpleQueryAction.java
@@ -1,93 +1,80 @@
 package edu.wustl.catissuecore.action;
 
-import java.io.IOException;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.actionForm.SimpleQueryInterfaceForm;
 import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.util.MapDataParser;
 import edu.wustl.common.util.logger.Logger;
 
-
-
-
-public class ConfigureSimpleQueryAction extends Action
+public class ConfigureSimpleQueryAction extends BaseAction
 {
-
 	/**
 	 * This is the initialization action class for configuring Simple Search
 	 * @author Poornima Govindrao
 	 *  
 	 */
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
+    protected ActionForward executeAction(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException
+            throws Exception
     {
-    	try
-		{
-    		//Set the tables for the configuration 
-    		SimpleQueryInterfaceForm simpleQueryInterfaceForm =  (SimpleQueryInterfaceForm)form;
-    		HttpSession session =request.getSession();
-    		Map map = simpleQueryInterfaceForm.getValuesMap();
-    		Logger.out.debug("map size"+map.size());
-    		if(map.size()==0)
+    	//Set the tables for the configuration 
+    	SimpleQueryInterfaceForm simpleQueryInterfaceForm =  (SimpleQueryInterfaceForm)form;
+    	HttpSession session =request.getSession();
+    	Map map = simpleQueryInterfaceForm.getValuesMap();
+    	Logger.out.debug("Form map size"+map.size());
+    	Logger.out.debug("Form map"+map); 
+    	if(map.size()==0)
+    	{
+    		map=(Map)session.getAttribute(Constants.SIMPLE_QUERY_MAP);
+    		Logger.out.debug("Session map size"+map.size());
+    		Logger.out.debug("Session map"+map);
+    	}
+    	Iterator iterator = map.keySet().iterator();
+
+    	//Retrieve the size of the condition list to set size of array of tables.
+    	MapDataParser parser = new MapDataParser("edu.wustl.catissuecore.query");
+		Collection simpleConditionNodeCollection = parser.generateData(map, true);
+		int counter = simpleConditionNodeCollection.size();
+     	String[] selectedTables = new String[counter]; 
+    	int tableCount=0;
+    	while (iterator.hasNext())
+    	{
+    		String key = (String)iterator.next();
+    		Logger.out.debug("map key"+key);
+    		if(key.endsWith("_table"))
     		{
-    			map=(Map)session.getAttribute(Constants.SIMPLE_QUERY_MAP);
-    			Logger.out.debug("map size"+map.size());
-    		}
-    		Iterator iterator = map.keySet().iterator();
-    		String counter = (String)session.getAttribute(Constants.SIMPLE_QUERY_COUNTER);
-    		if(counter==null)
-    			counter = simpleQueryInterfaceForm.getCounter();
-    		Logger.out.debug("No. of tables in the query "+counter);
-    		int size = Integer.parseInt(counter);
-    		String[] selectedTables = new String[size]; 
-    		int tableCount=0;
-    		while (iterator.hasNext())
-    		{
-    			String key = (String)iterator.next();
-    			Logger.out.debug("map key"+key);
-    			if(key.endsWith("_table"))
+    			String table = (String)map.get(key);
+    			boolean exists = false;
+    			for(int arrayCount=0;arrayCount<selectedTables.length;arrayCount++)
     			{
-    				String table = (String)map.get(key);
-    				boolean exists = false;
-    				for(int arrayCount=0;arrayCount<selectedTables.length;arrayCount++)
+    				if(selectedTables[arrayCount]!=null)
     				{
-    					if(selectedTables[arrayCount]!=null)
-    					{
-    						if(selectedTables[arrayCount].equals(table))
-    							exists = true;
-    					}
+    					if(selectedTables[arrayCount].equals(table))
+    						exists = true;
     				}
-    				if(!exists)
-    					{
-    						selectedTables[tableCount]= table;
-    						tableCount++;
-    					}
+    			}
+    			if(!exists)
+    			{
+    				selectedTables[tableCount]= table;
+    				tableCount++;
     			}
     		}
-    		
-    		session.setAttribute(Constants.TABLE_ALIAS_NAME,selectedTables);
-    		request.setAttribute(Constants.PAGEOF,Constants.PAGEOF_SIMPLE_QUERY_INTERFACE);
-    		session.setAttribute(Constants.SIMPLE_QUERY_MAP,map);
-    		session.setAttribute(Constants.SIMPLE_QUERY_ALIAS_NAME,simpleQueryInterfaceForm.getAliasName());
-    		session.setAttribute(Constants.SIMPLE_QUERY_COUNTER,simpleQueryInterfaceForm.getCounter());
-		}
-		catch (Exception Exp)
-		{
-			Logger.out.debug(Exp.getMessage(), Exp);
-		}
+    	}
     	
+    	session.setAttribute(Constants.TABLE_ALIAS_NAME,selectedTables);
+    	request.setAttribute(Constants.PAGEOF,Constants.PAGEOF_SIMPLE_QUERY_INTERFACE);
+    	session.setAttribute(Constants.SIMPLE_QUERY_MAP,map);
     	return (mapping.findForward("success"));
     }
 
