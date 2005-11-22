bf4585f9374bf64caad8b44bb32f05b9dc3df8bb
Gautam <Gautam>
2005-11-22 05:51:54 +0000

    Added method for getting application url.
    SVN-Revision: 2126

--- WEB-INF/src/edu/wustl/catissuecore/action/ReportProblemAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ReportProblemAction.java
@@ -23,6 +23,7 @@ import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.actionForm.ReportedProblemForm;
 import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.catissuecore.util.global.Variables;
 import edu.wustl.common.beans.SessionDataBean;
 
 
@@ -52,6 +53,8 @@ public class ReportProblemAction extends Action
 			}
 		}
     	
+		setApplicationURL(request);
+		
         //Gets the value of the operation parameter.
         String operation = request.getParameter(Constants.OPERATION);
 
@@ -70,4 +73,19 @@ public class ReportProblemAction extends Action
                 Constants.REPORTED_PROBLEM_ACTIVITY_STATUS_VALUES);
         return mapping.findForward(Constants.SUCCESS);
     }
+    
+    /**
+	 * Sets the application URL in the Variables class.
+	 * @param request
+	 */
+	private void setApplicationURL(HttpServletRequest request)
+	{
+	    String requestURL = request.getRequestURL().toString();
+	    int indexOfFirstColon = requestURL.indexOf(":");
+	    int indexOfSecondColon = requestURL.indexOf(":", indexOfFirstColon+1);
+	    int indexOfSecondSlash = requestURL.indexOf("/", requestURL.indexOf("/", indexOfSecondColon+1)+1);
+	    
+	    if (Variables.catissueURL != null)
+	        Variables.catissueURL = requestURL.substring(0, indexOfSecondSlash);
+	}
 }
