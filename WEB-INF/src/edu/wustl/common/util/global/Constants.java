c7ac817e78b8206dbd1235360b148c404b5fb36a
Ajay <Ajay>
2005-11-30 09:33:05 +0000

    new global file for common package
    SVN-Revision: 2291

---
+++ WEB-INF/src/edu/wustl/common/util/global/Constants.java
@@ -0,0 +1,28 @@
+/*
+ * Created on Nov 30, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package edu.wustl.common.util.global;
+
+/**
+ * @author ajay_sharma
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+
+/**
+ * This classes is specific to common files. And contains all variables used by classes from 
+ * common package.  
+ * */
+public class Constants 
+{
+	public static final String SELECT_OPTION = "-- Select --";
+	public static final String CDE_CONF_FILE = "CDEConfig.xml";
+	public static final String ANY = "Any";
+	public static final String DELIMETER = ",";
+	public static final String TRUE = "true";
+	public static final String FALSE = "false";
+}
---
+++ WEB-INF/src/edu/wustl/common/util/global/Variables.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Nov 30, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package edu.wustl.common.util.global;
+
+/**
+ * @author ajay_sharma
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+
+/**
+ * This class is specific to common files. It is used by common package.
+ * */
+
+public class Variables {
+	/* To hold path of Catissue home directory */
+	public static String catissueHome=new String();
+
+}
