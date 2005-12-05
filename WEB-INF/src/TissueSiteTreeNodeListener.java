7cbb4a4e721b3752822bfdd2f8adb9f5f78e4663
sachin_lale <sachin_lale>
2005-12-05 06:23:19 +0000

    Implemented MouseInputLIstener for double click.
    SVN-Revision: 2363

--- WEB-INF/src/TissueSiteTreeNodeListener.java
+++ WEB-INF/src/TissueSiteTreeNodeListener.java
@@ -1,10 +1,16 @@
 import java.applet.Applet;
 import java.applet.AppletContext;
+import java.awt.Component;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTree;
+import javax.swing.event.MouseInputListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 import netscape.javascript.JSObject;
 import edu.wustl.catissuecore.tissuesite.TissueSiteTreeNode;
@@ -17,7 +23,7 @@ import edu.wustl.catissuecore.util.global.Constants;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class TissueSiteTreeNodeListener implements TreeSelectionListener
+public class TissueSiteTreeNodeListener implements MouseInputListener
 {
     /**
      * Corresponds to an applet environment.
@@ -40,31 +46,71 @@ public class TissueSiteTreeNodeListener implements TreeSelectionListener
         this.appletContext = appletContext;
     }
     
-    /**
-     * (non-Javadoc)
-     * @see javax.swing.event.TreeSelectionListener#valueChanged(javax.swing.event.TreeSelectionEvent)
-     */
-    public void valueChanged(TreeSelectionEvent event)
-    {
-        Object object = event.getSource();
-        JTree tree = null;
-
-        if (object instanceof JTree)
+    public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+    	if(e.getClickCount()==2)
         {
-            tree = (JTree) object;
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree
-                    .getLastSelectedPathComponent();
-            TissueSiteTreeNode  treeNode = (TissueSiteTreeNode) node
-                    .getUserObject();
-            
-            //Set the values in the parent window.
-            Applet applet = this.appletContext.getApplet(Constants.TREE_APPLET_NAME);
-            JSObject window = JSObject.getWindow(applet);
-            String propertyName = applet.getParameter(Constants.PROPERTY_NAME);
-            String setValue = "setParentWindowValue('"+propertyName+"','"+treeNode.toString()+"')";
-            window.eval(setValue);
-            window.eval("closeWindow()");
-        }
-    }
+    		Object object = e.getSource();
+    		JTree tree = null;
 
+    		if (object instanceof JTree)
+    		{
+    			tree = (JTree) object;
+    			
+    			DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree
+                     .getLastSelectedPathComponent();
+
+    			TissueSiteTreeNode  treeNode = (TissueSiteTreeNode) node
+                     .getUserObject();
+             
+    			//Set the values in the parent window.
+    			Applet applet = this.appletContext.getApplet(Constants.TREE_APPLET_NAME);
+    			JSObject window = JSObject.getWindow(applet);
+    			//commented as this will set in MouseClick
+    			String propertyName = applet.getParameter(Constants.PROPERTY_NAME);
+
+    			// if 'Tissue Site' Root node selected then do nothing
+    			if(treeNode.toString().equals(Constants.TISSUE_SITE))
+    			{
+    				return;
+    			}
+    			String setValue = "setParentWindowValue('"+propertyName+"','"+treeNode.toString()+"')";
+             
+    			//commented as this will set in MouseClick
+    			window.eval(setValue);
+    			window.eval("closeWindow()");
+    		}
+        }
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
