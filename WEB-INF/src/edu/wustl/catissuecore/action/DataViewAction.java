4859ea1e144ac55146ec2314b02116a4ab55b5a9
Poornima <Poornima>
2005-12-07 10:12:09 +0000

    Added Parent details to the treeNode to take care of Many to Many relation
    SVN-Revision: 2382

--- WEB-INF/src/edu/wustl/catissuecore/action/DataViewAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/DataViewAction.java
@@ -49,10 +49,28 @@ public class DataViewAction extends BaseAction
         String name = str.nextToken().trim();
         HttpSession session = request.getSession();
         String id = new String();
+        String parentName = new String();
+    	String parentId = new String();
         
+		//Get column display names from session which is set in session in AdvanceSearchResultsAction
+    	List columnDisplayNames = (List)session.getAttribute(Constants.COLUMN_DISPLAY_NAMES);
+    	/*List filteredColumnDisplayNames = new ArrayList();
+        
+    	if (name.equals(Constants.ROOT)|| name.equals(Constants.PARTICIPANT))
+        {
+    		filteredColumnDisplayNames = columnDisplayNames;
+        }*/
         if (!name.equals(Constants.ROOT))
         {
         	id = str.nextToken();
+        }
+        /*Incase of collection protocol selected, the whereCondition should contain the participant conditions also
+         * as Collection Protocol and Participant have many to many relationship
+         */ 
+        if(str.hasMoreTokens())
+        {
+        	parentName = str.nextToken();
+        	parentId = str.nextToken();
         }
 
         //get the type of view to show (spreadsheet/individual)
@@ -60,9 +78,16 @@ public class DataViewAction extends BaseAction
         
         if (viewType.equals(Constants.SPREADSHEET_VIEW))
         {
-            if (!name.equals(Constants.ROOT))
-            {
-                Map columnIdsMap = (Map)session.getAttribute(Constants.COLUMN_ID_MAP);
+        	String[] columnList= null;
+        	
+        	if (!name.equals(Constants.ROOT))
+            {	
+        		Map columnIdsMap = (Map)session.getAttribute(Constants.COLUMN_ID_MAP);
+                /*if (!name.equals(Constants.PARTICIPANT))
+                {
+                	columnList = getColumnNames(name,columnIdsMap,columnDisplayNames,filteredColumnDisplayNames);
+                }*/
+                
                 Logger.out.debug("alias name of selected node in adv tree:"+name);
                 Logger.out.debug("column ids map in data view action"+columnIdsMap);
                 String key = name+"."+Constants.IDENTIFIER;
@@ -70,9 +95,37 @@ public class DataViewAction extends BaseAction
                 Logger.out.debug("columnid of selected node:"+columnId+"in the map for key:"+key);
                 name=Constants.COLUMN+columnId;
                 Logger.out.debug("Column name of the selected column in the tree:"+name);
+                if(!parentName.equals(""))
+                {
+                    key = parentName+"."+Constants.IDENTIFIER;
+                    columnId = ((Integer)columnIdsMap.get(parentName+"."+Constants.IDENTIFIER)).intValue()-1;
+                    parentName=Constants.COLUMN+columnId;
+
+                }
             }
+        	String[] whereColumnName= new String[1]; 
+        	
+            String[] whereColumnValue = new String[1];
+            
+            String[] whereColumnCondition = new String[1];
+
+            if(!parentName.equals(""))
+            {
+            	whereColumnName=new String[2];
+            	whereColumnValue = new String[2];
+            	whereColumnCondition = new String[2];
+
+            	Logger.out.debug("parentname & id for coll prot"+parentName+"&"+parentId);
+            	whereColumnName[1]=parentName;
+            	whereColumnValue[1]=parentId;
+            	whereColumnCondition[1]="=";
+            }
+            whereColumnName[0]=name;
+            whereColumnValue[0] =id;
+            whereColumnCondition[0]="=";
+
         	List list = null;
-            String[] columnList= null;
+            
             //String[] columnList = {"Participant1_IDENTIFIER","CollectionProtocol1_IDENTIFIER","SpecimenCollectionGroup1_IDENTIFIER","Specimen1_IDENTIFIER"};
             
 
@@ -85,7 +138,7 @@ public class DataViewAction extends BaseAction
                 columnList = Constants.DEFAULT_SPREADSHEET_COLUMNS;
             }*/
             
-            list = resultData.getSpreadsheetViewData(name,id,columnList, getSessionData(request), Constants.OBJECT_LEVEL_SECURE_RETRIEVE);
+            list = resultData.getSpreadsheetViewData(whereColumnName,whereColumnValue,whereColumnCondition,columnList, getSessionData(request), Constants.OBJECT_LEVEL_SECURE_RETRIEVE);
             Logger.out.debug("list of data after advance search"+list);
      		// If the result contains no data, show error message.
     		if (list.isEmpty()) 
@@ -94,13 +147,12 @@ public class DataViewAction extends BaseAction
     			ActionErrors errors = new ActionErrors();
     			errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("advanceQuery.noRecordsFound"));
     			saveErrors(request, errors);
+    			request.setAttribute(Constants.PAGEOF,Constants.PAGEOF_QUERY_RESULTS);
+    			
     			//target = Constants.FAILURE;
     		}
     		else
     		{
-            
-    			//Get column display names from session which is set in session in AdvanceSearchResultsAction
-            	List columnDisplayNames = (List)session.getAttribute(Constants.COLUMN_DISPLAY_NAMES);
     			
             	//Created temporary column display names for the spreadsheet view
             	/*List rowList = (List)list.get(0);
@@ -113,6 +165,7 @@ public class DataViewAction extends BaseAction
             	Iterator keySetitr = keySet.iterator();
             	List columnDisplayNames = new ArrayList(keySet);*/
     			
+    			//request.setAttribute(Constants.SPREADSHEET_COLUMN_LIST,filteredColumnDisplayNames);
     			request.setAttribute(Constants.SPREADSHEET_COLUMN_LIST,columnDisplayNames);
     			request.setAttribute(Constants.SPREADSHEET_DATA_LIST,list);
     			request.setAttribute(Constants.PAGEOF,Constants.PAGEOF_QUERY_RESULTS);
@@ -129,13 +182,25 @@ public class DataViewAction extends BaseAction
                 url = new String(Constants.QUERY_PARTICIPANT_SEARCH_ACTION+id+"&"+Constants.PAGEOF+"="+
                 														Constants.PAGEOF_PARTICIPANT_QUERY);
             }
-            else
+            else if (name.equals(Constants.COLLECTION_PROTOCOL))
             {
-                if (name.equals(Constants.ACCESSION))
-                {
-                    url = new String(Constants.QUERY_ACCESSION_SEARCH_ACTION+id);
-                }
+            	url = new String(Constants.QUERY_COLLECTION_PROTOCOL_SEARCH_ACTION+id+"&"+Constants.PAGEOF+"="+
+            															Constants.PAGEOF_COLLECTION_PROTOCOL_QUERY);
+            			
             }
+            else if (name.equals(Constants.SPECIMEN_COLLECTION_GROUP))
+            {
+            	url = new String(Constants.QUERY_SPECIMEN_COLLECTION_GROUP_SEARCH_ACTION+id+"&"+Constants.PAGEOF+"="+
+            															Constants.PAGEOF_SPECIMEN_COLLECTION_GROUP_QUERY);
+            			
+            }
+            else if (name.equals(Constants.SPECIMEN))
+            {
+            	url = new String(Constants.QUERY_SPECIMEN_SEARCH_ACTION+id+"&"+Constants.PAGEOF+"="+
+            															Constants.PAGEOF_SPECIMEN_QUERY);
+            			
+            }
+            //request.setAttribute(Constants.OPERATION,Constants.VIEW);
             Logger.out.debug("url of object view:"+url);
             RequestDispatcher requestDispatcher = request.getRequestDispatcher(url);
             requestDispatcher.forward(request,response);
@@ -144,5 +209,52 @@ public class DataViewAction extends BaseAction
         
         return mapping.findForward(target);
     }
+    //Function for filtering column names for Spreadsheet view for individual objects.
+   /* private String[] getColumnNames(String name,Map columnIdsMap,List columnDisplayNames,List filteredColumnDisplayNames)
+    {
+    	String[] columnNames = new String[columnIdsMap.size()];
+    	Set columnIdsKeySet = columnIdsMap.keySet();
+    	Iterator columnKeysItr = columnIdsKeySet.iterator();
+    	Logger.out.debug("size of columnids map:"+columnIdsMap.size());
+    	Logger.out.debug("size of column display names list:"+columnDisplayNames.size());
+    	//Logger.out.debug("size of columnids map:"+columnIdsMap.size());
+    	//List filteredColumnDisplayNames = new ArrayList();
+    	int i=0;
+    	while(columnKeysItr.hasNext())
+    	{
+    		String key = (String)columnKeysItr.next();
+    		Logger.out.debug("key in map for column list:"+key);
+    		Logger.out.debug("value in map for column list:"+columnIdsMap.get(key));
+    		if(name.equals(Constants.COLLECTION_PROTOCOL))
+    		{
+    			if(!key.startsWith(Constants.PARTICIPANT))
+    			{
+    				int columnId = ((Integer)columnIdsMap.get(key)).intValue()-1;
+    				Logger.out.debug("column id inside if condition"+columnId);
+    				columnNames[i++] = Constants.COLUMN+columnId;
+    				filteredColumnDisplayNames.add(columnDisplayNames.get(columnId));
+    			}
+    		}
+    		else if(name.equals(Constants.SPECIMEN_COLLECTION_GROUP))
+    		{
+    			if(!key.startsWith(Constants.PARTICIPANT) && !key.startsWith(Constants.COLLECTION_PROTOCOL))
+    			{
+    				int columnId = ((Integer)columnIdsMap.get(key)).intValue()-1;
+    				columnNames[i++] = Constants.COLUMN+columnId;
+    				filteredColumnDisplayNames.add(columnDisplayNames.get(columnId));
+    			}
+    		}
+    		else if(name.equals(Constants.SPECIMEN))
+    		{
+    			if(key.startsWith(Constants.SPECIMEN))
+    			{
+    				int columnId = ((Integer)columnIdsMap.get(key)).intValue()-1;
+    				columnNames[i++] = Constants.COLUMN+columnId;
+    				filteredColumnDisplayNames.add(columnDisplayNames.get(columnId));
+    			}
+    		}
+    	}
+    	return columnNames;
+    }*/
 
 }
