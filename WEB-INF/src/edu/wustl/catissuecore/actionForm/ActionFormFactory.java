c3ff3ece7f02b9672041a3049d4653764b3ade6a
Aniruddha <Aniruddha>
2005-12-20 12:18:39 +0000

    A factory that returns formbean instance as per domain object
    SVN-Revision: 2635

---
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/ActionFormFactory.java
@@ -0,0 +1,114 @@
+/**
+ * <p>Title: ActionFormFactory Class>
+ * <p>Description:	ActionFormFactory is a factory that returns instances of action formbeans 
+ * as per the domain objects.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Aniruddha Phadnis
+ * @version 1.00
+ * Created on Dec 19, 2005
+ */
+
+package edu.wustl.catissuecore.actionForm;
+
+import edu.wustl.common.actionForm.AbstractActionForm;
+import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.catissuecore.domain.Biohazard;
+import edu.wustl.catissuecore.domain.CancerResearchGroup;
+import edu.wustl.catissuecore.domain.CollectionProtocol;
+import edu.wustl.catissuecore.domain.CollectionProtocolRegistration;
+import edu.wustl.catissuecore.domain.Department;
+import edu.wustl.catissuecore.domain.Distribution;
+import edu.wustl.catissuecore.domain.DistributionProtocol;
+import edu.wustl.catissuecore.domain.Institution;
+import edu.wustl.catissuecore.domain.Participant;
+import edu.wustl.catissuecore.domain.Site;
+import edu.wustl.catissuecore.domain.Specimen;
+import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
+import edu.wustl.catissuecore.domain.StorageContainer;
+import edu.wustl.catissuecore.domain.StorageType;
+import edu.wustl.catissuecore.domain.User;
+
+/**
+ * ActionFormFactory is a factory that returns instances of action formbeans 
+ * as per the domain objects.
+ * @author aniruddha_phadnis
+ */
+public class ActionFormFactory
+{
+	/**
+     * Returns the instance of formbean as per given domain object.
+     * @param object The instance of domain object
+     * @return the instance of formbean.
+     * @see #setMessageList(List)
+     */
+	public static AbstractActionForm getFormBean(AbstractDomainObject object)
+	{
+		AbstractActionForm form = null;
+
+		if(object instanceof User)
+		{
+			form = new UserForm();
+		}
+		else if(object instanceof Institution)
+		{
+			form = new InstitutionForm();
+		}
+		else if(object instanceof Department)
+		{
+			form = new DepartmentForm();
+		}
+		else if(object instanceof CancerResearchGroup)
+		{
+			form = new CancerResearchGroupForm();
+		}
+		else if(object instanceof Site)
+		{
+			form = new SiteForm();
+		}
+		else if(object instanceof StorageType)
+		{
+			form = new StorageTypeForm();
+		}
+		else if(object instanceof StorageContainer)
+		{
+			form = new StorageContainerForm();
+		}
+		else if(object instanceof Biohazard)
+		{
+			form = new BiohazardForm();
+		}
+		else if(object instanceof CollectionProtocol)
+		{
+			form = new CollectionProtocolForm();
+		}
+		else if(object instanceof DistributionProtocol)
+		{
+			form = new DistributionProtocolForm();
+		}
+		else if(object instanceof Participant)
+		{
+			form = new ParticipantForm();
+		}
+		else if(object instanceof CollectionProtocolRegistration)
+		{
+			form = new CollectionProtocolRegistrationForm();
+		}
+		else if(object instanceof SpecimenCollectionGroup)
+		{
+			form = new SpecimenCollectionGroupForm();
+		}
+		else if(object instanceof Specimen)
+		{
+			form = new NewSpecimenForm();
+		}
+		else if(object instanceof Distribution)
+		{
+			form = new DistributionForm();
+		}
+		
+		//form.setAllValues(object);
+		
+		return form;
+	}
+}
