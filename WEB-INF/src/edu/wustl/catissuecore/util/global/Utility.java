6cd388d6977bfd65e80e131294d92681997164c3
Aniruddha <Aniruddha>
2006-01-04 06:24:50 +0000

    New Methods added, getSpecimenTypeMap(), getSpecimenTypes(), getSpecimenClassName(), getEventParametersFormId()
    SVN-Revision: 2843

--- WEB-INF/src/edu/wustl/catissuecore/util/global/Utility.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/global/Utility.java
@@ -15,12 +15,36 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import edu.wustl.catissuecore.domain.CellSpecimen;
+import edu.wustl.catissuecore.domain.CellSpecimenRequirement;
+import edu.wustl.catissuecore.domain.CheckInCheckOutEventParameter;
+import edu.wustl.catissuecore.domain.CollectionEventParameters;
+import edu.wustl.catissuecore.domain.EmbeddedEventParameters;
+import edu.wustl.catissuecore.domain.FixedEventParameters;
+import edu.wustl.catissuecore.domain.FluidSpecimen;
+import edu.wustl.catissuecore.domain.FluidSpecimenRequirement;
+import edu.wustl.catissuecore.domain.FrozenEventParameters;
+import edu.wustl.catissuecore.domain.MolecularSpecimen;
+import edu.wustl.catissuecore.domain.MolecularSpecimenRequirement;
+import edu.wustl.catissuecore.domain.ReceivedEventParameters;
+import edu.wustl.catissuecore.domain.Specimen;
+import edu.wustl.catissuecore.domain.SpecimenEventParameters;
+import edu.wustl.catissuecore.domain.SpecimenRequirement;
+import edu.wustl.catissuecore.domain.TissueSpecimen;
+import edu.wustl.catissuecore.domain.TissueSpecimenRequirement;
+import edu.wustl.catissuecore.domain.TissueSpecimenReviewEventParameters;
+import edu.wustl.common.beans.NameValueBean;
+import edu.wustl.common.cde.CDE;
+import edu.wustl.common.cde.CDEManager;
+import edu.wustl.common.cde.PermissibleValue;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -296,20 +320,127 @@ public class Utility
 		
 		return retStr;
 	}
-   
-	//	public static void main(String[] args)
-//  {
-//	    try{
-//	      String date = "2005-10-22";  
-//	      String pattern = "yyyy-MM-dd HH:mm aa";
-//	      Date d = parseDate(date,pattern);
-//	      String dd = parseDateToString(d,pattern);
-//	      System.out.println("Date........."+d);
-//	      System.out.println("String......."+dd);
-//	    }
-//	    catch(ParseException pexcp)
-//	    {
-//	        System.out.println("Exception"+pexcp.getMessage());
-//	    }
-//  }
+	
+	public static Map getSpecimenTypeMap()
+	{
+		CDE specimenClassCDE = CDEManager.getCDEManager().getCDE(Constants.CDE_NAME_SPECIMEN_CLASS);
+    	Set setPV = specimenClassCDE.getPermissibleValues();
+    	Iterator itr = setPV.iterator();
+    	
+		List specimenClassList = CDEManager.getCDEManager().getList(Constants.CDE_NAME_SPECIMEN_CLASS,null);
+		Map subTypeMap = new HashMap();
+    	specimenClassList.add(new NameValueBean(Constants.SELECT_OPTION,"-1"));
+		
+		while(itr.hasNext())
+		{
+			List innerList =  new ArrayList();
+			Object obj = itr.next();
+			PermissibleValue pv = (PermissibleValue)obj;
+			String tmpStr = pv.getValue();
+			specimenClassList.add(new NameValueBean(tmpStr,tmpStr));
+			
+			Set list1 = pv.getSubPermissibleValues();
+			Iterator itr1 = list1.iterator();
+			innerList.add(new NameValueBean(Constants.SELECT_OPTION,"-1"));
+			
+			while(itr1.hasNext())
+			{
+				Object obj1 = itr1.next();
+				PermissibleValue pv1 = (PermissibleValue)obj1;
+				//Setting Specimen Type
+				String tmpInnerStr = pv1.getValue(); 
+				innerList.add(new NameValueBean( tmpInnerStr,tmpInnerStr));  
+			}
+			
+			subTypeMap.put(pv.getValue(),innerList);
+		}
+		
+		return subTypeMap;
+	}
+	
+	public static List getSpecimenTypes(String specimenClass)
+	{
+		Map specimenTypeMap = getSpecimenTypeMap();
+		List typeList = (List)specimenTypeMap.get(specimenClass);
+		
+		return typeList;
+	}
+	
+	public static String getSpecimenClassName(SpecimenRequirement requirement)
+	{
+		if(requirement instanceof CellSpecimenRequirement)
+		{
+			return Constants.CELL;
+		}
+		else if(requirement instanceof MolecularSpecimenRequirement)
+		{
+			return Constants.MOLECULAR;
+		}
+		else if(requirement instanceof FluidSpecimenRequirement)
+		{
+			return Constants.FLUID;
+		}
+		else if(requirement instanceof TissueSpecimenRequirement)
+		{
+			return Constants.TISSUE;
+		}
+		
+		return null;
+	}
+	
+	public static String getSpecimenClassName(Specimen specimen)
+	{
+		if(specimen instanceof CellSpecimen)
+		{
+			return Constants.CELL;
+		}
+		else if(specimen instanceof MolecularSpecimen)
+		{
+			return Constants.MOLECULAR;
+		}
+		else if(specimen instanceof FluidSpecimen)
+		{
+			return Constants.FLUID;
+		}
+		else if(specimen instanceof TissueSpecimen)
+		{
+			return Constants.TISSUE;
+		}
+		
+		return null;
+	}
+	
+	public static int getEventParametersFormId(SpecimenEventParameters eventParameter)
+	{
+		if(eventParameter instanceof CheckInCheckOutEventParameter)
+		{
+			return Constants.CHECKIN_CHECKOUT_EVENT_PARAMETERS_FORM_ID;
+		}
+		else if(eventParameter instanceof CollectionEventParameters)
+		{
+			return Constants.COLLECTION_EVENT_PARAMETERS_FORM_ID;
+		}
+		else if(eventParameter instanceof EmbeddedEventParameters)
+		{
+			return Constants.EMBEDDED_EVENT_PARAMETERS_FORM_ID;
+		}
+		else if(eventParameter instanceof FixedEventParameters)
+		{
+			return Constants.FIXED_EVENT_PARAMETERS_FORM_ID;
+		}
+		else if(eventParameter instanceof FrozenEventParameters)
+		{
+			return Constants.FROZEN_EVENT_PARAMETERS_FORM_ID;
+		}
+		else if(eventParameter instanceof ReceivedEventParameters)
+		{
+			return Constants.RECEIVED_EVENT_PARAMETERS_FORM_ID;
+		}
+		else if(eventParameter instanceof TissueSpecimenReviewEventParameters)
+		{
+			return Constants.TISSUE_SPECIMEN_REVIEW_EVENT_PARAMETERS_FORM_ID;
+		}
+		
+		return -1;
+	}
 }
