5da63eb4d62c43b9ee0cafb3efdc815165b78d24
Poornima <Poornima>
2006-01-09 09:55:46 +0000

    Setting Configured columns in session.
    SVN-Revision: 2893

--- WEB-INF/src/edu/wustl/catissuecore/action/ConfigureAdvanceSearchResultsAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ConfigureAdvanceSearchResultsAction.java
@@ -69,7 +69,10 @@ public class ConfigureAdvanceSearchResultsAction extends BaseAction
         
         session.setAttribute(Constants.CONFIGURED_SELECT_COLUMN_LIST,selectColumnNames);
         session.setAttribute(Constants.CONFIGURED_COLUMN_DISPLAY_NAMES,columnDisplayNames);
+        session.setAttribute(Constants.CONFIGURED_COLUMN_NAMES,configuredColumns);
+        
         request.setAttribute(Constants.SPREADSHEET_DATA_LIST,list);
+        
     	
         request.setAttribute(Constants.SPREADSHEET_COLUMN_LIST,columnDisplayNames);
         request.setAttribute(Constants.PAGEOF,Constants.PAGEOF_QUERY_RESULTS);
