15657bd57616f67a8f936f96e794a2dc596a615a
Gautam <Gautam>
2006-02-09 11:12:59 +0000

    Changed the Distributor Last Name to Principal Investigator Last Name and Distributor First Name to Principal Investigator First Name in Distribution.
    SVN-Revision: 3108

---
+++ SQL/Common/initDB_update_09_Feb.sql
@@ -0,0 +1,5 @@
+update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator Last Name' where RELATIONSHIP_ID=51 and COL_
+ID=292;
+
+update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator First Name' where RELATIONSHIP_ID=51 and COL
+_ID=293;
