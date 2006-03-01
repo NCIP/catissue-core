4a7fe5cdc5df7473c6639b3b5c5b4596da25a784
Gautam <Gautam>
2006-03-01 13:22:35 +0000

    *** empty log message ***
    SVN-Revision: 3160

--- SQL/Common/initDB_update_09_Feb.sql
+++ SQL/Common/initDB_update_09_Feb.sql
@@ -1,5 +1,2 @@
-update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator Last Name' where RELATIONSHIP_ID=51 and COL_
-ID=292;
-
-update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator First Name' where RELATIONSHIP_ID=51 and COL
-_ID=293;
+update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator Last Name' where RELATIONSHIP_ID=51 and COL_ID=292;
+update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator First Name' where RELATIONSHIP_ID=51 and COL_ID=293;
