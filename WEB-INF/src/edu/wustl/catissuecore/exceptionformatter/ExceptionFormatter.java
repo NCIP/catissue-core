4b5627145bb91b4a108a06be251dbf5c965bb704
Gautam <Gautam>
2006-04-07 09:26:02 +0000

    moved to common package.
    SVN-Revision: 3302

--- WEB-INF/src/edu/wustl/catissuecore/exceptionformatter/ExceptionFormatter.java
+++
@@ -1,9 +0,0 @@
-package edu.wustl.catissuecore.exceptionformatter;
-/**
- * 
- * @author sachin_lale
- * Description: Interface defines method for formatting the database specific Exception message  
- */
-public interface ExceptionFormatter {
-	public String formatMessage(Exception objExcp,Object args[]);
-}
