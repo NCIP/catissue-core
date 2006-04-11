d228900d7a1e8ea49ea939d7aa833a906ec60650
Mandar <Mandar>
2006-04-11 08:05:08 +0000

    REJECTED USERS ARE ALSO SELECTED Bug id : 1380
    SVN-Revision: 3306

--- WEB-INF/src/edu/wustl/catissuecore/action/DomainObjectListAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/DomainObjectListAction.java
@@ -64,9 +64,11 @@ public class DomainObjectListAction extends SecureAction
             //If start page is to be shown retrieve the list from the database.
             if (abstractForm.getFormId() == Constants.APPROVE_USER_FORM_ID)
             {
-                String [] whereColumnNames = {"activityStatus","activityStatus"};
-                String [] whereColumnConditions = {"=","="};
-                String [] whereColumnValues = {"New","Pending"};
+            	//Mandar 06-Apr-06 : bugid:1380 :- Rejected users to be displayed in approve users list. start
+                String [] whereColumnNames = {Constants.ACTIVITY_STATUS ,Constants.ACTIVITY_STATUS,Constants.ACTIVITY_STATUS};
+                String [] whereColumnConditions = {"=","=","="};
+                String [] whereColumnValues = {"New","Pending","Reject"};
+                // Mandar 06-Apr-06 : bugid:1380 :- Rejected users to be displayed in approve users list.end 
                 list = abstractBizLogic.retrieve(DomainObjectFactory.getDomainObjectName(abstractForm.getFormId()),
     					whereColumnNames,whereColumnConditions,whereColumnValues,Constants.OR_JOIN_CONDITION);
             }
