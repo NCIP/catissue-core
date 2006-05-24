7ee2b7e677052a6d81f898aa991f399cffb2daa1
vaishali_khandelwal <vaishali_khandelwal>
2006-05-24 12:37:46 +0000

    Added Constants variables
    SVN-Revision: 3475

--- WEB-INF/src/edu/wustl/catissuecore/action/ParticipantLookupAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ParticipantLookupAction.java
@@ -32,47 +32,48 @@ import edu.wustl.common.util.XMLPropertyHandler;
 
 public class ParticipantLookupAction extends BaseAction
 {
-	protected ActionForward executeAction(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception
+	public ActionForward executeAction(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception 
 	{
 		AbstractDomainObject abstractDomain = null;
 		AbstractActionForm abstractForm = (AbstractActionForm) form;
-
+		
 		AbstractDomainObjectFactory abstractDomainObjectFactory = (AbstractDomainObjectFactory) MasterFactory
 				.getFactory("edu.wustl.catissuecore.domain.DomainObjectFactory");
-
+		
 		//Creating the column list which is used in Data grid to display column headings
-		String[] columnHeaderList = new String[]{"System Identifier", "Last Name", "First Name",
-				"Middle Name", "Birth Date", "SSN", "Probability"};
+		String[] columnHeaderList = new String[]{Constants.PARTICIPANT_SYSTEM_IDENTIFIER, Constants.PARTICIPANT_LAST_NAME,Constants.PARTICIPANT_FIRST_NAME,Constants.PARTICIPANT_MIDDLE_NAME,Constants.PARTICIPANT_BIRTH_DATE,Constants.PARTICIPANT_SOCIAL_SECURITY_NUMBER,Constants.PARTICIPANT_PROBABLITY_MATCH};
 		List ColumnList = new ArrayList();
+		
 		for (int i = 0; i < columnHeaderList.length; i++)
 		{
 			ColumnList.add(columnHeaderList[i]);
 		}
-
+		
 		request.setAttribute(Constants.SPREADSHEET_COLUMN_LIST, ColumnList);
 
 		abstractDomain = abstractDomainObjectFactory.getDomainObject(abstractForm.getFormId(),
 				abstractForm);
-
+		
 		Participant participant = (Participant) abstractDomain;
 
 		//getting the instance of ParticipantLookupLogic class
 		ParticipantLookupLogic participantLookupLogic = (ParticipantLookupLogic) Utility
-				.getObject(XMLPropertyHandler.getValue("ParticipantLookup"));
-
+				.getObject(XMLPropertyHandler.getValue(Constants.PARTICIPANT_LOOKUP_ALGO));
+		
 		List ParticipantList = participantLookupLogic.participantLookup(participant);
-
+		
 		//if any matching participants are there then show the participants otherwise add the participant
-		if (ParticipantList.size() > 0 || request.getParameter("ParticipantLookup") != null)
+		if (ParticipantList.size() > 0 || request.getParameter(Constants.PARTICIPANT_LOOKUP_PARAMETER) != null)
 		{
 			request.setAttribute(Constants.SPREADSHEET_DATA_LIST, ParticipantList);
 		}
 		else
 		{
-			return mapping.findForward("participantAdd");
+			return mapping.findForward(Constants.PARTICIPANT_ADD_FORWARD);
 		}
-
-		return (mapping.findForward("success"));
+		
+		return (mapping.findForward(Constants.PARTICIPANT_LOOKUP_SUCCESS));
 	}
 }
