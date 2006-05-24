7f0ef72157888dfea848dd8a70dfd657ea7aeabc
vaishali_khandelwal <vaishali_khandelwal>
2006-05-24 09:21:57 +0000

    added comments
    SVN-Revision: 3471

--- WEB-INF/src/edu/wustl/catissuecore/bizlogic/ParticipantLookupLogic.java
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/ParticipantLookupLogic.java
@@ -1,9 +1,13 @@
 package edu.wustl.catissuecore.bizlogic;
 
 import java.util.List;
-
 import edu.wustl.catissuecore.domain.Participant;
 
+/**
+ * @author vaishali_khandelwal
+ *
+ * This interface is for finding out the matching participant with respect to given participant
+ */
 
 public interface ParticipantLookupLogic
 {
