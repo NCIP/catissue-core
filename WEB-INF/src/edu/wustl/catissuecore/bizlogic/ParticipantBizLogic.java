81043725441211c81f7a0f2e42df8e35a837d61a
krunal_thakkar <krunal_thakkar>
2006-06-06 06:44:38 +0000

    changes related to caTies Integration added
    SVN-Revision: 3552

--- WEB-INF/src/edu/wustl/catissuecore/bizlogic/ParticipantBizLogic.java
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/ParticipantBizLogic.java
@@ -10,6 +10,11 @@
 
 package edu.wustl.catissuecore.bizlogic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -19,10 +24,13 @@ import java.util.Set;
 import edu.wustl.catissuecore.domain.CollectionProtocolRegistration;
 import edu.wustl.catissuecore.domain.Participant;
 import edu.wustl.catissuecore.domain.ParticipantMedicalIdentifier;
+import edu.wustl.catissuecore.integration.IntegrationManager;
+import edu.wustl.catissuecore.integration.IntegrationManagerFactory;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Utility;
+import edu.wustl.common.beans.NameValueBean;
 import edu.wustl.common.beans.SessionDataBean;
-import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.bizlogic.AbstractBizLogic;
 import edu.wustl.common.cde.CDEManager;
 import edu.wustl.common.dao.DAO;
 import edu.wustl.common.security.SecurityManager;
@@ -37,7 +45,7 @@ import edu.wustl.common.util.logger.Logger;
  * ParticipantHDAO is used to to add Participant's information into the database using Hibernate.
  * @author aniruddha_phadnis
  */
-public class ParticipantBizLogic extends DefaultBizLogic
+public class ParticipantBizLogic extends IntegrationBizLogic
 {
 	/**
      * Saves the Participant object in the database.
@@ -214,5 +222,68 @@ public class ParticipantBizLogic extends DefaultBizLogic
 		}
         
 		return true;
+    }
+	
+	/**
+     * This method fetches linked data from integrated application i.e. CAE/caTies.
+     */
+	public List getLinkedAppData(Long systemIdentifier, String applicationID)
+	{
+	    Logger.out.debug("In getIntegrationData() of ParticipantBizLogic ");
+	    
+	    Logger.out.debug("ApplicationName in getIntegrationData() of ParticipantBizLogic==>"+applicationID);
+	    
+	    long identifiedPatientId = 0;
+	    
+	    try
+	    {
+		    //JDBC call to get matching identifier from database
+		    Class.forName("org.gjt.mm.mysql.Driver");
+		    
+		    Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/catissuecore","catissue_core","catissue_core");
+		    
+		    Statement stmt= connection.createStatement();
+		    
+		    String identifiedPatientIdQuery = "select IDENTIFIER from CATISSUE_IDENTIFIED_PATIENT where PARTICIPANT_ID="+systemIdentifier;
+		    
+		    ResultSet identifiedPatientResultSet=stmt.executeQuery(identifiedPatientIdQuery);
+		    
+		    while(identifiedPatientResultSet.next())
+		    {
+		        identifiedPatientId = identifiedPatientResultSet.getLong(1);
+		        break;
+		    }
+		    Logger.out.debug("IdentifiedPatientId==>"+identifiedPatientId);
+		    identifiedPatientResultSet.close();
+		    if(identifiedPatientId==0)
+		    {
+		        List exception=new ArrayList();
+		        exception.add(new NameValueBean("IdentifiedPatientId is not available for linked Participant", "IdentifiedPatientId is not available for linked Participant"));
+		        return exception;
+		    }
+		    
+		    stmt.close();
+		    
+		    connection.close();
+		    
+	    }
+	    catch(Exception e)
+	    {
+	        Logger.out.debug("JDBC Exception==>"+e.getMessage());
+	    }
+	    
+	    IntegrationManager integrationManager=IntegrationManagerFactory.getIntegrationManager(applicationID);
+
+	    return integrationManager.getLinkedAppData(new Participant(), new Long(identifiedPatientId));
+	}
+	
+	public String getPageToShow()
+    {
+        return new String();
+    }
+	
+	public List getMatchingObjects()
+    {
+        return new ArrayList();
     }
 }
