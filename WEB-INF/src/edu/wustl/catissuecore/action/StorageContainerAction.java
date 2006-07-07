c8866b0e3446db6e0af046880b224b66030c884a
vaishali_khandelwal <vaishali_khandelwal>
2006-07-07 10:50:22 +0000

    chnages according to new model
    SVN-Revision: 3661

--- WEB-INF/src/edu/wustl/catissuecore/action/StorageContainerAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/StorageContainerAction.java
@@ -11,10 +11,10 @@
 package edu.wustl.catissuecore.action;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,15 +28,13 @@ import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
 import edu.wustl.catissuecore.bizlogic.StorageContainerBizLogic;
 import edu.wustl.catissuecore.domain.CollectionProtocol;
 import edu.wustl.catissuecore.domain.Site;
+import edu.wustl.catissuecore.domain.SpecimenClass;
 import edu.wustl.catissuecore.domain.StorageContainer;
 import edu.wustl.catissuecore.domain.StorageType;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Utility;
 import edu.wustl.common.action.SecureAction;
 import edu.wustl.common.beans.NameValueBean;
-import edu.wustl.common.cde.CDE;
-import edu.wustl.common.cde.CDEManager;
-import edu.wustl.common.cde.PermissibleValue;
 import edu.wustl.common.util.MapDataParser;
 import edu.wustl.common.util.logger.Logger;
 
@@ -54,7 +52,7 @@ public class StorageContainerAction  extends SecureAction
 
 		//	set the menu selection 
     	request.setAttribute(Constants.MENU_SELECTED, "7"  ); 
-		
+		Logger.out.info("Add New Attribute in StorageContainerAction:"+request.getAttribute(Constants.SUBMITTED_FOR));
 		//List of keys used in map of ActionForm
 		List key = new ArrayList();
     	key.add("StorageContainerDetails:i_parameterName");
@@ -89,74 +87,34 @@ public class StorageContainerAction  extends SecureAction
     	String []siteDisplayField = {"name"};
     	list = bizLogic.getList(Site.class.getName(),siteDisplayField, valueField, true);
     	request.setAttribute(Constants.SITELIST,list);
-    	/* added by vaishali on 19th June 2006 3.47 pm */
+    	
     	Logger.out.debug("Getting Collection Protocol Ids");
-//    	//populating collection protocol list.
-		
-		String [] displayNameFields = {"title"};
-		valueField = Constants.SYSTEM_IDENTIFIER;
-	  	List collectionProtocolList = bizLogic.getList(CollectionProtocol.class.getName(),displayNameFields,valueField, true);
-	  	Logger.out.debug("Protocol list size:"+collectionProtocolList.size());
-	  	Iterator collcectionProtocolItr=collectionProtocolList.iterator();
-	  	List collectionProtocolList1=new ArrayList();
-	  	while(collcectionProtocolItr.hasNext())
-	  	{
-	  		NameValueBean nvb=(NameValueBean)collcectionProtocolItr.next();
-	  		if(nvb.getValue().equals("-1"))
-	  		{
-	  			NameValueBean nvb1=new NameValueBean("-- Any --","-1");
-	  			collectionProtocolList1.add(nvb1);
-	  		}
-	  		else
-	  		{
-	  			collectionProtocolList1.add(nvb);
-	  		}
-	  	}
-	  	request.setAttribute(Constants.PROTOCOL_LIST, collectionProtocolList1);
-		
-//		Setting the specimen class list
-		CDE specimenClassCDE = CDEManager.getCDEManager().getCDE(Constants.CDE_NAME_SPECIMEN_CLASS);
-    	Set setPV = specimenClassCDE.getPermissibleValues();
     	
+    	//populating collection protocol list.
+    	List list1=bizLogic.retrieve(CollectionProtocol.class.getName());
+    	List collectionProtocolList=getCollectionProtocolList(list1);
+    	request.setAttribute(Constants.PROTOCOL_LIST, collectionProtocolList);
+	  	
+        //Gets the Storage Type List and sets it in request 
+        List list2=bizLogic.retrieve(StorageType.class.getName());
+    	List storageTypeList=getStorageTypeList(list2);
+    	request.setAttribute(Constants.HOLDS_LIST1, storageTypeList);
     	
-    	List holdsList=new ArrayList();
-    	holdsList.add(new NameValueBean("-- Any --","-1"));
-    	
-    	List typeList=bizLogic.retrieve(StorageType.class.getName());
-    	Logger.out.debug("Type List Size:"+typeList.size());
-    	Iterator typeItr=typeList.iterator();
-    	while(typeItr.hasNext())
-    	{
-    		StorageType type=(StorageType)typeItr.next();
-    		holdsList.add(new NameValueBean(type.getType(),type.getSystemIdentifier()));
-    	}
-        
-    	holdsList.add(new NameValueBean("Any Specimen","-1"));
-        Iterator itr = setPV.iterator();
-          	
-    	while(itr.hasNext())
-    	{
-    		Object obj = itr.next();
-    		PermissibleValue pv = (PermissibleValue)obj;
-    		String tmpStr = pv.getValue()+" Specimen";
-    		Logger.out.debug(" Specimen value-------------"+tmpStr);
-    		holdsList.add(new NameValueBean( tmpStr,tmpStr));
-    		
-    	}	
-    	request.setAttribute(Constants.HOLDS_LIST, holdsList);
-    	/* added finish */
-    	
+    	//Gets the Specimen Class Type List and sets it in request
+    	List list3=bizLogic.retrieve(SpecimenClass.class.getName());
+        List specimenClassTypeList = getSpecimenClassTypeList(list3);
+	  	request.setAttribute(Constants.HOLDS_LIST2, specimenClassTypeList);
+	  	
     	boolean isOnChange = false; 
-    	/* added by vaishali on 21st June 2006 1.37 pm */
     	String type_name="";
     	String site_name="";
-    	/* added finish */
-		String str = request.getParameter("isOnChange");
+    	String str = request.getParameter("isOnChange");
 		
 		if(str!=null && str.equals("true"))
 		{
 			isOnChange = true; 
 		}
+		Logger.out.info("Onchange parameter in StorageContainerAction:"+isOnChange);
 		// Mandar : code for Addnew Storage Type data 23-Jan-06
 		String storageTypeID = (String)request.getAttribute(Constants.ADD_NEW_STORAGE_TYPE_ID);
 		if(storageTypeID != null && storageTypeID.trim().length() > 0 )
@@ -175,14 +133,14 @@ public class StorageContainerAction  extends SecureAction
 		// -- 24-Jan-06 end
   
 		
-    	if(isOnChange)
+    	if(isOnChange || request.getAttribute(Constants.SUBMITTED_FOR)!=null)
     	{
-
         	long typeSelected=-1;
         	String selectedType = String.valueOf(storageContainerForm.getTypeId());
         	Logger.out.debug(">>>>>>>>>>><<<<<<<<<<<<<<<<>>>>>>>>>>>>> ST : "+ selectedType  );
         	if(selectedType != null && !selectedType.equals("-1"))
             {
+        		
             	typeSelected = Long.parseLong(selectedType);
             	list = bizLogic.retrieve(StorageType.class.getName(),valueField,new Long(typeSelected));
             	if(!list.isEmpty())
@@ -196,10 +154,24 @@ public class StorageContainerAction  extends SecureAction
             		storageContainerForm.setOneDimensionLabel(type.getOneDimensionLabel());
             		storageContainerForm.setTwoDimensionLabel(Utility.toString(type.getTwoDimensionLabel()));
             		
-            		/* added by vaishali on 21st June 2006 1.38 pm */
             		type_name=type.getType();
             		Logger.out.debug("Type Name:"+type_name);
-            		/* added finish */
+            		
+            		if(operation!=null && operation.equals(Constants.ADD))
+            		{
+            			long[] defHoldsStorageTypeList=getDefaultHoldStorageTypeList(type);
+            			if(defHoldsStorageTypeList!=null)
+            			{
+            	      		storageContainerForm.setHoldsStorageTypeIds(defHoldsStorageTypeList);
+            			}
+            			
+            			long[] defHoldsSpecimenClassTypeList=getDefaultHoldsSpecimenClasstypeList(type);
+            			if(defHoldsSpecimenClassTypeList!=null)
+            			{
+            	      		storageContainerForm.setHoldsSpecimenClassTypeIds(defHoldsSpecimenClassTypeList);
+            			}
+
+            		}
             	}
             }
             else
@@ -219,8 +191,6 @@ public class StorageContainerAction  extends SecureAction
         	{
         	    Logger.out.debug("storageContainerForm.getSiteId()......................."+storageContainerForm.getSiteId());
         	    Logger.out.debug("storageContainerForm.getTypeId()......................."+storageContainerForm.getTypeId());
-        		startNumber = bizLogic.getNextContainerNumber(storageContainerForm.getSiteId(),storageContainerForm.getTypeId(),true);
-        		/* added by vaishali on 21st june 2006 1.39 pm */
         		list = bizLogic.retrieve(Site.class.getName(),valueField,new Long(storageContainerForm.getSiteId()));
             	if(!list.isEmpty())
             	{
@@ -229,14 +199,12 @@ public class StorageContainerAction  extends SecureAction
             		Logger.out.debug("Site Name :"+site_name);
             		
             	}	
-        		/* added finish */
         	}
         	else
         	{
-        		
-        	    Logger.out.debug("Long.parseLong(request.getParameter(parentContainerId)......................."+request.getParameter("parentContainerId"));
+        		Logger.out.debug("Long.parseLong(request.getParameter(parentContainerId)......................."+request.getParameter("parentContainerId"));
         	    Logger.out.debug("storageContainerForm.getTypeId()......................."+storageContainerForm.getTypeId());
-        		startNumber = bizLogic.getNextContainerNumber(Long.parseLong(request.getParameter("parentContainerId")),storageContainerForm.getTypeId(),false);
+        		
         		
         		list = bizLogic.retrieve(StorageContainer.class.getName(),valueField,new Long(request.getParameter("parentContainerId")));
             	if(!list.isEmpty())
@@ -249,19 +217,12 @@ public class StorageContainerAction  extends SecureAction
         	}
         	Logger.out.debug("Start Number : " + startNumber); 
         	storageContainerForm.setStartNumber(String.valueOf(startNumber));
-        	/* adde byvaishali on 21st june 2006 1.46 pm -*/
         	int containerName=bizLogic.getNextContainerName();
-        	
-        	//if(storageContainerForm.getContainerName()!=null && storageContainerForm.getContainerName().equals(""))
-        //	{
-        		if(!type_name.equals("")&& !site_name.equals(""))
-        		{	
-        			storageContainerForm.setContainerName(site_name+"_"+type_name+"_"+String.valueOf(containerName));
-        		}
-        		/*added finish*/
-        			
-        	//}     
-    	}	
+        	if(!type_name.equals("")&& !site_name.equals(""))
+        	{	
+        		storageContainerForm.setContainerName(site_name+"_"+type_name+"_"+String.valueOf(containerName));
+        	}
+        }	
         // ---------- Add new
 		String reqPath = request.getParameter(Constants.REQ_PATH);
 		
@@ -269,9 +230,105 @@ public class StorageContainerAction  extends SecureAction
 		{
 			request.setAttribute(Constants.REQ_PATH, reqPath);
 		}
-
-		Logger.out.debug("StorageContainerAction redirect :---------- "+ reqPath);
-    
         return mapping.findForward((String)request.getParameter(Constants.PAGEOF));
+    }
+		
+	/* This function gets the list of all collection protocols as argument and  
+     * create a list in which nameValueBean is stored with Title and Identifier of Collection Protocol.
+     * and returns this list
+     */ 
+    private List getCollectionProtocolList(List list)
+    {
+    	List collectionProtocolList=new ArrayList();
+    	collectionProtocolList.add(new NameValueBean("-- Any --","-1"));
+    	
+    	Iterator cpItr=list.iterator();
+    	while(cpItr.hasNext())
+    	{
+    		CollectionProtocol cp=(CollectionProtocol)cpItr.next();
+    		collectionProtocolList.add(new NameValueBean(cp.getTitle(),cp.getSystemIdentifier()));
+    	}
+    	return collectionProtocolList;
+    }
+    /* this Function gets the list of all storage types as argument and  
+     * create a list in which nameValueBean is stored with Type and Identifier of storage type.
+     * and returns this list
+     */ 
+    private List getStorageTypeList(List list)
+    {
+    	List storageTypeList=new ArrayList();
+    	storageTypeList.add(new NameValueBean("-- Any --","-1"));
+    	
+    	Iterator typeItr=list.iterator();
+    	while(typeItr.hasNext())
+    	{
+    		StorageType type=(StorageType)typeItr.next();
+    		storageTypeList.add(new NameValueBean(type.getType(),type.getSystemIdentifier()));
+    	}
+    	return storageTypeList;
+    }
+    /* this Function gets the list of all Specimen Class Types as argument and  
+     * create a list in which nameValueBean is stored with Name and Identifier of specimen Class Type.
+     * and returns this list
+     */
+    private List getSpecimenClassTypeList(List list)
+    {
+    	List specimenClassTypeList=new ArrayList();
+    	specimenClassTypeList.add(new NameValueBean("-- Any Specimen--","-1"));
+    	
+    	Iterator specimentypeItr=list.iterator();
+    	while(specimentypeItr.hasNext())
+    	{
+    		SpecimenClass specimenClassType=(SpecimenClass)specimentypeItr.next();
+    		specimenClassTypeList.add(new NameValueBean(specimenClassType.getName(),specimenClassType.getSystemIdentifier()));
+    	}
+    	return specimenClassTypeList;
+    }
+    
+    private long[] getDefaultHoldStorageTypeList(StorageType type)
+    {
+    	//Populating the storage type-id array
+    	
+		Logger.out.info("Storage type size:"+type.getStorageTypeCollection().size());
+		Collection storageTypeCollection = type.getStorageTypeCollection();
+		
+		if(storageTypeCollection != null)
+		{
+			long holdsStorageTypeList[] = new long[storageTypeCollection.size()];
+			int i=0;
+			Iterator it = storageTypeCollection.iterator();
+			while(it.hasNext())
+			{
+				StorageType holdStorageType = (StorageType)it.next();
+				holdsStorageTypeList[i] = holdStorageType.getSystemIdentifier().longValue();
+				i++;
+			}
+			return holdsStorageTypeList;
+		}
+    	return null;
+    }
+    
+    private long[] getDefaultHoldsSpecimenClasstypeList(StorageType type)
+    {
+    	//Populating the specimen class type-id array
+		Logger.out.info("Specimen class type size:"+type.getSpecimenClassCollection().size());
+		Collection specimenClassTypeCollection = type.getSpecimenClassCollection();
+		
+		if(specimenClassTypeCollection != null)
+		{
+			long holdsSpecimenClassList[] = new long[specimenClassTypeCollection.size()];
+			int i=0;
+
+			Iterator it = specimenClassTypeCollection.iterator();
+			while(it.hasNext())
+			{
+				SpecimenClass specimenClass = (SpecimenClass)it.next();
+				holdsSpecimenClassList[i] = specimenClass.getSystemIdentifier().longValue();
+				i++;
+			}
+			return holdsSpecimenClassList;
+			
+		}
+    	return null;
     }
 }
