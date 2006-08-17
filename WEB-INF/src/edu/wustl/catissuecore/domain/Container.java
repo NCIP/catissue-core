3b7a67375b711046a4285e45f8ca1d6a58c5f850
Gautam <Gautam>
2006-08-17 09:15:00 +0000

    Container class
    SVN-Revision: 3817

---
+++ WEB-INF/src/edu/wustl/catissuecore/domain/Container.java
@@ -0,0 +1,275 @@
+/**
+ * <p>Title: Container Class>
+ * <p>Description:  Container class represents the base class of container class.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Gautam Shetty
+ * @version 1.00
+ */
+
+package edu.wustl.catissuecore.domain;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+
+import edu.wustl.common.actionForm.AbstractActionForm;
+import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.common.exception.AssignDataException;
+
+/**
+ * Container class represents the base class of container class.
+ * @author gautam_shetty
+ * @author Ashwin Gupta 
+ * @hibernate.class table="CATISSUE_CONTAINER"
+ */
+public class Container extends AbstractDomainObject implements Serializable 
+{
+    
+    protected static final long serialVersionUID = 1234567890L;
+    
+	protected Long systemIdentifier;
+	
+	protected Boolean full;
+	
+	protected String name;
+	
+	protected String barcode;
+	
+	protected String activityStatus;
+	
+	protected Integer positionDimensionOne;
+	
+	protected Integer positionDimensionTwo;
+	
+	protected String comment;
+	
+	protected Capacity capacity = new Capacity();
+	 
+	protected Collection children = new HashSet();
+	
+	protected Container parent;
+	
+	public Container()
+	{
+	}
+	
+	/**
+     * Set the id.
+     */
+    public void setId(Long id)
+    {
+        this.systemIdentifier = id;
+    }
+    
+    /**
+     * @param id The id to set.
+     */
+    public Long getId()
+    {
+        return this.systemIdentifier;
+    }
+	
+	/**
+     * @return Returns the id.
+     * @hibernate.id name="systemIdentifier" column="IDENTIFIER" type="long" length="30"
+     * unsaved-value="null" generator-class="native" 
+     * @hibernate.generator-param name="sequence" value="CATISSUE_CONTAINER_SEQ"
+     */
+    public Long getSystemIdentifier()
+    {
+        return systemIdentifier;
+    }
+    
+    /**
+     * @param systemIdentifier The id to set.
+     */
+    public void setSystemIdentifier(Long systemIdentifier)
+    {
+        this.systemIdentifier = systemIdentifier;
+    }
+	
+    /**
+     * Returns the activity status of the container.
+	 * @hibernate.property name="activityStatus" type="string" column="ACTIVITY_STATUS" length="20"
+	 * @return Returns the activity status of the container.
+     */
+    public String getActivityStatus()
+    {
+        return activityStatus;
+    }
+    
+    /**
+     * @param activityStatus The activityStatus to set.
+     */
+    public void setActivityStatus(String activityStatus)
+    {
+        this.activityStatus = activityStatus;
+    }
+    
+    /**
+     * @return Returns the barcode.
+     * @hibernate.property name="barcode" type="string" column="BARCODE" length="100"
+     */
+    public String getBarcode()
+    {
+        return barcode;
+    }
+    
+    /**
+     * @param barcode The barcode to set.
+     */
+    public void setBarcode(String barcode)
+    {
+        this.barcode = barcode;
+    }
+    
+    /**
+     * @return Returns the capacity.
+     * @hibernate.many-to-one column="CAPACITY_ID" class="edu.wustl.catissuecore.domain.Capacity" constrained="true"
+     */
+    public Capacity getCapacity()
+    {
+        return capacity;
+    }
+    
+    /**
+     * @param capacity The capacity to set.
+     */
+    public void setCapacity(Capacity capacity)
+    {
+        this.capacity = capacity;
+    }
+    
+    /**
+     * @return Returns the children.
+     * @hibernate.set name="children" table="CATISSUE_CONTAINER" cascade="save-update" 
+	 * @hibernate.collection-key column="PARENT_CONTAINER_ID"
+	 * @hibernate.collection-one-to-many class="edu.wustl.catissuecore.domain.Container"
+     */
+    public Collection getChildren()
+    {
+        return children;
+    }
+    
+    /**
+     * @param children The children to set.
+     */
+    public void setChildren(Collection children)
+    {
+        this.children = children;
+    }
+    
+    /**
+     * @return Returns the parent.
+     * @hibernate.many-to-one column="PARENT_CONTAINER_ID" 
+     * class="edu.wustl.catissuecore.domain.Container" constrained="true"
+     */
+    public Container getParent()
+    {
+        return parent;
+    }
+    
+    /**
+     * @param parent The parent to set.
+     */
+    public void setParent(Container parent)
+    {
+        this.parent = parent;
+    }
+    
+    /**
+     * @return Returns the comment.
+     * @hibernate.property name="comment" type="string" column="COMMENT" length="2000"
+     */
+    public String getComment()
+    {
+        return comment;
+    }
+    
+    /**
+     * @param comment The comment to set.
+     */
+    public void setComment(String comment)
+    {
+        this.comment = comment;
+    }
+    
+    /**
+     * @return Returns the full.
+     * @hibernate.property name="isFull" type="boolean" column="FULL"
+     */
+    public Boolean isFull()
+    {
+        return full;
+    }
+    
+    /**
+     * @param full The full to set.
+     */
+    public void setFull(Boolean full)
+    {
+        this.full = full;
+    }
+    
+    /**
+     * @return Returns the name.
+     * @hibernate.property name="name" type="string" column="NAME" length="100"
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     * @param name The name to set.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    /**
+     * @return Returns the positionDimensionOne.
+     * @hibernate.property name="positionDimensionOne" type="int" column="POSITION_DIMENSION_ONE" length="30"
+     */
+    public Integer getPositionDimensionOne()
+    {
+        return positionDimensionOne;
+    }
+    
+    /**
+     * @param positionDimensionOne The positionDimensionOne to set.
+     */
+    public void setPositionDimensionOne(Integer positionDimensionOne)
+    {
+        this.positionDimensionOne = positionDimensionOne;
+    }
+    
+    /**
+     * @return Returns the positionDimensionTwo.
+     * @hibernate.property name="positionDimensionTwo" type="int" column="POSITION_DIMENSION_TWO" length="30"
+     */
+    public Integer getPositionDimensionTwo()
+    {
+        return positionDimensionTwo;
+    }
+    
+    /**
+     * @param positionDimensionTwo The positionDimensionTwo to set.
+     */
+    public void setPositionDimensionTwo(Integer positionDimensionTwo)
+    {
+        this.positionDimensionTwo = positionDimensionTwo;
+    }
+    
+    /**
+     * (non-Javadoc)
+     * @see edu.wustl.common.domain.AbstractDomainObject#setAllValues(edu.wustl.common.actionForm.AbstractActionForm)
+     */
+    public void setAllValues(AbstractActionForm arg0)
+            throws AssignDataException
+    {
+        // TODO Auto-generated method stub
+    }
+}
