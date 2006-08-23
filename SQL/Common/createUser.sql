0a13a2f0472b2c3e90f7c3042eb168dd41d6bc78
Mandar <Mandar>
2006-08-23 07:36:43 +0000

    Updated for the First user script
    SVN-Revision: 3921

--- SQL/Common/createUser.sql
+++ SQL/Common/createUser.sql
@@ -1,24 +1,24 @@
-insert into catissue_department (name) values('MD Depart1');
-insert into catissue_institution (name) values('MD Inst1');
-insert into catissue_cancer_research_group (name) values('MD CRG1');
+insert into catissue_department (name) values('@@first.admin.department@@');
+insert into catissue_institution (name) values('@@first.admin.institution@@');
+insert into catissue_cancer_research_group (name) values('@@first.admin.cancerresearchgroup@@');
 insert into catissue_address (state,country,zipcode) values(null,null,null);
 
-UPDATE CSM_USER SET LOGIN_NAME='mandar_deshmukh@persistent.co.in',
+UPDATE CSM_USER SET LOGIN_NAME='@@first.admin.emailAddress@@',
 					DEPARTMENT=1,
-					EMAIL_ID='mandar_deshmukh@persistent.co.in',
-					PASSWORD='6c416f576765696c6e63316f326d3365'
+					EMAIL_ID='@@first.admin.emailAddress@@',
+					PASSWORD='@@first.admin.encodedPassword@@'
 				WHERE USER_ID=1;	
 
-UPDATE catissue_user SET EMAIL_ADDRESS='mandar_deshmukh@persistent.co.in',
-							LOGIN_NAME='mandar_deshmukh@persistent.co.in',
+UPDATE catissue_user SET EMAIL_ADDRESS='@@first.admin.emailAddress@@',
+							LOGIN_NAME='@@first.admin.emailAddress@@',
 							DEPARTMENT_ID=1,
 							INSTITUTION_ID=1,
 							CANCER_RESEARCH_GROUP_ID=1,
-							ADDRESS_ID=1
+							ADDRESS_ID=1,
 						WHERE 
 							IDENTIFIER = 1;
 
 							
-UPDATE catisue_password set PASSWORD='6c416f576765696c6e63316f326d3365'	
+UPDATE catissue_password set PASSWORD='@@first.admin.encodedPassword@@'
 						WHERE 
-							IDENTIFIER = 1;	
+							IDENTIFIER = 1;
