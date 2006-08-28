1e09a5c6af47c44e76d71f2644ea1de8d1115c82
Ashwin <Ashwin>
2006-08-28 09:25:31 +0000

    added for specimen array type biz logic
    SVN-Revision: 3946

---
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/SpecimenArrayTypeBizLogic.java
@@ -0,0 +1,34 @@
+/*
+ * <p>Title: SpecimenArrayTypeBizLogic Class </p>
+ * <p>Description:This class performs business level logic for Specimen Array Type </p>
+ * Copyright: Copyright (c) year 2006
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @version 1.1
+ * Created on Aug 24,2006
+ */
+package edu.wustl.catissuecore.bizlogic;
+
+import edu.wustl.catissuecore.domain.SpecimenArrayType;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.dao.DAO;
+import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
+import edu.wustl.common.util.dbManager.DAOException;
+
+
+/**
+ * <p>This class initializes the fields of SpecimenArrayTypeBizLogic.java</p>
+ * @author Ashwin Gupta
+ * @version 1.1
+ */
+public class SpecimenArrayTypeBizLogic extends DefaultBizLogic {
+	
+	/* (non-Javadoc)
+	 * @see edu.wustl.common.bizlogic.AbstractBizLogic#insert(java.lang.Object, edu.wustl.common.dao.DAO, edu.wustl.common.beans.SessionDataBean)
+	 */
+	protected void insert(Object obj, DAO dao, SessionDataBean sessionDataBean) throws DAOException, UserNotAuthorizedException {
+		SpecimenArrayType arrayType = (SpecimenArrayType) obj;
+		dao.insert(arrayType.getCapacity(),sessionDataBean, true, false);
+		dao.insert(arrayType,sessionDataBean, true, false);
+	}
+}
