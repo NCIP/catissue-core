0d9071964f48375643df81fd6115132271f7d9bf
vaishali_khandelwal <vaishali_khandelwal>
2006-09-01 08:26:55 +0000

    new sql file for default entries of storage type
    SVN-Revision: 3984

---
+++ SQL/Common/insert_default_data.sql
@@ -0,0 +1,4 @@
+
+/*********Inserting Any value in storage type tables********/
+insert into catissue_container_type (IDENTIFIER,CAPACITY_ID,NAME,ONE_DIMENSION_LABEL,TWO_DIMENSION_LABEL,COMMENT) values ( '1',NULL,'Any',NULL,NULL,NULL);
+insert into catissue_storage_type (IDENTIFIER,DEFAULT_TEMPERATURE,ACTIVITY_STATUS) values ( '1',NULL,'Disabled');
