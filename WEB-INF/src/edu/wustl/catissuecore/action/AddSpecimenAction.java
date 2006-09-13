311694bba105e974cd3b62f5024009622fd6bafc
santosh_chandak <santosh_chandak>
2006-09-13 06:16:53 +0000

    New class. Created to get Id from label or barcode
    SVN-Revision: 4060

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/AddSpecimenAction.java
@@ -0,0 +1,105 @@
+/**
+ * <p>Title: CreateSpecimenAction Class>
+ * <p>Description:	CreateSpecimenAction initializes the fields in the Create Specimen page.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Aniruddha Phadnis
+ * @version 1.00
+ */
+
+package edu.wustl.catissuecore.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.Globals;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.actionForm.CreateSpecimenForm;
+import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.domain.Specimen;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.bizlogic.IBizLogic;
+import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.global.ApplicationProperties;
+
+/**
+ * AddSpecimenAction gets the Specimen Id from Label or Barcode
+ * @author santosh_chandak
+ */
+public class AddSpecimenAction extends Action
+{
+
+	/**
+	 * Overrides the execute method of Action class.
+	 */
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException, DAOException
+	{
+
+		CreateSpecimenForm createForm = (CreateSpecimenForm) form;
+		String sourceObjectName = Specimen.class.getName();
+		String[] selectColumnName = {Constants.SYSTEM_IDENTIFIER};
+
+		String[] whereColumnName = new String[1];
+		Object[] whereColumnValue = new Object[1];
+		String errorString = null;
+
+		// checks whether label or barcode is selected
+		if (createForm.getCheckedButton().equals("1"))
+		{
+			whereColumnName[0] = Constants.SYSTEM_LABEL;
+			whereColumnValue[0] = createForm.getParentSpecimenLabel();
+			errorString = ApplicationProperties.getValue("quickEvents.specimenLabel");
+
+		}
+		else
+		{
+			whereColumnName[0] = Constants.SYSTEM_BARCODE;
+			whereColumnValue[0] = createForm.getParentSpecimenBarcode();
+			errorString = ApplicationProperties.getValue("quickEvents.barcode");
+		}
+
+		String[] whereColumnCondition = {"="};
+
+		String joinCondition = null;
+		IBizLogic bizLogic = BizLogicFactory.getInstance().getBizLogic(Constants.DEFAULT_BIZ_LOGIC);
+
+		List list = bizLogic.retrieve(sourceObjectName, selectColumnName, whereColumnName, whereColumnCondition, whereColumnValue, joinCondition);
+
+		/**
+		 *  If list is not empty, set the Parent specimen Id and forward to success. 
+		 *  If list is null or empty, forward to failure.
+		 */
+		if (list != null && !list.isEmpty())
+		{
+			Object obj = list.get(0);
+			Long specimen = (Long) obj;
+			createForm.setParentSpecimenId("" + specimen.longValue());
+			createForm.setReset(false);   // Will not reset the parameters   
+			return mapping.findForward(Constants.SUCCESS);
+		}
+		else
+		{
+			ActionErrors errors = (ActionErrors) request.getAttribute(Globals.ERROR_KEY);
+			if (errors == null)
+			{
+				errors = new ActionErrors();
+			}
+			errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("quickEvents.specimen.notExists", errorString));
+			saveErrors(request, errors);
+
+			return mapping.findForward(Constants.FAILURE);
+		}
+
+	}
+}
