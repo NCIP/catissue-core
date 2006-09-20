29d557929863bc328dcc6c6626106e581c68f399
Mandar <Mandar>
2006-09-20 08:42:11 +0000

    Handler classes for various events.
    SVN-Revision: 4196

---
+++ WEB-INF/src/edu/wustl/catissuecore/applet/listener/MapButtonHamdler.java
@@ -0,0 +1,31 @@
+/*
+ * This class will handle all the events of Map button.
+ * Created on Sep 19, 2006
+ * @author mandar_deshmukh
+ */
+package edu.wustl.catissuecore.applet.listener;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTable;
+
+/**
+ * @author mandar_deshmukh
+ * 
+ * This class will handle all the events of Map button.
+ */
+public class MapButtonHamdler extends caTissueHandler {
+
+	protected void handleAction(ActionEvent event) {
+		super.handleAction(event);
+		System.out.println("Inside MapButtonHandler");
+	}
+	
+	/**
+	 * @param table
+	 */
+	public MapButtonHamdler(JTable table) {
+		super(table);
+	}
+
+}
---
+++ WEB-INF/src/edu/wustl/catissuecore/applet/listener/TextFieldHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Sep 19, 2006
+ *
+ * This class will handle all the events related to TextFields.
+ * 
+ */
+package edu.wustl.catissuecore.applet.listener;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTable;
+
+/**
+ * @author mandar_deshmukh
+ * 
+ * This class will handle all the events related to TextFields.
+ */
+public class TextFieldHandler extends caTissueHandler {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.wustl.catissuecore.appletui.listener.caTissueHandler#handleAction(java.awt.event.ActionEvent)
+	 */
+	protected void handleAction(ActionEvent event) {
+		super.handleAction(event);
+		System.out.println("Inside TextFieldHandler");
+	}
+
+	/**
+	 * @param table
+	 */
+	public TextFieldHandler(JTable table) {
+		super(table);
+		// TODO Auto-generated constructor stub
+	}
+}
