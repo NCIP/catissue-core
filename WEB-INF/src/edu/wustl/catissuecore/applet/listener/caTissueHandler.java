063ae3af8e470e6f4d811863ecc4e72f2001b4ae
Rahul_Ner <Rahul_Ner>
2006-09-20 08:07:45 +0000

    package restructuring for applet
    SVN-Revision: 4190

---
+++ WEB-INF/src/edu/wustl/catissuecore/applet/listener/caTissueHandler.java
@@ -0,0 +1,67 @@
+package edu.wustl.catissuecore.applet.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTable;
+
+
+/**
+ * This is base Handler class for all of the component in tissuecore.
+ * 
+ * @author Mandar Deshmukh
+ * @author Rahul Ner
+ *
+ */
+public class caTissueHandler implements ActionListener
+{
+	/**
+	 * 
+	 */
+	protected JTable table;
+	
+	public caTissueHandler(JTable table)
+	{
+		this.table = table;
+	}
+	
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent event)
+	{
+		preActionPerformed();		
+		handleAction(event);
+		postActionPerformed();
+	}
+
+	/**
+	 * This method provides a hook to specific Listener classes that needs to do some functionality before action executed. 
+	 */
+	protected void preActionPerformed()
+	{
+	
+	}
+
+	/**
+	 * This method provides a hook to specific Listener classes that needs to do some functionality after action executed. 
+	 */
+	protected void postActionPerformed()
+	{
+		//fireEditingStopped();
+		table.getModel().setValueAt(getSelectedValue(),table.getSelectedRow(),table.getSelectedColumn());
+
+	}
+
+	protected Object getSelectedValue() {
+		return null;
+	}
+	
+	/**
+	 * This method handles specific action. Each subclass can provide custom handling.
+	 */
+	protected void handleAction(ActionEvent event)
+	{
+		
+	}
+}
