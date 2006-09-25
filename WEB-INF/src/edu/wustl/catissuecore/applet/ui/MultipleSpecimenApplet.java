e65d6b82b663901bfed451e8b060fa9bc59c2af2
Rahul_Ner <Rahul_Ner>
2006-09-25 12:45:16 +0000

    *** empty log message ***
    SVN-Revision: 4292

--- WEB-INF/src/edu/wustl/catissuecore/applet/ui/MultipleSpecimenApplet.java
+++ WEB-INF/src/edu/wustl/catissuecore/applet/ui/MultipleSpecimenApplet.java
@@ -1,13 +1,18 @@
+
 package edu.wustl.catissuecore.applet.ui;
 
 import java.awt.BorderLayout;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import edu.wustl.catissuecore.applet.AppletServerCommunicator;
 import edu.wustl.catissuecore.applet.component.BaseTable;
 import edu.wustl.catissuecore.applet.listener.BaseActionHandler;
+import edu.wustl.catissuecore.applet.model.BaseAppletModel;
 import edu.wustl.catissuecore.applet.model.MultipleSpecimenTableModel;
 import edu.wustl.catissuecore.applet.model.RowHeaderColumnModel;
 import edu.wustl.catissuecore.applet.model.SpecimenColumnModel;
@@ -16,7 +21,8 @@ import edu.wustl.catissuecore.applet.model.SpecimenColumnModel;
  * This applet displays main UI for multiple specimen page.
  * @author mandar_deshmukh
  */
-public class MultipleSpecimenApplet extends BaseApplet {
+public class MultipleSpecimenApplet extends BaseApplet
+{
 
 	/**
 	 * Default Serial Version ID
@@ -24,47 +30,70 @@ public class MultipleSpecimenApplet extends BaseApplet {
 	private static final long serialVersionUID = 1L;
 
 	public void doInit()
-    {
-		int columnNumber = Integer.parseInt(this.getParameter("noOfSpecimen")); 
-		MultipleSpecimenTableModel model = new MultipleSpecimenTableModel(columnNumber);
-        
+	{
+		int columnNumber = Integer.parseInt(this.getParameter("noOfSpecimen"));
+		MultipleSpecimenTableModel model = new MultipleSpecimenTableModel(columnNumber,getInitDataMap());
+
 		BaseTable table = new BaseTable(model)
-        {
-            public Class getColumnClass(int column)
-            {
-                return getValueAt(0, column).getClass();
-            }
-        };
-        
-			
+		{
+
+			public Class getColumnClass(int column)
+			{
+				return getValueAt(0, column).getClass();
+			}
+		};
+
 		//table.getColumnModel().setColumnSelectionAllowed(true);
 		table.setColumnSelectionAllowed(true);
 		//table.setRowHeight(3,50);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		 
-		JScrollPane scrollPane = new JScrollPane( table );
-        getContentPane().add( scrollPane );
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		JScrollPane scrollPane = new JScrollPane(table);
+		getContentPane().add(scrollPane);
 		JButton showAll = new JButton("ShowData");
-		
+
 		showAll.addActionListener(new BaseActionHandler(table));
 
-		getContentPane().add( showAll,BorderLayout.SOUTH );
+		getContentPane().add(showAll, BorderLayout.SOUTH);
 
-        //  Create custom columns
-		new RowHeaderColumnModel(table);		//	for zeroth column
-		for(int cnt = 1; cnt < columnNumber; cnt++)
+		//  Create custom columns
+		new RowHeaderColumnModel(table); //	for zeroth column
+		for (int cnt = 1; cnt < columnNumber; cnt++)
 		{
 			new SpecimenColumnModel(table, cnt);
 		}
-//		SpecimenColumnModel SpecimenFieldColumn1= new SpecimenColumnModel(table, 1);
-//		SpecimenColumnModel SpecimenFieldColumn2 = new SpecimenColumnModel(table, 2);
-//		SpecimenColumnModel SpecimenFieldColumn3 = new SpecimenColumnModel(table, 3);
-		
-    }
- 
-    public static void main(String[] args)
-    {
-    	MultipleSpecimenApplet frame = new MultipleSpecimenApplet();
-    }
+		//		SpecimenColumnModel SpecimenFieldColumn1= new SpecimenColumnModel(table, 1);
+		//		SpecimenColumnModel SpecimenFieldColumn2 = new SpecimenColumnModel(table, 2);
+		//		SpecimenColumnModel SpecimenFieldColumn3 = new SpecimenColumnModel(table, 3);
+
+	}
+
+	public static void main(String[] args)
+	{
+		MultipleSpecimenApplet frame = new MultipleSpecimenApplet();
+	}
+
+	/**
+	 * This method initialize data lists 
+	 */
+	private Map getInitDataMap()
+	{
+		BaseAppletModel appletModel = new BaseAppletModel();
+		appletModel.setData(new HashMap());
+		try
+		{
+			String url = getServerURL() + "/MultipleSpecimenAppletAction.do?method=initData";
+
+			appletModel = (BaseAppletModel) AppletServerCommunicator.doAppletServerCommunication(
+					url, appletModel);
+
+			return appletModel.getData();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
