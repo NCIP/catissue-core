f2592d6925a875dce9553c58677b4a5687577e00
Ashwin <Ashwin>
2006-09-27 10:19:13 +0000

    add unique constraint to container
    SVN-Revision: 4410

--- SQL/DBUpgrade/MySql/DBUpgrade_1.0_to_1.1.sql
+++ SQL/DBUpgrade/MySql/DBUpgrade_1.0_to_1.1.sql
@@ -468,6 +468,7 @@ alter table CATISSUE_USER add column FIRST_TIME_LOGIN bit default 1;
 /* storage type unique changes */
 update catissue_container_type set name = 'All' where identifier = 1;
 alter table CATISSUE_container_type add constraint NAME unique (NAME);
+alter table CATISSUE_container add constraint NAME unique (NAME);
 
 /*Poornima -26/09/06-Added unspecified value to Vital status and NOT SPECIFIED value in Tissue Site- */
 INSERT INTO CATISSUE_PERMISSIBLE_VALUE (IDENTIFIER, VALUE, PARENT_IDENTIFIER, PUBLIC_ID) VALUES(2641,'Unspecified',NULL,'2004001');
