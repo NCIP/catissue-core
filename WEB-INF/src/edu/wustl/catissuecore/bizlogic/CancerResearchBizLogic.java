e86af4bca0d618ddb4a10bd279fed0b0dac0ec78
ashish_gupta <ashish_gupta>
2006-10-03 06:56:10 +0000

    New biz logic created for CancerResearch for access through Api
    SVN-Revision: 4521

---
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/CancerResearchBizLogic.java
@@ -0,0 +1,41 @@
+/**
+ * <p>Title: CancerResearchBizLogic Class>
+ * <p>Description:	CancerResearchBizLogic </p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Ashish Gupta
+ * @version 1.00
+ */
+
+package edu.wustl.catissuecore.bizlogic;
+
+import edu.wustl.catissuecore.domain.CancerResearchGroup;
+import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.dao.DAO;
+import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.global.ApplicationProperties;
+import edu.wustl.common.util.global.Validator;
+
+public class CancerResearchBizLogic extends DefaultBizLogic
+{
+
+	protected boolean validate(Object obj, DAO dao, String operation) throws DAOException
+	{
+		CancerResearchGroup cancerResearchGroupObj = (CancerResearchGroup) obj;
+		String message = "";
+		if (cancerResearchGroupObj == null)
+		{
+			message = ApplicationProperties.getValue("user.cancerResearchGroup");
+
+			throw new DAOException("domain.object.null.err.msg", new String[]{message});
+		}
+		Validator validator = new Validator();
+		if (validator.isEmpty(cancerResearchGroupObj.getName()))
+		{
+			message = ApplicationProperties.getValue("cancerResearchGroup.name");
+			throw new DAOException("errors.item.required", new String[]{message});
+		}
+		return true;
+	}
+
+}
