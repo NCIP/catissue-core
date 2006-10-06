6f5a926391fc9363d0588c019f155de6bc3bdfbd
Mandar <Mandar>
2006-10-06 13:28:43 +0000

    column addition code updated
    SVN-Revision: 4599

--- WEB-INF/src/edu/wustl/catissuecore/applet/listener/AddColumnHandler.java
+++ WEB-INF/src/edu/wustl/catissuecore/applet/listener/AddColumnHandler.java
@@ -15,6 +15,7 @@ import javax.swing.table.TableColumn;
 import edu.wustl.catissuecore.applet.component.BaseTable;
 import edu.wustl.catissuecore.applet.model.MultipleSpecimenTableModel;
 import edu.wustl.catissuecore.applet.model.SpecimenColumnModel;
+import edu.wustl.catissuecore.applet.ui.MultipleSpecimenApplet;
 
 /**
  * This class handles the Add Specimen button events. 
@@ -25,34 +26,46 @@ import edu.wustl.catissuecore.applet.model.SpecimenColumnModel;
  */
 public class AddColumnHandler extends ButtonHandler {
 
-	
+	private MultipleSpecimenApplet applet;
 	/* (non-Javadoc)
 	 * @see edu.wustl.catissuecore.applet.listener.BaseActionHandler#handleAction(java.awt.event.ActionEvent)
 	 */
 	protected void handleAction(ActionEvent event) {
 		System.out.println("Inside AddColumnHandler");
-		addColumnToTable(table);
+		System.out.println("table.getColumnCount() : "+ table.getColumnCount());
+		int tableColumnCount = table.getColumnCount();
+		MultipleSpecimenTableModel model = (MultipleSpecimenTableModel)table.getModel();
+		System.out.println("table.getColumnCount() : "+ table.getColumnCount()+" | model.getColumnsPerPage(): "+model.getColumnsPerPage());
+		if(tableColumnCount == model.getColumnsPerPage())
+		{
+			System.out.println("Applet to be updated for page buttons.");
+			model.addColumn(); 
+			applet.updateOnAddSpecimen();
+		}else if(tableColumnCount < model.getColumnsPerPage())
+		{
+			addColumnToTable(table);	
+		}
 	}
 
-	public AddColumnHandler(JTable table) {
+	public AddColumnHandler(JTable table, MultipleSpecimenApplet applet) {
 		super(table);
+		this.applet = applet;
 	}
 
 	//	 This method adds a new column to table without reconstructing
     // all the other columns.
-    private void addColumnToTable(JTable table) {
+    private void addColumnToTable(JTable table) 
+    {
     	BaseTable baseTable = (BaseTable )table;
     	
-    	if (baseTable.getAutoCreateColumnsFromModel()) {
+    	if (baseTable.getAutoCreateColumnsFromModel()) 
+    	{
             throw new IllegalStateException();
         }
-
-       	
     	MultipleSpecimenTableModel model = (MultipleSpecimenTableModel)baseTable.getModel();
     	System.out.println("Start -- Table colCount : " +table.getColumnCount() + " Model ColCount : " + model.getColumnCount());
     	  
     	String headerLabel =model.getColumnName(model.getColumnCount());
-//    	TableColumn col = new TableColumn(model.getColumnCount());
     	TableColumn col = new TableColumn(table.getColumnCount());
     	col.setHeaderValue(headerLabel);
 
@@ -60,17 +73,7 @@ public class AddColumnHandler extends ButtonHandler {
     	table.addColumn(col);
     	System.out.println("After Table AddColumn -- Table colCount : " +table.getColumnCount() + " Model ColCount : " + model.getColumnCount());
 
-//    	//2
-//    	table.getColumnModel().addColumn(col);
-//    	System.out.println("After TableColumnModel AddColumn -- Table colCount : " +table.getColumnCount() + " Model ColCount : " + model.getColumnCount());
-
-//    	commenting to test addition of single column.
-    	//3
-//    	model.addColumn();
-//    	System.out.println("After Model AddColumn -- Table colCount : " +table.getColumnCount() + " Model ColCount : " + model.getColumnCount());
-    	
     	int colno = table.getColumnCount() - 1;
-//    	int colno = model.getColumnCount() - 1 ;
     	new SpecimenColumnModel(baseTable,colno );
 
     	System.out.println("End -- Table colCount : " +table.getColumnCount() + " Model ColCount : " + model.getColumnCount());
@@ -80,19 +83,5 @@ public class AddColumnHandler extends ButtonHandler {
     	col = new TableColumn(table.getColumnCount());
     	table.removeColumn(col );
     	table.getColumnModel().removeColumn(col ); 
-    	// ----------- this code adds 2 columns to the ui. Not getting Y.
-//   	
-//    	MultipleSpecimenTableModel model = (MultipleSpecimenTableModel)baseTable.getModel();
-//    	int colno = model.getColumnCount();
-//    	
-//    	String headerLabel =model.getColumnName(model.getColumnCount());
-//    	TableColumn col = new TableColumn(model.getColumnCount());
-//    	col.setHeaderValue(headerLabel);
-//    	table.addColumn(col);
-//    	table.getColumnModel().addColumn(col);
-//    	new SpecimenColumnModel(baseTable,colno );
-//    	model.addColumn();
-    	// ----------- this code adds 2 columns to the ui. Not getting Y.
-       
     }
 }
