35c11f18c66793d9d7f1fdaff820f24b59a592f3
santosh_chandak <santosh_chandak>
2006-10-07 08:00:23 +0000

    commented specimen = new CellSpecimen(); as it was throwing exception while saving ---> need to discuss
    SVN-Revision: 4630

--- WEB-INF/src/edu/wustl/catissuecore/domain/SpecimenEventParameters.java
+++ WEB-INF/src/edu/wustl/catissuecore/domain/SpecimenEventParameters.java
@@ -65,8 +65,9 @@ public abstract class SpecimenEventParameters extends EventParameters implements
 		
 		//Temporary fix.
 		if(abstractForm.isAddOperation())
-				specimen = new CellSpecimen();
-		
+		{
+			//specimen = new CellSpecimen();
+		}
 		SpecimenEventParametersForm specimenEventParametersForm = (SpecimenEventParametersForm) abstractForm;
 		Logger.out.debug("specimenEventParametersForm.getSpecimenId()............................."+specimenEventParametersForm.getSpecimenId());
 		if(specimen!=null)
