0f7946b613e14fc92d318a5f77cc492eabbe099f
santosh_chandak <santosh_chandak>
2006-11-01 11:59:59 +0000

    changed for copy paste of associated objects
    SVN-Revision: 5078

--- WEB-INF/src/edu/wustl/catissuecore/applet/listener/MultipleSpecimenCopyActionHandler.java
+++ WEB-INF/src/edu/wustl/catissuecore/applet/listener/MultipleSpecimenCopyActionHandler.java
@@ -1,6 +1,8 @@
+
 package edu.wustl.catissuecore.applet.listener;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +14,7 @@ import edu.wustl.catissuecore.applet.AppletConstants;
 import edu.wustl.catissuecore.applet.AppletServerCommunicator;
 import edu.wustl.catissuecore.applet.CopyPasteOperationValidatorModel;
 import edu.wustl.catissuecore.applet.model.BaseAppletModel;
+import edu.wustl.catissuecore.applet.model.MultipleSpecimenTableModel;
 import edu.wustl.catissuecore.applet.ui.MultipleSpecimenApplet;
 import edu.wustl.catissuecore.applet.util.CommonAppletUtil;
 
@@ -22,10 +25,11 @@ import edu.wustl.catissuecore.applet.util.CommonAppletUtil;
  * @author mandar_deshmukh
  * @version 1.1
  */
-public class MultipleSpecimenCopyActionHandler extends AbstractCopyActionHandler 
+public class MultipleSpecimenCopyActionHandler extends AbstractCopyActionHandler
 {
+
 	private JButton paste;
-	
+
 	/**
 	 * Empty constructor
 	 */
@@ -33,6 +37,7 @@ public class MultipleSpecimenCopyActionHandler extends AbstractCopyActionHandler
 	{
 		super();
 	}
+
 	/**
 	 * @param table
 	 */
@@ -45,142 +50,88 @@ public class MultipleSpecimenCopyActionHandler extends AbstractCopyActionHandler
 	/**
 	 * @see edu.wustl.catissuecore.applet.listener.AbstractCopyActionHandler#doActionPerformed(java.awt.event.ActionEvent)
 	 */
-	protected void doActionPerformed(ActionEvent event) 
+	protected void doActionPerformed(ActionEvent event)
 	{
 		CommonAppletUtil.getSelectedData(table);
 		paste.setEnabled(true);
 		super.doActionPerformed(event);
-		/*
-		 Commented as code move to common abstractcopy action handler -- Ashwin 
-		//to set selected data in model.
-		//super.handleAction(event);
-		System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
-		System.out.println("Inside MultipleSpecimenCopyActionHandler");
 		
-		CopyPasteOperationValidatorModel validatorModel = new CopyPasteOperationValidatorModel();
-		int[] selectedColumns = table.getSelectedColumns();
-		int[] selectedRows = table.getSelectedRows();
-		System.out.println("selectedRows : ");
-		CommonAppletUtil.printArray(selectedRows );
-		System.out.println(" \n selectedColumns : ");
-		CommonAppletUtil.printArray(selectedColumns );		  
-		
-		validatorModel.setSelectedCopiedRows(CommonAppletUtil.createListFromArray(selectedRows));
-		validatorModel.setSelectedCopiedCols(CommonAppletUtil.createListFromArray(selectedColumns));
-		validatorModel.setCopiedData(CommonAppletUtil.getSelectedData(table) );
-//for validator
-		validatorModel.setOperation(AppletConstants.COPY_OPERATION);
-		validatorModel.setColumnCount(CommonAppletUtil.getMultipleSpecimenTableModel(table).getTotalColumnCount());
-		validatorModel.setRowCount(table.getRowCount());
+		MultipleSpecimenTableModel multipleSpecimenTableModel = CommonAppletUtil.getMultipleSpecimenTableModel(table);
+		multipleSpecimenTableModel.showMapData();
 
-		// for validation
-		MultipleSpecimenCopyPasteValidator copyPasteValidator = new MultipleSpecimenCopyPasteValidator(table,validatorModel);
-		String errorMessage = copyPasteValidator.validateForCopy();
-		System.out.println("Message from copyPasteValidator.validateForCopy() : "+ errorMessage);
-		if(errorMessage.trim().length()>0)
-		{
-		    Object[] parameters = new Object[]{errorMessage }; 
-		    CommonAppletUtil.callJavaScriptFunction((JButton) e.getSource(),getJSMethodName(), parameters);
-		}
-		// for validation end
-		CommonAppletUtil.getMultipleSpecimenTableModel(table).setCopyPasteOperationValidatorModel( validatorModel);
-		System.out.println("\n >>>>>>>>>>>>>>   Copy Data Set.    >>>>>>>>>>>>");
-		*/
-		CommonAppletUtil.getMultipleSpecimenTableModel(table).showMapData(); 
-				
-		CopyPasteOperationValidatorModel validatorModel = CommonAppletUtil.getMultipleSpecimenTableModel(table).getCopyPasteOperationValidatorModel();
+		CopyPasteOperationValidatorModel validatorModel = multipleSpecimenTableModel.getCopyPasteOperationValidatorModel();
 		List selectedCopiedRows = validatorModel.getSelectedCopiedRows();
 	
 		
+	if(isValidateSuccess)	
+	{
 		/**
 		 *  check if button(s) also copied
 		 */
-	      boolean isButtonCopied = false;
-			for (int i = 0; i < selectedCopiedRows.size(); i++)
+		boolean isButtonCopied = false;
+		for (int i = 0; i < selectedCopiedRows.size(); i++)
+		{
+			int copiedRow = ((Integer) selectedCopiedRows.get(i)).intValue();
+			if (copiedRow >= AppletConstants.SPECIMEN_COMMENTS_ROW_NO)
 			{
-				int copiedRow = ((Integer) selectedCopiedRows.get(i)).intValue();
-				if(copiedRow >= AppletConstants.SPECIMEN_COMMENTS_ROW_NO)
-				{
-					System.out.println("copiedRow-->"+copiedRow);
-					isButtonCopied = true;
-					break;
-				}
+				isButtonCopied = true;
+				break;
 			}
-
-	if(isButtonCopied)
-	{
-		BaseAppletModel appletModel = new BaseAppletModel();
-		System.out.println("inside -->" + isButtonCopied);
-		Map validatorDataMap = new HashMap();
-		validatorDataMap.put(AppletConstants.VALIDATOR_MODEL,validatorModel);
-	//	validatorDataMap.put(AppletConstants.VALIDATOR_MODEL,"String");
-		appletModel.setData(validatorDataMap);
-		try
-		{
-			MultipleSpecimenApplet applet = (MultipleSpecimenApplet) CommonAppletUtil
-					.getBaseApplet(table);
-			String url = applet.getServerURL()
-					+ "/MultipleSpecimenCopyPasteAction.do?method=copy";
-
-			appletModel = (BaseAppletModel) AppletServerCommunicator.doAppletServerCommunication(
-					url, appletModel);
-				
 		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			System.out.println("Exception");
-		}
-	}	
+
 		
+		if (isButtonCopied)
+		{
+			List selectedCopiedCols = validatorModel.getSelectedCopiedCols();
+			List tempSelectedCopiedCols = new ArrayList();
+			for (int i = 0; i < selectedCopiedCols.size(); i++)
+			{
+				int copiedCol = ((Integer) selectedCopiedCols.get(i)).intValue();
+				Integer tempCopiedCol = new Integer(multipleSpecimenTableModel.getActualColumnNo(copiedCol));
+				tempSelectedCopiedCols.add(tempCopiedCol);
+			}
+		
+		    validatorModel.setSelectedCopiedCols(tempSelectedCopiedCols);
+		    validatorModel.setPageIndexWhenCopied(multipleSpecimenTableModel.getCurrentPageIndex());
+			BaseAppletModel appletModel = new BaseAppletModel();
+			Map validatorDataMap = new HashMap();
+			validatorDataMap.put(AppletConstants.VALIDATOR_MODEL, validatorModel);
+			appletModel.setData(validatorDataMap);
+			try
+			{
+				MultipleSpecimenApplet applet = (MultipleSpecimenApplet) CommonAppletUtil.getBaseApplet(table);
+				String url = applet.getServerURL() + "/MultipleSpecimenCopyPasteAction.do?method=copy";
+
+				appletModel = (BaseAppletModel) AppletServerCommunicator.doAppletServerCommunication(url, appletModel);
+
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				System.out.println("Exception");
+			}
+			validatorModel.setSelectedCopiedCols(selectedCopiedCols);
+		}
+	}
+
 		System.out.println("\n >>>>>>>>>>>>>>  DONE >>>>>>>>>>>>");
 	}
-	
-//	/*
-//	 * This method returns the map holding the selected data.
-//	 * The key is represented by the row@column format.
-//	 */
-//	private HashMap getSelectedData(int[] selectedRows, int[] selectedColumns)
-//	{
-//		System.out.println("\n/////////// inside getSelectedData ///////////////////\n");
-//		HashMap map = new HashMap();
-//		for(int rowIndex=0;rowIndex<selectedRows.length; rowIndex++  )
-//		{
-//			for(int columnIndex=0; columnIndex<selectedColumns.length; columnIndex++)
-//			{
-//				String key = CommonAppletUtil.getDataKey(selectedRows[rowIndex], selectedColumns[columnIndex]);
-//				//commented to check the values from cell editor
-////				Object value = table.getValueAt(selectedRows[rowIndex],selectedColumns[columnIndex] );
-//				//--------
-//				TableColumnModel columnModel = table.getColumnModel();
-//				SpecimenColumnModel scm = (SpecimenColumnModel)columnModel.getColumn(selectedColumns[columnIndex]).getCellEditor();
-//				JComponent component = ((JComponent)scm.getTableCellEditorComponent(table,null,true,selectedRows[rowIndex],selectedColumns[columnIndex]));
-//				Object value =scm.getCellEditorValue(); 	
-//				// -------
-//				map.put(key,value );
-//			}
-//		}
-//		System.out.println("Returning Map -------------------------\n");
-//		System.out.println(map);
-//		return map;
-//	}
-	
 
 
-/**
-* @return JS method name for this button.
-*/
-protected String getJSMethodName()
-{
-	return "showErrorMessage";
-}
+	/**
+	 * @return JS method name for this button.
+	 */
+	protected String getJSMethodName()
+	{
+		return "showErrorMessage";
+	}
 
-/**
- * @return total coumn count
- */
-protected int getColumnCount()
-{
-	return CommonAppletUtil.getMultipleSpecimenTableModel(table).getTotalColumnCount();		
-}
-	
+	/**
+	 * @return total coumn count
+	 */
+	protected int getColumnCount()
+	{
+		return CommonAppletUtil.getMultipleSpecimenTableModel(table).getTotalColumnCount();
+	}
+
 }
--- WEB-INF/src/edu/wustl/catissuecore/applet/listener/MultipleSpecimenPasteActionHandler.java
+++ WEB-INF/src/edu/wustl/catissuecore/applet/listener/MultipleSpecimenPasteActionHandler.java
@@ -2,6 +2,7 @@
 package edu.wustl.catissuecore.applet.listener;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,7 @@ import edu.wustl.catissuecore.applet.AppletConstants;
 import edu.wustl.catissuecore.applet.AppletServerCommunicator;
 import edu.wustl.catissuecore.applet.CopyPasteOperationValidatorModel;
 import edu.wustl.catissuecore.applet.model.BaseAppletModel;
+import edu.wustl.catissuecore.applet.model.MultipleSpecimenTableModel;
 import edu.wustl.catissuecore.applet.model.SpecimenColumnModel;
 import edu.wustl.catissuecore.applet.ui.MultipleSpecimenApplet;
 import edu.wustl.catissuecore.applet.util.CommonAppletUtil;
@@ -40,52 +42,16 @@ public class MultipleSpecimenPasteActionHandler extends AbstractPasteActionHandl
 	protected void doActionPerformed(ActionEvent event)
 	{
 			super.doActionPerformed(event);
-			//super.handleAction(event);
-			/*
-			 * Commented as code move to common abstractcopy action handler -- Ashwin
-			System.out.println("\n<<<<<<<<<<<          PASTE         <<<<<<<<<<<<<<<<<<<<<<\n");
-			System.out.println("Inside MultipleSpecimenPasteActionHandler");
 			
-			CopyPasteOperationValidatorModel validatorModel = CommonAppletUtil.getMultipleSpecimenTableModel(table).getCopyPasteOperationValidatorModel();
-			
-			int[] selectedColumns = table.getSelectedColumns();
-			int[] selectedRows = table.getSelectedRows();
-			System.out.println("selectedRows : ");
-			CommonAppletUtil.printArray(selectedRows );
-			System.out.println(" \n selectedColumns : ");
-			CommonAppletUtil.printArray(selectedColumns );		  
+			MultipleSpecimenTableModel multipleSpecimenTableModel = CommonAppletUtil.getMultipleSpecimenTableModel(table);
+			multipleSpecimenTableModel.showMapData(); 
+					
+			CopyPasteOperationValidatorModel validatorModel = multipleSpecimenTableModel.getCopyPasteOperationValidatorModel();
+			List selectedCopiedRows = validatorModel.getSelectedCopiedRows();
 
-			validatorModel.setSelectedPastedRows(CommonAppletUtil.createListFromArray(selectedRows));
-			validatorModel.setSelectedPastedCols(CommonAppletUtil.createListFromArray(selectedColumns));
-			//for validatior
-			validatorModel.setOperation(AppletConstants.PASTE_OPERATION  );
-			validatorModel.setColumnCount(CommonAppletUtil.getMultipleSpecimenTableModel(table).getTotalColumnCount());
-			validatorModel.setRowCount(table.getRowCount());
-			
-			System.out.println(" Cell info set for Pasting ");
-			
-			//for validation
-			MultipleSpecimenCopyPasteValidator copyPasteValidator = new MultipleSpecimenCopyPasteValidator(table,validatorModel);
-			String errorMessage = copyPasteValidator.validateForPaste();
-			System.out.println("Message from copyPasteValidator.validateForPaste() : "+ errorMessage);
-			if(errorMessage.trim().length()>0)
-			{
-			    Object[] parameters = new Object[]{errorMessage }; 
-			    CommonAppletUtil.callJavaScriptFunction((JButton) e.getSource(),
-				getJSMethodName(), parameters);
-			}	// validation end
-			else if (CommonAppletUtil.validateCell())
-			{
-				updateUI(validatorModel);
-			}
-		System.out.println("\n >>>>>>>>>>>>>> PASTE DONE >>>>>>>>>>>>");
-		*/
-		CommonAppletUtil.getMultipleSpecimenTableModel(table).showMapData();
-		System.out.println("\n >>>>>>>>>>>>>>MAP in PASTE  >>>>>>>>>>>>");
-		
-		CopyPasteOperationValidatorModel validatorModel = CommonAppletUtil.getMultipleSpecimenTableModel(table).getCopyPasteOperationValidatorModel();
-		List selectedCopiedRows = validatorModel.getSelectedCopiedRows();
-	
+		if(isValidateSuccess)	
+		{
+	     
 		/**
 		 *  check if button(s) also copied
 		 */
@@ -99,13 +65,37 @@ public class MultipleSpecimenPasteActionHandler extends AbstractPasteActionHandl
 					break;
 				}
 			}
-
+			
 	if(isButtonCopied)
 	{
+		List selectedCopiedCols = validatorModel.getSelectedCopiedCols();
+		List tempSelectedCopiedCols = new ArrayList();
+		for (int i = 0; i < selectedCopiedCols.size(); i++)
+		{
+			int copiedCol = ((Integer) selectedCopiedCols.get(i)).intValue();
+			int pageIndex = validatorModel.getPageIndexWhenCopied();
+			int columnsPerPage = multipleSpecimenTableModel.getColumnsPerPage();
+			int colNumberWhenCopied = (columnsPerPage * ( pageIndex - 1) ) + copiedCol;
+			Integer tempCopiedCol = new Integer(colNumberWhenCopied);
+			tempSelectedCopiedCols.add(tempCopiedCol);
+		}
+	
+	    validatorModel.setSelectedCopiedCols(tempSelectedCopiedCols);
+		
+	    List selectedPastedCols = validatorModel.getSelectedPastedCols();
+		List tempSelectedPastedCols = new ArrayList();
+		
+		for (int i = 0; i < selectedPastedCols.size(); i++)
+		{
+			int copiedCol = ((Integer) selectedPastedCols.get(i)).intValue();
+			Integer tempCopiedCol = new Integer(multipleSpecimenTableModel.getActualColumnNo(copiedCol));
+			tempSelectedPastedCols.add(tempCopiedCol);
+		}
+		validatorModel.setSelectedPastedCols(tempSelectedPastedCols);
 		BaseAppletModel appletModel = new BaseAppletModel();
 
 		Map validatorDataMap = new HashMap();
-		validatorDataMap.put(AppletConstants.VALIDATOR_MODEL,validatorModel);
+		 validatorDataMap.put(AppletConstants.VALIDATOR_MODEL,validatorModel);
 		appletModel.setData(validatorDataMap);
 		try
 		{
@@ -123,111 +113,13 @@ public class MultipleSpecimenPasteActionHandler extends AbstractPasteActionHandl
 			e.printStackTrace();
 			System.out.println("Exception");
 		}
+	    validatorModel.setSelectedCopiedCols(selectedCopiedCols);
+		validatorModel.setSelectedPastedCols(selectedPastedCols);
 	}
 	}
-	
-	/*
-	private void updateTableModel(int row, int col, Object value)
-	{
-		System.out.println("\n---X------X------X------X------X------X------X---\n");
-		System.out.println("Inside MultipleSpecimenPasteActionHandler updateTableModel. >>>>>>>>>>>>>>>>>>>>>\n");
-		System.out.println("Setting value at row : "+row+" , col : "+col+" , value : "+value);
-		CommonAppletUtil.getMultipleSpecimenTableModel(table).setValueAt(value,row,col );
-		System.out.println("Value Set");
-		System.out.println("\n---X------X------X------X------X------X------X---\n");
-	}
-	*/
-	/*
-	private void updateUI(CopyPasteOperationValidatorModel validatorModel)
-	{
-		System.out.println("This method updates the table UI.");
-		HashMap dataMap = validatorModel.getCopiedData();
-		System.out.println("\n Received Copied Data Map >>>>>>>>>>> \n");
-		System.out.println(dataMap);
-		System.out.println("\n-------------------------\n");
-
-		System.out.println("Rows : "+ validatorModel.getSelectedPastedRows());
-		System.out.println("Cols : " + validatorModel.getSelectedPastedCols());
-		
-		System.out.println("\n----- Setting value --------------------\n");
-		//commenting to test multiple cell pasting
-//		int selectedRow = table.getSelectedRow();
-//		int selectedCol = table.getSelectedColumn ();
-//		
-//		int r = ((Integer)(validatorModel.getSelectedPastedRows().get(0))).intValue() ;
-//		int c = ((Integer)(validatorModel.getSelectedPastedCols().get(0))).intValue() ;
-//		
-//		String key = CommonAppletUtil.getDataKey(((Integer)(validatorModel.getSelectedCopiedRows().get(0))).intValue(),((Integer)(validatorModel.getSelectedCopiedCols().get(0))).intValue()); 
-//			
-//		System.out.println("Key : "+ key);
-//		Object value = dataMap.get(key );
-//		System.out.println("Value : "+ value);
-//		TableColumnModel columnModel = table.getColumnModel();
-//		SpecimenColumnModel scm = (SpecimenColumnModel)columnModel.getColumn(table.getSelectedColumn()).getCellEditor();
-//		scm.updateComponentValue(selectedRow,value.toString() );
-		System.out.println("Setting values to multiple cells\n-------------------------------\n");
-
-		int selectedRow = table.getSelectedRow();
-		int selectedCol = table.getSelectedColumn ();
-
-		setUI(validatorModel, dataMap, selectedRow, selectedCol);
-		System.out.println("\n>>>>>>>>>>>>>>>> UI data Set. <<<<<<<<<<<<<<<<<<<<<  \n");
 	}
 	
-	private void setUI(CopyPasteOperationValidatorModel validatorModel, HashMap dataMap, int selectedRow, int selectedCol)
-	{
-		int tmpSelectedRow = selectedRow;
-		
-		// copeid rows, columns
-		List copiedRows = validatorModel.getSelectedCopiedRows();
-		List copiedCols = validatorModel.getSelectedCopiedCols();
-		Collections.sort(copiedRows);
-		Collections.sort(copiedCols);
-		
-//		//paste rows, columns
-//		List pasteRows = validatorModel.getSelectedPastedRows();
-//		List pasteCols = validatorModel.getSelectedPastedCols();
-//		Collections.sort(pasteRows);
-//		Collections.sort(pasteRows);
-		
-		System.out.println("CopiedRows Size: "+ copiedRows.size()+" | "+copiedRows);
-		System.out.println("CopiedColumns Size: "+ copiedCols.size()+" | "+ copiedCols);
-//		int selectedColumnIndex = table.getSelectedColumn();
-		int selectedColumnIndex = selectedCol;
-
-		for(int copiedColumnCount=0; copiedColumnCount<copiedCols.size(); copiedColumnCount++)
-		{
-			int copiedCol = ((Integer)(copiedCols.get(copiedColumnCount))).intValue();
-			selectedRow = tmpSelectedRow;
-			for(int count=0;count<copiedRows.size();count++ )
-			{
-				int copiedRow = ((Integer)(copiedRows.get(count))).intValue();
-				//check for disabled rows
-				if(!isDisabledRow(selectedRow))
-				{
-					System.out.println("Count : "+count+ " ,copiedRow: "+ copiedRow + " , copiedCol: "+ copiedCol);
-					String key = CommonAppletUtil.getDataKey(copiedRow, copiedCol);
-					System.out.println("Key : "+ key);
-					Object value = dataMap.get(key );
-					System.out.println("Value : "+ value);
-
-					TableColumnModel columnModel = table.getColumnModel();
-					SpecimenColumnModel scm = (SpecimenColumnModel)columnModel.getColumn(selectedColumnIndex).getCellEditor();
-					scm.updateComponentValue(selectedRow,value.toString() );
-					SpecimenColumnModel scmRenderer = (SpecimenColumnModel)columnModel.getColumn(selectedColumnIndex).getCellRenderer();
-					scmRenderer.updateComponent(selectedRow );
-					//To update the model.
-					updateTableModel(selectedRow, selectedColumnIndex,  value);
-
-					System.out.println("Row updated : " + selectedRow );
-				}
-				selectedRow = selectedRow + 1;
-			}
-			selectedColumnIndex++;
-		}
-		SwingUtilities.updateComponentTreeUI(table);
-	}
-	*/
+	
 	protected void doPasteData(int selectedRow, int selectedCol, List valueList)
 	{
 		if(selectedRow < AppletConstants.SPECIMEN_COMMENTS_ROW_NO)
