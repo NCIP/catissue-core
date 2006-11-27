5ed2842c44b4c95fcca6618f4b88bb38210fe113
santosh_chandak <santosh_chandak>
2006-11-27 12:51:51 +0000

    changes according to new  UI which now has 3 radio buttons for selection of storage container
    SVN-Revision: 5608

--- WEB-INF/src/edu/wustl/catissuecore/action/MultipleSpecimenSubmitAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/MultipleSpecimenSubmitAction.java
@@ -4,6 +4,7 @@
  * TODO To change the template for this generated file go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
+
 package edu.wustl.catissuecore.action;
 
 import java.util.ArrayList;
@@ -12,11 +13,11 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.Globals;
 import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
@@ -26,15 +27,18 @@ import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
 
 import edu.wustl.catissuecore.actionForm.MultipleSpecimenStorageLocationForm;
+import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.bizlogic.StorageContainerBizLogic;
 import edu.wustl.catissuecore.domain.Specimen;
+import edu.wustl.catissuecore.domain.StorageContainer;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.action.BaseAction;
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.bizlogic.IBizLogic;
 import edu.wustl.common.factory.AbstractBizLogicFactory;
+import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.global.ApplicationProperties;
 import edu.wustl.common.util.logger.Logger;
-
 
 /**
  * @author mandar_deshmukh
@@ -45,6 +49,8 @@ import edu.wustl.common.util.logger.Logger;
 public class MultipleSpecimenSubmitAction extends BaseAction
 {
 
+	private int specimenCounter = 1;
+
 	/**
 	 * 
 	 */
@@ -53,158 +59,212 @@ public class MultipleSpecimenSubmitAction extends BaseAction
 		//super();
 		// TODO Auto-generated constructor stub
 	}
+
 	/* (non-Javadoc)
 	 * @see edu.wustl.common.action.BaseAction#executeAction(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
-	protected ActionForward executeAction(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception
+	protected ActionForward executeAction(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
+			
 	{
 		Logger.out.debug("Inside : MultipleSpecimenSubmitAction");
-		MultipleSpecimenStorageLocationForm aForm = (MultipleSpecimenStorageLocationForm)form;
-		Logger.out.debug("\naForm.getSpecimenOnUIMap():\n---------------\n"+aForm.getSpecimenOnUIMap()+"\n-----------\n");
+		MultipleSpecimenStorageLocationForm aForm = (MultipleSpecimenStorageLocationForm) form;
+		Logger.out.debug("\naForm.getSpecimenOnUIMap():\n---------------\n" + aForm.getSpecimenOnUIMap() + "\n-----------\n");
 		String target = Constants.FAILURE;
-//		if(dataValidate())
-//		{
-			Map specimenMap = setDataInSpecimens(aForm,request);
-			try
+		//		if(dataValidate())
+		//		{
+		specimenCounter = 1;
+		Map specimenMap = null;
+		try
+		{
+			specimenMap = setDataInSpecimens(aForm, request);
+		}
+		catch (DAOException e)
+		{
+			
+			ActionErrors errors = (ActionErrors) request.getAttribute(Globals.ERROR_KEY);
+			if (errors == null || errors.size() == 0)
 			{
-				List specimenList = insertSpecimens(request, specimenMap);
-				request.setAttribute(Constants.MULTIPLE_SPECIMEN_SUBMIT_SUCCESSFUL,Constants.MULTIPLE_SPECIMEN_SUBMIT_SUCCESSFUL);
-				target = Constants.SUCCESS;
-				// ----------------- report page
-				Collection specimenCollection = (Collection) request.getSession().getAttribute(Constants.SAVED_SPECIMEN_COLLECTION);
-				request.getSession().setAttribute(Constants.SAVED_SPECIMEN_COLLECTION, null);
-
-				//to display all inserted specimens Mandar: 16Nov06 
-//				request.setAttribute(Constants.SAVED_SPECIMEN_COLLECTION, specimenCollection);
-				request.setAttribute(Constants.SAVED_SPECIMEN_COLLECTION, specimenList);
-				
-
-				ActionMessages msgs = new ActionMessages();
-				msgs.add("success", new ActionMessage("multipleSpecimen.add.success", String.valueOf(specimenList.size())));
-				saveMessages(request, msgs);
-
+				errors = new ActionErrors();
 			}
-			catch(Exception excp)
-			{
-				Logger.out.error(excp);
-				ActionErrors errors = new ActionErrors();
-				errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("empty.message",excp.getMessage() ));
-				saveErrors(request, errors);
-				target = Constants.FAILURE;
-			}
-//		}
-	
-		return mapping.findForward(target ) ;
+			errors.add(ActionErrors.GLOBAL_ERROR,new ActionError("errors.item.format",ApplicationProperties.getValue("specimen.positionInStorageContainer")));
+			saveErrors(request,errors);
+			e.printStackTrace();
+			return mapping.findForward(target);
+		}
+		try
+		{
+			List specimenList = insertSpecimens(request, specimenMap);
+			request.setAttribute(Constants.MULTIPLE_SPECIMEN_SUBMIT_SUCCESSFUL, Constants.MULTIPLE_SPECIMEN_SUBMIT_SUCCESSFUL);
+			target = Constants.SUCCESS;
+			// ----------------- report page
+			Collection specimenCollection = (Collection) request.getSession().getAttribute(Constants.SAVED_SPECIMEN_COLLECTION);
+			request.getSession().setAttribute(Constants.SAVED_SPECIMEN_COLLECTION, null);
+
+			//to display all inserted specimens Mandar: 16Nov06 
+			//				request.setAttribute(Constants.SAVED_SPECIMEN_COLLECTION, specimenCollection);
+			request.setAttribute(Constants.SAVED_SPECIMEN_COLLECTION, specimenList);
+
+			ActionMessages msgs = new ActionMessages();
+			msgs.add("success", new ActionMessage("multipleSpecimen.add.success", String.valueOf(specimenList.size())));
+			saveMessages(request, msgs);
+
+		}
+		catch (Exception excp)
+		{
+			Logger.out.error(excp);
+			ActionErrors errors = new ActionErrors();
+			errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("empty.message", excp.getMessage()));
+			saveErrors(request, errors);
+			target = Constants.FAILURE;
+		}
+		//		}
+
+		return mapping.findForward(target);
 	}
-	
+
 	private boolean dataValidate()
 	{
 		return true;
 	}
-	
-	private Map setDataInSpecimens(MultipleSpecimenStorageLocationForm aForm, HttpServletRequest request)
+
+	private Map setDataInSpecimens(MultipleSpecimenStorageLocationForm aForm, HttpServletRequest request) throws DAOException
 	{
 		Map specimenOnUIMap = aForm.getSpecimenOnUIMap();
-		Logger.out.debug("\n\n>>>>>>>>>>     UI Map fetched\n\n>>>>>>>>>>>>>\n");
-		List specimenOrderList = (List)request.getSession().getAttribute(Constants.MULTIPLE_SPECIMEN_SPECIMEN_ORDER_LIST);
-		Logger.out.debug("\n\n>>>>>>>>>>     Specimen Order List fetched\n\n>>>>>>>>>>>>>\n");
-		Map originalSpecimenMap = (HashMap)request.getSession().getAttribute(Constants.SPECIMEN_MAP_KEY);
-		Logger.out.debug("\n\n>>>>>>>>>>     Specimen Original Map fetched\n\n>>>>>>>>>>>>>\n");
-//		printMap(originalSpecimenMap);		
-		if(specimenOrderList!=null && !specimenOrderList.isEmpty())
+		List specimenOrderList = (List) request.getSession().getAttribute(Constants.MULTIPLE_SPECIMEN_SPECIMEN_ORDER_LIST);
+		Map originalSpecimenMap = (HashMap) request.getSession().getAttribute(Constants.SPECIMEN_MAP_KEY);
+
+		//		printMap(originalSpecimenMap);		
+		if (specimenOrderList != null && !specimenOrderList.isEmpty())
 		{
-			for(int count=0; count< specimenOrderList.size(); count++)
+			for (int count = 0; count < specimenOrderList.size(); count++)
 			{
-				Specimen specimenFromList = (Specimen)specimenOrderList.get(count);
-				
+				Specimen specimenFromList = (Specimen) specimenOrderList.get(count);
+
 				Iterator specimenKeys = originalSpecimenMap.keySet().iterator();
-				int cnt=1;
-				while(specimenKeys.hasNext())
+				int cnt = 1;
+				while (specimenKeys.hasNext())
 				{
-					Specimen originalSpecimen = (Specimen)specimenKeys.next();
-					
-					if(originalSpecimen.equals(specimenFromList))
+					Specimen originalSpecimen = (Specimen) specimenKeys.next();
+
+					if (originalSpecimen.equals(specimenFromList))
 					{
-						String keyPrefix ="Specimen:"+(count+1)+"_"; 
-						String labelKey = keyPrefix+"Label";
-						String barKey = keyPrefix+"Barcode";
-						String storageContainerKey = keyPrefix+"StorageContainer";
-						String positionOneKey = keyPrefix+"PositionOne";
-						String positionTwoKey = keyPrefix+"PositionTwo";
-						String virtuallyLocatedKey = keyPrefix+"virtuallyLocated";
-						String deriveCountKey = keyPrefix+"DeriveCount";
+						String keyPrefix = "Specimen:" + (count + 1) + "_";
+						String labelKey = keyPrefix + "Label";
+						String barKey = keyPrefix + "Barcode";
+						String storageContainerKey = keyPrefix + "StorageContainer";
+						String positionOneKey = keyPrefix + "PositionOne";
+						String positionTwoKey = keyPrefix + "PositionTwo";
+						String virtuallyLocatedKey = keyPrefix + "virtuallyLocated";
+						String deriveCountKey = keyPrefix + "DeriveCount";
 
-						setSpecimenValues(specimenOnUIMap,originalSpecimen,labelKey,barKey,storageContainerKey,positionOneKey,positionTwoKey,virtuallyLocatedKey );
+						setSpecimenValues(specimenOnUIMap, originalSpecimen, labelKey, barKey, storageContainerKey, positionOneKey, positionTwoKey,
+								virtuallyLocatedKey);
 
-						String deriveCount = (String)specimenOnUIMap.get(deriveCountKey);
-						int deriveCountValue =Integer.parseInt(deriveCount);
-						if(deriveCountValue > 0)
+						String deriveCount = (String) specimenOnUIMap.get(deriveCountKey);
+						int deriveCountValue = Integer.parseInt(deriveCount);
+						if (deriveCountValue > 0)
 						{
-							setDerivedSpecimens(originalSpecimenMap, originalSpecimen, keyPrefix, specimenOnUIMap );
+							setDerivedSpecimens(originalSpecimenMap, originalSpecimen, keyPrefix, specimenOnUIMap);
 						}
 					}
 				}
 			}
-			Logger.out.debug(">>>>>>>>>>>>>>>>>>>> Specimen Data Set >>>>>>>>>>>>>>" ); 
+			Logger.out.debug(">>>>>>>>>>>>>>>>>>>> Specimen Data Set >>>>>>>>>>>>>>");
 		}
-		Logger.out.debug("Updated Map : " );
-//		printMap(originalSpecimenMap);
+		Logger.out.debug("Updated Map : ");
+		//		printMap(originalSpecimenMap);
 		return originalSpecimenMap;
 
 	}
-	
-	private void setDerivedSpecimens(Map originalSpecimenMap, Specimen mainSpecimen,String parentKey, Map specimenOnUIMap)
+
+	private void setDerivedSpecimens(Map originalSpecimenMap, Specimen mainSpecimen, String parentKey, Map specimenOnUIMap) throws DAOException
 	{
-		List derivedList = (List)originalSpecimenMap.get(mainSpecimen );
-		if(derivedList!=null && !derivedList.isEmpty()  )
+		List derivedList = (List) originalSpecimenMap.get(mainSpecimen);
+		if (derivedList != null && !derivedList.isEmpty())
 		{
-			for(int cnt=1;cnt<=derivedList.size(); cnt++  )
+			for (int cnt = 1; cnt <= derivedList.size(); cnt++)
 			{
-				String derivedPrefix = parentKey+"DerivedSpecimen:"; 
-				Specimen derivedSpecimen = (Specimen)derivedList.get(cnt-1);
-				String labelKey = derivedPrefix+cnt+"_Label";
-				String barKey = derivedPrefix+cnt+"_Barcode";
-				String storageContainerKey = derivedPrefix+cnt+"_StorageContainer";
-				String positionOneKey = derivedPrefix+cnt+"_PositionOne";
-				String positionTwoKey = derivedPrefix+cnt+"_PositionTwo";
-				String virtuallyLocatedKey = derivedPrefix+cnt+"_virtuallyLocated";
-				
-				setSpecimenValues(specimenOnUIMap,derivedSpecimen,labelKey,barKey,storageContainerKey,positionOneKey,positionTwoKey,virtuallyLocatedKey );
+				String derivedPrefix = parentKey + "DerivedSpecimen:";
+				Specimen derivedSpecimen = (Specimen) derivedList.get(cnt - 1);
+				String labelKey = derivedPrefix + cnt + "_Label";
+				String barKey = derivedPrefix + cnt + "_Barcode";
+				String storageContainerKey = derivedPrefix + cnt + "_StorageContainer";
+				String positionOneKey = derivedPrefix + cnt + "_PositionOne";
+				String positionTwoKey = derivedPrefix + cnt + "_PositionTwo";
+				String virtuallyLocatedKey = derivedPrefix + cnt + "_virtuallyLocated";
+
+				setSpecimenValues(specimenOnUIMap, derivedSpecimen, labelKey, barKey, storageContainerKey, positionOneKey, positionTwoKey,
+						virtuallyLocatedKey);
+
 			}
 		}
 	}
-	
 
-	private void setSpecimenValues(Map specimenOnUIMap,Specimen specimen,String labelKey,String barKey,String storageContainerKey,String positionOneKey,String positionTwoKey,String virtuallyLocatedKey )
+	private void setSpecimenValues(Map specimenOnUIMap, Specimen specimen, String labelKey, String barKey, String storageContainerKey,
+			String positionOneKey, String positionTwoKey, String virtuallyLocatedKey) throws DAOException
 	{
 		//fetch from UI map
-		String label = (String)specimenOnUIMap.get(labelKey);
-		String barcode = (String)specimenOnUIMap.get(barKey);
-		String virtuallyLocated = (String)specimenOnUIMap.get(virtuallyLocatedKey);
+		String label = (String) specimenOnUIMap.get(labelKey);
+		String barcode = (String) specimenOnUIMap.get(barKey);
+		String virtuallyLocated = (String) specimenOnUIMap.get(virtuallyLocatedKey);
+		String radioButonKey = "radio_" + specimenCounter;
 
 		//setting values in main specimen
-		specimen.setLabel(label); 
+		specimen.setLabel(label);
 		specimen.setBarcode(barcode);
-		if(virtuallyLocated != null)
+
+		//get the container values based on user selection from dropdown or map
+		if (specimenOnUIMap.get(radioButonKey).equals("1"))
 		{
 			specimen.setStorageContainer(null);
-			specimen.setPositionDimensionOne(null );
-			specimen.setPositionDimensionTwo(null );
+			specimen.setPositionDimensionOne(null);
+			specimen.setPositionDimensionTwo(null);
 		}
-		else
+		if (specimenOnUIMap.get(radioButonKey).equals("2"))
 		{
-			String containerId = (String)specimenOnUIMap.get(storageContainerKey );
-			specimen.getStorageContainer().setId(new Long(containerId) );
-			
-			String positionOne = (String)specimenOnUIMap.get(positionOneKey );
-			specimen.setPositionDimensionOne(new Integer(positionOne) ) ;
-			
-			String positionTwo = (String)specimenOnUIMap.get(positionTwoKey );
-			specimen.setPositionDimensionTwo(new Integer(positionTwo) ) ;
+			String containerId = (String) specimenOnUIMap.get(storageContainerKey);
+			String posDim1 = (String) specimenOnUIMap.get(positionOneKey);
+			String posDim2 = (String) specimenOnUIMap.get(positionTwoKey);
+			specimen.getStorageContainer().setId(new Long(containerId));
+			specimen.setPositionDimensionOne(new Integer(posDim1));
+         	specimen.setPositionDimensionTwo(new Integer(posDim2));
 		}
+		else if (specimenOnUIMap.get(radioButonKey).equals("3"))
+		{
+			String containerName = (String) specimenOnUIMap.get("Specimen:" + specimenCounter + "_StorageContainer_name" + "_fromMap");
+
+			String sourceObjectName = StorageContainer.class.getName();
+			String[] selectColumnName = {"id"};
+			String[] whereColumnName = {"name"};
+			String[] whereColumnCondition = {"="};
+			Object[] whereColumnValue = {containerName};
+			String joinCondition = null;
+
+			StorageContainerBizLogic bizLogic = (StorageContainerBizLogic) BizLogicFactory.getInstance().getBizLogic(
+					Constants.STORAGE_CONTAINER_FORM_ID);
+
+			List list = bizLogic.retrieve(sourceObjectName, selectColumnName, whereColumnName, whereColumnCondition, whereColumnValue, joinCondition);
+			Long containerId = null;
+			if (!list.isEmpty())
+			{
+				containerId = (Long) list.get(0);
+			}
+			else
+			{
+				String message = ApplicationProperties.getValue("specimen.storageContainer");
+				throw new DAOException(ApplicationProperties.getValue("errors.invalid", message));
+			}
+			String posDim1 = (String) specimenOnUIMap.get("Specimen:" + specimenCounter + "_positionDimensionOne" + "_fromMap");
+			String posDim2 = (String) specimenOnUIMap.get("Specimen:" + specimenCounter + "_positionDimensionTwo" + "_fromMap");
+			
+			specimen.getStorageContainer().setId(containerId);
+			specimen.setPositionDimensionOne(new Integer(posDim1));
+			specimen.setPositionDimensionTwo(new Integer(posDim2));
+			
+		}
+		specimenCounter++;
 	}
-	
+
 	/**
 	 * This method saves collection of specimens to the database.
 	 *  TODO Error handling. 
@@ -233,7 +293,7 @@ public class MultipleSpecimenSubmitAction extends BaseAction
 	private List createList(Map specimenMap)
 	{
 		List specimenList = new ArrayList();
-		
+
 		Iterator specimenIterator = specimenMap.keySet().iterator();
 		while (specimenIterator.hasNext())
 		{
@@ -251,47 +311,7 @@ public class MultipleSpecimenSubmitAction extends BaseAction
 				}
 			}
 		}
-		return specimenList; 
-	}
-	
-	//------------------------------------- to delete after testing
-	private void printMap(Map map)
-	{
-		Iterator itr = map.keySet().iterator();
-		while(itr.hasNext() )
-		{
-			Specimen specimen = (Specimen)itr.next();
-			showSpecimenData(specimen);
-			List dl = (List)map.get(specimen );
-			if(dl!=null)
-			{
-				for(int i=0;i<dl.size();i++)
-				{
-					Specimen dspecimen = (Specimen)dl.get(i);
-					showSpecimenData(dspecimen);
-				}
-			}
-		}
-	}
-	
-	private void showSpecimenData(Specimen specimen)
-	{
-		Logger.out.debug(">>>>>>>>>>>>>>>>>                <<<<<<<<<<<<<<<<<<<<<<<   ");
-		Logger.out.debug(" ");
-		Logger.out.debug("specimen.getLabel() : "+specimen.getLabel());
-		Logger.out.debug("specimen.getBarcode() : "+specimen.getBarcode() );
-		if(specimen.getStorageContainer() != null)
-		{
-			Logger.out.debug("specimen.getStorageContainer().getId() : "+specimen.getStorageContainer().getId()   );
-			Logger.out.debug("specimen.getLabel() : "+specimen.getPositionDimensionOne() );
-			Logger.out.debug("specimen.getLabel() : "+specimen.getPositionDimensionTwo() );
-		}
-		else
-		{
-			Logger.out.debug("StorageContainer is null");
-		}
-		Logger.out.debug("================================================================");
+		return specimenList;
 	}
 }
-
 
