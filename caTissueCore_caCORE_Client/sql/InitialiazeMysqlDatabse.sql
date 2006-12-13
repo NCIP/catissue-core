f44d66aa51f5979522c9cfb77778a833ce0a3879
jitendra_agrawal <jitendra_agrawal>
2006-12-13 08:02:17 +0000

    Removed sql files which were written for Nightly build.
    SVN-Revision: 5954

--- caTissueCore_caCORE_Client/sql/InitialiazeMysqlDatabse.sql
+++
@@ -1,7 +0,0 @@
-DROP DATABASE nightlybuildcatissue;
-
-CREATE DATABASE nightlybuildcatissue;
-
-GRANT ALL PRIVILEGES ON nightlybuildcatissue.* TO 'root'@'localhost' IDENTIFIED BY 'catissue_core' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON nightlybuildcatissue.* TO 'catissue_core' IDENTIFIED BY 'catissue_core' WITH GRANT OPTION;
