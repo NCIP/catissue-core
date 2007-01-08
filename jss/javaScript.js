b83e9c3e717e2d11f0fdd9bf0202c82b28485792
vaishali_khandelwal <vaishali_khandelwal>
2007-01-08 14:27:54 +0000

    refreshTree function added
    SVN-Revision: 6417

--- jss/javaScript.js
+++ jss/javaScript.js
@@ -787,5 +787,14 @@ function onRadioButtonGroupClick(element)
 				document.forms[0].customListBox_1_2.disabled = true;
 			}
     	}
+    	
+    	//This function gets called whenever a specimen/ scg gets added in system.
+    	function refreshTree(cpAndParticipantsFrame, treeFrame,cpSearchCpId,cpSearchParticipantId,nodeId)
+    	{
+    		var cpId = window.parent.frames[cpAndParticipantsFrame].document.getElementById("cpId").value;
+			var participantId = window.parent.frames[cpAndParticipantsFrame].document.getElementById("participantId").value;
+			window.parent.frames[treeFrame].location="showTree.do?"+cpSearchCpId+"="+cpId+"&"+cpSearchParticipantId+"="+participantId+"&nodeId="+nodeId;
+   		
+    	}
 
 		
