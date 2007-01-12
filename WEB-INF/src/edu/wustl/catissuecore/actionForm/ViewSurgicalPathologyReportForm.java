883c7f919ea60e2089b0bae0f0683b3b217a7590
vijay_pande <vijay_pande>
2007-01-12 07:25:00 +0000

    - Comments added
    SVN-Revision: 6483

--- WEB-INF/src/edu/wustl/catissuecore/actionForm/ViewSurgicalPathologyReportForm.java
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/ViewSurgicalPathologyReportForm.java
@@ -16,7 +16,10 @@ import edu.wustl.common.actionForm.AbstractActionForm;
 import edu.wustl.common.domain.AbstractDomainObject;
 import edu.wustl.common.util.logger.Logger;
 
-
+/**
+ * @author vijay_pande
+ * ActionForm bean class for viewSurgicalPathologyReport.jsp
+ */
 public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 {
 	protected String firstName;
@@ -46,141 +49,226 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 	private long id;
 	protected Collection reportId;
 	
+	/**
+	 * @return date of birth of particicpant
+	 */
 	public String getBirthDate()
 	{
 		return birthDate;
 	}
 	
+	/**
+	 * @param dateOfBirth date of birth
+	 */
 	public void setBirthDate(String dateOfBirth)
 	{
 		this.birthDate = dateOfBirth;
 	}
 	
+	/**
+	 * @return date Of Death of particicpant
+	 */
 	public String getDeathDate()
 	{
 		return deathDate;
 	}
 	
+	/**
+	 * @param dateOfDeath date Of Death
+	 */
 	public void setDeathDate(String dateOfDeath)
 	{
 		this.deathDate = dateOfDeath;
 	}
 	
+	/**
+	 * @return ethinicity of particicpant
+	 */
 	public String getEthinicity()
 	{
 		return ethinicity;
 	}
 	
+	/**
+	 * @param ethinicity 
+	 */
 	public void setEthinicity(String ethinicity)
 	{
 		this.ethinicity = ethinicity;
 	}
 	
+	/**
+	 * @return first name of particicpant
+	 */
 	public String getFirstName()
 	{
 		return firstName;
 	}
 	
+	/**
+	 * @param firstName 
+	 */
 	public void setFirstName(String firstName)
 	{
 		this.firstName = firstName;
 	}
 	
+	/**
+	 * @return gender of particicpant
+	 */
 	public String getGender()
 	{
 		return gender;
 	}
 	
+	/**
+	 * @param gender
+	 */
 	public void setGender(String gender)
 	{
 		this.gender = gender;
 	}
 	
+	/**
+	 * @return last name of particicpant
+	 */
 	public String getLastName()
 	{
 		return lastName;
 	}
 	
+	/**
+	 * @param lastName
+	 */
 	public void setLastName(String lastName)
 	{
 		this.lastName = lastName;
 	}
 	
+	/**
+	 * @return collection of MedicalIdentifierNumbers of particicpant
+	 */
 	public Collection getMedicalIdentifierNumbers()
 	{
 		return medicalIdentifierNumbers;
 	}
 	
+	/**
+	 * @param medicalIdentifierNumbers
+	 */
 	public void setMedicalIdentifierNumbers(Collection medicalIdentifierNumbers)
 	{
 		this.medicalIdentifierNumbers = medicalIdentifierNumbers;
 	}
 	
+	/**
+	 * @return collection of race of particicpant
+	 */
 	public Collection getRace()
 	{
 		return race;
 	}
 	
+	/**
+	 * @param race
+	 */
 	public void setRace(Collection race)
 	{
 		this.race = race;
 	}
 	
+	/**
+	 * @return sex genotype of particicpant
+	 */
 	public String getSexGenotype()
 	{
 		return sexGenotype;
 	}
 	
+	/**
+	 * @param sexGenotype
+	 */
 	public void setSexGenotype(String sexGenotype)
 	{
 		this.sexGenotype = sexGenotype;
 	}
 	
+	/**
+	 * @return Social Security Number of particicpant
+	 */
 	public String getSocialSecurityNumber()
 	{
 		return socialSecurityNumber;
 	}
 
+	/**
+	 * @param socialsecurityNumber
+	 */
 	public void setSocialSecurityNumber(String socialsecurityNumber)
 	{
 		this.socialSecurityNumber = socialsecurityNumber;
 	}
 	
+	/**
+	 * @return Accession Number of Identified Report
+	 */
 	public String getIdentifiedReportAccessionNumber()
 	{
 		return identifiedReportAccessionNumber;
 	}
 	
+	/**
+	 * @param accessionNumber
+	 */
 	public void setIdentifiedReportAccessionNumber(String accessionNumber)
 	{
 		this.identifiedReportAccessionNumber = accessionNumber;
 	}
 	
+	/**
+	 * @return comments for review or quarantine
+	 */
 	public String getComments()
 	{
 		return comments;
 	}
 	
+	/**
+	 * @param comments
+	 */
 	public void setComments(String comments)
 	{
 		this.comments = comments;
 	}
 	
+	/**
+	 * @return site of Identified Report
+	 */
 	public String getIdentifiedReportSite()
 	{
 		return identifiedReportSite;
 	}
 	
+	/**
+	 * @param site
+	 */
 	public void setIdentifiedReportSite(String site)
 	{
 		this.identifiedReportSite = site;
 	}
 	
+	/**
+	 * Constructor
+	 */
 	public ViewSurgicalPathologyReportForm()
 	{
 		reset();
 	}
 
+	/** (non-Javadoc)
+	 * @see edu.wustl.common.actionForm.AbstractActionForm#reset()
+	 * Reset all values of form
+	 */
 	protected void reset() 
 	{
 		this.firstName=null;
@@ -210,6 +298,10 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 		this.id=0;	
 	}
 
+	/** (non-Javadoc)
+	 * @see edu.wustl.common.actionForm.AbstractActionForm#setAllValues(edu.wustl.common.domain.AbstractDomainObject)
+	 * Set values of all variables of form
+	 */
 	public void setAllValues(AbstractDomainObject abstractDomain) 
 	{
 		IdentifiedSurgicalPathologyReport ispr=(IdentifiedSurgicalPathologyReport)abstractDomain;
@@ -226,6 +318,10 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 		this.comments=null;
 	}
 	
+	/**
+	 * @param deidentifiedSurgicalPathologyReport
+	 * set values of De-Identified report related variables
+	 */
 	public void setDeIdentifiedReport(DeidentifiedSurgicalPathologyReport deidentifiedSurgicalPathologyReport)
 	{
 		try
@@ -238,10 +334,17 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 		catch(NullPointerException ex)
 		{
 			Logger.out.error("viewSPR:De-identified Report information is null");
-			this.deIdentifiedReportTextContent=new String("De-Identified Report Not Found !");
+			if(this.deIdentifiedReportTextContent==null)
+			{
+				this.deIdentifiedReportTextContent=new String("De-Identified Report Not Found !");
+			}
 		}
 	}
 
+	/**
+	 * @param ispr identified Surgical pathology report
+	 *  set values of Identified report related variables
+	 */
 	public void setIdentifiedReport(IdentifiedSurgicalPathologyReport ispr)
 	{
 		try
@@ -254,10 +357,17 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 		catch(NullPointerException ex)
 		{
 			Logger.out.error("viewSPR:Identified Report information is null");
-			this.identifiedReportTextContent=new String("Identified Report Not Found !");
+			if(this.identifiedReportTextContent==null)
+			{
+				this.identifiedReportTextContent=new String("Identified Report Not Found !");
+			}
 		}
 	}
 
+	/**
+	 * @param p participant
+	 *  set values of participant related variables
+	 */
 	public void setParticipant(final Participant p)
 	{
 		try
@@ -318,6 +428,11 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 		}
 	}
 	
+	/** (non-Javadoc)
+	 * @see edu.wustl.common.actionForm.AbstractActionForm#getFormId()
+	 * On the basis of Request for submitting comments different form ID will be returned to save two different kind of comments
+	 * review comment and quarantine comments
+	 */
 	public int getFormId()
 	{	
 		if(this.submittedFor.equalsIgnoreCase("quarantine"))
@@ -327,116 +442,188 @@ public class ViewSurgicalPathologyReportForm extends AbstractActionForm
 		return Constants.SURGICAL_PATHOLOGY_REPORT_FORM_ID;
 	}
 	
+	/**
+	 * @return text content of deidentified report
+	 */
 	public String getDeIdentifiedReportTextContent()
 	{
 		return deIdentifiedReportTextContent;
 	}
 	
+	/**
+	 * @param deIdentifiedReportTextContent
+	 */
 	public void setDeIdentifiedReportTextContent(String deIdentifiedReportTextContent)
 	{
 		this.deIdentifiedReportTextContent = deIdentifiedReportTextContent;
 	}
 	
+	/**
+	 * @return text content of identified report
+	 */
 	public String getIdentifiedReportTextContent()
 	{
 		return identifiedReportTextContent;
 	}
 	
+	/**
+	 * @param identifiedReportTextContent
+	 */
 	public void setIdentifiedReportTextContent(String identifiedReportTextContent)
 	{
 		this.identifiedReportTextContent = identifiedReportTextContent;
 	}
 	
+	/**
+	 * @return counter
+	 */
 	public int getCounter()
 	{
 		return counter;
 	}
 	
+	/**
+	 * @param counter
+	 */
 	public void setCounter(int counter)
 	{
 		this.counter = counter;
 	}
 	
+	/**
+	 * @param key
+	 * @param value
+	 */
 	public void setValue(String key, Object value) 
     {
 		if (isMutable())values.put(key, value);
     }
 
+	/**
+	 * @param key
+	 * @return
+	 */
 	public Object getValue(String key) 
     {
 		return values.get(key);
     }
       
+ 	/**
+ 	 * @return
+ 	 */
  	public Collection getAllValues() 
  	{
  		return values.values();
  	}
 
+ 	/**
+ 	 * @param values
+ 	 */
  	public void setValues(Map values)
  	{
  		this.values = values;
  	}
  
+ 	/**
+ 	 * @return
+ 	 */
  	public Map getValues()
  	{
  		return this.values;
  	}
 
+	/** (non-Javadoc)
+	 * @see edu.wustl.common.actionForm.AbstractActionForm#getId()
+	 * @return id identifier of current domain object
+	 */
 	public long getId()
 	{
 		return id;
 	}
 	
+	/** (non-Javadoc)
+	 * @see edu.wustl.common.actionForm.AbstractActionForm#setId(long)
+	 */
 	public void setId(long id)
 	{
 		this.id = id;
 	}
 
+	/**
+	 * @return collection of Report ID (In case of Participant page)
+	 */
 	public Collection getReportId()
 	{
 		return reportId;
 	}
 
+	/**
+	 * @param reportId
+	 */
 	public void setReportId(Collection reportId)
 	{
 		this.reportId = reportId;
 	}
 
+	/**
+	 * @return report id of Identified report
+	 */
 	public long getIdentifiedReportId()
 	{
 		return identifiedReportId;
 	}
 	
+	/**
+	 * @param currentReportId
+	 */
 	public void setIdentifiedReportId(long currentReportId)
 	{
 		this.identifiedReportId = currentReportId;
 	}
 	
+	/**
+	 * @return accession number of de-identified report
+	 */
 	public String getDeIdentifiedReportAccessionNumber()
 	{
 		return deIdentifiedReportAccessionNumber;
 	}
 	
+	/**
+	 * @param deIdentifiedReportAccessionNumber
+	 */
 	public void setDeIdentifiedReportAccessionNumber(String deIdentifiedReportAccessionNumber)
 	{
 		this.deIdentifiedReportAccessionNumber = deIdentifiedReportAccessionNumber;
 	}
 
+	/**
+	 * @return report id of de-identified report
+	 */
 	public long getDeIdentifiedReportId()
 	{
 		return deIdentifiedReportId;
 	}
 	
+	/**
+	 * @param deIdentifiedReportId
+	 */
 	public void setDeIdentifiedReportId(long deIdentifiedReportId)
 	{
 		this.deIdentifiedReportId = deIdentifiedReportId;
 	}
 	
+	/**
+	 * @return site of de-identified report
+	 */
 	public String getDeIdentifiedReportSite()
 	{
 		return deIdentifiedReportSite;
 	}
 	
+	/**
+	 * @param deIdentifiedReportSite
+	 */
 	public void setDeIdentifiedReportSite(String deIdentifiedReportSite)
 	{
 		this.deIdentifiedReportSite = deIdentifiedReportSite;
