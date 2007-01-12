0e3a4ecca067090bcf2006e2cd103882666f960a
vijay_pande <vijay_pande>
2007-01-12 07:27:35 +0000

    - file renamed old name was PathologyReportReviewParameterSetBizLogic
    SVN-Revision: 6486

---
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/PathologyReportReviewParameterBizLogic.java
@@ -0,0 +1,67 @@
+package edu.wustl.catissuecore.bizlogic;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import edu.wustl.catissuecore.domain.User;
+import edu.wustl.catissuecore.domain.pathology.PathologyReportReviewParameter;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.dao.DAO;
+import edu.wustl.common.security.SecurityManager;
+import edu.wustl.common.security.exceptions.SMException;
+import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
+import edu.wustl.common.util.dbManager.DAOException;
+
+public class PathologyReportReviewParameterBizLogic extends IntegrationBizLogic
+{
+	/**
+	 * Saves the Pathology Report Review Parameter object in the database.
+	 * @param obj The storageType object to be saved.
+	 * @param session The session in which the object is saved.
+	 * @throws DAOException 
+	 */
+	protected void insert(Object obj, DAO dao, SessionDataBean sessionDataBean) throws DAOException, UserNotAuthorizedException
+	{
+		PathologyReportReviewParameter reviewParam = (PathologyReportReviewParameter) obj;
+		
+		String className;
+		String colName=new String(Constants.SYSTEM_IDENTIFIER);
+		className=User.class.getName();
+		List userList=dao.retrieve(className, colName, sessionDataBean.getUserId());
+		reviewParam.setUser((User)userList.get(0));
+		dao.insert(reviewParam, sessionDataBean, true, true);
+		
+		Set protectionObjects = new HashSet();
+		protectionObjects.add(reviewParam);
+		try
+		{
+			SecurityManager.getInstance(this.getClass()).insertAuthorizationData(null, protectionObjects, null);
+		}
+		catch (SMException e)
+		{
+			throw handleSMException(e);
+		}
+	}
+
+	/**
+	 * Updates the persistent object in the database.
+	 * @param obj The object to be updated.
+	 * @param session The session in which the object is saved.
+	 * @throws DAOException 
+	 */
+	protected void update(DAO dao, Object obj, Object oldObj, SessionDataBean sessionDataBean) throws DAOException, UserNotAuthorizedException
+	{
+		try{
+			PathologyReportReviewParameter reviewParam = (PathologyReportReviewParameter) obj;
+			if(reviewParam.getUser().getId()==null){
+				dao.insert(reviewParam.getUser(), sessionDataBean, false, false);	
+			}
+			dao.update(reviewParam, sessionDataBean, true, false, false);
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+}
