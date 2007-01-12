80b615e8b43dd5acb9465e884bea8c78097eb643
vijay_pande <vijay_pande>
2007-01-12 07:25:59 +0000

    - code for saving comments is removed from this class
    SVN-Revision: 6484

--- WEB-INF/src/edu/wustl/catissuecore/action/ViewSurgicalPathologyReportAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ViewSurgicalPathologyReportAction.java
@@ -1,14 +1,11 @@
 package edu.wustl.catissuecore.action;
 
 import java.util.Collection;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -21,22 +18,26 @@ import edu.wustl.catissuecore.domain.Participant;
 import edu.wustl.catissuecore.domain.Specimen;
 import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
 import edu.wustl.catissuecore.domain.User;
-import edu.wustl.catissuecore.domain.pathology.DeidentifiedSurgicalPathologyReport;
 import edu.wustl.catissuecore.domain.pathology.IdentifiedSurgicalPathologyReport;
-import edu.wustl.catissuecore.domain.pathology.PathologyReportReviewParameterSet;
-import edu.wustl.catissuecore.domain.pathology.QuarantineEventParameterSet;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.action.BaseAction;
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.security.SecurityManager;
 import edu.wustl.common.util.dbManager.DAOException;
 
-
+/**
+ * @author vijay_pande
+ * Action class to show Surgical Pathology  Report
+ */
 public class ViewSurgicalPathologyReportAction extends BaseAction
 {
 
 	private String forward;
 	private ViewSurgicalPathologyReportForm viewSPR;
+	/**
+	 * @see edu.wustl.common.action.BaseAction#executeAction(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	protected ActionForward executeAction(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
 		viewSPR=(ViewSurgicalPathologyReportForm)form;
@@ -44,19 +45,12 @@ public class ViewSurgicalPathologyReportAction extends BaseAction
 		String operation = (String)request.getParameter(Constants.OPERATION);
 		String submittedFor="";
 		Long id=new Long((String)request.getParameter(Constants.SYSTEM_IDENTIFIER));
-		HttpSession session=request.getSession();
 		viewSPR.setId(id);
+		boolean isAuthorized;
 		if(operation.equalsIgnoreCase(Constants.VIEW_SURGICAL_PATHOLOGY_REPORT))
 		{
-			retrieveAndSetObject(pageOf,id);
-		}
-		if(operation.equalsIgnoreCase("submit"))
-		{
-			SessionDataBean sessionBean=getSessionData(request);
-			submittedFor=request.getParameter(Constants.SUBMITTED_FOR);
-			addComments(viewSPR, submittedFor, pageOf, id,sessionBean, new Long(request.getParameter("currentReportId")));
-			retrieveAndSetObject(pageOf,id);
-			viewSPR.setComments(new String("Comments added Successfully !"));
+			isAuthorized=isAuthorized(getSessionBean(request));
+			retrieveAndSetObject(pageOf,id,isAuthorized);
 		}
 		request.setAttribute(Constants.PAGEOF, pageOf);
 		request.setAttribute(Constants.OPERATION, Constants.VIEW_SURGICAL_PATHOLOGY_REPORT);
@@ -66,23 +60,36 @@ public class ViewSurgicalPathologyReportAction extends BaseAction
 		return mapping.findForward(forward);
 	}
 	
-	private void retrieveAndSetObject(String pageOf,long id) throws DAOException
+	/**
+	 * @param pageOf pageOf variable to find out domain object 
+	 * @param id Identifier of the domain object
+	 * @throws DAOException exception occured while DB handling
+	 * This method retrives the appropriate SurgicalPathologyReport object and set values of ViewSurgicalPathologyReportForm object
+	 */
+	private void retrieveAndSetObject(String pageOf,long id,boolean isAuthorized) throws DAOException
 	{
 		String className;
 		String colName=new String(Constants.SYSTEM_IDENTIFIER);
-		long colValue=id;
-		
-		DefaultBizLogic defaultBizLogic=new DefaultBizLogic();
-		
+		long colValue=id;	
+		DefaultBizLogic defaultBizLogic=new DefaultBizLogic();		
+		//if page is of Specimen Collection group then the domain object is SpecimenCollectionGroup
 		if(pageOf.equalsIgnoreCase(Constants.PAGEOF_SPECIMEN_COLLECTION_GROUP))
 		{
 			forward=new String(Constants.SPECIMEN_COLLECTION_GROUP);
 			className=SpecimenCollectionGroup.class.getName();
 			List scgList=defaultBizLogic.retrieve(className, colName, colValue);
 			SpecimenCollectionGroup scg=(SpecimenCollectionGroup)scgList.get(0);
-			viewSPR.setAllValues(scg.getIdentifiedSurgicalPathologyReport());
-			viewSPR.setParticipant(scg.getParticipant());
+			if(isAuthorized)
+			{
+				viewSPR.setAllValues(scg.getIdentifiedSurgicalPathologyReport());
+			}
+			else
+			{
+				viewSPR.setParticipant(scg.getParticipant());
+				viewSPR.setDeIdentifiedReport(scg.getDeIdentifiedSurgicalPathologyReport());
+			}
 		}
+		//if page is of Specimen then the domain object is Specimen
 		if(pageOf.equalsIgnoreCase(Constants.PAGEOF_SPECIMEN))
 		{
 			forward=new String(Constants.SPECIMEN);
@@ -91,8 +98,18 @@ public class ViewSurgicalPathologyReportAction extends BaseAction
 			Specimen specimen=(Specimen)specimenList.get(0);
 			SpecimenCollectionGroup scg=specimen.getSpecimenCollectionGroup();
 			viewSPR.setAllValues(scg.getIdentifiedSurgicalPathologyReport());
-			viewSPR.setParticipant(scg.getParticipant());
+			if(isAuthorized)
+			{
+				viewSPR.setAllValues(scg.getIdentifiedSurgicalPathologyReport());
+			}
+			else
+			{
+				viewSPR.setParticipant(scg.getParticipant());
+				viewSPR.setDeIdentifiedReport(scg.getDeIdentifiedSurgicalPathologyReport());
+			}
 		}
+		// if page is of Participant then the domain object is Participant
+		// Also needs to retrieve a list of SurgicalPathologyReport objects (One-to-Many relationship)
 		if(pageOf.equalsIgnoreCase(Constants.PAGEOF_PARTICIPANT))
 		{
 			forward=new String(Constants.SPECIMEN);
@@ -109,12 +126,25 @@ public class ViewSurgicalPathologyReportAction extends BaseAction
 				className=SpecimenCollectionGroup.class.getName();
 				List scgList=defaultBizLogic.retrieve(className, colName, colValue);
 				SpecimenCollectionGroup scg=(SpecimenCollectionGroup)scgList.get(0);
-				viewSPR.setAllValues(scg.getIdentifiedSurgicalPathologyReport());
+				if(isAuthorized)
+				{
+					viewSPR.setAllValues(scg.getIdentifiedSurgicalPathologyReport());
+				}
+				else
+				{
+					viewSPR.setParticipant(scg.getParticipant());
+					viewSPR.setDeIdentifiedReport(scg.getDeIdentifiedSurgicalPathologyReport());
+				}
 			}
 			viewSPR.setReportId(getReportIdList(scgCollection));
 		}
 	}
 	
+	/**
+	 * @param scgCollection A collection of SpecimenCollectionGroup Id
+	 * @return List of SurgicalPathologyReport Id
+	 * @throws DAOException Exception occured while handling DB
+	 */
 	private List getReportIdList(Collection scgCollection)throws DAOException
 	{
 		Long[] scg=null;
@@ -138,8 +168,7 @@ public class ViewSurgicalPathologyReportAction extends BaseAction
 			{
 				scg[i++]=(Long)iter.next();
 			}
-		
-		
+			
 			whereColumnValue=scg;
 			BizLogicFactory bizLogicFactory = BizLogicFactory.getInstance();
 			IdentifiedSurgicalPathologyReportBizLogic bizLogic =(IdentifiedSurgicalPathologyReportBizLogic) bizLogicFactory.getBizLogic(IdentifiedSurgicalPathologyReport.class.getName());
@@ -148,84 +177,34 @@ public class ViewSurgicalPathologyReportAction extends BaseAction
 		return reportIDList;
 	}
 	
-	private void addComments(ViewSurgicalPathologyReportForm form, String submittedFor, String pageOf, long id, SessionDataBean sessionBean, long currentReportId) throws DAOException
+	private SessionDataBean getSessionBean(HttpServletRequest request)
 	{
-		if(submittedFor.equalsIgnoreCase("review"))
+		try
 		{
-			saveReviewComment(getISPR(pageOf, id, currentReportId), sessionBean, form.getComments());
+			SessionDataBean sessionBean = (SessionDataBean) request.getSession().getAttribute(Constants.SESSION_DATA);
+			return sessionBean;
 		}
-		else if(submittedFor.equalsIgnoreCase("quarantine"))
+		catch(Exception ex)
 		{
-			saveQuarantineComment(getISPR(pageOf, id, currentReportId).getDeidentifiedSurgicalPathologyReport(), sessionBean, form.getComments());
-		}	
-	}
-	
-	
-	private IdentifiedSurgicalPathologyReport getISPR(String pageOf, long id, long currentReportId)throws DAOException
-	{
-		IdentifiedSurgicalPathologyReport ispr=null;
-		
-		String className;
-		String colName=new String(Constants.SYSTEM_IDENTIFIER);
-		long colValue=id;
-		PathologyReportReviewParameterSet reviewParam=new PathologyReportReviewParameterSet();
-	
-		DefaultBizLogic defaultBizLogic=new DefaultBizLogic();
-		
-		forward=new String(Constants.SPECIMEN_COLLECTION_GROUP);
-		className=IdentifiedSurgicalPathologyReport.class.getName();
-		List isprList=defaultBizLogic.retrieve(className, colName, currentReportId);
-		ispr=(IdentifiedSurgicalPathologyReport)isprList.get(0);
-		
-		return ispr;
-	}
-	
-	protected SessionDataBean getSessionData(HttpServletRequest request) 
-	{
-		Object obj = request.getSession().getAttribute(Constants.SESSION_DATA);
-		if(obj!=null)
-		{
-			SessionDataBean sessionData = (SessionDataBean) obj;
-			return  sessionData;
+			return null;
 		}
-		return null;
 	}
 	
-	private void saveReviewComment(IdentifiedSurgicalPathologyReport ispr,SessionDataBean sessionBean, String comments) throws DAOException
+	private boolean isAuthorized(SessionDataBean sessionBean) throws Exception
 	{
-		
-		String className;
-		String colName=new String(Constants.SYSTEM_IDENTIFIER);
-		
-		PathologyReportReviewParameterSet reviewParam=new PathologyReportReviewParameterSet();
-		DefaultBizLogic defaultBizLogic=new DefaultBizLogic();
-		className=User.class.getName();
-		List userList=defaultBizLogic.retrieve(className, colName, sessionBean.getUserId());
-		reviewParam.setUser((User)userList.get(0));
-		reviewParam.setComments(comments);
-		reviewParam.setTimestamp(new Date());
-		Set pathologyReportReviewParameterSetCollection=ispr.getPathologyReportReviewParameterSetCollection();
-		pathologyReportReviewParameterSetCollection.add(reviewParam);
-		ispr.setPathologyReportReviewParameterSetCollection(pathologyReportReviewParameterSetCollection);
-		
-	}
-	
-	private void saveQuarantineComment(DeidentifiedSurgicalPathologyReport dispr,SessionDataBean sessionBean, String comments) throws DAOException
-	{
-		String className;
-		String colName=new String(Constants.SYSTEM_IDENTIFIER);
-		
-		QuarantineEventParameterSet quarantineParam=new QuarantineEventParameterSet();
-		DefaultBizLogic defaultBizLogic=new DefaultBizLogic();
-		className=User.class.getName();
-		List userList=defaultBizLogic.retrieve(className, colName, sessionBean.getUserId());
-		quarantineParam.setUser((User)userList.get(0));
-		quarantineParam.setComments(comments);
-		quarantineParam.setTimestamp(new Date());
-		Set quarantinEventParameterSetCollection=dispr.getQuarantinEventParameterSetCollection();
-		quarantinEventParameterSetCollection.add(quarantineParam);
-		dispr.setQuarantinEventParameterSetCollection(quarantinEventParameterSetCollection);
-	}
+		SecurityManager sm=SecurityManager.getInstance(User.class);
+		try{
+			if(sm.isAuthorizedToExecuteAction(sessionBean.getUserName(), this.getClass().getName()))
+			{
+				return true;
+			}
+		}
+		catch(Exception  ex)
+		{
+			return false;
+		}
+		return false;
+	}	
 }
 
 
