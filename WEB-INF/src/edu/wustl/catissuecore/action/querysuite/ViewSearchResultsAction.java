1bc51725b47821f909be20877885fba7f4c773e4
deepti_shelar <deepti_shelar>
2007-02-05 06:01:15 +0000

    Adding tree creation biz logic
    SVN-Revision: 6770

--- WEB-INF/src/edu/wustl/catissuecore/action/querysuite/ViewSearchResultsAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/querysuite/ViewSearchResultsAction.java
@@ -14,14 +14,14 @@ import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.action.BaseAppletAction;
 import edu.wustl.catissuecore.bizlogic.querysuite.CreateQueryObjectBizLogic;
+import edu.wustl.catissuecore.bizlogic.querysuite.QueryOutputTreeBizLogic;
 import edu.wustl.catissuecore.util.global.Constants;
-import edu.wustl.common.querysuite.factory.SqlGeneratorFactory;
 import edu.wustl.common.querysuite.queryobject.IQuery;
 import edu.wustl.common.util.logger.Logger;
 
 /**
  * This action is a applet action called from DiagrammaticViewApplet class when user clicks on seach button of AddLimits.jsp.
- * This class gets IQuery Object from the applet and also generates sql out of itwith the help of CreateQueryObjectBizLogic.
+ * This class gets IQuery Object from the applet and also generates sql out of it with the help of CreateQueryObjectBizLogic.
  * This sql is then fired and the results are stored in session so that then they can be seen on ViewSeachResults jsp screen.
  * An emplty map is sent back to the applet. 
  * @author deepti_shelar
@@ -45,9 +45,9 @@ public class ViewSearchResultsAction extends BaseAppletAction
 		if (inputDataMap != null && !inputDataMap.isEmpty())
 		{
 			IQuery query = (IQuery) inputDataMap.get("queryObject");
-			String sql = SqlGeneratorFactory.getInstance().generateSQL(query);
-			System.out.println("**********************sql:  "+sql);
-			sql= "Select ParticipantMedicalIdentif_1.MEDICAL_RECORD_NUMBER From CATISSUE_PART_MEDICAL_ID ParticipantMedicalIdentif_1  left join CATISSUE_PARTICIPANT Participant_2 on (ParticipantMedicalIdentif_1.participant_id=Participant_2.Identifier)  Where (ParticipantMedicalIdentif_1.MEDICAL_RECORD_NUMBER is NOT NULL) And(Participant_2.GENDER like 'm%')";
+			QueryOutputTreeBizLogic outputTreeBizLogic = new QueryOutputTreeBizLogic();
+			outputTreeBizLogic.createOutputTree(query);
+			String sql= "Select ParticipantMedicalIdentif_1.MEDICAL_RECORD_NUMBER From CATISSUE_PART_MEDICAL_ID ParticipantMedicalIdentif_1  left join CATISSUE_PARTICIPANT Participant_2 on (ParticipantMedicalIdentif_1.participant_id=Participant_2.Identifier)  Where (ParticipantMedicalIdentif_1.MEDICAL_RECORD_NUMBER is NOT NULL) And(Participant_2.GENDER like 'm%')";
 			sql = "Select User_1.ACTIVITY_STATUS, User_1.STATUS_COMMENT, User_1.LOGIN_NAME, User_1.START_DATE, User_1.EMAIL_ADDRESS, User_1.FIRST_NAME, User_1.CSM_USER_ID, User_1.LAST_NAME From CATISSUE_USER User_1 left join CATISSUE_INSTITUTION Institution_2 on (User_1.INSTITUTION_ID=Institution_2.IDENTIFIER) Where (User_1.FIRST_NAME like 'a%') And(Institution_2.NAME is NOT NULL)";
 			Logger.out.info(sql);
 			CreateQueryObjectBizLogic bizLogic = new CreateQueryObjectBizLogic();
