2a745ade92a3e1a3e90e1f33bbc716ee559aa5d6
deepti_shelar <deepti_shelar>
2007-02-07 12:24:27 +0000

    Returning query tree sql to action class
    SVN-Revision: 6829

--- WEB-INF/src/edu/wustl/catissuecore/bizlogic/querysuite/QueryOutputTreeBizLogic.java
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/querysuite/QueryOutputTreeBizLogic.java
@@ -2,12 +2,12 @@
 package edu.wustl.catissuecore.bizlogic.querysuite;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 import edu.common.dynamicextensions.domain.DomainObjectFactory;
 import edu.common.dynamicextensions.domain.databaseproperties.ColumnProperties;
@@ -22,16 +22,22 @@ import edu.common.dynamicextensions.entitymanager.EntityManagerInterface;
 import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.dao.JDBCDAO;
 import edu.wustl.common.querysuite.EntityManagerMock;
 import edu.wustl.common.querysuite.QueryGeneratorMock;
 import edu.wustl.common.querysuite.exceptions.DuplicateChildException;
+import edu.wustl.common.querysuite.exceptions.MultipleRootsException;
+import edu.wustl.common.querysuite.exceptions.SqlException;
 import edu.wustl.common.querysuite.factory.QueryObjectFactory;
+import edu.wustl.common.querysuite.factory.SqlGeneratorFactory;
 import edu.wustl.common.querysuite.metadata.associations.IIntraModelAssociation;
+import edu.wustl.common.querysuite.queryengine.ISqlGenerator;
 import edu.wustl.common.querysuite.queryobject.IOutputTreeNode;
 import edu.wustl.common.querysuite.queryobject.IQuery;
 import edu.wustl.common.querysuite.queryobject.util.QueryObjectProcessor;
+import edu.wustl.common.tree.QueryTreeNodeData;
 import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.logger.Logger;
 
@@ -44,27 +50,166 @@ import edu.wustl.common.util.logger.Logger;
  */
 public class QueryOutputTreeBizLogic
 {
+	Map<Long, String> nodeColumnIdMap = new HashMap<Long, String>();
 	/**
 	 * This method is called from ViewSearchResultsAction. It first creates a temp table for output tree.
 	 * Then it creates a dynamic extensions entity which represents the newly created table.
 	 * @param query IQuery , this object has tree associated with it
 	 */
-	public void createOutputTree(IQuery query)
+	public String createOutputTree(IQuery query, SessionDataBean sessionData)
 	{
-		IOutputTreeNode rootNode = getDummyTreeNodes();
-		query.setRootOutputClass(rootNode);
-		createOutputTreeTable(query);
-		EntityInterface dynExtEntity = createDynExtEntity(query);
-		System.out.println(dynExtEntity);
+		ISqlGenerator sqlGenerator = SqlGeneratorFactory.getInstance();
+		String  selectSql = "";
+		try
+		{
+			selectSql = sqlGenerator.generateSQL(query);
+			System.out.println(selectSql);
+		}
+		catch (MultipleRootsException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SqlException e)
+		{
+			e.printStackTrace();
+		}
+		Map<Long, Map<AttributeInterface, String>>  nodeAttributeColumnNameMap = sqlGenerator.getColumnMap();
+		String tableName = createOutputTreeTable(query);
+		Vector treeDataVector = createOutputTreeDataVector(tableName, query, sessionData,nodeAttributeColumnNameMap);
+		System.out.println(treeDataVector);
+		return selectSql;
 	}
+
+	/**
+	 * 
+	 * @param tableName
+	 * @param query
+	 * @param sessionData
+	 * @param nodeAttributeColumnNameMap
+	 * @return
+	 */
+	Vector<QueryTreeNodeData> createOutputTreeDataVector(String tableName, IQuery query, SessionDataBean sessionData,Map<Long, Map<AttributeInterface, String>>  nodeAttributeColumnNameMap)
+	{
+		JDBCDAO jdbcDao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
+		List dataList = null;
+		try
+		{
+			jdbcDao.openSession(sessionData);
+			dataList = jdbcDao.retrieve(tableName);
+		}
+		catch (DAOException excp)
+		{
+			Logger.out.error(excp.getMessage(), excp);
+			excp.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				jdbcDao.closeSession();
+			}
+			catch (DAOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		Vector<QueryTreeNodeData> treeDataVector = new Vector<QueryTreeNodeData>();
+		Iterator dataListIterator = dataList.iterator();
+		List rowList = new ArrayList();
+		IOutputTreeNode root = query.getRootOutputClass();
+		List<IOutputTreeNode> allChildNodes = new ArrayList<IOutputTreeNode>();
+		List<IOutputTreeNode> childNodes = getAllChildNodes(root, allChildNodes);
+		while (dataListIterator.hasNext())
+		{
+			rowList = (List) dataListIterator.next();
+			treeDataVector = addNodeToTree(rowList,childNodes,treeDataVector,nodeAttributeColumnNameMap);
+		}
+		return treeDataVector;
+	}
+	/**
+	 * Adds the nodes one by one to the vector.
+	 * @param rowList
+	 * @param childNodes
+	 * @param treeDataVector
+	 */
+	Vector<QueryTreeNodeData> addNodeToTree(List rowList,List<IOutputTreeNode> childNodes,Vector<QueryTreeNodeData>treeDataVector,
+			Map<Long, Map<AttributeInterface, String>>  nodeAttributeColumnNameMap)
+			{
+		boolean isNodeAlreadyPresentInTree = false;
+		QueryTreeNodeData treeNode = null;
+		for (IOutputTreeNode node : childNodes)
+		{
+			Map<AttributeInterface, String> map = nodeAttributeColumnNameMap.get(node.getId());
+			Set<AttributeInterface> set1 = map.keySet();
+            for (Iterator<AttributeInterface> iterator = set1.iterator(); iterator.hasNext();)
+            {
+                    AttributeInterface attr = iterator.next();
+                    if(attr.getName().equalsIgnoreCase(Constants.ID))
+                    {
+                    	String idColumnName = map.get(attr);
+            			int index = Integer.parseInt(idColumnName.substring("Column".length(),idColumnName.length()));
+            			System.out.println(index);
+            			String treeNodeId = (String)rowList.get(index);
+            			Iterator iterTreeData = treeDataVector.iterator();
+            			while(iterTreeData.hasNext())
+            			{
+            				treeNode = (QueryTreeNodeData)iterTreeData.next();
+            				if(treeNode.getIdentifier().equals(treeNodeId))
+            				{
+            					isNodeAlreadyPresentInTree = true;
+            				}
+            			}
+            			if(!isNodeAlreadyPresentInTree)
+            			{
+            				treeNode = new QueryTreeNodeData();
+            				treeNode.setIdentifier(treeNodeId);
+            				treeNode.setObjectName(node.toString());
+            				treeNode.setDisplayName(node.toString());
+            				IOutputTreeNode parentNode = node.getParent();
+            				if(parentNode != null)
+            				{
+            					idColumnName = nodeColumnIdMap.get(parentNode);
+            					treeNodeId = (String)rowList.get(rowList.indexOf(idColumnName));
+            					treeNode.setParentIdentifier(treeNodeId);
+            					treeNode.setParentObjectName(parentNode.toString());
+            				}
+            				else
+            				{
+            					treeNode.setParentIdentifier(null);
+            					treeNode.setParentObjectName("");
+            				}
+            				treeDataVector.add(treeNode);
+            			}
+                    }
+                    System.out.println(attr.getName()+":"+map.get(attr));
+            }
+			
+		}
+		return treeDataVector;
+			}
+
 	/**
 	 * Creates new table which has the same structure and also same data , as the output tree structurs has.  
 	 * @param query IQuery , this object has tree associated with it
 	 */
-	void createOutputTreeTable(IQuery query)
+	String createOutputTreeTable(IQuery query)
 	{
-		//		sql = SqlGeneratorFactory.getInstance().generateSQL(query);
-		String selectSql = "Select Participant_1.IDENTIFIER, Participant_1.MIDDLE_NAME, Participant_1.FIRST_NAME,  Participant_1.GENDER, Participant_1.LAST_NAME ,  cpr_1.REGISTRATION_DATE From CATISSUE_PARTICIPANT Participant_1 ,catissue_coll_prot_reg cpr_1";
+		ISqlGenerator sqlGenerator = SqlGeneratorFactory.getInstance();
+		String  selectSql = "";
+		try
+		{
+			selectSql = sqlGenerator.generateSQL(query);
+			System.out.println(selectSql);
+		}
+		catch (MultipleRootsException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SqlException e)
+		{
+			e.printStackTrace();
+		}
+		//String selectSql = "Select Participant_1.IDENTIFIER, Participant_1.MIDDLE_NAME, Participant_1.FIRST_NAME,  Participant_1.GENDER, Participant_1.LAST_NAME ,  cpr_1.REGISTRATION_DATE From CATISSUE_PARTICIPANT Participant_1 ,catissue_coll_prot_reg cpr_1";
 		String tableName = "temp_OutputTree";
 		String createTableSql = "Create table " + tableName + " As " + selectSql;
 		Logger.out.debug("sql for create table " + createTableSql);
@@ -85,6 +230,7 @@ public class QueryOutputTreeBizLogic
 		{
 			e.printStackTrace();
 		}
+		return tableName;
 	}
 
 	/**
@@ -140,61 +286,68 @@ public class QueryOutputTreeBizLogic
 	 * @param query IQuery , this object has tree associated with it
 	 * @return EntityInterface DynExt entity
 	 */
-	private EntityInterface createDynExtEntity(IQuery query)
+	private EntityInterface createDynExtEntity(IQuery query, String tableName, Map attributeColumnNameMap)
 	{
+		ISqlGenerator sqlGenerator = SqlGeneratorFactory.getInstance();
+		try
+		{
+			String  str = sqlGenerator.generateSQL(query);
+		}
+		catch (MultipleRootsException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SqlException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		IOutputTreeNode root =query.getRootOutputClass();
+		Map<Long, Map<AttributeInterface, String>>  map = sqlGenerator.getColumnMap();
 		EntityInterface entity = DomainObjectFactory.getInstance().createEntity();
-		IOutputTreeNode root = query.getRootOutputClass();
 		List<IOutputTreeNode> allChildNodes = new ArrayList<IOutputTreeNode>();
 		List<IOutputTreeNode> childNodes = getAllChildNodes(root, allChildNodes);
 
-		Collection<AttributeInterface> attrs = new ArrayList<AttributeInterface>();
-		Map<Long, AttributeInterface> idAndAttributeMap = new HashMap<Long, AttributeInterface>();
 		for (IOutputTreeNode node : childNodes)
 		{
-			Collection<AttributeInterface> attrColl = node.getOutputEntity().getDynamicExtensionsEntity().getAttributeCollection();
-			Collection<AttributeInterface> attrCollClone = (Collection<AttributeInterface>) QueryObjectProcessor.getObjectCopy(attrColl);
-			attrs.addAll(attrCollClone);
-		}
-		LinkedHashMap attributeIndexMap = getAttributeIndexMappings(attrs);
-		for (AttributeInterface attr : attrs)
-		{
-			idAndAttributeMap.put(attr.getId(), attr);
-		}
-		entity = addAttributesToEntity(entity, attributeIndexMap, idAndAttributeMap);
-		return entity;
-	}
-
-	/**
-	 * All the attributes of all the nodes present in a tree are added to an entity maintaining the same index.
-	 * @param entity entity to which attributes to be added
-	 * @param attributeIndexMap map of attribute and its index 
-	 * @param idAndAttributeMap id and attribute map
-	 * @return
-	 */
-	EntityInterface addAttributesToEntity(EntityInterface entity, LinkedHashMap attributeIndexMap, Map idAndAttributeMap)
-	{
-		for (int index = 0; index < attributeIndexMap.size(); index++)
-		{
-			AttributeInterface attr = (AttributeInterface) idAndAttributeMap.get(attributeIndexMap.get(index));
-			entity.addAttribute(attr);
+			entity = addAttributesToEntity(node, entity, tableName, attributeColumnNameMap);
 		}
 		return entity;
 	}
 
 	/**
-	 * creates a dummy map for attribute and its index, this will later replaced by a map generates from sqlgenerator.
-	 * @param attrs collection of attribute obj
-	 * @return Map of index and the attribute's id.
+	 * This method takes a map as an input , key : Attribute AND value : columnName.
+	 * One by one these attributes are cloned and added to entity along with their tableproperties and column properties are also set. 
+	 * @param entity EntityInterface obj to which the attributes are added
+	 * @param tableName this name is then set as tableproperties for this entity
+	 * @param attributeColumnNameMap Map of attribute and its columnname.
+	 * @return EntityInterface the entity with all the attributes and their properties added with it. 
 	 */
-	LinkedHashMap getAttributeIndexMappings(Collection<AttributeInterface> attrs)
+	EntityInterface addAttributesToEntity(IOutputTreeNode node, EntityInterface entity, String tableName,
+			Map<AttributeInterface, String> attributeColumnNameMap)
 	{
-		LinkedHashMap dummyMap = new LinkedHashMap();
-		int index = 0;
-		for (AttributeInterface attr : attrs)
+		TablePropertiesInterface tableProperty = new TableProperties();
+		tableProperty.setName(tableName);
+		entity.setTableProperties(tableProperty);
+		Iterator iter = attributeColumnNameMap.entrySet().iterator();
+		while (iter.hasNext())
 		{
-			dummyMap.put(index++, attr.getId());
+			Map.Entry<AttributeInterface, String> entry = (Map.Entry) iter.next();
+			AttributeInterface attribute = entry.getKey();
+			String columnName = entry.getValue();
+			if (attribute.getName().equalsIgnoreCase(Constants.ID))
+			{
+				nodeColumnIdMap.put(node.getId(), columnName);
+			}
+
+			AttributeInterface attrCopy = (AttributeInterface) QueryObjectProcessor.getObjectCopy(attribute);
+			ColumnPropertiesInterface columnProperty = new ColumnProperties();
+			columnProperty.setName(columnName);
+			attrCopy.setColumnProperties(columnProperty);
+			entity.addAttribute(attrCopy);
 		}
-		return dummyMap;
+		return entity;
 	}
 
 	/**
@@ -216,33 +369,6 @@ public class QueryOutputTreeBizLogic
 			}
 		}
 		return all;
-	}
-	/**
-	 * This method takes a map as an input , key : Attribute AND value : columnName.
-	 * One by one these attributes are cloned and added to entity along with their tableproperties and column properties are also set. 
-	 * @param entity EntityInterface obj to which the attributes are added
-	 * @param tableName this name is then set as tableproperties for this entity
-	 * @param attributeColumnNameMap Map of attribute and its columnname.
-	 * @return EntityInterface the entity with all the attributes and their properties added with it. 
-	 */
-	EntityInterface addAttributesToEntity1(EntityInterface entity, String tableName ,HashMap<AttributeInterface, String> attributeColumnNameMap)
-	{
-		TablePropertiesInterface tableProperty = new TableProperties();
-		tableProperty.setName(tableName);
-		entity.setTableProperties(tableProperty);
-		Iterator iter = attributeColumnNameMap.entrySet().iterator();
-		while(iter.hasNext())
-		{
-			Map.Entry<AttributeInterface,String> entry = (Map.Entry)iter.next();
-			AttributeInterface attribute = entry.getKey();
-			String columnName = entry.getValue();
-			AttributeInterface attrCopy = (AttributeInterface)QueryObjectProcessor.getObjectCopy(attribute);
-			ColumnPropertiesInterface columnProperty = new ColumnProperties();
-			columnProperty.setName(columnName);
-			attrCopy.setColumnProperties(columnProperty);
-			entity.addAttribute(attrCopy);
-		}
-		return entity;
 	}
 
 }
