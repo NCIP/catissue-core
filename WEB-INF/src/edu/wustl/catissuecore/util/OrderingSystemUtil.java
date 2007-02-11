650b117b2b4fe27668b8a02225de975b4ac1e1bb
ashish_gupta <ashish_gupta>
2007-02-11 11:43:19 +0000

    added common methods.
    SVN-Revision: 6904

--- WEB-INF/src/edu/wustl/catissuecore/util/OrderingSystemUtil.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/OrderingSystemUtil.java
@@ -14,9 +14,13 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import edu.wustl.catissuecore.bean.DefinedArrayRequestBean;
 import edu.wustl.catissuecore.bean.RequestViewBean;
 import edu.wustl.catissuecore.domain.OrderDetails;
+import edu.wustl.catissuecore.domain.PathologicalCaseOrderItem;
 import edu.wustl.catissuecore.domain.Specimen;
+import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
+import edu.wustl.catissuecore.domain.SpecimenOrderItem;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Utility;
 import edu.wustl.common.beans.NameValueBean;
@@ -151,5 +155,109 @@ public class OrderingSystemUtil
 			}
 		}
 		return finalChildrenSpecimenList;
+	}
+	/**
+	 * @param listToConvert Collection
+	 * @return List. The namevaluebean list of children specimen to display.
+	 */
+	public static List getNameValueBeanList(Collection listToConvert)
+	{
+		List nameValueBeanList = new ArrayList();
+		
+		Iterator iter = listToConvert.iterator();		
+		while(iter.hasNext())
+		{
+			Specimen specimen = (Specimen)iter.next();
+			nameValueBeanList.add(new NameValueBean(specimen.getLabel(),specimen.getId().toString()));
+		}
+		
+		return nameValueBeanList;
+	}
+	/**
+	 * @param specimenCollectionGroup
+	 * @param pathologicalCaseOrderItem
+	 * @return
+	 */
+	public static List getRequestForListForPathologicalCases(SpecimenCollectionGroup specimenCollectionGroup,PathologicalCaseOrderItem pathologicalCaseOrderItem)
+	{
+		Collection childrenSpecimenList = specimenCollectionGroup.getSpecimenCollection();
+		List totalChildrenSpecimenColl = new ArrayList();
+    	//List childrenSpecimenListToDisplay = new ArrayList();
+    	
+    	Iterator childrenSpecimenListIterator = childrenSpecimenList.iterator();
+    	while (childrenSpecimenListIterator.hasNext())
+    	{
+    		Specimen specimen = (Specimen)childrenSpecimenListIterator.next();
+    		List childSpecimenCollection = OrderingSystemUtil.getAllChildrenSpecimen(specimen,specimen.getChildrenSpecimen());
+    		List finalChildrenSpecimenCollection = null;
+    		if(pathologicalCaseOrderItem.getSpecimenClass() != null && pathologicalCaseOrderItem.getSpecimenType() != null && !pathologicalCaseOrderItem.getSpecimenClass().trim().equalsIgnoreCase("") && !pathologicalCaseOrderItem.getSpecimenType().trim().equalsIgnoreCase(""))
+    	    {	//"Derived"	
+    			finalChildrenSpecimenCollection = OrderingSystemUtil.getChildrenSpecimenForClassAndType(childSpecimenCollection,pathologicalCaseOrderItem.getSpecimenClass(),pathologicalCaseOrderItem.getSpecimenType());
+    		}	    	
+		    else
+		    {  	//"Block" . Specimen class = "Tissue" , Specimen Type = "Block".
+		    	finalChildrenSpecimenCollection = OrderingSystemUtil.getChildrenSpecimenForClassAndType(childSpecimenCollection,"Tissue","Block");
+		    }
+    		//adding specimen to the collection
+    		if(specimen.getClassName().equalsIgnoreCase(pathologicalCaseOrderItem.getSpecimenClass()) && specimen.getType().equalsIgnoreCase(pathologicalCaseOrderItem.getSpecimenType()))
+			{
+    			finalChildrenSpecimenCollection.add(specimen);
+			}
+    		if(finalChildrenSpecimenCollection!=null)
+    		{
+    			Iterator finalChildrenSpecimenCollectionIterator = finalChildrenSpecimenCollection.iterator();
+    			while(finalChildrenSpecimenCollectionIterator.hasNext())
+    			{	    		
+    				totalChildrenSpecimenColl.add((Specimen)(finalChildrenSpecimenCollectionIterator.next()));
+    			}	    			
+    		}
+    	} 	
+    	
+    	return totalChildrenSpecimenColl;
+	}
+	/**
+	 * @param specimenOrderItemCollection collection
+	 * @param arrayRequestBean objetc
+	 * @return DefinedArrayRequestBean object
+	 */
+	public static String determineCreateArrayCondition(Collection specimenOrderItemCollection)
+	{
+		Iterator iter = specimenOrderItemCollection.iterator();
+		int readyCounter = 0;
+		int rejectedCounter = 0;
+		int pendingCounter = 0;
+		int newCounter = 0;
+		while(iter.hasNext())
+		{
+			SpecimenOrderItem specimenOrderItem = (SpecimenOrderItem)iter.next();
+			if(specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_NEW))
+			{
+				newCounter++;
+			}
+			else if(specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_PENDING_FOR_DISTRIBUTION)
+					|| specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_PENDING_PROTOCOL_REVIEW)
+					|| specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_PENDING_SPECIMEN_PREPARATION))
+			{
+				pendingCounter++;
+			}
+			else if(specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_REJECTED_INAPPROPRIATE_REQUEST)
+					|| specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_REJECTED_SPECIMEN_UNAVAILABLE)
+					|| specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_REJECTED_UNABLE_TO_CREATE))
+			{
+				rejectedCounter++;
+			}
+			else if(specimenOrderItem.getStatus().trim().equalsIgnoreCase(Constants.ORDER_REQUEST_STATUS_READY_FOR_ARRAY_PREPARATION))
+			{
+				readyCounter++;
+			}
+		}
+		if((newCounter > 0) || (pendingCounter > 0)	|| (specimenOrderItemCollection.size() == rejectedCounter) || (specimenOrderItemCollection.size() != rejectedCounter+readyCounter))
+		{
+			return "true"; 
+		}
+		else
+		{
+			return "false"; 
+		}		
 	}
 }
