3df0bb814672230a2057290f4b2f54f69bd8839f
sachin_lale <sachin_lale>
2007-03-09 11:47:29 +0000

    Code- Model Sync - added marital_status to participant table
    SVN-Revision: 7242

--- SQL/Oracle/catissuecore.sql
+++ SQL/Oracle/catissuecore.sql
@@ -555,6 +555,7 @@ create table CATISSUE_PARTICIPANT (
    ACTIVITY_STATUS varchar(50),
    DEATH_DATE date,
    VITAL_STATUS varchar(50),
+   MARITAL_STATUS varchar(50),
    primary key (IDENTIFIER)
 );
 create table CATISSUE_SPECIMEN_CHAR (
