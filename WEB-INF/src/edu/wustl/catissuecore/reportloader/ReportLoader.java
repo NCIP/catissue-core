6ca0c939dde60af42017e4ccf4082bb442e4573f
vijay_pande <vijay_pande>
2007-03-22 08:41:38 +0000

    updates for error handling and logger statement
    SVN-Revision: 7392

--- WEB-INF/src/edu/wustl/catissuecore/reportloader/ReportLoader.java
+++ WEB-INF/src/edu/wustl/catissuecore/reportloader/ReportLoader.java
@@ -1,28 +1,13 @@
 package edu.wustl.catissuecore.reportloader;
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
-import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
-import edu.wustl.catissuecore.bizlogic.CollectionProtocolBizLogic;
-import edu.wustl.catissuecore.bizlogic.ParticipantBizLogic;
-import edu.wustl.catissuecore.domain.CollectionProtocol;
-import edu.wustl.catissuecore.domain.CollectionProtocolEvent;
-import edu.wustl.catissuecore.domain.CollectionProtocolRegistration;
 import edu.wustl.catissuecore.domain.Participant;
 import edu.wustl.catissuecore.domain.Site;
 import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
-import edu.wustl.catissuecore.domain.User;
 import edu.wustl.catissuecore.domain.pathology.IdentifiedSurgicalPathologyReport;
 import edu.wustl.catissuecore.domain.pathology.TextContent;
-import edu.wustl.catissuecore.util.CatissueCoreCacheManager;
-import edu.wustl.catissuecore.util.global.Constants;
-import edu.wustl.common.bizlogic.DefaultBizLogic;
-import edu.wustl.common.lookup.DefaultLookupResult;
-import edu.wustl.common.util.XMLPropertyHandler;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -49,10 +34,14 @@ public class ReportLoader
 	private IdentifiedSurgicalPathologyReport identifiedReport;
 	
 	/**
-	 * prticipant exists flag
+	 * Specimen Collection Group
 	 */
-	private boolean isParticipantExists;
+	private SpecimenCollectionGroup scg;
 	
+	/**
+	 * Surgical Pathology Number for Specimen Collection Group
+	 */
+	private String surgicalPathologyNumber;
 	
 	/**
 	 * @param p participant 
@@ -66,51 +55,21 @@ public class ReportLoader
 		this.identifiedReport=report;
 		this.site=s;
 	}
-	
 	/**
-	 * @param p participant
+	 * @param p participant 
 	 * @param report identified report
-	 * @param s Site
-	 * @param participantExists 
+	 * @param s site
 	 * Constructor
-	 * 
 	 */
-	public ReportLoader(Participant p,IdentifiedSurgicalPathologyReport report,Site s, boolean participantExists)
+	public ReportLoader(Participant p,IdentifiedSurgicalPathologyReport report,Site s, SpecimenCollectionGroup scg, String surgicalPathologyNumber)
 	{
 		this.participant=p;
 		this.identifiedReport=report;
 		this.site=s;
-		this.isParticipantExists=participantExists;
+		this.scg=scg;
+		this.surgicalPathologyNumber=surgicalPathologyNumber;
 	}
-	
-	/**
-	 * @return number of matching participants
-	 * @throws Exception throws exception
-	 */
-	public int checkForParticipant()throws Exception
-	{
-		int size=0;
-		List participantList=null;
-		BizLogicFactory bizLogicFactory = BizLogicFactory.getInstance();
-		ParticipantBizLogic bizLogic =(ParticipantBizLogic) bizLogicFactory.getBizLogic(Participant.class.getName());
-		participantList = bizLogic.getListOfMatchingParticipants(this.participant);
-   		if(participantList!=null && participantList.size()>0)
-		{
-			if(participantList.size()==1)
-			{
-				DefaultLookupResult participantResult=(DefaultLookupResult)participantList.get(0);
-			    //these probability is being removed now
-				//Double probability = Double.valueOf(XMLPropertyHandler.getValue(Constants.MAX_PARTICIPANT_MATCHING_PERCENTAGE));
-				//if(probability.doubleValue() <= participantResult.getProbablity().doubleValue())
-				//{	
-					this.participant = (Participant)participantResult.getObject();
-				//}	
-				size=1;
-			}
-		}
-		return size;
-	}
-	
+			
 	/**
 	 * @throws Exception
 	 * processes the reporting data. Validats for participant,site and and report
@@ -118,183 +77,74 @@ public class ReportLoader
 	 */
 	public void process()throws Exception
 	{
-		boolean isExists=false;
-		int size=0;
-		SpecimenCollectionGroup scg=null;
+		Logger.out.info("Processing Report ");
+		identifiedReport.setReportStatus(Parser.PENDING_FOR_DEID);
+		identifiedReport.setSource(this.site);
 		try
-		{
-			
-		/*	if(!isParticipantExists)
+		{	
+			// check for existing specimen collection group(scg)
+			if(this.scg==null || this.scg.getId()==0)
 			{
-				size =checkForParticipant();
-			}	
-			if(size==0)
-			{
-				checkForSite();
-				checkForSpecimenCollectionGroup();
-				saveObject(this.participant);
-				addParticipantToCache();
+				// if scg is null create new scg
+				Logger.out.info("Null SCG found in ReportLoader, Creating New SCG");
+				this.scg=ReportLoaderUtil.createNewSpecimenCollectionGroup(this.participant, this.identifiedReport,this.site, this.surgicalPathologyNumber);
+				ReportLoaderUtil.saveObject(scg);
 			}
-			else if(size==1)
-			{*/
-				checkForSite();
-				scg=checkForSpecimenCollectionGroup();
-				identifiedReport.setReportStatus(Parser.PENDING_FOR_DEID);
-				identifiedReport.setSource(this.site);
-				if(scg!=null)
+			else
+			{
+				// use existing scg
+				if(this.scg.getIdentifiedSurgicalPathologyReport()!=null)
 				{
-					ReportLoaderUtil.saveObject(scg);
-				}	
-			//}
+					this.scg.setIdentifiedSurgicalPathologyReport(identifiedReport);
+					ReportLoaderUtil.updateObject(scg);
+				}
+			}	
+			Logger.out.info("Processing finished for Report ");
 		}
 		catch(Exception ex)
 		{
-			Logger.out.error("participant " + this.participant.getSocialSecurityNumber(), ex);
+			Logger.out.error("Failed to process report ");
+			throw new Exception(ex.getMessage());
 		}
 	}
-	
-	/**
-	 * @return  site for the pathology report data.
-	 * @throws Exception throws exception
-	 * Checks for existing site in the system.
-	 */
-	public Site checkForSite()throws Exception
-	{
-		Site s=null;
-		List list = ReportLoaderUtil.getObject(site.getClass().getName(),"name",this.site.getName());
-		if(list!=null && list.size()>0)
-		{
-			this.site=(Site)list.get(0);
-		}
-		else
-		{
-			list = ReportLoaderUtil.getObject(User.class.getName(),"loginName","admin@admin.com");
-			this.site.setCoordinator((User)list.get(0));
-			ReportLoaderUtil.saveObject(this.site);
-		}
-		return s;
-	}
-	
+
 	/**
 	 * @return specimen collection group
 	 * @throws Exception throws exception
 	 */
-	public SpecimenCollectionGroup checkForSpecimenCollectionGroup()throws Exception
+	public SpecimenCollectionGroup checkForSpecimenCollectionGroup(Participant participant)throws Exception
 	{
 		List scgSet=null;
-		SpecimenCollectionGroup scg=null;
+		SpecimenCollectionGroup existingSCG=null;
 		Iterator scgIterator=null;
 		boolean isExists=false;
 		try
 		{
-			scgSet=ReportLoaderUtil.getSCGList(this.participant);
+			// het list of all the scg associated with participant
+			scgSet=ReportLoaderUtil.getSCGList(participant);
 			if(scgSet!=null && scgSet.size()>0)
 			{
 				scgIterator=scgSet.iterator();
 				while(scgIterator.hasNext())
 				{
-					scg=(SpecimenCollectionGroup)scgIterator.next();
-					isExists=checkForReport(scg);
+					// check for mathcing scg
+					existingSCG=(SpecimenCollectionGroup)scgIterator.next();
+					isExists=checkForReport(existingSCG);
 					if(isExists)
 					{
-						scg=null;
-						break;
+						return existingSCG;
 					}
 				}
-				if(!isExists)
-				{
-					scg=createNewSpecimenCollectionGroup();
-				}
-			}
-			else
-			{
-//				this.participant.setSpecimenCollectionGroupCollection(new HashSet());
-				scg=createNewSpecimenCollectionGroup();
 			}
 		}
 		catch(Exception ex)
 		{
-			Logger.out.error("Error while checking specimen collection group ",ex);	
+			Logger.out.error("Error while checking specimen collection group ",ex);
+			throw ex;
 		}
-		return scg;
+		return null;
 	}
-	
-	/**
-	 * Adds participant to the cahe of participants
-	 * @throws Exception throws exception
-	 */
-	private void addParticipantToCache()throws Exception 
-	{
 		
-		CatissueCoreCacheManager catissueCoreCacheManager = CatissueCoreCacheManager.getInstance();
-		HashMap participantMap = (HashMap)catissueCoreCacheManager.getObjectFromCache("listOfParticipants");
-		participantMap.put(this.participant.getId(), this.participant);
-		catissueCoreCacheManager.addObjectToCache(Constants.MAP_OF_PARTICIPANTS, participantMap);
-	}
-	/**
-	 * Method to create new SCG
-	 * @return scg object of SpecimenColectionGroup 
-	 * @throws Exception generic exception
-	 */
-	public SpecimenCollectionGroup createNewSpecimenCollectionGroup()throws Exception
-	{
-		SpecimenCollectionGroup scg = new SpecimenCollectionGroup();
-		scg.setActivityStatus(Constants.ACTIVITY_STATUS_ACTIVE);
-//		scg.setParticipant(this.participant);
-		scg.setClinicalDiagnosis(Constants.NOT_SPECIFIED);
-		scg.setClinicalStatus(Constants.NOT_SPECIFIED);
-		scg.setSpecimenCollectionSite(this.site); 
-		scg.setIdentifiedSurgicalPathologyReport(this.identifiedReport);
-		this.identifiedReport.setSpecimenCollectionGroup(scg);
-	
-		scg.setName("caties_"+ new Date().getTime());
-		
-		String className=CollectionProtocol.class.getName();
-		String colName=new String("title");
-		String colValue=XMLPropertyHandler.getValue("collectionProtocolTitle");
-			
-		BizLogicFactory bizLogicFactory=BizLogicFactory.getInstance();
-		CollectionProtocolBizLogic cpBizLogic=(CollectionProtocolBizLogic)bizLogicFactory.getBizLogic(CollectionProtocol.class.getName());
-		List cpList=cpBizLogic.retrieve(className, colName, colValue);
-		CollectionProtocol collectionProtocol=(CollectionProtocol)cpList.get(0);
-		Set collProtocolEventList=(Set)collectionProtocol.getCollectionProtocolEventCollection();
-		Iterator cpEventIterator=collProtocolEventList.iterator();
-		
-		if(!cpEventIterator.hasNext())
-		{
-			Logger.out.info("Associated Collection Protocol Event not found for "+ collectionProtocol.getTitle());
-		}
-		else
-		{
-			CollectionProtocolEvent collProtocolEvent=(CollectionProtocolEvent)cpEventIterator.next();
-			scg.setCollectionProtocolEvent(collProtocolEvent);
-			CollectionProtocolRegistration collProtocolReg=new CollectionProtocolRegistration();
-			
-			collProtocolReg.setActivityStatus(Constants.ACTIVITY_STATUS_ACTIVE);
-			collProtocolReg.setRegistrationDate(new Date());
-			collProtocolReg.setParticipant(this.participant);	
-			collProtocolReg.setCollectionProtocol(collProtocolEvent.getCollectionProtocol());
-			((Set)this.participant.getCollectionProtocolRegistrationCollection()).add(collProtocolReg);
-			try
-			{
-				ReportLoaderUtil.saveObject(collProtocolReg);
-			}
-			catch(Exception ex)
-			{
-				Logger.out.error("Error: Could not save object of CollectionProtocolRegistration",ex);
-			}
-			
-			
-			//((Set)collProtocolReg.getSpecimenCollectionGroupCollection()).add(scg);
-			scg.setCollectionProtocolRegistration(collProtocolReg);
-			
-		}
-			
-		return scg;
-	}
-	
-	
-	
 	/**
 	 * @param specimenCollectionGroup specimen collection group 
 	 * @return boolean value which represents existance of the report
@@ -302,16 +152,20 @@ public class ReportLoader
 	 * @throws Exception throws exception
 	 * 
 	 */
+	
 	public boolean checkForReport(SpecimenCollectionGroup specimenCollectionGroup)throws Exception
 	{
+		Logger.out.info("Inside checkFOrReport function");
 		boolean isExists=false;
 		IdentifiedSurgicalPathologyReport report=specimenCollectionGroup.getIdentifiedSurgicalPathologyReport();
 		if(report !=null)
 		{
-			if(false)//report.getAccessionNumber().equals(this.identifiedReport.getAccessionNumber()))
+			// checking for matching scg
+			if((specimenCollectionGroup.getSurgicalPathologyNumber()).equals(this.scg.getSurgicalPathologyNumber())&& (specimenCollectionGroup.getSpecimenCollectionSite().getName()).equals(this.scg.getSpecimenCollectionSite().getName()))
 			{	isExists = checkForTextContent(report);
 				if(!isExists)
 				{
+					// if report text is not null then set report text
 					report.setTextContent(this.identifiedReport.getTextContent());
 					this.identifiedReport.getTextContent().setSurgicalPathologyReport(report);
 					ReportLoaderUtil.updateObject(report);
@@ -329,16 +183,13 @@ public class ReportLoader
 	 */
 	public boolean checkForTextContent(IdentifiedSurgicalPathologyReport report)
 	{
+		Logger.out.info("Inside checkForTextContent function");
 		TextContent content=report.getTextContent();
-		if(content !=null)
+		// check for null report text content
+		if(content !=null && content.getData()!=null && content.getData().length()>0)
 		{
 			return true;
 		}
 		return false;
-	}
-	
-	
-
-		
-	
+	}	
 }
