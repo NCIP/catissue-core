efb91e39c51de480fb0fd04f44a5ef73c135247b
vijay_pande <vijay_pande>
2007-03-22 14:07:36 +0000

    comments added
    SVN-Revision: 7417

--- WEB-INF/src/edu/wustl/catissuecore/deid/StopDeidServer.java
+++ WEB-INF/src/edu/wustl/catissuecore/deid/StopDeidServer.java
@@ -24,7 +24,7 @@ public class StopDeidServer
 	
 	    try 
 	    {    
-	    
+	    	// initialization of stopDeidServer
 	    	Variables.applicationHome = System.getProperty("user.dir");
 			Logger.out = org.apache.log4j.Logger.getLogger("");
 			Logger.configure(Variables.applicationHome + File.separator+"ApplicationResources.properties");
@@ -32,11 +32,14 @@ public class StopDeidServer
 			System.setProperty("gov.nih.nci.security.configFile",
 					"./catissuecore-properties"+File.separator+"ApplicationSecurityConfig.xml");
 	    	XMLPropertyHandler.init("./catissuecore-properties"+File.separator+"caTissueCore_Properties.xml");
+	    	// get port number of DeidServer
 	    	int PORT=Integer.parseInt(XMLPropertyHandler.getValue("deid.port"));
+	    	// Create client socket to connect to server
 	        Socket s = new Socket("localhost",PORT);
 	        Logger.out.info(XMLPropertyHandler.getValue("deid.port"));
 	        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 	        PrintWriter out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()),true);
+	        // send stop command to stop the server
 	        out.write("stop");
 	        s.close();
 	        Logger.out.info("Message sent to stop server");
