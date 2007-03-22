14a7c9ded5208a2a2a8a4a5c445752da8857dad4
vijay_pande <vijay_pande>
2007-03-22 14:20:23 +0000

    Updated to set common logger for caties related server
    SVN-Revision: 7421

--- WEB-INF/src/edu/wustl/catissuecore/reportloader/CSVLogger.java
+++ WEB-INF/src/edu/wustl/catissuecore/reportloader/CSVLogger.java
@@ -1,44 +1,145 @@
-/**
- * <p>Title: CVSLogger Class>
- * <p>Description:  CVSLogger for Report Loader class</p>
- * Copyright:    Copyright (c) year
- * Company: Washington University, School of Medicine, St. Louis.
- * @author Vijay Pande
- * 
- * FIXME: Java doc. 
- */
 package edu.wustl.catissuecore.reportloader;
 
+/**
+ * @author vijay_pande
+ * Common Logger class that is used by all the caTIES server to log CSV formated status for each report
+ */
 public abstract class CSVLogger
 {
-	public static org.apache.log4j.Logger out;
+	/**
+	 * Logger for file poller
+	 */
+	private static org.apache.log4j.Logger filePollerOut;
+	/**
+	 * Logger for queue processor
+	 */
+	private static org.apache.log4j.Logger queueProcessorOut;
+	/**
+	 * Logger for deid server
+	 */
+	private static org.apache.log4j.Logger deidServerOut;
+	/**
+	 * Logger for concept coder
+	 */
+	private static org.apache.log4j.Logger conceptCoderOut;
 	
-    /**
-     * Configures the logger with the properties of the specified category name in the log4j.xml file.
-     * The category should be present in the logger.properties file.
-     * @param categoryName The category name. 
-     */
-    public static void configure(String categoryName)
+	/**
+	 * @param categoryName categoryNmae for logger
+	 */
+	public static void configure(String categoryName)
 	{
-		if(out==null)
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_FILE_POLLER))
 		{
-		    out=org.apache.log4j.Logger.getLogger(categoryName);
+			filePollerOut=org.apache.log4j.Logger.getLogger(categoryName);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_QUEUE_PROCESSOR))
+		{
+			queueProcessorOut=org.apache.log4j.Logger.getLogger(categoryName);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_DEID_SERVER))
+		{
+			deidServerOut=org.apache.log4j.Logger.getLogger(categoryName);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_CONCEPT_CODER))
+		{
+			conceptCoderOut=org.apache.log4j.Logger.getLogger(categoryName);
 		}
 	}
-    
-    /**
-     * Configures the logger properties from the log4j.properties file.
-     * The log4j.properties file should be present in the classpath for configuring.
-     */
-    public static void configure()
-    {
-        if (out == null)
-        {
-            out=org.apache.log4j.Logger.getLogger("");
-        }
-    }
-    
-	private CSVLogger()
+	
+	/**
+	 * @param categoryName categoryNmae for logger
+	 * @param message message log as info
+	 */
+	public static void info(String categoryName, String message)
 	{
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_FILE_POLLER))
+		{
+			filePollerOut.info(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_QUEUE_PROCESSOR))
+		{
+			queueProcessorOut.info(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_DEID_SERVER))
+		{
+			deidServerOut.info(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_CONCEPT_CODER))
+		{
+			conceptCoderOut.info(message);
+		}
+	}
+	
+	/**
+	 * @param categoryName categoryNmae for logger
+	 * @param message message log as debug
+	 */
+	public static void debug(String categoryName, String message)
+	{
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_FILE_POLLER))
+		{
+			filePollerOut.debug(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_QUEUE_PROCESSOR))
+		{
+			queueProcessorOut.debug(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_DEID_SERVER))
+		{
+			deidServerOut.debug(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_CONCEPT_CODER))
+		{
+			conceptCoderOut.debug(message);
+		}
+	}
+	
+	/**
+	 * @param categoryName categoryNmae for logger
+	 * @param message message log as error
+	 */
+	public static void error(String categoryName, String message)
+	{
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_FILE_POLLER))
+		{
+			filePollerOut.error(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_QUEUE_PROCESSOR))
+		{
+			queueProcessorOut.error(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_DEID_SERVER))
+		{
+			deidServerOut.error(message);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_CONCEPT_CODER))
+		{
+			conceptCoderOut.error(message);
+		}
+	}
+	
+	/**
+	 * @param categoryName categoryNmae for logger
+	 * @param message message log as error
+	 * @param obj Throwable object
+	 */
+	public static void error(String categoryName, String message,Throwable obj)
+	{
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_FILE_POLLER))
+		{
+			filePollerOut.error(message, obj);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_QUEUE_PROCESSOR))
+		{
+			queueProcessorOut.error(message, obj);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_DEID_SERVER))
+		{
+			deidServerOut.error(message, obj);
+		}
+		if(categoryName.equalsIgnoreCase(Parser.LOGGER_CONCEPT_CODER))
+		{
+			conceptCoderOut.error(message, obj);
+		}
 	}
 }
