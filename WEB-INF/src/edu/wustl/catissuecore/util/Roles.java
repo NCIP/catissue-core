88a732e4f50e4fcd7f7a08d6333e18937724e20d
virender_mehta <virender_mehta>
2007-04-03 08:37:20 +0000

    extending Roles.java from roles.java in Common package
    SVN-Revision: 7511

--- WEB-INF/src/edu/wustl/catissuecore/util/Roles.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/Roles.java
@@ -8,22 +8,9 @@
  */ 
 package edu.wustl.catissuecore.util;
 
-
-/**
- *<p>Title: </p>
- *<p>Description:  </p>
- *<p>Copyright: (c) Washington University, School of Medicine 2005</p>
- *<p>Company: Washington University, School of Medicine, St. Louis.</p>
- *@author Aarti Sharma
- *@version 1.0
- */
-
-public interface Roles
+public interface Roles extends edu.wustl.common.util.Roles
 {
-    public static final String ADMINISTRATOR = "ADMINISTRATOR";
-    public static final String SUPERVISOR = "SUPERVISOR";
-    public static final String TECHNICIAN = "TECHNICIAN";
-    public static final String PI = "PI";
+	public static final String PI = "PI";
     public static final String READ_ONLY = "READ_ONLY";
     public static final String USE_ONLY = "USE_ONLY";
     public static final String UPDATE_ONLY = "UPDATE_ONLY";
