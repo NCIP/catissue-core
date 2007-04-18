9b8b1164a57e7ea5353c6a517285a55dfa7ff713
sachin_lale <sachin_lale>
2007-04-18 13:56:26 +0000

    Changes mage for permissible values of Gender,Race and ethnicity
    SVN-Revision: 7645

--- SQL/DBUpgrade/Common/initDB_Common.sql
+++ SQL/DBUpgrade/Common/initDB_Common.sql
@@ -1,3 +1,9 @@
 /*Vijay Pande: Bug:3834: For Specimen class = 'Molecular' following two permissible values are added*/
 INSERT INTO CATISSUE_PERMISSIBLE_VALUE (IDENTIFIER, VALUE, PARENT_IDENTIFIER, PUBLIC_ID) VALUES(2651,'Total Nucleic Acid',1,NULL);
 INSERT INTO CATISSUE_PERMISSIBLE_VALUE (IDENTIFIER, VALUE, PARENT_IDENTIFIER, PUBLIC_ID) VALUES(2652,'Whole Genome Amplified DNA',1,NULL);
+/* Virender Mehta Non Required valuess 'Not specified' for race and ethnicity for CatissueV1.2 requirement*/
+DELETE FROM CATISSUE_PERMISSIBLE_VALUE where IDENTIFIER=2649;
+DELETE FROM CATISSUE_PERMISSIBLE_VALUE where IDENTIFIER=2650;
+/* Virender Mehta update Male and Female values for CatissueV1.2 requirement*/
+UPDATE CATISSUE_PERMISSIBLE_VALUE SET VALUE = "Male Gender" WHERE identifier=59;
+UPDATE CATISSUE_PERMISSIBLE_VALUE SET VALUE = "Female Gender" WHERE identifier=61;
