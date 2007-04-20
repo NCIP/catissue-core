282ff707b254c9a73fce0d0395f4dac8b352efb8
ashish_gupta <ashish_gupta>
2007-04-20 15:20:42 +0000

    fix for bug 2741
    SVN-Revision: 7698

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/ScgEventsAjaxHandlerAction.java
@@ -0,0 +1,191 @@
+/**
+ * <p>Title: ScgEventsAjaxHandlerAction Class>
+ * <p>Description:  This class populates the events fields in the Specimen page based on the SCG selected.</p>
+ * Copyright:  Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Ashish Gupta
+ * @version 1.00
+ * Created on April 17, 2007
+ */
+package edu.wustl.catissuecore.action;
+
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.bizlogic.SpecimenCollectionGroupBizLogic;
+import edu.wustl.catissuecore.domain.CollectionEventParameters;
+import edu.wustl.catissuecore.domain.ReceivedEventParameters;
+import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.catissuecore.util.global.Utility;
+import edu.wustl.common.action.BaseAction;
+
+
+/**
+ * @author ashish_gupta
+ *
+ */
+public class ScgEventsAjaxHandlerAction extends BaseAction
+{
+	CollectionEventParameters collectionEventParameters = null;
+	ReceivedEventParameters receivedEventParameters = null;
+	/**
+     * Overrides the execute method of Action class.
+     * Sets the various Collection and Received events based on SCG id.
+     */
+	public ActionForward  executeAction(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+    	String scgId = request.getParameter("scgId");
+    	
+    	StringBuffer xmlData = new StringBuffer();
+    	if(scgId != null && !scgId.equals(""))
+    	{
+    		SpecimenCollectionGroupBizLogic specimenCollectionGroupBizLogic = (SpecimenCollectionGroupBizLogic)BizLogicFactory.getInstance().getBizLogic(Constants.SPECIMEN_COLLECTION_GROUP_FORM_ID);
+    		String colName = "id";	
+    		List scgListFromDB = specimenCollectionGroupBizLogic.retrieve(SpecimenCollectionGroup.class.getName(), colName, scgId);
+    		if(scgListFromDB != null && !scgListFromDB.isEmpty())
+    		{
+    			SpecimenCollectionGroup specimenCollectionGroup = (SpecimenCollectionGroup)scgListFromDB.get(0);
+    			Collection eventsColl = specimenCollectionGroup.getSpecimenEventParametersCollection();
+    			if(eventsColl != null && !eventsColl.isEmpty())
+    			{
+    				Iterator iter = eventsColl.iterator();
+    				while(iter.hasNext())
+    				{
+    					Object temp = iter.next();
+    					if(temp instanceof CollectionEventParameters)
+    					{
+    						collectionEventParameters = (CollectionEventParameters)temp;
+    					}
+    					else if(temp instanceof ReceivedEventParameters)
+    					{
+    						receivedEventParameters = (ReceivedEventParameters)temp;
+    					}
+    					
+    				}
+    				xmlData = makeXMLData(xmlData);
+    			}
+    		}
+    	}
+//    	Writing to response
+		PrintWriter out = response.getWriter();	
+		response.setContentType("text/xml");
+		out.write(xmlData.toString());
+		
+    	return null;
+    }
+	/**
+	 * @return
+	 */
+	private StringBuffer makeXMLData(StringBuffer xmlData)
+	{		
+		
+		xmlData.append("<Events>");
+		
+		xmlData.append("<CollectionEvents>");
+		xmlData = appendCollectionEvents(xmlData);
+		xmlData.append("</CollectionEvents>");
+		
+		xmlData.append("<ReceivedEvents>");
+		xmlData = appendReceivedEvents(xmlData);
+		xmlData.append("</ReceivedEvents>");
+		
+		xmlData.append("</Events>");
+		return xmlData;
+		
+	}
+	/**
+	 * @param xmlData
+	 * @return
+	 */
+	private StringBuffer appendCollectionEvents(StringBuffer xmlData)
+	{
+		xmlData.append("<CollectorId>");
+		xmlData.append(collectionEventParameters.getUser().getId().toString());
+		xmlData.append("</CollectorId>");
+		
+		xmlData.append("<CollectorName>");
+		xmlData.append(collectionEventParameters.getUser().getLastName() + "," + collectionEventParameters.getUser().getFirstName());
+		xmlData.append("</CollectorName>");
+		
+		xmlData.append("<CollectionDate>");
+		xmlData.append(Utility.parseDateToString(collectionEventParameters.getTimestamp(),Constants.DATE_PATTERN_MM_DD_YYYY));
+		xmlData.append("</CollectionDate>");
+		
+		Calendar calender = Calendar.getInstance();
+		calender.setTime(collectionEventParameters.getTimestamp());
+		
+		xmlData.append("<CollectionTimeHours>");
+		xmlData.append(Utility.toString(Integer.toString( calender.get(Calendar.HOUR_OF_DAY))));
+		xmlData.append("</CollectionTimeHours>");  
+		
+		xmlData.append("<CollectionTimeMinutes>");
+		xmlData.append(Utility.toString(Integer.toString(calender.get(Calendar.MINUTE))));
+		xmlData.append("</CollectionTimeMinutes>");
+		
+		xmlData.append("<CollectionProcedure>");
+		xmlData.append(collectionEventParameters.getCollectionProcedure());
+		xmlData.append("</CollectionProcedure>");
+		
+		xmlData.append("<CollectionContainer>");
+		xmlData.append(collectionEventParameters.getContainer());
+		xmlData.append("</CollectionContainer>");
+		
+		xmlData.append("<CollectionComments>");
+		xmlData.append(collectionEventParameters.getComments());
+		xmlData.append("</CollectionComments>");	
+		
+		return xmlData;
+	}
+	/**
+	 * @param xmlData
+	 * @return
+	 */
+	private StringBuffer appendReceivedEvents(StringBuffer xmlData)
+	{
+		xmlData.append("<ReceiverId>");
+		xmlData.append(receivedEventParameters.getUser().getId().toString());
+		xmlData.append("</ReceiverId>");
+		
+		xmlData.append("<ReceiverName>");
+		xmlData.append(receivedEventParameters.getUser().getLastName() + "," + receivedEventParameters.getUser().getFirstName());
+		xmlData.append("</ReceiverName>");
+		
+		xmlData.append("<ReceivedDate>");
+		xmlData.append(Utility.parseDateToString(receivedEventParameters.getTimestamp(),Constants.DATE_PATTERN_MM_DD_YYYY));
+		xmlData.append("</ReceivedDate>");
+		
+		Calendar calender = Calendar.getInstance();
+		calender.setTime(receivedEventParameters.getTimestamp());
+		
+		xmlData.append("<ReceivedTimeHours>");
+		xmlData.append(Utility.toString(Integer.toString( calender.get(Calendar.HOUR_OF_DAY))));
+		xmlData.append("</ReceivedTimeHours>");  
+		
+		xmlData.append("<ReceivedTimeMinutes>");
+		xmlData.append(Utility.toString(Integer.toString(calender.get(Calendar.MINUTE))));
+		xmlData.append("</ReceivedTimeMinutes>");
+		
+		xmlData.append("<ReceivedQuality>");
+		xmlData.append(receivedEventParameters.getReceivedQuality());
+		xmlData.append("</ReceivedQuality>");
+		
+		xmlData.append("<ReceivedComments>");
+		xmlData.append(receivedEventParameters.getComments());
+		xmlData.append("</ReceivedComments>");	
+		
+		return xmlData;
+	}
+}
--- WEB-INF/src/edu/wustl/catissuecore/actionForm/SpecimenCollectionGroupForm.java
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/SpecimenCollectionGroupForm.java
@@ -9,15 +9,19 @@
  */
 
 package edu.wustl.catissuecore.actionForm;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
-
 import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionMapping;
-
 import edu.wustl.catissuecore.domain.ClinicalReport;
+import edu.wustl.catissuecore.domain.CollectionEventParameters;
 import edu.wustl.catissuecore.domain.Participant;
+import edu.wustl.catissuecore.domain.ReceivedEventParameters;
 import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
+import edu.wustl.catissuecore.util.EventsUtil;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.DefaultValueManager;
 import edu.wustl.catissuecore.util.global.Utility;
@@ -50,6 +54,35 @@ public class SpecimenCollectionGroupForm extends AbstractActionForm
 	
 	private long participantsMedicalIdentifierId;
 	
+	   /**
+	 * Name : Ashish Gupta
+	 * Reviewer Name : Sachin Lale 
+	 * Bug ID: 2741
+	 * Patch ID: 2741_9	 
+	 * Description: Event Attributes
+	*/
+	private long collectionEventId;																											// Mandar : CollectionEvent 10-July-06
+	private long collectionEventSpecimenId;
+	private long collectionEventUserId;
+	private String collectionEventdateOfEvent;
+	private String collectionEventTimeInHours;
+	private String collectionEventTimeInMinutes;
+	private String collectionEventCollectionProcedure;
+	private String collectionEventContainer;
+	private String collectionEventComments;
+	
+	
+	private long receivedEventId;
+	private long receivedEventSpecimenId;
+	private long receivedEventUserId;
+	private String receivedEventDateOfEvent;
+	private String receivedEventTimeInHours;
+	private String receivedEventTimeInMinutes;
+	private String receivedEventReceivedQuality;
+	private String receivedEventComments;
+	
+
+	
 	/**
 	 * An id which refers to the site of the container if it is parent container.
 	 */
@@ -75,6 +108,14 @@ public class SpecimenCollectionGroupForm extends AbstractActionForm
 	
 	//For AddNew functionality
 	private long collectionProtocolRegistrationId;
+	/**
+	 * Name : Ashish Gupta
+	 * Reviewer Name : Sachin Lale 
+	 * Bug ID: 2741
+	 * Patch ID: 2741_4	 
+	 * Description: Attribute to set events in specimens associated with this scg
+	*/
+	private boolean applyEventsToSpecimens = false;
     
     
     /**
@@ -316,8 +357,62 @@ public class SpecimenCollectionGroupForm extends AbstractActionForm
 		Logger.out.debug("protocolParticipantIdentifier........................."+protocolParticipantIdentifier);
 		Logger.out.debug("SCgForm --------- checkButton : -- " + checkedButton );
 		siteId = specimenCollectionGroup.getSite().getId().longValue();
+		
+		/**
+	 * Name : Ashish Gupta
+	 * Reviewer Name : Sachin Lale 
+	 * Bug ID: 2741
+	 * Patch ID: 2741_10	 
+	 * Description: Method to populate Events in SCG form
+	*/
+		//Populating the events
+		setSCGEvents(specimenCollectionGroup);
 	}
-	
+	/**
+	 * @param specimenCollectionGroup
+	 */
+	private void setSCGEvents(SpecimenCollectionGroup specimenCollectionGroup)
+	{
+		Collection eventsParametersColl = specimenCollectionGroup.getSpecimenEventParametersCollection();
+		if(eventsParametersColl != null && !eventsParametersColl.isEmpty())
+		{
+			Iterator iter = eventsParametersColl.iterator();
+			while(iter.hasNext())
+			{
+				Object tempObj = iter.next();
+				Calendar calender = Calendar.getInstance();
+				if(tempObj instanceof CollectionEventParameters)
+				{
+					CollectionEventParameters collectionEventParameters = (CollectionEventParameters)tempObj;
+					this.collectionEventId = collectionEventParameters.getId().longValue();																											// Mandar : CollectionEvent 10-July-06
+					//this.collectionEventSpecimenId = collectionEventParameters.getSpecimen().getId().longValue();
+					this.collectionEventUserId = collectionEventParameters.getUser().getId().longValue();					
+				
+				 	calender.setTime(collectionEventParameters.getTimestamp());
+					this.collectionEventdateOfEvent = Utility.parseDateToString(collectionEventParameters.getTimestamp(),Constants.DATE_PATTERN_MM_DD_YYYY);
+					this.collectionEventTimeInHours = Utility.toString(Integer.toString( calender.get(Calendar.HOUR_OF_DAY)));
+					this.collectionEventTimeInMinutes  = Utility.toString(Integer.toString(calender.get(Calendar.MINUTE)));
+					this.collectionEventCollectionProcedure = collectionEventParameters.getCollectionProcedure();
+					this.collectionEventContainer = collectionEventParameters.getContainer();
+					this.collectionEventComments = collectionEventParameters.getComments();
+				}
+				else if(tempObj instanceof ReceivedEventParameters)
+				{
+					ReceivedEventParameters receivedEventParameters = (ReceivedEventParameters)tempObj;
+					
+					calender.setTime(receivedEventParameters.getTimestamp());
+					this.receivedEventId = receivedEventParameters.getId().longValue();
+				//	this.receivedEventSpecimenId = receivedEventParameters.getSpecimen().getId().longValue();
+					this.receivedEventUserId = receivedEventParameters.getUser().getId().longValue();
+					this.receivedEventDateOfEvent = Utility.parseDateToString(receivedEventParameters.getTimestamp(),Constants.DATE_PATTERN_MM_DD_YYYY);
+					this.receivedEventTimeInHours = Utility.toString(Integer.toString( calender.get(Calendar.HOUR_OF_DAY)));
+					this.receivedEventTimeInMinutes = Utility.toString(Integer.toString(calender.get(Calendar.MINUTE)));
+					this.receivedEventReceivedQuality = receivedEventParameters.getReceivedQuality();
+					this.receivedEventComments = receivedEventParameters.getComments();
+				}
+			}
+		}
+	}
 	  
 	/* (non-Javadoc)
 	 * @see edu.wustl.catissuecore.actionForm.AbstractActionForm#getFormId()
@@ -500,25 +595,45 @@ public class SpecimenCollectionGroupForm extends AbstractActionForm
 				// here this field will be alltogether disabled
 				// No need of any condition.
 			}
+			/**
+	 * Name : Ashish Gupta
+	 * Reviewer Name : Sachin Lale 
+	 * Bug ID: 2741
+	 * Patch ID: 2741_4	 
+	 * Description: Methods for validation of events in scg
+	*/
+//			CollectionEvent validation.
+    		EventsUtil.validateCollectionEvent(errors,validator,collectionEventUserId,collectionEventdateOfEvent,collectionEventCollectionProcedure);
+    		//ReceivedEvent validation
+    		EventsUtil.validateReceivedEvent(errors,validator,receivedEventUserId,receivedEventDateOfEvent,receivedEventReceivedQuality );
 			
 			//Added by Ashish for Multiple Specimens
 			/**
- * Name : Ashish Gupta
- * Reviewer Name : Sachin Lale 
- * Bug ID: Multiple Specimen Bug
- * Patch ID: Multiple Specimen Bug_4 
- * See also: 1-8
- * Description: Remove the page on which number of multiple specimens are entered while going to multiple specimen page.
-	*/
+			 * Name : Ashish Gupta
+			 * Reviewer Name : Sachin Lale 
+			 * Bug ID: Multiple Specimen Bug
+			 * Patch ID: Multiple Specimen Bug_4 
+			 * See also: 1-8
+			 * Description: Remove the page on which number of multiple specimens are entered while going to multiple specimen page.
+			*/
 			String buttonName = request.getParameter("button");
 
 			if(buttonName != null && !buttonName.equals(""))
 			{
 				if(numberOfSpecimens < 1)
-		        {
-		        	errors.add(ActionErrors.GLOBAL_ERROR,new ActionError("errors.multiplespecimen.minimumspecimen"));
+		        {	
+					setNumberOfSpecimens(1);
+		        	errors.add(ActionErrors.GLOBAL_ERROR,new ActionError("errors.multiplespecimen.minimumspecimen"));		        	
 		        }
 			}
+			request.getSession().setAttribute("scgForm", this);
+			//For setting whether to set specimen 
+			String applyToString = request.getParameter("applyToSpecimenValue");
+			if(applyToString != null && applyToString.equals("true") )
+			{
+				applyEventsToSpecimens = true;
+			}
+			
 		}
 		catch (Exception excp)
 		{
@@ -557,6 +672,400 @@ public class SpecimenCollectionGroupForm extends AbstractActionForm
             setCheckedButton(2);
         }
     }
+
+	
+	/**
+	 * @return the collectionEventCollectionProcedure
+	 */
+	public String getCollectionEventCollectionProcedure()
+	{
+		return collectionEventCollectionProcedure;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventCollectionProcedure the collectionEventCollectionProcedure to set
+	 */
+	public void setCollectionEventCollectionProcedure(String collectionEventCollectionProcedure)
+	{
+		this.collectionEventCollectionProcedure = collectionEventCollectionProcedure;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventComments
+	 */
+	public String getCollectionEventComments()
+	{
+		return collectionEventComments;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventComments the collectionEventComments to set
+	 */
+	public void setCollectionEventComments(String collectionEventComments)
+	{
+		this.collectionEventComments = collectionEventComments;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventContainer
+	 */
+	public String getCollectionEventContainer()
+	{
+		return collectionEventContainer;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventContainer the collectionEventContainer to set
+	 */
+	public void setCollectionEventContainer(String collectionEventContainer)
+	{
+		this.collectionEventContainer = collectionEventContainer;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventdateOfEvent
+	 */
+	public String getCollectionEventdateOfEvent()
+	{
+		return collectionEventdateOfEvent;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventdateOfEvent the collectionEventdateOfEvent to set
+	 */
+	public void setCollectionEventdateOfEvent(String collectionEventdateOfEvent)
+	{
+		this.collectionEventdateOfEvent = collectionEventdateOfEvent;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventId
+	 */
+	public long getCollectionEventId()
+	{
+		return collectionEventId;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventId the collectionEventId to set
+	 */
+	public void setCollectionEventId(long collectionEventId)
+	{
+		this.collectionEventId = collectionEventId;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventSpecimenId
+	 */
+	public long getCollectionEventSpecimenId()
+	{
+		return collectionEventSpecimenId;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventSpecimenId the collectionEventSpecimenId to set
+	 */
+	public void setCollectionEventSpecimenId(long collectionEventSpecimenId)
+	{
+		this.collectionEventSpecimenId = collectionEventSpecimenId;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventTimeInHours
+	 */
+	public String getCollectionEventTimeInHours()
+	{
+		return collectionEventTimeInHours;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventTimeInHours the collectionEventTimeInHours to set
+	 */
+	public void setCollectionEventTimeInHours(String collectionEventTimeInHours)
+	{
+		this.collectionEventTimeInHours = collectionEventTimeInHours;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventTimeInMinutes
+	 */
+	public String getCollectionEventTimeInMinutes()
+	{
+		return collectionEventTimeInMinutes;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventTimeInMinutes the collectionEventTimeInMinutes to set
+	 */
+	public void setCollectionEventTimeInMinutes(String collectionEventTimeInMinutes)
+	{
+		this.collectionEventTimeInMinutes = collectionEventTimeInMinutes;
+	}
+
+
+
+	
+	/**
+	 * @return the collectionEventUserId
+	 */
+	public long getCollectionEventUserId()
+	{
+		return collectionEventUserId;
+	}
+
+
+
+	
+	/**
+	 * @param collectionEventUserId the collectionEventUserId to set
+	 */
+	public void setCollectionEventUserId(long collectionEventUserId)
+	{
+		this.collectionEventUserId = collectionEventUserId;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventComments
+	 */
+	public String getReceivedEventComments()
+	{
+		return receivedEventComments;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventComments the receivedEventComments to set
+	 */
+	public void setReceivedEventComments(String receivedEventComments)
+	{
+		this.receivedEventComments = receivedEventComments;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventDateOfEvent
+	 */
+	public String getReceivedEventDateOfEvent()
+	{
+		return receivedEventDateOfEvent;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventDateOfEvent the receivedEventDateOfEvent to set
+	 */
+	public void setReceivedEventDateOfEvent(String receivedEventDateOfEvent)
+	{
+		this.receivedEventDateOfEvent = receivedEventDateOfEvent;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventId
+	 */
+	public long getReceivedEventId()
+	{
+		return receivedEventId;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventId the receivedEventId to set
+	 */
+	public void setReceivedEventId(long receivedEventId)
+	{
+		this.receivedEventId = receivedEventId;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventReceivedQuality
+	 */
+	public String getReceivedEventReceivedQuality()
+	{
+		return receivedEventReceivedQuality;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventReceivedQuality the receivedEventReceivedQuality to set
+	 */
+	public void setReceivedEventReceivedQuality(String receivedEventReceivedQuality)
+	{
+		this.receivedEventReceivedQuality = receivedEventReceivedQuality;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventSpecimenId
+	 */
+	public long getReceivedEventSpecimenId()
+	{
+		return receivedEventSpecimenId;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventSpecimenId the receivedEventSpecimenId to set
+	 */
+	public void setReceivedEventSpecimenId(long receivedEventSpecimenId)
+	{
+		this.receivedEventSpecimenId = receivedEventSpecimenId;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventTimeInHours
+	 */
+	public String getReceivedEventTimeInHours()
+	{
+		return receivedEventTimeInHours;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventTimeInHours the receivedEventTimeInHours to set
+	 */
+	public void setReceivedEventTimeInHours(String receivedEventTimeInHours)
+	{
+		this.receivedEventTimeInHours = receivedEventTimeInHours;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventTimeInMinutes
+	 */
+	public String getReceivedEventTimeInMinutes()
+	{
+		return receivedEventTimeInMinutes;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventTimeInMinutes the receivedEventTimeInMinutes to set
+	 */
+	public void setReceivedEventTimeInMinutes(String receivedEventTimeInMinutes)
+	{
+		this.receivedEventTimeInMinutes = receivedEventTimeInMinutes;
+	}
+
+
+
+	
+	/**
+	 * @return the receivedEventUserId
+	 */
+	public long getReceivedEventUserId()
+	{
+		return receivedEventUserId;
+	}
+
+
+
+	
+	/**
+	 * @param receivedEventUserId the receivedEventUserId to set
+	 */
+	public void setReceivedEventUserId(long receivedEventUserId)
+	{
+		this.receivedEventUserId = receivedEventUserId;
+	}
+
+
+
+	
+	/**
+	 * @return the applyEventsToSpecimens
+	 */
+	public boolean isApplyEventsToSpecimens()
+	{
+		return applyEventsToSpecimens;
+	}
+
+
+
+	
+	/**
+	 * @param applyEventsToSpecimens the applyEventsToSpecimens to set
+	 */
+	public void setApplyEventsToSpecimens(boolean applyEventsToSpecimens)
+	{
+		this.applyEventsToSpecimens = applyEventsToSpecimens;
+	}
 
 
     /**
