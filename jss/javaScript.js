95dcdab382243cdd69f63ca2fbe8cbf6de69e6e2
chetan_patil <chetan_patil>
2007-05-03 12:30:48 +0000

    Fix for Bug#4129
    SVN-Revision: 7841

--- jss/javaScript.js
+++ jss/javaScript.js
@@ -981,3 +981,26 @@ function mapButtonClickedOnSpecimen(frameUrl)
 	frameUrl+="&storageContainerName="+storageContainer;
 	openPopupWindow(frameUrl);
 }
+
+// Patch ID: Bug#4129_4
+// Description: Function to verify the validity of number of specimen value.
+//  check for valid numeric strings	
+function isNumeric(strString)
+{
+	var strValidChars = "0123456789.-";
+	var strChar;
+	var blnResult = true;
+
+	if (strString.length == 0) return false;
+		
+	//  test strString consists of valid characters listed above
+	for (i = 0; i < strString.length && blnResult == true; i++)
+	{
+		strChar = strString.charAt(i);
+		if (strValidChars.indexOf(strChar) == -1)
+		{
+			blnResult = false;
+		}
+	}
+	return blnResult;
+}
