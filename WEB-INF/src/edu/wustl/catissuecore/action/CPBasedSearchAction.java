370b7d7ac90e713cb9e98e98f61682ec1a904b92
deepti_shelar <deepti_shelar>
2007-05-08 10:32:02 +0000

    Removing unnecessary imports
    SVN-Revision: 7882

--- WEB-INF/src/edu/wustl/catissuecore/action/CPBasedSearchAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/CPBasedSearchAction.java
@@ -7,12 +7,9 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import edu.wustl.catissuecore.bizlogic.UserBizLogic;
-import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.action.SecureAction;
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.security.SecurityManager;
-import gov.nih.nci.security.authorization.domainobjects.Role;
 
 /**
  * This Action is for forwarding to CPResultsView Page 
