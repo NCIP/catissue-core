7c8eb34cc1f70fff9235ac40920be3540e451bf7
sachin_lale <sachin_lale>
2007-07-27 05:36:33 +0000

    Test
    SVN-Revision: 8188

--- WEB-INF/src/TestRollBack.java
+++ WEB-INF/src/TestRollBack.java
@@ -28,7 +28,7 @@ public class TestRollBack
 		Logger.out = org.apache.log4j.Logger.getLogger("");
 		PropertyConfigurator.configure(Variables.applicationHome+"\\WEB-INF\\src\\"+"ApplicationResources.properties");
 		
-		Logger.out.debug("here1");
+		Logger.out.debug("here123");
         
         Department dept = new Department();
         dept.setName("ABCD");
