9ddada72bed07d15021907ee429470495831fe41
vijay_pande <vijay_pande>
2007-08-01 11:09:27 +0000

    caTIES related utility classes
    SVN-Revision: 8254

---
+++ WEB-INF/src/edu/wustl/catissuecore/caties/util/StopServer.java
@@ -0,0 +1,57 @@
+package edu.wustl.catissuecore.caties.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import edu.wustl.common.util.logger.Logger;
+
+/**
+ * Common class which is a thread to stop caties servers depending on there port
+ * @author vijay_pande
+ */
+public class StopServer extends Thread 
+{
+	private String port;
+	
+	/**
+	 * Constructor for the class
+	 * @param port key to fetch port number from catissueCore-properties.xml file
+	 */
+	public StopServer(String port)
+	{
+		this.port=port;
+	}
+	/**
+	 * Default method for the thread, which listen to socket and stop current thread when recieve any responce
+	 */
+	public void run()
+	{
+		try
+		{
+			// get port number for server from catissueCore-properties.xml file
+			int port=Integer.parseInt(CaTIESProperties.getValue(this.port));
+			ServerSocket serv = new ServerSocket(port);
+		  	BufferedReader r;
+	    	Socket sock = serv.accept();
+	    	r =new BufferedReader (new InputStreamReader (sock.getInputStream()));
+	    	PrintWriter out = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()),true);
+	    	String str=r.readLine();
+	    	
+	    	Logger.out.info("Stopping server");
+	    	r.close();
+	    	sock.close(); 
+	    	// close server socket
+		    serv.close();
+		    // stop server
+		    System.exit(0);
+		}
+		catch(Exception e)
+		{
+			Logger.out.error("Error stopping server ",e);
+		}
+	}
+}
---
+++ WEB-INF/src/edu/wustl/catissuecore/caties/util/ViewSPRUtil.java
@@ -0,0 +1,76 @@
+package edu.wustl.catissuecore.caties.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.wustl.catissuecore.bean.ConceptHighLightingBean;
+import edu.wustl.catissuecore.domain.pathology.ConceptReferent;
+import edu.wustl.catissuecore.domain.pathology.ConceptReferentClassification;
+import edu.wustl.catissuecore.domain.pathology.DeidentifiedSurgicalPathologyReport;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.util.dbManager.DAOException;
+
+public class ViewSPRUtil 
+{
+	/**
+	 * @param request
+	 * @param deidentifiedSurgicalPathologyReport
+	 */
+	public static List getConceptBeanList(HttpServletRequest request,DeidentifiedSurgicalPathologyReport deidentifiedSurgicalPathologyReport) throws DAOException
+	{		
+		List conceptBeanList = new ArrayList();
+		if(deidentifiedSurgicalPathologyReport != null)
+		{
+			DefaultBizLogic defaultBizLogic=new DefaultBizLogic();
+			Collection conceptReferentColl =(Set)defaultBizLogic.retrieveAttribute(DeidentifiedSurgicalPathologyReport.class.getName(), deidentifiedSurgicalPathologyReport.getId(), Constants.COLUMN_NAME_CONCEPT_REF_COLL); //deidentifiedSurgicalPathologyReport.getConceptReferentCollection();
+			//temporary map to make a list of concept referent classification objects
+			Map tempMap = new HashMap();
+			if(conceptReferentColl != null)
+			{
+				Iterator iter = conceptReferentColl.iterator();
+				while(iter.hasNext())
+				{
+					ConceptReferent conceptReferent = (ConceptReferent)iter.next();
+					ConceptReferentClassification conceptReferentClassification = conceptReferent.getConceptReferentClassification();
+					ConceptHighLightingBean conceptHighLightingBean = null;
+					if(tempMap.get(conceptReferentClassification.getName().toUpperCase()) == null)
+					{	// if concept classification obj is not present in the list						
+						conceptHighLightingBean = new ConceptHighLightingBean();
+						conceptHighLightingBean.setClassificationName(conceptReferentClassification.getName());
+						conceptHighLightingBean.setConceptName(conceptReferent.getConcept().getName());
+						conceptHighLightingBean.setStartOffsets(conceptReferent.getStartOffset().toString());
+						conceptHighLightingBean.setEndOffsets(conceptReferent.getEndOffset().toString());
+						
+						tempMap.put(conceptReferentClassification.getName().toUpperCase(), conceptHighLightingBean);
+					}
+					else
+					{
+						conceptHighLightingBean =(ConceptHighLightingBean) tempMap.get(conceptReferentClassification.getName().toUpperCase());
+						
+						conceptHighLightingBean.setConceptName(conceptHighLightingBean.getConceptName() + "," + conceptReferent.getConcept().getName());
+						conceptHighLightingBean.setStartOffsets(conceptHighLightingBean.getStartOffsets() + "," + conceptReferent.getStartOffset());
+						conceptHighLightingBean.setEndOffsets(conceptHighLightingBean.getEndOffsets() + "," + conceptReferent.getEndOffset());
+						
+						tempMap.put(conceptReferentClassification.getName().toUpperCase(), conceptHighLightingBean);
+					}
+				}
+				
+				Set keySet = tempMap.keySet();
+				Iterator keySetIter = keySet.iterator();
+				while(keySetIter.hasNext())
+				{
+					conceptBeanList.add(tempMap.get(keySetIter.next()));
+				}
+			}
+		}
+		return conceptBeanList;
+	}
+}
