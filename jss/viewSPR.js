ecf3b87c0fbd4869167e975843a138e1da6e3c1c
vijay_pande <vijay_pande>
2007-08-01 10:47:43 +0000

    file for javascript related to view surgical pathology report page
    SVN-Revision: 8238

---
+++ jss/viewSPR.js
@@ -0,0 +1,324 @@
+//<!-- JavaScript for ViewSurigicalPathologyReport.jsp  start -->
+	
+//<!-- function collapse or expand the participant information table   -->
+function switchStyle(action)
+{
+	imageObj = document.getElementById('image');	
+	if(action== 'hide') //Clicked on - image
+	{
+		hide('paricipantInformation');				
+		imageObj.innerHTML = '<img src="images/nolines_plus.gif" border="0" /> ';
+		imageObj.href="javascript:switchStyle('show');";
+	}
+	else  							   //Clicked on + image
+	{
+		show('paricipantInformation');
+		imageObj.innerHTML = '<img src="images/nolines_minus.gif" border="0" /> ';
+		imageObj.href="javascript:switchStyle('hide');";
+	}
+}
+//<!--function to show object on UI -->
+function show(obj)
+{
+	switchObj=document.getElementById(obj);
+	switchObj.style.display = '';
+}
+//<!--function to hide object from UI -->
+function hide(obj)
+{
+	switchObj=document.getElementById(obj);
+	switchObj.style.display='none'
+}
+//<!--function to display deidentified report table on UI -->
+function clickOnLinkShowDeidReport()
+{
+
+	if(document.getElementById('showDeReportChkbox').checked==true)
+	{
+		hide('identifiedReportInfo');
+		switchStyle('hide');
+		show('deidReportInfo');
+
+	}
+	else
+	{
+		hide('deidReportInfo');
+		show('identifiedReportInfo');
+		switchStyle('show');
+	}
+}
+//<!--function to display default view  -->
+function clickOnLinkReport()
+{
+	hide('deidReportInfo');
+	show('reportTable');
+	show('categoryHighlighter');
+	show('identifiedReportInfo');
+	switchStyle('show');
+}
+//<!--function to display identified and de-identified report compare view  -->
+function clickOnLinkCompareReport()
+{
+	switchStyle('hide')
+	show('reportTable');
+	show('identifiedReportInfo');
+	show('deidReportInfo');
+	show('categoryHighlighter');
+}
+//<!--function to display user requests table  -->
+function clickOnLinkMyRequests()
+{
+	
+}
+//<!--function to display dialog box for confirmation of submitting comments-->
+function confirmSubmit()
+{
+	if (confirm('Are you sure you want to Submit Comments?'))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//<!--function to submit rview comments-->
+function submitReviewComments(consentTierCounter)
+{
+	
+	if(confirmSubmit())
+	{
+		document.forms[0].submittedFor.value='review';
+		document.forms[0].forwardTo.value='success';
+		var action="SurgicalPathologyReportEventParam.do?operation=add&consentTierCounter="+consentTierCounter;
+		document.forms[0].action=action;
+		document.forms[0].submit();
+	}
+}
+//<!--function to submit quarantine comments-->
+function submitQuarantineComments(consentTierCounter)
+{
+	if(confirmSubmit())
+	{
+		document.forms[0].submittedFor.value='quarantine';
+		document.forms[0].forwardTo.value='success';
+		var action="SurgicalPathologyReportEventParam.do?operation=add&consentTierCounter="+consentTierCounter;
+		document.forms[0].action=action;
+		document.forms[0].submit();
+	}
+}
+
+function finishReview()
+{
+	if(confirmSubmit())
+	{
+		document.forms[0].submittedFor.value='review';
+		document.forms[0].forwardTo.value='success';
+		var action="SurgicalPathologyReportEventParam.do?operation=edit&requestFor=REVIEW"
+		document.forms[0].action=action;
+		document.forms[0].submit();
+	}
+}
+
+function submitAcceptComments()
+{
+	if(confirmSubmit())
+	{
+		document.forms[0].submittedFor.value='quarantine';
+		document.forms[0].forwardTo.value='success';
+		var action="SurgicalPathologyReportEventParam.do?operation=edit&requestFor=ACCEPT"
+		document.forms[0].acceptReject.value=1;
+		document.forms[0].action=action;
+		document.forms[0].submit();
+	}
+}
+//<!--function to submit quarantine comments-->
+function submitRejectComments()
+{
+	if(confirmSubmit())
+	{
+		document.forms[0].submittedFor.value='quarantine';
+		document.forms[0].forwardTo.value='success';
+		var action="SurgicalPathologyReportEventParam.do?operation=edit&requestFor=REJECT"
+		document.forms[0].acceptReject.value=2;
+		document.forms[0].action=action;
+		document.forms[0].submit();
+		
+	}
+}
+var spanSize=65;
+function replaceString(startOff,innerHtml,subStr,text)
+{
+	var tempStr=innerHtml.substring(0,startOff);
+	var tempArray=tempStr.split("<span");
+	var spanCount=tempArray.size();
+	var total=startOff;
+	if(spanCount>1)
+	{
+		total=startOff+((spanCount-1)*spanSize);
+	}
+
+	var textBeforeString = innerHtml.substring(0,total);
+	var toReplace=innerHtml.substring(total);
+	var textAfterString = toReplace.replace(subStr,text);
+	return (textBeforeString.concat(textAfterString));
+}
+function getNewLineCount(str)
+{
+	var count=0;
+	var tempStr=str.split("\r");
+	count=tempStr.size();
+	return count;
+}
+
+//Added by Ashish
+function selectByOffset(checkbox,start,end,colour,conceptName)
+{
+	if(checkbox.checked==true)
+	{
+		var innerHtml = document.getElementById("deidentifiedReportText").innerHTML;
+		var tempStr = ReplaceTags(innerHtml);
+
+		var startArr = start.split(",");
+		var endArr = end.split(",");
+		var conceptNameArr = conceptName.split(",");
+		var newtext = "";
+		var count = 0;
+		for(var x=0;x<startArr.length;x++)
+		{		
+			var startOff=startArr[x]-1;
+			var endOff=endArr[x]-1;
+			
+			var newLineCount=getNewLineCount(tempStr.substring(0,startOff));
+			startOff=startOff+newLineCount-1;
+			endOff=endOff+newLineCount-1;
+			
+			var subStr = tempStr.substring(startOff,endOff);
+			var textBeforeString = tempStr.substring(0,startOff);
+			var textAfterString = tempStr.substring(endOff);
+			if(trimString(subStr)!="")
+			{
+				if(checkbox.checked==false)
+				{
+					//background color is set to default 'light-gray' color. Refer stylesheet.jss
+					colour='#F4F4F5';
+					//conceptName="";
+				}
+				var text = "<span title="+conceptNameArr[x]+" style='background-color:"+colour+"'>"+subStr+"</span>";
+					
+				if(count == 0)
+				{
+					innerHtml = replaceString(startOff,innerHtml,subStr,text);
+					count++;
+				}
+				else
+				{
+					innerHtml = replaceString(startOff,innerHtml,subStr,text)
+				}	
+			}
+			//newtext = 	textBeforeString + text + textAfterString;
+		}
+		document.getElementById("deidentifiedReportText").innerHTML=innerHtml;
+	}
+	
+}
+function trimString(sInString) {
+  sInString = sInString.replace( /^\s+/g, "" );// strip leading
+  return sInString.replace( /\s+$/g, "" );// strip trailing
+}
+
+var regExp = /<\/?[^>]+>/gi;
+function ReplaceTags(xStr)
+{
+  xStr = xStr.replace(regExp,"");
+  return xStr;
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>> AJAX code start
+	var colours=new Array();
+	var conceptName=new Array();
+	var startOff=new Array();
+	var endOff=new Array();
+	// function to send request to server	
+	var request;
+	function sendRequestForReportInfo()
+	{
+		request = newXMLHTTPReq();			
+		var actionURL;
+		var handlerFunction = getReadyStateHandler(request,setReport,true);	
+		request.onreadystatechange = handlerFunction;				
+		actionURL = "reportId="+document.getElementById('reportId').value;			
+		var url = "FetchReport.do";
+		<!-- Open connection to servlet -->
+		request.open("POST",url,true);	
+		request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");	
+		request.send(actionURL);	
+			
+	}
+//To set the values of form that are fetched using AJAX call
+	function setReport()
+	{
+		if(request.readyState==4 && request.status == 200)
+		{
+			/* Response contains required output.
+			 * Get the response from server.
+			 */				
+			var responseString = request.responseText;
+			
+			if(responseString != null && responseString != "")
+			{	
+				var xmlDocument = getDocumentElementForXML(responseString); 
+				
+				var surgicalPathologyNumber = xmlDocument.getElementsByTagName('SurgicalPathologyNumber')[0].firstChild.nodeValue;	
+				var reportSite = xmlDocument.getElementsByTagName('IdentifiedReportSite')[0].firstChild.nodeValue;			
+				var identifierReportText = xmlDocument.getElementsByTagName('IdentifiedReportTextContent')[0].firstChild.nodeValue;
+				var deIdentifierReportText = xmlDocument.getElementsByTagName('DeIdentifiedReportTextContent')[0].firstChild.nodeValue;
+				var scriptFunction = xmlDocument.getElementsByTagName('JavaScriptFunction')[0].firstChild.nodeValue;
+				
+				document.getElementById("surgicalPathologyNumber").innerHTML = surgicalPathologyNumber;
+				document.getElementById("identifiedReportSite").innerHTML = reportSite;
+				document.getElementById("identifiedReportText").innerHTML = "<PRE>"+identifierReportText+"</PRE>";
+				document.getElementById("deidentifiedReportText").innerHTML = "<PRE>"+deIdentifierReportText+"</PRE>";
+				document.getElementById("deidText").innerHTML = "<PRE>"+deIdentifierReportText+"</PRE>";
+				
+				conceptName=new Array();
+				startOff=new Array();
+				endOff=new Array();
+				var spanText="";
+				for(i=0;i<xmlDocument.getElementsByTagName('ConceptBean').length;i++)
+				{
+					var conceptBeans = xmlDocument.getElementsByTagName('ConceptBean')[i];
+					var classificationName = conceptBeans.getElementsByTagName('ClassificationName')[0].firstChild.nodeValue;
+					conceptName[i] = conceptBeans.getElementsByTagName('ConceptName')[0].firstChild.nodeValue;
+					startOff[i] = conceptBeans.getElementsByTagName('StartOff')[0].firstChild.nodeValue;
+					endOff[i] = conceptBeans.getElementsByTagName('EndOff')[0].firstChild.nodeValue;
+					
+					spanText += "<td class=\"formRequiredLabelWithAllBorder\">";
+					spanText += "<input type=\"checkbox\" id=select"+i+" onclick=\"checkBoxClicked()\" />";
+					spanText += "<span id=\"classificationName\" style="+"background-color:"+colours[i]+">";
+					spanText += classificationName + "</span>";	
+					spanText += "</td>";
+				}
+				document.getElementById("classificationNames").innerHTML = spanText;
+			}
+		}
+	}
+	
+	function getDocumentElementForXML(xmlString)
+	{
+	    var document = null;
+	    if (window.ActiveXObject) // code for IE
+	    {
+	                document = new ActiveXObject("Microsoft.XMLDOM");
+	                document.async="false";
+	                document.loadXML(xmlString);
+	    }
+	    else // code for Mozilla, Firefox, Opera, etc.
+	    {
+	                var parser = new DOMParser();
+	                document = parser.parseFromString(xmlString,"text/xml");
+	    }           
+		return document;
+	}	
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>AJAX code end
+
