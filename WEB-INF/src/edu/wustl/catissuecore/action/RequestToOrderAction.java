17ae193b90a173395ffdf75ea3dec7227e1f66bd
vaishali_khandelwal <vaishali_khandelwal>
2007-08-14 09:50:44 +0000

    bug 5068
    SVN-Revision: 8356

--- WEB-INF/src/edu/wustl/catissuecore/action/RequestToOrderAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/RequestToOrderAction.java
@@ -2,6 +2,7 @@
  * Copyright: Copyright (c) year
  * Company: Washington University, School of Medicine, St. Louis.
  */
+
 package edu.wustl.catissuecore.action;
 
 import java.util.List;
@@ -14,12 +15,16 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.bizlogic.BizLogicFactory;
+import edu.wustl.catissuecore.bizlogic.UserBizLogic;
 import edu.wustl.catissuecore.domain.DistributionProtocol;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.action.BaseAction;
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.bizlogic.IBizLogic;
+import edu.wustl.common.security.SecurityManager;
 import edu.wustl.common.util.dbManager.DAOException;
+import flex.messaging.io.ArrayList;
+import gov.nih.nci.security.authorization.domainobjects.Role;
 
 /**
  * <p>Title: RequestToOrderAction Class>
@@ -44,11 +49,14 @@ public class RequestToOrderAction extends BaseAction
 		//Sets the Distribution Protocol Id List.
 		SessionDataBean sessionLoginInfo = getSessionData(request);
 		Long loggedInUserID = sessionLoginInfo.getUserId();
-		List distributionProtocolList = loadDistributionProtocol(loggedInUserID);
+		long csmUserId = new Long(sessionLoginInfo.getCsmUserId()).longValue();
+		Role role = SecurityManager.getInstance(UserBizLogic.class).getUserRole(csmUserId);
+
+		List distributionProtocolList = loadDistributionProtocol(loggedInUserID, role.getName());
 		request.setAttribute(Constants.DISTRIBUTIONPROTOCOLLIST, distributionProtocolList);
 		return mapping.findForward("requestOrderPage");
 	}
-	
+
 	/**
 	 * This method loads the title as Name and id as value of distribution protocol from database 
 	 * and return the namevalue bean of ditribution protocol for a given PI.
@@ -56,24 +64,33 @@ public class RequestToOrderAction extends BaseAction
 	 * @return Returns the list of namevalue bean of ditribution protocol for a given PI.
 	 * @throws DAOException Throws DAOException if any database releated error occures
 	 **/
-	private List loadDistributionProtocol(final Long piID) throws DAOException
+	private List loadDistributionProtocol(final Long piID, String roleName) throws DAOException
 	{
 		IBizLogic bizLogic = BizLogicFactory.getInstance().getBizLogic(Constants.DEFAULT_BIZ_LOGIC);
+		List distributionProtocolList = new ArrayList();
 
 		String sourceObjectName = DistributionProtocol.class.getName();
 		String[] displayName = {"title"};
 		String valueFieldCol = Constants.ID;
-		String[] whereColumnName = {"principalInvestigator.id"};
-		String[] colCondition = {"="};
-		Object[] whereColumnValue = {piID};
-		String joinCondition = null;
-		String separatorBetweenFields = "";
-		boolean isToExcludeDisabled = true;
 
-		//Get data from database
-		List distributionProtocolList = bizLogic.getList(sourceObjectName, displayName, valueFieldCol, 
-				whereColumnName, colCondition, whereColumnValue, joinCondition, separatorBetweenFields, isToExcludeDisabled);
-		
+		// checking for the role. if role is admin / supervisor then show all the distribution protocols.
+		if (roleName.equals(Constants.ADMINISTRATOR) || roleName.equals(Constants.SUPERVISOR))
+		{
+			distributionProtocolList = bizLogic.getList(sourceObjectName, displayName, valueFieldCol, true);
+		}
+		else
+		{
+			String[] whereColumnName = {"principalInvestigator.id"};
+			String[] colCondition = {"="};
+			Object[] whereColumnValue = {piID};
+			String joinCondition = null;
+			String separatorBetweenFields = "";
+			boolean isToExcludeDisabled = true;
+
+			//Get data from database
+			distributionProtocolList = bizLogic.getList(sourceObjectName, displayName, valueFieldCol, whereColumnName, colCondition,
+					whereColumnValue, joinCondition, separatorBetweenFields, isToExcludeDisabled);
+		}
 		return distributionProtocolList;
 	}
 }
