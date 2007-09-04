6973c18897770074aad78c5b0a1b2610eebb7099
aniket_pandit <aniket_pandit>
2007-09-04 13:39:40 +0000

    DAG constants file
    SVN-Revision: 8531

---
+++ WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGConstant.java
@@ -0,0 +1,13 @@
+package edu.wustl.catissuecore.flex.dag;
+
+public class DAGConstant {
+	public static final String QUERY_OBJECT = "queryObject";
+	public static final String CONSTRAINT_VIEW_NODE ="ConstraintViewNode";
+	public static final String VIEW_ONLY_NODE="ViewOnlyNode";
+	public static final String HTML_STR="HTMLSTR";
+	public static final String EXPRESSION="EXPRESSION";
+	public static final String ADD_LIMIT="Add";
+	public static final String EDIT_LIMIT="Edit";
+	
+	
+}
