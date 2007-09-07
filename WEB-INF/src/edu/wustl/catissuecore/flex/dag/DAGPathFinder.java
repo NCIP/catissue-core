553e2afbdf9757dd8a221a472936bb993dc62b67
aniket_pandit <aniket_pandit>
2007-09-07 07:45:16 +0000

    Code Indentation
    SVN-Revision: 8565

--- WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGPathFinder.java
+++ WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGPathFinder.java
@@ -1,22 +1,15 @@
 package edu.wustl.catissuecore.flex.dag;
 
-import java.io.IOException;
+
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
 import edu.common.dynamicextensions.domaininterface.AssociationInterface;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 import edu.wustl.cab2b.client.ui.query.IPathFinder;
-import edu.wustl.catissuecore.applet.AppletConstants;
-import edu.wustl.catissuecore.applet.AppletServerCommunicator;
-import edu.wustl.catissuecore.applet.model.AppletModelInterface;
-import edu.wustl.catissuecore.applet.model.BaseAppletModel;
-import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.querysuite.metadata.associations.IInterModelAssociation;
 import edu.wustl.common.querysuite.metadata.path.ICuratedPath;
 import edu.wustl.common.querysuite.metadata.path.IPath;
@@ -26,11 +19,8 @@ public class DAGPathFinder implements IPathFinder {
 
 	
 	public List<IPath> getAllPossiblePaths(EntityInterface source, EntityInterface destination) {
-		//List<IPath> paths = null;
-		
-		Map inputMap = new HashMap(); 
+		 
 		CommonPathFinder pathFinder = new CommonPathFinder();
-		Map pathsMap = new HashMap<EntityInterface, List<IPath>>(); 
 		List<IPath> allPossiblePaths = pathFinder.getAllPossiblePaths(source, destination);
 		
 		return allPossiblePaths;
