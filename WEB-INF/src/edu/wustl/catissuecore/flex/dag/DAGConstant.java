828e239fda466c1cf58b2176d1708aee18a9e368
aniket_pandit <aniket_pandit>
2007-09-07 07:41:53 +0000

    new constant CONSTRAINT_ONLY_NODE added
    SVN-Revision: 8562

--- WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGConstant.java
+++ WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGConstant.java
@@ -4,6 +4,7 @@ public class DAGConstant {
 	public static final String QUERY_OBJECT = "queryObject";
 	public static final String CONSTRAINT_VIEW_NODE ="ConstraintViewNode";
 	public static final String VIEW_ONLY_NODE="ViewOnlyNode";
+	public static final String CONSTRAINT_ONLY_NODE ="ConstraintOnlyNode";
 	public static final String HTML_STR="HTMLSTR";
 	public static final String EXPRESSION="EXPRESSION";
 	public static final String ADD_LIMIT="Add";
