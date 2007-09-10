b07c336f8f75dfc917d7fe3e37c616a74b792255
baljeet_dhindhwal <baljeet_dhindhwal>
2007-09-10 13:05:51 +0000

    New bean class is added for flex related changes
    SVN-Revision: 8582

---
+++ WEB-INF/src/edu/wustl/catissuecore/bean/CpAndParticipentsBean.java
@@ -0,0 +1,81 @@
+package edu.wustl.catissuecore.bean;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+
+public class CpAndParticipentsBean implements Externalizable
+{
+	private static final long serialVersionUID = 1L;
+	
+	
+    private String name;
+	
+	private String value;
+	
+	/*
+	 * Default Constructor
+	 */
+	public CpAndParticipentsBean()
+	{
+		
+	}
+	
+	public CpAndParticipentsBean(String name, String value)
+	{
+		this.name = name;
+		this.value = value;
+	}
+	
+	
+	/**
+	 * @return Returns the name.
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	
+	/**
+	 * @param name The name to set.
+	 */
+	public void setName(String name)
+	{
+        System.out.println("The name written is:"+name); 
+		this.name = name;
+	}
+
+	
+	/**
+	 * @return Returns the value.
+	 */
+	public String getValue()
+	{
+		return value;
+	}
+
+	
+	/**
+	 * @param value The value to set.
+	 */
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+	
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException 
+	{
+		name=in.readUTF();
+	}
+ 	
+	public void writeExternal(ObjectOutput out) throws IOException
+	{
+		System.out.println("In Cp & Participants bean class");
+		System.out.println("Cp written is:"+ name);
+		out.writeUTF(name);
+		out.writeUTF(value);
+		
+	}
+}
