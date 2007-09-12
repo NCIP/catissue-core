a02f357ee914fa1de3cb0c6fefb36f7ff37a1b9d
virender_mehta <virender_mehta>
2007-09-12 10:34:18 +0000

    Committing CPRE bean
    SVN-Revision: 8633

---
+++ WEB-INF/src/edu/wustl/catissuecore/bean/CollectionProtocolEventBean.java
@@ -0,0 +1,163 @@
+package edu.wustl.catissuecore.bean;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class CollectionProtocolEventBean implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	private String uniqueIdentifier;
+	private Double studyCalenderEventPoint;
+	private String collectionPointLabel;
+	private String clinicalDiagnosis;
+	private String clinicalStatus;
+	private String collectionProcedure;
+	private String collectionContainer;
+	private String receivedQuality;
+	private String collectedEventComments;
+	private String receivedEventComments;
+	private Map specimenRequirementbeanMap = new LinkedHashMap();
+	
+	
+	public Double getStudyCalenderEventPoint()
+	{
+		return studyCalenderEventPoint;
+	}
+	
+	public void setStudyCalenderEventPoint(Double studyCalenderEventPoint)
+	{
+		this.studyCalenderEventPoint = studyCalenderEventPoint;
+	}
+	
+	public String getCollectionPointLabel()
+	{
+		return collectionPointLabel;
+	}
+	
+	public void setCollectionPointLabel(String collectionPointLabel)
+	{
+		this.collectionPointLabel = collectionPointLabel;
+	}
+	
+	public String getClinicalDiagnosis()
+	{
+		return clinicalDiagnosis;
+	}
+	
+	public void setClinicalDiagnosis(String clinicalDiagnosis)
+	{
+		this.clinicalDiagnosis = clinicalDiagnosis;
+	}
+	
+	public String getClinicalStatus()
+	{
+		return clinicalStatus;
+	}
+	
+	public void setClinicalStatus(String clinicalStatus)
+	{
+		this.clinicalStatus = clinicalStatus;
+	}
+
+	
+	public String getCollectionProcedure()
+	{
+		return collectionProcedure;
+	}
+
+	
+	public void setCollectionProcedure(String collectionProcedure)
+	{
+		this.collectionProcedure = collectionProcedure;
+	}
+
+	
+	public String getCollectionContainer()
+	{
+		return collectionContainer;
+	}
+
+	
+	public void setCollectionContainer(String collectionContainer)
+	{
+		this.collectionContainer = collectionContainer;
+	}
+
+	
+	public String getReceivedQuality()
+	{
+		return receivedQuality;
+	}
+
+	
+	public void setReceivedQuality(String receivedQuality)
+	{
+		this.receivedQuality = receivedQuality;
+	}
+
+	
+	public String getCollectedEventComments()
+	{
+		return collectedEventComments;
+	}
+
+	
+	public void setCollectedEventComments(String collectedEventComments)
+	{
+		this.collectedEventComments = collectedEventComments;
+	}
+
+	
+	public String getReceivedEventComments()
+	{
+		return receivedEventComments;
+	}
+
+	
+	public void setReceivedEventComments(String receivedEventComments)
+	{
+		this.receivedEventComments = receivedEventComments;
+	}
+
+	
+	public Map getSpecimenRequirementbeanMap()
+	{
+		if(specimenRequirementbeanMap==null)
+			specimenRequirementbeanMap = new LinkedHashMap();
+		return specimenRequirementbeanMap;
+	}
+
+	
+	public void setSpecimenRequirementbeanMap(Map specimenRequirementbeanMap)
+	{
+		this.specimenRequirementbeanMap = specimenRequirementbeanMap;
+	}
+
+	public void addSpecimenRequirementBean(SpecimenRequirementBean specimenRequirementBean)
+	{
+		if(specimenRequirementbeanMap==null)
+		{
+			specimenRequirementbeanMap = new LinkedHashMap<String,SpecimenRequirementBean>();
+		}
+		specimenRequirementbeanMap.put(specimenRequirementBean.getUniqueIdentifier(), specimenRequirementBean);
+	}
+
+	
+	public String getUniqueIdentifier()
+	{
+		return uniqueIdentifier;
+	}
+
+	
+	public void setUniqueIdentifier(String uniqueIdentifier)
+	{
+		this.uniqueIdentifier = uniqueIdentifier;
+	}
+	
+		public static long getSerialVersionUID()
+	{
+		return serialVersionUID;
+	}	
+}
