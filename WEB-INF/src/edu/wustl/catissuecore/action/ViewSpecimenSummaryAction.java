4fd02e813fd47967f39fb887731400056c2e20c9
virender_mehta <virender_mehta>
2007-09-12 09:08:14 +0000

    Removed extra code
    SVN-Revision: 8624

--- WEB-INF/src/edu/wustl/catissuecore/action/ViewSpecimenSummaryAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ViewSpecimenSummaryAction.java
@@ -1,6 +1,5 @@
 package edu.wustl.catissuecore.action;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
@@ -14,22 +13,18 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.actionForm.ViewSpecimenSummaryForm;
-import edu.wustl.catissuecore.bean.MultipleSpecimenList;
 import edu.wustl.catissuecore.bean.GenericSpecimen;
-import edu.wustl.catissuecore.domain.Specimen;
 import edu.wustl.catissuecore.util.global.Constants;
-import edu.wustl.common.action.BaseAction;
-import edu.wustl.common.action.SecureAction;
 
-public class ViewSpecimenSummaryAction extends Action {
+public class ViewSpecimenSummaryAction extends Action 
+{
 
-	
-
-	
-	public ActionForward execute(ActionMapping mapping,
-			ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-try{
+	public ActionForward execute(ActionMapping mapping,ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception 
+	{
+		
+		try
+		{
 		
 		HttpSession session = request.getSession();
 		ViewSpecimenSummaryForm summaryForm = (ViewSpecimenSummaryForm) form;
@@ -44,11 +39,14 @@ try{
 
 		
 		LinkedHashMap<String, GenericSpecimen> specimenMap;
-		if (eventId != null) {
+		if (eventId != null)
+		{
 			specimenMap = (LinkedHashMap) session
 					.getAttribute(Constants.COLLECTION_PROTOCOL_httpSession_BEAN);
 
-		} else {
+		}
+		else 
+		{
 			specimenMap = (LinkedHashMap) session
 					.getAttribute(Constants.SPECIMEN_LIST_httpSession_BEAN);
 		}
@@ -62,9 +60,9 @@ try{
 			
 			if (selectedSpecimen != null){
 				HashMap<String, GenericSpecimen> aliqutesList = selectedSpecimen
-						.getAliquotes();
+						.getAliquotSpecimenCollection();
 				HashMap<String, GenericSpecimen> derivedList = selectedSpecimen
-						.getDerived();
+						.getDeriveSpecimenCollection();
 				if(aliqutesList != null){
 					summaryForm.setAliquoteList(aliqutesList.values());
 				}
@@ -77,160 +75,13 @@ try{
 		summaryForm.setEventId(eventId);
 		
 		return mapping.findForward(Constants.SUCCESS);
-}catch(Exception e)
+}
+catch(Exception e)
 {
 	e.printStackTrace();
 	throw e;
 }
-	}
+}
 
-	private void testData(HttpSession session) {
-		MultipleSpecimenList specimenList = new MultipleSpecimenList() {
 
-			private LinkedHashMap<String, GenericSpecimen> list = 
-					new LinkedHashMap<String, GenericSpecimen>();
-
-			{
-				GenericSpecimen spec1 =getspecimenObject("");
-				GenericSpecimen spec2 =getspecimenObject("");
-				GenericSpecimen spec3 =getspecimenObject("");
-				GenericSpecimen spec4 =getspecimenObject("");
-
-				GenericSpecimen aliq1 =getspecimenObject(spec1.getSpecimenLabel());
-				GenericSpecimen aliq2 =getspecimenObject(spec1.getSpecimenLabel());
-				GenericSpecimen aliq3 =getspecimenObject(spec1.getSpecimenLabel());
-				GenericSpecimen aliq4 =getspecimenObject(spec1.getSpecimenLabel());
-
-				GenericSpecimen deri1 =getspecimenObject(spec1.getSpecimenLabel());
-				GenericSpecimen deri2 =getspecimenObject(spec1.getSpecimenLabel());
-				
-				this.addSpecimen(spec1.getUniqueIdentifier(), spec1);
-				this.addSpecimen(spec2.getUniqueIdentifier(), spec2);
-				this.addSpecimen(spec3.getUniqueIdentifier(), spec3);
-				this.addSpecimen(spec4.getUniqueIdentifier(), spec4);
-
-				LinkedHashMap<String, GenericSpecimen> al = new LinkedHashMap<String, GenericSpecimen>();
-				al.put(aliq1.getUniqueIdentifier(), aliq1);
-				al.put(aliq2.getUniqueIdentifier(), aliq2);
-				al.put(aliq3.getUniqueIdentifier(), aliq3);
-				al.put(aliq4.getUniqueIdentifier(), aliq4);
-				
-				((SpecimenTest)spec1).setAliquots(al);
-	
-				LinkedHashMap<String, GenericSpecimen> dr = new LinkedHashMap<String, GenericSpecimen>();
-				dr.put(deri1.getUniqueIdentifier(), deri1);
-				dr.put(deri2.getUniqueIdentifier(), deri2);
-				((SpecimenTest)spec1).setDeriveds(dr);
-				
-			}
-			
-			public LinkedHashMap<String, GenericSpecimen> getSpecimenList(
-					String eventKey) {
-				
-				return list;
-			}
-			
-			public void addSpecimen(String key, GenericSpecimen spec){
-				list.put(key, spec);
-				System.out.println(list.size());
-			}
-		};
-		
-		session.setAttribute(Constants.COLLECTION_PROTOCOL_httpSession_BEAN, specimenList);
-		
-	}
-	private static int id=1;
-	
-	public GenericSpecimen getspecimenObject(String q) {
-		
-		GenericSpecimen tmp = new SpecimenTest(q);
-		return tmp;
-	}
-
-	class SpecimenTest implements GenericSpecimen{
-		String specName = "specimen" + id++;
-		String parentName = null;
-		LinkedHashMap<String, GenericSpecimen> aliquotList = null;
-		LinkedHashMap<String, GenericSpecimen> derivedList = null;
-		public SpecimenTest(String parentName){
-			this.parentName = parentName;
-		}
-		public void setAliquots(LinkedHashMap<String, GenericSpecimen> al){
-			aliquotList = al;
-		}
-		public void setDeriveds(LinkedHashMap<String, GenericSpecimen> dr){
-			derivedList = dr;
-		}
-
-		
-		public LinkedHashMap<String, GenericSpecimen> getAliquotes() {
-			return aliquotList;
-		}
-
-		
-		public String getConcentration() {
-			return "";
-		}
-
-		
-		public LinkedHashMap<String, GenericSpecimen> getDerived() {
-			return derivedList;
-		}
-
-		
-		public String getPathologyStatus() {
-			return "Malignant";
-		}
-
-		
-		public String getQuantity() {
-			return "13";
-		}
-
-		
-		public String getSpecimenClassName() {
-			return "Tissue";
-		}
-
-		
-		public String getSpecimenLabel() {
-			return specName;
-		}
-
-		
-		public String getSpecimenType() {
-			return "Fix tissue";
-		}
-
-		
-		public String getStorage() {
-			return "";
-		}
-
-		
-		public String getTissueSide() {
-			return "Not specified";
-		}
-
-		
-		public String getTissueSite() {
-			return "Not specified";
-		}
-
-		
-		public String getUniqueIdentifier() {
-			return specName;
-		}
-
-		
-		public String getParentName() {
-			return this.parentName;
-		}
-		
-		public void setSpecimenLabel(String label) {
-			specName = label;
-			
-		}
-		
-	}
 }
