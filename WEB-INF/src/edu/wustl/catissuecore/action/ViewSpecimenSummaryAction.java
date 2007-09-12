04b3ef604978d1c5a21a4d870da79b05cf50eb3d
virender_mehta <virender_mehta>
2007-09-12 10:26:38 +0000

    Modified action
    SVN-Revision: 8631

--- WEB-INF/src/edu/wustl/catissuecore/action/ViewSpecimenSummaryAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/ViewSpecimenSummaryAction.java
@@ -2,86 +2,77 @@ package edu.wustl.catissuecore.action;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.actionForm.ViewSpecimenSummaryForm;
+import edu.wustl.catissuecore.bean.CollectionProtocolEventBean;
 import edu.wustl.catissuecore.bean.GenericSpecimen;
 import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.action.BaseAction;
 
-public class ViewSpecimenSummaryAction extends Action 
+public class ViewSpecimenSummaryAction extends BaseAction 
 {
-
-	public ActionForward execute(ActionMapping mapping,ActionForm form, HttpServletRequest request,
+	public ActionForward executeAction(ActionMapping mapping,ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception 
 	{
-		
 		try
 		{
-		
-		HttpSession session = request.getSession();
-		ViewSpecimenSummaryForm summaryForm = (ViewSpecimenSummaryForm) form;
-		String eventId = summaryForm.getEventId();
-		if(eventId==null){
-			//testData(session);
-			eventId = (String) request.getParameter("Event_Id");
-		}
-		System.out.println("Action called with event id "+ eventId);
-		System.out.println("Action called with Selection id "+ 
-				summaryForm.getSelectedSpecimenId());
-
-		
-		LinkedHashMap<String, GenericSpecimen> specimenMap;
-		if (eventId != null)
-		{
-			specimenMap = (LinkedHashMap) session
-					.getAttribute(Constants.COLLECTION_PROTOCOL_httpSession_BEAN);
-
-		}
-		else 
-		{
-			specimenMap = (LinkedHashMap) session
-					.getAttribute(Constants.SPECIMEN_LIST_httpSession_BEAN);
-		}
-
-		summaryForm.setSpecimenList( specimenMap.values());
-//		summaryForm.setSelectedSpecimenId("specimen1");
-		String selectedSpecimenId = summaryForm.getSelectedSpecimenId();
-		System.out.println(selectedSpecimenId);
-		if (selectedSpecimenId != null) {
-			GenericSpecimen selectedSpecimen = specimenMap.get(selectedSpecimenId);
+			HttpSession session = request.getSession();
+			ViewSpecimenSummaryForm summaryForm = (ViewSpecimenSummaryForm) form;
+			String eventId = summaryForm.getEventId();
+			if(eventId==null)
+			{
+				eventId = (String) request.getParameter("Event_Id");
+			}
+					
+			LinkedHashMap<String, GenericSpecimen> specimenMap;
+			if (eventId != null)
+			{
+				Map collectionProtocolEventMap = (Map)session.getAttribute("collectionProtocolEventMap");
+				CollectionProtocolEventBean collectionProtocolEventBean =(CollectionProtocolEventBean)collectionProtocolEventMap.get(eventId);
+				specimenMap = (LinkedHashMap)collectionProtocolEventBean.getSpecimenRequirementbeanMap();
+			}
+			else 
+			{
+				specimenMap = (LinkedHashMap) session.getAttribute(Constants.SPECIMEN_LIST_httpSession_BEAN);
+			}
+	
+			summaryForm.setSpecimenList(specimenMap.values());
+			String selectedSpecimenId = summaryForm.getSelectedSpecimenId();
 			
-			if (selectedSpecimen != null){
-				HashMap<String, GenericSpecimen> aliqutesList = selectedSpecimen
-						.getAliquotSpecimenCollection();
-				HashMap<String, GenericSpecimen> derivedList = selectedSpecimen
-						.getDeriveSpecimenCollection();
-				if(aliqutesList != null){
-					summaryForm.setAliquoteList(aliqutesList.values());
-				}
-				if(derivedList != null){				
-					summaryForm.setDerivedList(derivedList.values());
+			if (selectedSpecimenId != null) 
+			{
+				GenericSpecimen selectedSpecimen = specimenMap.get(selectedSpecimenId);
+				if (selectedSpecimen != null)
+				{
+					HashMap<String, GenericSpecimen> aliqutesList = selectedSpecimen.getAliquotSpecimenCollection();
+					HashMap<String, GenericSpecimen> derivedList = selectedSpecimen.getDeriveSpecimenCollection();
+					if(aliqutesList != null)
+					{
+						summaryForm.setAliquoteList(aliqutesList.values());
+					}
+					if(derivedList != null)
+					{				
+						summaryForm.setDerivedList(derivedList.values());
+					}
 				}
 			}
+			summaryForm.setEventId(eventId);
+			return mapping.findForward(Constants.SUCCESS);
 		}
-
-		summaryForm.setEventId(eventId);
-		
-		return mapping.findForward(Constants.SUCCESS);
-}
-catch(Exception e)
-{
-	e.printStackTrace();
-	throw e;
-}
-}
-
-
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
 }
