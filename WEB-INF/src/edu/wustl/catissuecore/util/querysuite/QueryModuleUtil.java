66a42abac168c6d5bbc85a74b9998bdcb04e62c7
chetan_patil <chetan_patil>
2007-09-18 08:09:11 +0000

    Added method to execute query
    SVN-Revision: 8763

--- WEB-INF/src/edu/wustl/catissuecore/util/querysuite/QueryModuleUtil.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/querysuite/QueryModuleUtil.java
@@ -4,20 +4,32 @@ package edu.wustl.catissuecore.util.querysuite;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 import edu.wustl.catissuecore.actionForm.CategorySearchForm;
+import edu.wustl.catissuecore.applet.AppletConstants;
+import edu.wustl.catissuecore.bizlogic.querysuite.QueryOutputSpreadsheetBizLogic;
+import edu.wustl.catissuecore.bizlogic.querysuite.QueryOutputTreeBizLogic;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Utility;
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.dao.JDBCDAO;
 import edu.wustl.common.dao.QuerySessionData;
+import edu.wustl.common.querysuite.exceptions.MultipleRootsException;
+import edu.wustl.common.querysuite.exceptions.SqlException;
+import edu.wustl.common.querysuite.factory.SqlGeneratorFactory;
+import edu.wustl.common.querysuite.queryengine.impl.SqlGenerator;
+import edu.wustl.common.querysuite.queryobject.IQuery;
 import edu.wustl.common.querysuite.queryobject.impl.OutputTreeDataNode;
 import edu.wustl.common.querysuite.queryobject.impl.metadata.QueryOutputTreeAttributeMetadata;
+import edu.wustl.common.querysuite.queryobject.util.QueryObjectProcessor;
+import edu.wustl.common.tree.QueryTreeNodeData;
+import edu.wustl.common.util.XMLPropertyHandler;
 import edu.wustl.common.util.dbManager.DAOException;
 
 /**
@@ -35,7 +47,8 @@ public abstract class QueryModuleUtil
 	 * @throws ClassNotFoundException 
 	 * @throws DAOException 
 	 */
-	public static List<List<String>> executeQuery(String selectSql, SessionDataBean sessionData) throws ClassNotFoundException, DAOException
+	public static List<List<String>> executeQuery(String selectSql, SessionDataBean sessionData)
+			throws ClassNotFoundException, DAOException
 	{
 		JDBCDAO dao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
 		List<List<String>> dataList = new ArrayList<List<String>>();
@@ -59,7 +72,8 @@ public abstract class QueryModuleUtil
 	 * @param sessionData session data.
 	 * @throws DAOException DAOException 
 	 */
-	public static void executeCreateTable(String tableName, String createTableSql, SessionDataBean sessionData) throws DAOException
+	public static void executeCreateTable(String tableName, String createTableSql,
+			SessionDataBean sessionData) throws DAOException
 	{
 		JDBCDAO jdbcDao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
 		try
@@ -79,6 +93,7 @@ public abstract class QueryModuleUtil
 			jdbcDao.closeSession();
 		}
 	}
+
 	/**
 	 * Takes data from the map and generates out put data accordingly so that spreadsheet will be updated.
 	 * @param spreadSheetDatamap map which holds data for columns and records.
@@ -87,18 +102,19 @@ public abstract class QueryModuleUtil
 	 */
 	public static String prepareOutputSpreadsheetDataString(Map spreadSheetDatamap)
 	{
-		List<List<String>> dataList = (List<List<String>>) spreadSheetDatamap.get(Constants.SPREADSHEET_DATA_LIST);
-		
+		List<List<String>> dataList = (List<List<String>>) spreadSheetDatamap
+				.get(Constants.SPREADSHEET_DATA_LIST);
+
 		String outputSpreadsheetDataStr = "";
 		String dataStr = "";
 		for (List<String> row : dataList)
 		{
-			StringBuffer gridStrBuff  = new StringBuffer();
-			for(Object columnData : row)
+			StringBuffer gridStrBuff = new StringBuffer();
+			for (Object columnData : row)
 			{
-				Object gridObj = (Object)Utility.toNewGridFormat(columnData);
+				Object gridObj = (Object) Utility.toNewGridFormat(columnData);
 				String gridStr = gridObj.toString();
-				gridStrBuff.append(gridStr+",");
+				gridStrBuff.append(gridStr + ",");
 			}
 			dataStr = dataStr + "|" + gridStrBuff.toString();
 		}
@@ -109,6 +125,7 @@ public abstract class QueryModuleUtil
 		outputSpreadsheetDataStr = columns + "&" + dataStr;
 		return outputSpreadsheetDataStr;
 	}
+
 	/**
 	 * Returns SQL for root node in tree.
 	 * @param root root node of the tree
@@ -118,7 +135,8 @@ public abstract class QueryModuleUtil
 	public static String getSQLForRootNode(OutputTreeDataNode root, String tableName)
 	{
 		String columnNames = getColumnNamesForSelectpart(root);
-		String indexStr = columnNames.substring(columnNames.lastIndexOf(";") + 1, columnNames.length());
+		String indexStr = columnNames.substring(columnNames.lastIndexOf(";") + 1, columnNames
+				.length());
 		int index = -1;
 		if (!indexStr.equalsIgnoreCase("null"))
 		{
@@ -129,14 +147,16 @@ public abstract class QueryModuleUtil
 			columnNames = columnNames.substring(0, columnNames.lastIndexOf(";"));
 		}
 		String idColumnName = columnNames;
-		if(columnNames.indexOf(",") != -1)
+		if (columnNames.indexOf(",") != -1)
 		{
-			idColumnName = columnNames.substring(0,columnNames.indexOf(","));
+			idColumnName = columnNames.substring(0, columnNames.indexOf(","));
 		}
-		String selectSql = "select distinct " + columnNames + " from " + tableName +" where "+idColumnName +" is not null";
+		String selectSql = "select distinct " + columnNames + " from " + tableName + " where "
+				+ idColumnName + " is not null";
 		selectSql = selectSql + Constants.NODE_SEPARATOR + index;
 		return selectSql;
 	}
+
 	/**
 	 * Forms select part of the query.
 	 * @param node Node of Uotput tree .
@@ -150,7 +170,7 @@ public abstract class QueryModuleUtil
 		String displayNameColumnName = null;
 		String index = null;
 		List<QueryOutputTreeAttributeMetadata> attributes = node.getAttributes();
-		for(QueryOutputTreeAttributeMetadata attributeMetaData : attributes)
+		for (QueryOutputTreeAttributeMetadata attributeMetaData : attributes)
 		{
 			AttributeInterface attribute = attributeMetaData.getAttribute();
 			String columnName = attributeMetaData.getColumnName();
@@ -180,6 +200,7 @@ public abstract class QueryModuleUtil
 		columnNames = columnNames + ";" + index;
 		return columnNames;
 	}
+
 	/**
 	 * Returns true if the attribute name can be used to form label for tree node.
 	 * @param attrName String
@@ -198,6 +219,7 @@ public abstract class QueryModuleUtil
 		}
 		return false;
 	}
+
 	/**
 	 * This is used to set the default selections for the UI components when the screen is loaded for the first time.
 	 * @param actionForm form bean
@@ -225,22 +247,24 @@ public abstract class QueryModuleUtil
 		actionForm.setSelected("text_radioButton");
 		return actionForm;
 	}
-	
+
 	/**
 	 * When passes treeNumber , this method returns the root node of that tree. 
 	 * @param rootOutputTreeNodeList tree deta
 	 * @param treeNo number of tree
 	 * @return root node of the tree
 	 */
-	public static OutputTreeDataNode getRootNodeOfTree(List<OutputTreeDataNode> rootOutputTreeNodeList,String treeNo)
+	public static OutputTreeDataNode getRootNodeOfTree(
+			List<OutputTreeDataNode> rootOutputTreeNodeList, String treeNo)
 	{
-		for(OutputTreeDataNode node :rootOutputTreeNodeList)
+		for (OutputTreeDataNode node : rootOutputTreeNodeList)
 		{
 			if (node.getTreeNo() == new Integer(treeNo).intValue())
 				return node;
 		}
 		return null;
 	}
+
 	/**
 	 * Returns column name of nodes id when passed a node to it 
 	 * @param node {@link OutputTreeDataNode}
@@ -248,13 +272,13 @@ public abstract class QueryModuleUtil
 	 */
 	public static String getParentIdColumnName(OutputTreeDataNode node)
 	{
-		if(node != null)
+		if (node != null)
 		{
 			List<QueryOutputTreeAttributeMetadata> attributes = node.getAttributes();
-			for(QueryOutputTreeAttributeMetadata attributeMetaData : attributes)
+			for (QueryOutputTreeAttributeMetadata attributeMetaData : attributes)
 			{
 				AttributeInterface attribute = attributeMetaData.getAttribute();
-				if(attribute.getName().equalsIgnoreCase(Constants.ID))
+				if (attribute.getName().equalsIgnoreCase(Constants.ID))
 				{
 					String sqlColumnName = attributeMetaData.getColumnName();
 					return sqlColumnName;
@@ -263,7 +287,7 @@ public abstract class QueryModuleUtil
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Sets required data for grid.
 	 * @param request HTTPRequest
@@ -272,25 +296,116 @@ public abstract class QueryModuleUtil
 	public static void setGridData(HttpServletRequest request, Map spreadSheetDatamap)
 	{
 		int pageNum = Constants.START_PAGE;
-		OutputTreeDataNode object = (OutputTreeDataNode)spreadSheetDatamap.get(Constants.CURRENT_SELECTED_OBJECT);
+		OutputTreeDataNode object = (OutputTreeDataNode) spreadSheetDatamap
+				.get(Constants.CURRENT_SELECTED_OBJECT);
 		HttpSession session = request.getSession();
-		session.setAttribute(Constants.CURRENT_SELECTED_OBJECT,object);
-		request.setAttribute(Constants.PAGE_NUMBER,Integer.toString(pageNum));
-		QuerySessionData querySessionData = (QuerySessionData) spreadSheetDatamap.get(Constants.QUERY_SESSION_DATA);
+		session.setAttribute(Constants.CURRENT_SELECTED_OBJECT, object);
+		request.setAttribute(Constants.PAGE_NUMBER, Integer.toString(pageNum));
+		QuerySessionData querySessionData = (QuerySessionData) spreadSheetDatamap
+				.get(Constants.QUERY_SESSION_DATA);
 		int totalNumberOfRecords = querySessionData.getTotalNumberOfRecords();
-		List<List<String>> dataList = (List<List<String>>) spreadSheetDatamap.get(Constants.SPREADSHEET_DATA_LIST);
-		List<List<AttributeInterface>>attributeList = (List<List<AttributeInterface>>)spreadSheetDatamap.get(Constants.ATTRIBUTES);
-		request.setAttribute(Constants.SPREADSHEET_DATA_LIST,dataList);
-		request.setAttribute(Constants.PAGINATION_DATA_LIST,dataList);
+		List<List<String>> dataList = (List<List<String>>) spreadSheetDatamap
+				.get(Constants.SPREADSHEET_DATA_LIST);
+		List<List<AttributeInterface>> attributeList = (List<List<AttributeInterface>>) spreadSheetDatamap
+				.get(Constants.ATTRIBUTES);
+		request.setAttribute(Constants.SPREADSHEET_DATA_LIST, dataList);
+		request.setAttribute(Constants.PAGINATION_DATA_LIST, dataList);
 		List columnsList = (List) spreadSheetDatamap.get(Constants.SPREADSHEET_COLUMN_LIST);
-		if(columnsList != null)
-			session.setAttribute(Constants.SPREADSHEET_COLUMN_LIST,columnsList);
-		session.setAttribute(Constants.TOTAL_RESULTS,new Integer(totalNumberOfRecords));	
+		if (columnsList != null)
+			session.setAttribute(Constants.SPREADSHEET_COLUMN_LIST, columnsList);
+		session.setAttribute(Constants.TOTAL_RESULTS, new Integer(totalNumberOfRecords));
 		session.setAttribute(Constants.QUERY_SESSION_DATA, querySessionData);
 		session.setAttribute(Constants.ATTRIBUTES, attributeList);
-		String pageOf = (String)request.getParameter(Constants.PAGEOF);
-		if(pageOf == null)
+		String pageOf = (String) request.getParameter(Constants.PAGEOF);
+		if (pageOf == null)
 			pageOf = "pageOfQueryModule";
-		request.setAttribute(Constants.PAGEOF,pageOf);
+		request.setAttribute(Constants.PAGEOF, pageOf);
 	}
+
+	/**
+	 * This method extracts query object and forms results for tree and grid.
+	 * @param request HttpRequest
+	 * @param query IQuery
+	 * @throws MultipleRootsException will be thrown when more than one root are present in query
+	 * @throws SqlException error in sql 
+	 * @throws DAOException error in DAO 
+	 * @throws ClassNotFoundException ClassNotFoundException
+	 */
+	public static boolean setResultData(HttpServletRequest request, IQuery query)
+			throws MultipleRootsException, SqlException, DAOException, ClassNotFoundException
+	{
+		HttpSession session = request.getSession();
+		int recordsPerPage = 0;
+		String recordsPerPageSessionValue = (String) session
+				.getAttribute(Constants.RESULTS_PER_PAGE);
+		if (recordsPerPageSessionValue == null)
+		{
+			recordsPerPage = Integer.parseInt(XMLPropertyHandler
+					.getValue(Constants.RECORDS_PER_PAGE_PROPERTY_NAME));
+			session.setAttribute(Constants.RESULTS_PER_PAGE, recordsPerPage + "");
+		}
+		else
+		{
+			recordsPerPage = new Integer(recordsPerPageSessionValue).intValue();
+		}
+
+		session.setAttribute(AppletConstants.QUERY_OBJECT, query);
+
+		SessionDataBean sessionData = (SessionDataBean) request.getSession().getAttribute(
+				Constants.SESSION_DATA);
+		if (sessionData == null)
+		{
+			return false;
+		}
+
+		QueryOutputTreeBizLogic outputTreeBizLogic = new QueryOutputTreeBizLogic();
+		SqlGenerator sqlGenerator = (SqlGenerator) SqlGeneratorFactory.getInstance();
+		String selectSql = sqlGenerator.generateSQL(query);
+		outputTreeBizLogic.createOutputTreeTable(selectSql, sessionData);
+
+		List<OutputTreeDataNode> rootOutputTreeNodeList = sqlGenerator.getRootOutputTreeNodeList();
+		OutputTreeDataNode rootNode = rootOutputTreeNodeList.get(0);
+
+		session.setAttribute(Constants.CURRENT_SELECTED_OBJECT, rootNode);
+		session.setAttribute(Constants.TREE_ROOTS, rootOutputTreeNodeList);
+
+		Long noOfTrees = new Long(rootOutputTreeNodeList.size());
+		session.setAttribute(Constants.NO_OF_TREES, noOfTrees);
+
+		Map<String, OutputTreeDataNode> uniqueIdNodesMap = QueryObjectProcessor
+				.getAllChildrenNodes(rootOutputTreeNodeList);
+		session.setAttribute(Constants.ID_NODES_MAP, uniqueIdNodesMap);
+
+		int i = 0;
+		boolean isZeroRecordsFound = false;
+		for (OutputTreeDataNode node : rootOutputTreeNodeList)
+		{
+			Vector<QueryTreeNodeData> treeData = outputTreeBizLogic.createDefaultOutputTreeData(i,
+					node, sessionData);
+			if (treeData.size() == 0)
+			{
+				isZeroRecordsFound = true;
+			}
+			session.setAttribute(Constants.TREE_DATA + "_" + i++, treeData);
+		}
+
+		OutputTreeDataNode node = rootOutputTreeNodeList.get(0);
+		QueryOutputSpreadsheetBizLogic outputSpreadsheetBizLogic = new QueryOutputSpreadsheetBizLogic();
+		Map<String, List<String>> spreadSheetDatamap = outputSpreadsheetBizLogic
+				.createSpreadsheetData("0", node, sessionData, null, recordsPerPage);
+		QuerySessionData querySessionData = (QuerySessionData) spreadSheetDatamap
+				.get(Constants.QUERY_SESSION_DATA);
+		int totalNumberOfRecords = querySessionData.getTotalNumberOfRecords();
+		session.setAttribute(Constants.QUERY_SESSION_DATA, querySessionData);
+		session.setAttribute(Constants.TOTAL_RESULTS, new Integer(totalNumberOfRecords));
+
+		List<String> list = spreadSheetDatamap.get(Constants.SPREADSHEET_DATA_LIST);
+		session.setAttribute(Constants.PAGINATION_DATA_LIST, list);
+		session.setAttribute(Constants.SPREADSHEET_COLUMN_LIST, spreadSheetDatamap
+				.get(Constants.SPREADSHEET_COLUMN_LIST));
+		session.setAttribute(Constants.SELECTED_COLUMN_META_DATA, null);
+
+		return isZeroRecordsFound;
+	}
+
 }
