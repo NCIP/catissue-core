41388657933a6147029694b30c4805ce9e54e4d6
atul_jawale <atul_jawale>
2007-09-18 08:46:40 +0000

    Action to load selected condition on save query page
    SVN-Revision: 8774

--- WEB-INF/src/edu/wustl/catissuecore/action/querysuite/LoadSaveQueryPageAction.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/querysuite/LoadSaveQueryPageAction.java
@@ -1,4 +1,3 @@
-
 package edu.wustl.catissuecore.action.querysuite;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,21 +9,31 @@ import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.catissuecore.applet.AppletConstants;
 import edu.wustl.catissuecore.bizlogic.querysuite.CreateUIFromQueryObjectBizLogic;
+import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.common.action.BaseAction;
 import edu.wustl.common.querysuite.queryobject.IQuery;
 
-public class LoadSaveQueryPageAction extends BaseAction
-{
-	protected ActionForward executeAction(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception
-	{
+public class LoadSaveQueryPageAction extends BaseAction {
+	@Override
+	protected ActionForward executeAction(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
 		IQuery queryObject = (IQuery) request.getSession().getAttribute(
 				AppletConstants.QUERY_OBJECT);
+		boolean isShowAll = request.getParameter("showall") == null ? false
+				: true;
 		CreateUIFromQueryObjectBizLogic createUIFromQueryObject = new CreateUIFromQueryObjectBizLogic();
-		String htmlContents = createUIFromQueryObject.getHTMLForSavedQuery(queryObject);
+		String htmlContents = createUIFromQueryObject.getHTMLForSavedQuery(
+				queryObject, isShowAll);
 		request.setAttribute("HTMLContents", htmlContents);
-
+		String showAllLink = isShowAll ? Constants.SHOW_SELECTED_ATTRIBUTE
+				: Constants.SHOW_ALL_ATTRIBUTE;
+		request.setAttribute("showAllLink", showAllLink);
+		if (!isShowAll)
+			request.setAttribute("showall", "showall=true");
 		return mapping.findForward("success");
+
 	}
 
 }
