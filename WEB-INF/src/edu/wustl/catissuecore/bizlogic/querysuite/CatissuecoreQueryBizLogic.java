8381c64ec5c57b43dc9779f62ad602b23eadae6b
chetan_patil <chetan_patil>
2007-09-18 08:12:23 +0000

    Bizlogic for query module
    SVN-Revision: 8765

---
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/querysuite/CatissuecoreQueryBizLogic.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+package edu.wustl.catissuecore.bizlogic.querysuite;
+
+import edu.wustl.common.querysuite.bizLogic.QueryBizLogic;
+import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
+
+/**
+ * @author chetan_patil
+ * @created Sep 13, 2007, 7:39:46 PM
+ */
+public class CatissuecoreQueryBizLogic extends QueryBizLogic<IParameterizedQuery>
+{
+
+	/**
+	 * Default Constructor
+	 */
+	public CatissuecoreQueryBizLogic()
+	{
+
+	}
+	
+}
