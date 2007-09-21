9c4bfb6bee615e3d931ef771085d2a7a10bc07b1
deepti_shelar <deepti_shelar>
2007-09-21 16:38:25 +0000

    Adding action class to be called when results in tree are exceeded than the given limit
    SVN-Revision: 8932

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/querysuite/OpenDecisionMakingPageAction.java
@@ -0,0 +1,55 @@
+package edu.wustl.catissuecore.action.querysuite;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.actionForm.CategorySearchForm;
+import edu.wustl.catissuecore.applet.AppletConstants;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.catissuecore.util.querysuite.QueryModuleUtil;
+import edu.wustl.common.action.BaseAction;
+import edu.wustl.common.querysuite.queryobject.IQuery;
+/**
+ * 
+ * @author deepti_shelar
+ *
+ */
+public class OpenDecisionMakingPageAction extends BaseAction
+{
+	/**
+	 * This method loads define results jsp.
+	 * @param mapping mapping
+	 * @param form form
+	 * @param request request
+	 * @param response response
+	 * @throws Exception Exception
+	 * @return ActionForward actionForward
+	 */
+	protected ActionForward executeAction(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+	throws Exception
+	{
+		CategorySearchForm actionForm = (CategorySearchForm)form;
+		HttpSession session = request.getSession();
+		IQuery query = (IQuery)session.getAttribute(AppletConstants.QUERY_OBJECT);
+		String noOfResults = (String)session.getAttribute(Constants.TREE_NODE_LIMIT_EXCEEDED_RECORDS);
+		String option = actionForm.getOptions();
+		
+		if(option != null)
+		{
+			QueryModuleUtil.searchQuery(session, query,option);
+			return mapping.findForward(Constants.VIEW_ALL_RECORDS);
+		}
+		ActionErrors errors = new ActionErrors();
+		ActionError addMsg = new ActionError("query.decision.making.message", noOfResults);
+		errors.add(ActionErrors.GLOBAL_ERROR, addMsg);
+		saveErrors(request, errors);
+		return mapping.findForward(Constants.SUCCESS);
+	}
+}
