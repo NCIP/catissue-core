01ed38fb269dd7a73e7ab6998f8e52855a592931
preeti_munot <preeti_munot>
2007-10-03 09:59:36 +0000

    COde for exporting entity group as XMI
    SVN-Revision: 9198

---
+++ WEB-INF/src/edu/wustl/catissuecore/annotations/xmi/XMIExporter.java
@@ -0,0 +1,78 @@
+/*
+ * Created on Sep 27, 2007
+ * @author
+ *
+ */
+package edu.wustl.catissuecore.annotations.xmi;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import edu.common.dynamicextensions.domaininterface.EntityGroupInterface;
+
+/**
+ * @author preeti_lodha
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class XMIExporter
+{
+	public static final String XMI_VERSION_1_1 = "1.1";
+	public static final String XMI_VERSION_1_2 = "1.2";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+
+		try
+		{
+		if(args.length<3)
+		{
+			throw new Exception("Please specify all parameters. '-Dgroupname <groupname> -Dfilename <export filename> -Dversion <version>'");
+		}
+		String groupName = args[0];
+		if(groupName==null)
+		{
+			throw new Exception("Please specify groupname to be exported");
+		}
+		else
+		{
+			String filename = args[1]; 
+			if(filename==null)
+			{
+				throw new Exception("Kindly specify the filename where XMI should be exported.");
+			}
+			else
+			{
+				String xmiVersion = args[2]; 
+				if(xmiVersion==null)
+				{
+					System.out.println("Export version not specified. Exporting as XMI 1.2");
+					xmiVersion = XMI_VERSION_1_2;
+				}
+				edu.common.dynamicextensions.xmi.exporter.XMIExporter xmiExporter = new edu.common.dynamicextensions.xmi.exporter.XMIExporter();
+				EntityGroupInterface entityGroup = XMIUtility.getEntityGroup(groupName);
+				XMIUtility.addHookEntitiesToGroup(entityGroup);
+				if(entityGroup==null)
+				{
+					throw new Exception("Specified group does not exist. Could not export to XMI");
+				}
+				else
+				{
+					xmiExporter.exportXMI(filename, entityGroup, xmiVersion);
+				}
+			}
+
+		}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ WEB-INF/src/edu/wustl/catissuecore/annotations/xmi/XMIUtility.java
@@ -0,0 +1,211 @@
+/*
+ * Created on Sep 27, 2007
+ * @author
+ *
+ */
+package edu.wustl.catissuecore.annotations.xmi;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import edu.common.dynamicextensions.domain.Entity;
+import edu.common.dynamicextensions.domain.integration.EntityMap;
+import edu.common.dynamicextensions.domaininterface.AssociationInterface;
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
+import edu.common.dynamicextensions.domaininterface.EntityGroupInterface;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
+import edu.common.dynamicextensions.entitymanager.EntityManager;
+import edu.common.dynamicextensions.entitymanager.EntityManagerConstantsInterface;
+import edu.common.dynamicextensions.entitymanager.EntityManagerInterface;
+import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
+import edu.wustl.catissuecore.bizlogic.AnnotationBizLogic;
+import edu.wustl.common.util.dbManager.DAOException;
+
+
+/**
+ * @author preeti_lodha
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class XMIUtility
+{
+	private static EntityManagerInterface entityManager = EntityManager.getInstance();
+	public static EntityGroupInterface getEntityGroup(String groupName) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
+	{
+		if(groupName!=null)
+		{
+			System.out.println("Searching " + groupName);
+			return entityManager.getEntityGroupByName(groupName);
+		}
+		return null;
+	}
+	public static void addHookEntitiesToGroup(EntityGroupInterface entityGroup) throws DAOException, DynamicExtensionsSystemException, DynamicExtensionsApplicationException
+	{
+		ContainerInterface mainContainer = getMainContainer(entityGroup);
+		if(mainContainer!=null)
+		{
+			AnnotationBizLogic annotationBizLogic = new AnnotationBizLogic();
+			System.out.println(mainContainer.getId());
+			Collection entityMapsForContainer = annotationBizLogic.getEntityMapsForContainer(mainContainer.getId());
+			System.out.println("entityMapsForContainer "  +entityMapsForContainer);
+			if(entityMapsForContainer!=null)
+			{
+				Iterator entityMapsForContainerIter = entityMapsForContainer.iterator();
+				//Add all associated static entities to uml model 
+				while(entityMapsForContainerIter.hasNext())
+				{
+					Object entityMap = entityMapsForContainerIter.next();
+					System.out.println();
+					if(entityMap!=null)
+					{
+						Long staticEntityId = ((EntityMap)entityMap).getStaticEntityId();
+						EntityInterface staticEntity = entityManager.getEntityByIdentifier(staticEntityId);
+						if(staticEntity!=null)
+						{
+							EntityInterface xmiStaticEntity = getHookEntityDetailsForXMI(staticEntity,mainContainer.getEntity());
+							entityGroup.addEntity(xmiStaticEntity);
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * @param staticEntity
+	 * @return
+	 * @throws DynamicExtensionsApplicationException 
+	 * @throws DynamicExtensionsSystemException 
+	 */
+	public static EntityInterface getHookEntityDetailsForXMI(EntityInterface srcEntity,EntityInterface targetEntity) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
+	{
+		//For XMI : add only id , name and table properties
+		EntityInterface xmiEntity = new Entity();
+		xmiEntity.setName(getHookEntityName(srcEntity.getName()));
+		xmiEntity.setDescription(srcEntity.getDescription());
+		xmiEntity.setTableProperties(srcEntity.getTableProperties());
+		xmiEntity.setId(srcEntity.getId());
+		xmiEntity.addAttribute(getIdAttribute(srcEntity));
+		xmiEntity.addAssociation(getHookEntityAssociation(srcEntity,targetEntity));
+		
+		return xmiEntity;
+	}
+
+	/**
+	 * @param name
+	 * @return
+	 */
+	public static String getHookEntityName(String name)
+	{
+		//Return last token from name
+		String hookEntityname = null;
+		StringTokenizer strTokenizer = new StringTokenizer(name,".");
+		while(strTokenizer.hasMoreElements())
+		{
+			hookEntityname = strTokenizer.nextToken();
+		}
+		return hookEntityname;
+	}
+
+	/**
+	 * @param srcEntity
+	 * @param targetEntity
+	 * @return
+	 * @throws DynamicExtensionsApplicationException 
+	 * @throws DynamicExtensionsSystemException 
+	 */
+	private static AssociationInterface getHookEntityAssociation(EntityInterface srcEntity, EntityInterface targetEntity) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
+	{
+		if((srcEntity!=null)&&(targetEntity!=null))
+		{
+			Collection<AssociationInterface> associations = srcEntity.getAllAssociations();
+			if(associations!=null)
+			{
+				Iterator assocIter = associations.iterator();
+				while(assocIter.hasNext())
+				{
+					AssociationInterface association = (AssociationInterface)assocIter.next();
+					if(association.getTargetEntity().equals(targetEntity))
+					{
+						String srcEntityName = getHookEntityName(srcEntity.getName());
+						//Change name of association 
+						association.setName("Assoc_" + srcEntityName+"_" +targetEntity.getName());
+						association.getSourceRole().setName(srcEntityName);
+						association.getTargetRole().setName(targetEntity.getName()+"Collection");
+						return association;
+					}
+				}
+			/*EntityManagerInterface entityManager = EntityManager.getInstance();
+			Collection<AssociationInterface> associations = return entityManager.getAssociations(srcEntity.getId(), targetEntity.getId());*/
+			
+			/*if(associations!=null)
+			{
+				Iterator assocIter = associations.iterator();
+				while(assocIter.hasNext())
+				{
+					AssociationInterface association = (AssociationInterface)assocIter.next();
+					ConstraintPropertiesInterface constraintProperties = association.getConstraintProperties();
+					if(constraintProperties!=null)
+					{
+						constraintProperties.setSourceEntityKey()
+					}
+					
+				}
+			}*/
+		}
+		}
+		return null;
+	}
+	/**
+	 * @return
+	 */
+	public static AttributeInterface getIdAttribute(EntityInterface entity)
+	{
+		if(entity!=null)
+		{
+			Collection<AttributeInterface> attributes = entity.getAllAttributes();
+			if(attributes!=null)
+			{
+				Iterator attributesIter = attributes.iterator();
+				while (attributesIter.hasNext())
+				{
+					AttributeInterface attribute = (AttributeInterface) attributesIter.next();
+					if((attribute!=null)&&(EntityManagerConstantsInterface.ID_ATTRIBUTE_NAME.equals(attribute.getName())))
+					{
+						return attribute;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	/**
+	 * @param entityGroup
+	 */
+	public static ContainerInterface getMainContainer(EntityGroupInterface entityGroup)
+	{
+		if(entityGroup!=null)
+		{
+			Collection<ContainerInterface> mainContainers = entityGroup.getMainContainerCollection();
+			if(mainContainers!=null)
+			{
+				Iterator mainContainerIter = mainContainers.iterator();
+				System.out.println("1");
+				if(mainContainerIter.hasNext())
+				{
+					//Return just the forst main container
+					ContainerInterface mainContainer = (ContainerInterface)mainContainerIter.next();
+					System.out.println("mainContainer "  + mainContainer);
+					return mainContainer;
+				}
+			}
+		}
+		return null;
+	}
+	
+}
