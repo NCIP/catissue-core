6708a4c5497eb47e7018944c302beb1f78162f71
vijay_pande <vijay_pande>
2007-11-21 08:23:50 +0000

    insert method updated. Code for saving identified report and de-identified report is removed
    SVN-Revision: 9878

--- WEB-INF/src/edu/wustl/catissuecore/bizlogic/SpecimenCollectionGroupBizLogic.java
+++ WEB-INF/src/edu/wustl/catissuecore/bizlogic/SpecimenCollectionGroupBizLogic.java
@@ -98,14 +98,6 @@ public class SpecimenCollectionGroupBizLogic extends DefaultBizLogic
 
 		dao.insert(specimenCollectionGroup, sessionDataBean, true, true);
 		
-		if(specimenCollectionGroup.getIdentifiedSurgicalPathologyReport()!=null)
-		{
-			dao.insert(specimenCollectionGroup.getIdentifiedSurgicalPathologyReport(), sessionDataBean, false, true);
-		}
-		if(specimenCollectionGroup.getDeIdentifiedSurgicalPathologyReport()!=null)
-		{
-			dao.insert(specimenCollectionGroup.getDeIdentifiedSurgicalPathologyReport(), sessionDataBean, false, true);
-		}
 		try
 		{
 			SecurityManager.getInstance(this.getClass()).insertAuthorizationData(null, getProtectionObjects(specimenCollectionGroup),
