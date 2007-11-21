f8e7984a34a596f2d7b82f59a59ffccd160d2a66
ganesh_naikwade <ganesh_naikwade>
2007-11-21 15:37:21 +0000

    Added Testcases
    SVN-Revision: 9899

--- caTissueCore_caCORE_Client/src/edu/wustl/catissuecore/bizlogic/test/StorageContainerTestCases.java
+++ caTissueCore_caCORE_Client/src/edu/wustl/catissuecore/bizlogic/test/StorageContainerTestCases.java
@@ -3,6 +3,7 @@ package edu.wustl.catissuecore.bizlogic.test;
 import java.util.Iterator;
 import java.util.List;
 
+import edu.wustl.catissuecore.domain.Site;
 import edu.wustl.catissuecore.domain.StorageContainer;
 import edu.wustl.common.domain.AbstractDomainObject;
 import edu.wustl.common.util.logger.Logger;
@@ -70,59 +71,39 @@ public class StorageContainerTestCases extends CaTissueBaseTestCase{
 	    }
 	}
 	
-	/*public void testEmptyObjectInInsert_StorageContainer(){
-		domainObject = new StorageContainer();
-		testEmptyDomainObjectInInsert(domainObject);
-	}
-	
-	public void testNullObjectInInsert_StorageContainer(){
-		domainObject = new StorageContainer();
-		testNullDomainObjectInInsert(domainObject);
-	}
-	
-	public void testNullSessionDatBeanInInsert_StorageContainer(){
-		domainObject = new StorageContainer();
-		testNullSessionDataBeanInInsert(domainObject);
-	}
-	
-	public void testNullSessionDataBeanInUpdate_StorageContainer(){
-		domainObject = new StorageContainer();
-		testNullSessionDataBeanInUpdate(domainObject);
-	}
-	
-	public void testNullOldDomainObjectInUpdate_StorageContainer(){
-		domainObject = new StorageContainer();
-		testNullOldDomainObjectInUpdate(domainObject);
-	}
-	
-	public void testNullCurrentDomainObjectInUpdate_StorageContainer(){
-		domainObject = new StorageContainer();
-		testNullCurrentDomainObjectInUpdate(domainObject);
-	}
-	
-	public void testWrongDaoTypeInUpdate_StorageContainer(){
-		domainObject = new StorageContainer();
-		testNullCurrentDomainObjectInUpdate(domainObject);
-	}
-	
-	public void testEmptyCurrentDomainObjectInUpdatet_StorageContainer()
+	public void testUpdateStorageContainerToClosedActivityStatus()
 	{
-		domainObject = new StorageContainer();
-		AbstractDomainObject initialisedDomainObject = BaseTestCaseUtility.initStorageContainer();
-		testEmptyCurrentDomainObjectInUpdate(domainObject, initialisedDomainObject);
+		try{
+			StorageContainer storageContainer= BaseTestCaseUtility.initStorageContainer();			
+			System.out.println(storageContainer);
+			storageContainer = (StorageContainer) appService.createObject(storageContainer); 
+			System.out.println("Object created successfully");
+			storageContainer.setActivityStatus("Closed");
+			StorageContainer updatedStorageContainer = (StorageContainer) appService.updateObject(storageContainer);
+			assertTrue("Object updated successfully", true);
+		 }
+		 catch(Exception e){
+			 Logger.out.error(e.getMessage(),e);
+			 e.printStackTrace();
+			 assertFalse("Could notclose Storage Container", true);
+		 }
 	}
 	
-	public void testEmptyOldDomainObjectInUpdate_StorageContainer()
+	public void testUpdateStorageContainerToDisabledActivityStatus()
 	{
-		domainObject = new StorageContainer();
-		AbstractDomainObject initialisedDomainObject = BaseTestCaseUtility.initStorageContainer();
-		testEmptyOldDomainObjectInUpdate(domainObject,initialisedDomainObject);
+		try{
+			StorageContainer storageContainer= BaseTestCaseUtility.initStorageContainer();			
+			System.out.println(storageContainer);
+			storageContainer = (StorageContainer) appService.createObject(storageContainer); 
+			System.out.println("Object created successfully");
+			storageContainer.setActivityStatus("Disabled");
+			StorageContainer updatedStorageContainer = (StorageContainer) appService.updateObject(storageContainer);
+			assertTrue("Object updated successfully", true);
+		 }
+		 catch(Exception e){
+			 Logger.out.error(e.getMessage(),e);
+			 e.printStackTrace();
+			 assertFalse("Could not disable Storage Container", true);
+		 }
 	}
-	public void testNullDomainObjectInRetrieve_StorageContainer()
-	{
-		domainObject = new StorageContainer();
-		testNullCurrentDomainObjectInRetrieve(domainObject);
-	}*/
-
-	
 }
