15cc89a3efd3adad14aa0f0853c6e009b234e2e7
vijay_pande <vijay_pande>
2008-01-09 11:09:14 +0000

    changes for HTTPS
    SVN-Revision: 10656

--- caTissueSuite_Client/src/edu/wustl/catissuecore/bizlogic/test/CaTissueBaseTestCase.java
+++ caTissueSuite_Client/src/edu/wustl/catissuecore/bizlogic/test/CaTissueBaseTestCase.java
@@ -41,6 +41,7 @@ public class CaTissueBaseTestCase extends BaseTestCase{
 //		DAOFactory.setDAOFactory(factory);
 		
 
+		System.setProperty("javax.net.ssl.trustStore", "D://jboss_18080//server//default//conf//chap8.keystore");
 		appService = ApplicationServiceProvider.getApplicationService();
 		ClientSession cs = ClientSession.getInstance();
 		try
