4688d0db68ba22d4de401d2e5bdf7f38fa0072af
vaishali_khandelwal <vaishali_khandelwal>
2008-01-22 12:44:11 +0000

    Added 2 complex CP
    SVN-Revision: 10941

--- caTissueSuite_Client/src/edu/wustl/catissuecore/bizlogic/test/ComplexCollectionProtocolTestCases.java
+++ caTissueSuite_Client/src/edu/wustl/catissuecore/bizlogic/test/ComplexCollectionProtocolTestCases.java
@@ -9,15 +9,15 @@ public class ComplexCollectionProtocolTestCases extends CaTissueBaseTestCase
 	{
 		try
 		 {
-			CollectionProtocol collectionProtocol = ComplexCollectionProtocolUtility.initComplexCollectionProtocol1();			
+			CollectionProtocol collectionProtocol = ComplexCollectionProtocolUtility.initComplexCollectionProtocol();			
 			collectionProtocol = (CollectionProtocol) appService.createObject(collectionProtocol);
 			TestCaseUtility.setObjectMap(collectionProtocol, CollectionProtocol.class);
 			assertTrue("Object added successfully", true);
 			
-			/**CollectionProtocol collectionProtocol1 = ComplexCollectionProtocolUtility.initComplexCollectionProtocol1();			
+			CollectionProtocol collectionProtocol1 = ComplexCollectionProtocolUtility.initComplexCollectionProtocol1();			
 			collectionProtocol1 = (CollectionProtocol) appService.createObject(collectionProtocol1);
 			TestCaseUtility.setObjectMap(collectionProtocol1, CollectionProtocol.class);
-			assertTrue("Object added successfully", true);*/
+			assertTrue("Object added successfully", true);
 		 }
 		 catch(Exception e)
 		 {
