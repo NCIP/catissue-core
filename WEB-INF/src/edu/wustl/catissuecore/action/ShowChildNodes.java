019cab7a5e4b39ef2fc7a34a7500d0f28c397b48
virender_mehta <virender_mehta>
2008-03-29 09:08:52 +0000

    New Action class added for AJAX call
    SVN-Revision: 12141

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/ShowChildNodes.java
@@ -0,0 +1,74 @@
+package edu.wustl.catissuecore.action;
+
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.bizlogic.StorageContainerBizLogic;
+import edu.wustl.common.action.BaseAction;
+import edu.wustl.common.tree.StorageContainerTreeNode;
+import edu.wustl.common.tree.TreeNodeImpl;
+
+/**
+ * This class class through AJAX call, when user clicks on [+] sign on UI, this class will retrieve all the 
+ * required containers under the clicked node. 
+ * @author virender_mehta
+ *
+ */
+public class ShowChildNodes extends BaseAction
+{
+	protected ActionForward executeAction(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		PrintWriter out = response.getWriter();
+		Vector containerNodeVector = new Vector();
+		StringBuffer xmlData = new StringBuffer();
+		String nodeName = request.getParameter("nodeName");
+		Long identifier =  new Long(request.getParameter("containerId"));
+		String parentId = request.getParameter("parentId");
+		StorageContainerBizLogic sc = new StorageContainerBizLogic();
+		containerNodeVector = sc.getStorageContainers(identifier,nodeName,parentId);
+		response.setContentType("text/xml");
+		xmlData = makeXMLData(containerNodeVector,xmlData);
+		out.print(xmlData.toString());
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param childVector This vector contains all the nodes, when user clics on [+] sign then ajax call retrieve
+	 * all the child node. 
+	 * @param xmlData This is a string buffer and have all the information of the node that is to be transfered to jsp.
+	 * @return xmlData
+	 */
+	private StringBuffer makeXMLData(Vector childVector,StringBuffer xmlData)
+	{		
+		Iterator childItr = childVector.iterator();
+		while(childItr.hasNext())
+		{
+			StorageContainerTreeNode childNode = (StorageContainerTreeNode)childItr.next();
+			
+			xmlData.append(childNode.getIdentifier());
+			xmlData.append(",");
+			xmlData.append(((TreeNodeImpl)childNode.getParentNode()).getIdentifier());
+			xmlData.append(",");
+			xmlData.append(((TreeNodeImpl)childNode.getParentNode()).getValue());
+			xmlData.append(",");
+			xmlData.append(childNode.getActivityStatus());
+			xmlData.append(",");
+			xmlData.append(childNode.getValue());
+			xmlData.append(",");
+			xmlData.append(childNode.getChildNodes());
+			xmlData.append("#");
+		}
+		return xmlData;
+	}
+	
+	
+}
