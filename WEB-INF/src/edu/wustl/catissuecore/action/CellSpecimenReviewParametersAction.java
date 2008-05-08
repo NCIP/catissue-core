8278ffdb061076a8d4e69216d9c2f2d2905b8a9f
anup_agrawal <anup_agrawal>
2008-05-08 13:03:23 +0000

    Action class added for code refactoring
    SVN-Revision: 12666

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/CellSpecimenReviewParametersAction.java
@@ -0,0 +1,32 @@
+package edu.wustl.catissuecore.action;
+
+import javax.servlet.http.HttpServletRequest;
+import edu.wustl.catissuecore.actionForm.CellSpecimenReviewParametersForm;
+import edu.wustl.catissuecore.actionForm.EventParametersForm;
+import edu.wustl.catissuecore.util.global.Constants;
+
+public class CellSpecimenReviewParametersAction extends SpecimenEventParametersAction
+{
+	protected void setRequestParameters(HttpServletRequest request,EventParametersForm eventParametersForm) throws Exception
+	{
+		String formName=null;
+	    boolean readOnlyValue;
+	   CellSpecimenReviewParametersForm cellSpecimenReviewParametersForm=(CellSpecimenReviewParametersForm)eventParametersForm;
+	    if (cellSpecimenReviewParametersForm.getOperation().equals(Constants.EDIT))
+	    {
+	        formName = Constants.CELL_SPECIMEN_REVIEW_PARAMETERS_EDIT_ACTION;
+	        readOnlyValue = true;
+	    }
+	    else
+	    {
+	        formName = Constants.CELL_SPECIMEN_REVIEW_PARAMETERS_ADD_ACTION;
+	        readOnlyValue = false;
+	    }
+	    String changeAction = "setFormAction('" + formName + "');";
+	    request.setAttribute("formName", formName);
+		request.setAttribute("readOnlyValue", readOnlyValue);
+		request.setAttribute("changeAction", changeAction);
+		request.setAttribute("CellSpecimenReviewParametersAdd",Constants.CELL_SPECIMEN_REVIEW_PARAMETERS_ADD_ACTION);
+	}
+		
+}
---
+++ WEB-INF/src/edu/wustl/catissuecore/action/SpunEventParametersAction.java
@@ -0,0 +1,15 @@
+package edu.wustl.catissuecore.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.wustl.catissuecore.actionForm.EventParametersForm;
+
+public class SpunEventParametersAction extends SpecimenEventParametersAction
+{
+@Override
+protected void setRequestParameters(HttpServletRequest request, EventParametersForm eventParametersForm) throws Exception
+{
+	// TODO Auto-generated method stub
+	
+}
+}
