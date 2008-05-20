f30a7228dc25cb86dce83ff2e653921b2f57c5ad
ravindra_jain <ravindra_jain>
2008-05-20 14:10:06 +0000

    *** empty log message ***
    SVN-Revision: 12868

--- WEB-INF/src/edu/wustl/catissuecore/util/listener/CatissueCoreSessionListener.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/listener/CatissueCoreSessionListener.java
@@ -26,26 +26,29 @@ import edu.wustl.common.util.logger.Logger;
 public class CatissueCoreSessionListener implements HttpSessionListener{
 
 	public void sessionCreated(HttpSessionEvent arg0) {
-		
+
 	}
 
 	//Cleanup after session invalidates.
 	public void sessionDestroyed(HttpSessionEvent arg0) {
 		HttpSession session = arg0.getSession();
 		SessionDataBean sessionData= (SessionDataBean)session.getAttribute(Constants.SESSION_DATA);
-				
+
 		if(sessionData!=null)
 			cleanUp(sessionData,(String)session.getAttribute(Constants.RANDOM_NUMBER));
-		
+
 		// To remove PrivilegeCache from the session, requires user LoginName
-		// Singleton instance of PrivilegeManager	
-		PrivilegeManager privilegeManager = PrivilegeManager.getInstance();
-		privilegeManager.removePrivilegeCache(sessionData.getUserName());
+		// Singleton instance of PrivilegeManager
+		if(sessionData != null)
+		{
+			PrivilegeManager privilegeManager = PrivilegeManager.getInstance();
+			privilegeManager.removePrivilegeCache(sessionData.getUserName());
+		}
 	}
-    private void cleanUp(SessionDataBean sessionData,String randomNumber)
-    {
+	private void cleanUp(SessionDataBean sessionData,String randomNumber)
+	{
 		//Delete Advance Query table if exists
-	 	//Advance Query table name with userID attached
+		//Advance Query table name with userID attached
 		String tempTableName = Constants.QUERY_RESULTS_TABLE+"_"+sessionData.getUserId();
 		try
 		{
@@ -70,5 +73,5 @@ public class CatissueCoreSessionListener implements HttpSessionListener{
 		{
 			Logger.out.error("Could not delete the Query Module Search temporary table."+ex.getMessage(),ex);
 		}
-    }
+	}
 }
