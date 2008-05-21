bfd3f140992a45f57e2e76a125de81740cfe6c23
baljeet_dhindhwal <baljeet_dhindhwal>
2008-05-21 09:23:06 +0000

    added new action class to handle ajax for adding new institution from user jsp
    SVN-Revision: 12886

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/AddInstitutionAction.java
@@ -0,0 +1,87 @@
+package edu.wustl.catissuecore.action;
+
+//import edu.wustl.catissuecore.actionForm.DepartmentForm;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.actionForm.InstitutionForm;
+import edu.wustl.catissuecore.bizlogic.InstitutionBizLogic;
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.action.CommonAddEditAction;
+import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.logger.Logger;
+
+public class AddInstitutionAction extends CommonAddEditAction
+{
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		String institutionName =(String)request.getParameter(Constants.INSTITUTION_NAME);
+		InstitutionBizLogic bizlogic = new InstitutionBizLogic();
+		String institutionId = null;
+		String responseString = "";
+		InstitutionForm institutionForm = (InstitutionForm)form;
+		
+		institutionForm.setOperation(Constants.ADD);
+		institutionForm.setName(institutionName);
+		
+		ActionForward forward = super.execute(mapping,institutionForm,request,response);
+		
+		
+		if((forward != null) && (forward.getName().equals(Constants.FAILURE)))
+		{
+			ActionErrors errors = (ActionErrors) request.getAttribute(Globals.ERROR_KEY);
+			Logger.out.info("Errors:" + errors);
+			if (errors != null || errors.size() != 0)
+			{
+				Iterator iterator = errors.get();
+				while (iterator.hasNext())
+				{
+					ActionError next = (ActionError)iterator.next();
+					Object[] values = next.getValues();
+					for(int j=0;j<values.length;j++)
+					{
+						responseString = (String)values[j];
+					}
+				}
+			}
+		}
+		else
+		{
+			try
+			{
+				institutionId = bizlogic.getLatestInstitution(institutionName);
+				responseString = institutionId + Constants.RESPONSE_SEPARATOR + institutionName;
+			}
+			catch(DAOException e)
+			{
+				Logger.out.error("Exception occurred in retrieving Institution");
+			e.printStackTrace();
+			}
+		}
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		/**
+		 * Sending the response
+		 */
+		out.write(responseString);
+		
+		return null;
+	}
+	
+}
