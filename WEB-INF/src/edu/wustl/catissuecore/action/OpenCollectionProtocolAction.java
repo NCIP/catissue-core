ee37adaedd83dc071bbe4737ec16925b7eb4f797
pathik_sheth <pathik_sheth>
2008-06-10 11:35:23 +0000

    This action class is forwarded to a jsp which create two frames.
    SVN-Revision: 13176

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/OpenCollectionProtocolAction.java
@@ -0,0 +1,38 @@
+package edu.wustl.catissuecore.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.common.action.BaseAction;
+/**
+ * Forward to collection protocol main page 
+ * @author pathik_sheth
+ *
+ */
+public class OpenCollectionProtocolAction extends BaseAction{
+
+	@Override
+	/**
+	 *Set  add/edit operation is being performed    
+	 */
+	protected ActionForward executeAction(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String operation = (String)request.getParameter(Constants.OPERATION);
+		String pageOf = (String)request.getParameter(Constants.PAGEOF);
+		HttpSession session = request.getSession();
+		if("pageOfmainCP".equalsIgnoreCase(pageOf))
+		{
+			session.removeAttribute(Constants.COLLECTION_PROTOCOL_SESSION_BEAN);
+		}
+
+		request.setAttribute(Constants.OPERATION, operation);
+		return mapping.findForward(Constants.SUCCESS);
+	}
+
+}
