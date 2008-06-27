36b87319ba26f0245ceca9b2f9d8713a687bcd57
falguni_sachde <falguni_sachde>
2008-06-27 11:33:13 +0000

    modified for UI modification for add/edit page of DE .added variable and Validate()
    SVN-Revision: 13636

--- WEB-INF/src/edu/wustl/catissuecore/actionForm/AnnotationForm.java
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/AnnotationForm.java
@@ -4,104 +4,137 @@
  *<p>Copyright:TODO</p>
  *@author 
  *@version 1.0
- */ 
+ */
 package edu.wustl.catissuecore.actionForm;
 
 import java.io.Serializable;
 import java.util.List;
 
-import org.apache.struts.action.ActionForm;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
 
 import edu.wustl.common.actionForm.AbstractActionForm;
 import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.common.util.global.ApplicationProperties;
+import edu.wustl.common.util.global.Validator;
+import edu.wustl.common.util.logger.Logger;
 
 /**
  * @author preeti_munot
- *
+ * 
  */
-public class AnnotationForm extends AbstractActionForm implements Serializable
-{
+public class AnnotationForm extends AbstractActionForm implements Serializable {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private String annotationGroupsXML;
-    private String annotationEntitiesXML;
-    private List systemEntitiesList;
-    private String selectedStaticEntityId;
-    private List  conditionalInstancesList;
-    private String[] conditionVal;
-    
-    
-    public String[] getConditionVal()
-    {
-        return conditionVal;
-    }
-    
-    public void setConditionVal(String[] conditionVal)
-    {
-        this.conditionVal = conditionVal;
-    }
-    public String getSelectedStaticEntityId()
-    {
-        return this.selectedStaticEntityId;
-    }
-    public void setSelectedStaticEntityId(String selectedStaticEntityId)
-    {
-        this.selectedStaticEntityId = selectedStaticEntityId;
-    }
-    public List getSystemEntitiesList()
-    {
-        return this.systemEntitiesList;
-    }
-    public void setSystemEntitiesList(List systemEntitiesList)
-    {
-        this.systemEntitiesList = systemEntitiesList;
-    }
-    public String getAnnotationEntitiesXML()
-    {
-        return this.annotationEntitiesXML;
-    }
-    public void setAnnotationEntitiesXML(String annotationEntitiesXML)
-    {
-        this.annotationEntitiesXML = annotationEntitiesXML;
-    }
-    public String getAnnotationGroupsXML()
-    {
-        return this.annotationGroupsXML;
-    }
-    public void setAnnotationGroupsXML(String annotationGroupsXML)
-    {
-        this.annotationGroupsXML = annotationGroupsXML;
-    }
+	private String annotationGroupsXML;
 
-    public int getFormId()
-    {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	private String annotationEntitiesXML;
 
-    public void setAllValues(AbstractDomainObject abstractDomain)
-    {
-        // TODO Auto-generated method stub
-        
-    }
+	private List systemEntitiesList;
 
-    protected void reset()
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
-    public List getConditionalInstancesList()
-    {
-        return conditionalInstancesList;
-    }
-    
-    public void setConditionalInstancesList(List conditionalInstancesList)
-    {
-        this.conditionalInstancesList = conditionalInstancesList;
-    }
+	private String selectedStaticEntityId;
+
+	private List conditionalInstancesList;
+
+	private String[] conditionVal;
+
+	private String deoperation;
+
+	public String[] getConditionVal() {
+		return conditionVal;
+	}
+
+	public void setConditionVal(String[] conditionVal) {
+		this.conditionVal = conditionVal;
+	}
+
+	public String getSelectedStaticEntityId() {
+		return this.selectedStaticEntityId;
+	}
+
+	public void setSelectedStaticEntityId(String selectedStaticEntityId) {
+		this.selectedStaticEntityId = selectedStaticEntityId;
+	}
+
+	public List getSystemEntitiesList() {
+		return this.systemEntitiesList;
+	}
+
+	public void setSystemEntitiesList(List systemEntitiesList) {
+		this.systemEntitiesList = systemEntitiesList;
+	}
+
+	public String getAnnotationEntitiesXML() {
+		return this.annotationEntitiesXML;
+	}
+
+	public void setAnnotationEntitiesXML(String annotationEntitiesXML) {
+		this.annotationEntitiesXML = annotationEntitiesXML;
+	}
+
+	public String getAnnotationGroupsXML() {
+		return this.annotationGroupsXML;
+	}
+
+	public void setAnnotationGroupsXML(String annotationGroupsXML) {
+		this.annotationGroupsXML = annotationGroupsXML;
+	}
+
+	public int getFormId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setAllValues(AbstractDomainObject abstractDomain) {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List getConditionalInstancesList() {
+		return conditionalInstancesList;
+	}
+
+	public void setConditionalInstancesList(List conditionalInstancesList) {
+		this.conditionalInstancesList = conditionalInstancesList;
+	}
+
+	public void setDeoperation(String deoperation) {
+		this.deoperation = deoperation;
+	}
+
+	public String getDeoperation() {
+		return deoperation;
+	}
+
+	public ActionErrors validate(ActionMapping mapping,
+			HttpServletRequest request) {
+		ActionErrors errors = new ActionErrors();
+		Validator validator = new Validator();
+		try {
+			if (deoperation != null && deoperation.equals("add")) {
+				if (validator.isValidOption(String
+						.valueOf(selectedStaticEntityId)) == false) {
+					errors.add(ActionErrors.GLOBAL_ERROR, new ActionError(
+							"errors.item.required", ApplicationProperties
+									.getValue("anno.Entity")));
+				}
+
+			}
+		} catch (Exception excp) {
+			Logger.out.error(excp.getMessage(), excp);
+		}
+		return errors;
+	}
 }
