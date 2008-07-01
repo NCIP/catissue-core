2c24f8092a4733eabeecc780540b8ee8dce2e82d
satish_pal <satish_pal>
2008-07-01 10:02:42 +0000

    for dana farber table creation Participant EMPI
    SVN-Revision: 13727

---
+++ SQL/MySql/danafarber.sql
@@ -0,0 +1,8 @@
+drop table if exists catissue_participant_empi;
+
+create table catissue_participant_empi (
+   Id int(11) NOT NULL default '0',
+   EMPI_ID int(11) NOT NULL default '0',
+   primary key (Id)
+);
+
