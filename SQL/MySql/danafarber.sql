41f0427650a82a1e2770430e4572665d352a93b3
satish_pal <satish_pal>
2008-07-01 11:10:47 +0000

    *** empty log message ***
    SVN-Revision: 13731

--- SQL/MySql/danafarber.sql
+++ SQL/MySql/danafarber.sql
@@ -6,3 +6,12 @@ create table catissue_participant_empi (
    primary key (Id)
 );
 
+INSERT INTO CSM_PROTECTION_ELEMENT 
+(PROTECTION_ELEMENT_ID,PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,ATTRIBUTE,PROTECTION_ELEMENT_TYPE,APPLICATION_ID,UPDATE_DATE) 
+VALUES (336,'Participant Empi','Participant Empi Class','edu.wustl.catissuecore.domain.ParticipantEmpi',
+'Participant Empi comments',NULL,1,'0000-00-00');
+
+INSERT INTO csm_pg_pe ( PG_PE_ID ,PROTECTION_GROUP_ID, PROTECTION_ELEMENT_ID
+,UPDATE_DATE) VALUES (409,1,336,'0000-00-00');
+
+commit;
