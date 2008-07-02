7b3f01dc248519a258be6ccf5fa606d79012bb1c
santhoshkumar_c <santhoshkumar_c>
2008-07-02 14:44:33 +0000

    Query Module related Constants class.
    SVN-Revision: 13753

---
+++ WEB-INF/src/edu/wustl/catissuecore/util/querysuite/QueryModuleConstants.java
@@ -0,0 +1,27 @@
+
+package edu.wustl.catissuecore.util.querysuite;
+
+public class QueryModuleConstants
+{
+	//CreateQueryObjectBizLogic Constants	
+	public static final int ARGUMENT_ZERO = 0;
+	public static final int INDEX_PARAM_ZERO = 0;
+	public static final int INDEX_PARAM_ONE = 1;
+	public static final int INDEX_PARAM_TWO = 2;
+	public static final int INDEX_LENGTH = 3;
+	public static final String BIG_INT = "bigint";
+	public static final String INTEGER = "integer";
+	public static final String LONG = "Long";
+	public static final String DOUBLE = "double";
+	public static final String TINY_INT = "tinyint";
+	public static final String UNDERSCORE = "_";
+	public static final String BOOLEAN_NO = "No";
+	public static final String BOOLEAN_YES = "Yes";	
+	public static final String Between = "Between";	
+	public static final String ENTITY_SEPARATOR = ";";
+	public static final String QUERY_VALUES_DELIMITER = "&";
+	public static final String QUERY_OPERATOR_DELIMITER = "!*=*!";	
+	public static final String QUERY_CONDITION_DELIMITER = "@#condition#@";
+	
+
+}
