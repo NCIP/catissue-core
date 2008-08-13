fddac4d1e68e676ed3ffb04c886a2361b08e68c4
baljeet_dhindhwal <baljeet_dhindhwal>
2008-08-13 11:36:26 +0000

    Constanta added for Single Node Repaint
    SVN-Revision: 14734

--- WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGConstant.java
+++ WEB-INF/src/edu/wustl/catissuecore/flex/dag/DAGConstant.java
@@ -11,6 +11,7 @@ public class DAGConstant {
 	public static final String EDIT_LIMIT="Edit";
 	public static final String DAG_NODE_LIST = "dagNodeList";
 	public static final String CUSTOM_FORMULA_NODE_LIST = "customFormulaNodeList";
+	public static final String SINGLE_NODE_CUSTOM_FORMULA_NODE_LIST ="singleNodeCFList";
 	public static final String REPAINT_OPERATION = "rePaint";
 	public static final String ISREPAINT = "isRepaint";
 	public static final String ISREPAINT_TRUE = "true";
