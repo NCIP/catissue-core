1187bdb14f7bf656744dc34283f473f760ce038c
vijay_pande <vijay_pande>
2008-08-22 15:15:27 +0000

    OrderDetails to Specimen curated path added
    SVN-Revision: 15037

--- WEB-INF/src/edu/wustl/catissuecore/querysuite/metadata/AddCuratedPath.java
+++ WEB-INF/src/edu/wustl/catissuecore/querysuite/metadata/AddCuratedPath.java
@@ -141,6 +141,12 @@ public class AddCuratedPath
 		entityList.add("edu.wustl.catissuecore.domain.Site,edu.wustl.catissuecore.domain.SpecimenCollectionGroup,edu.wustl.catissuecore.domain.FluidSpecimen");
 		entityList.add("edu.wustl.catissuecore.domain.Site,edu.wustl.catissuecore.domain.SpecimenCollectionGroup,edu.wustl.catissuecore.domain.MolecularSpecimen");
 		entityList.add("edu.wustl.catissuecore.domain.Site,edu.wustl.catissuecore.domain.SpecimenCollectionGroup,edu.wustl.catissuecore.domain.TissueSpecimen");
+		
+		entityList.add("edu.wustl.catissuecore.domain.OrderDetails,edu.wustl.catissuecore.domain.Distribution,edu.wustl.catissuecore.domain.Specimen");
+		entityList.add("edu.wustl.catissuecore.domain.OrderDetails,edu.wustl.catissuecore.domain.Distribution,edu.wustl.catissuecore.domain.CellSpecimen");
+		entityList.add("edu.wustl.catissuecore.domain.OrderDetails,edu.wustl.catissuecore.domain.Distribution,edu.wustl.catissuecore.domain.FluidSpecimen");
+		entityList.add("edu.wustl.catissuecore.domain.OrderDetails,edu.wustl.catissuecore.domain.Distribution,edu.wustl.catissuecore.domain.MolecularSpecimen");
+		entityList.add("edu.wustl.catissuecore.domain.OrderDetails,edu.wustl.catissuecore.domain.Distribution,edu.wustl.catissuecore.domain.TissueSpecimen");
 	}
 	
 	public AddCuratedPath(Connection connection)
