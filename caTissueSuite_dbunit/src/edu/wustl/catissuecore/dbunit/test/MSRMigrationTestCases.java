8e03b295ef8a90550b93f6381c109530725a2340
juberahamad_patel <juberahamad_patel>
2008-08-23 12:02:54 +0000

    *** empty log message ***
    SVN-Revision: 15065

--- caTissueSuite_dbunit/src/edu/wustl/catissuecore/dbunit/test/MSRMigrationTestCases.java
+++ caTissueSuite_dbunit/src/edu/wustl/catissuecore/dbunit/test/MSRMigrationTestCases.java
@@ -4,7 +4,10 @@
 
 package edu.wustl.catissuecore.dbunit.test;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.wustl.catissuecore.dbunit.test.DefaultCatissueDBUnitTestCase;
 import edu.wustl.catissuecore.domain.Site;
@@ -47,6 +50,7 @@ public class MSRMigrationTestCases extends DefaultCatissueDBUnitTestCase
 			User user = (User) o;
 			if (user.getRoleId().equals("2") || user.getRoleId().equals("3"))
 			{
+				user.getSiteCollection().clear();
 				user.getSiteCollection().addAll(sites);
 				try
 				{
@@ -60,5 +64,85 @@ public class MSRMigrationTestCases extends DefaultCatissueDBUnitTestCase
 
 		}
 	}
+	
+	/**
+	 * for the time being, checking that 
+	 * 1. there is at least one technician or supervisor, so that our core code is executed
+	 * 2. the site collection for each technician and supervisor equals the set of all sites in the database
+	 * 
+	 */
+	
+	public void testVerifyMigration()
+	{
+		
+		List users = null;
+		Set sites = null;
 
+		try
+		{
+			users = search(User.class);
+			sites = new HashSet(search(Site.class));
+		}
+		catch (BizLogicException e)
+		{
+			fail("failed to retrieve all the users and sites");
+		}
+
+		int techsOrSups = 0;
+		for (Object o : users)
+		{
+			User user = (User) o;
+			if (user.getRoleId().equals("2") || user.getRoleId().equals("3"))
+			{
+				techsOrSups++;
+				
+				if(!CompareSiteSets(sites, user.getSiteCollection()))
+				{
+					fail();
+				}
+			}
+			
+		}
+		
+		
+		if(techsOrSups==0)
+		{
+			fail();
+		}
+
+	}
+
+	/**
+	 * Due to absence of equals() method in the domain class hierarchy
+	 * 	we have to implement the comparison, based on site ids
+	 * 
+	 * @return true if they are equal
+	 */
+	private boolean CompareSiteSets(Set sites, Collection<Site> siteCollection)
+	{
+		if(siteCollection.size()!=sites.size())
+		{
+			return false;
+		}
+		
+		Set<Long> siteIds = new HashSet<Long>(siteCollection.size());
+		
+		for(Site site : siteCollection)
+		{
+			siteIds.add(site.getId());
+		}
+		
+		for(Object o : sites)
+		{
+			Long id = ((Site) o).getId();
+			if(!siteIds.contains(id))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+
+	}
+	
 }
