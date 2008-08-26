337f8b0b27addd50c75777ee896b30e6cb55ec70
janhavi_hasabnis <janhavi_hasabnis>
2008-08-26 05:45:12 +0000

    Fixed bug 8905
    SVN-Revision: 15121

--- WEB-INF/src/edu/wustl/catissuecore/util/IdComparator.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/IdComparator.java
@@ -3,8 +3,12 @@ package edu.wustl.catissuecore.util;
 
 import java.util.Comparator;
 
+import edu.wustl.catissuecore.bean.ConsentBean;
 import edu.wustl.catissuecore.bean.DeriveSpecimenBean;
 import edu.wustl.catissuecore.bean.GenericSpecimen;
+import edu.wustl.catissuecore.domain.ConsentTier;
+import edu.wustl.catissuecore.domain.ConsentTierResponse;
+import edu.wustl.catissuecore.domain.ConsentTierStatus;
 import edu.wustl.catissuecore.domain.Specimen;
 
 public class IdComparator implements Comparator
@@ -36,6 +40,38 @@ public class IdComparator implements Comparator
 				id2 = ((GenericSpecimen) arg1).getId();
 
 			}
+//			bug 8905 start
+			if(arg0 instanceof ConsentTierResponse && arg1 instanceof ConsentTierResponse)
+			{
+				id1 = ((ConsentTierResponse) arg0).getConsentTier().getId();
+				id2 = ((ConsentTierResponse) arg1).getConsentTier().getId();
+			}
+			if(arg0 instanceof ConsentTier && arg1 instanceof ConsentTier)
+			{
+				id1 = ((ConsentTier) arg0).getId();
+				id2 = ((ConsentTier) arg1).getId();
+			}
+			if(arg0 instanceof ConsentTierStatus && arg1 instanceof ConsentTierStatus)
+			{
+				id1 = ((ConsentTierStatus) arg0).getConsentTier().getId();
+				id2 = ((ConsentTierStatus) arg1).getConsentTier().getId();
+			}
+			if(arg0 instanceof ConsentBean && arg1 instanceof ConsentBean)
+			{
+				String id3 = ((ConsentBean) arg0).getConsentTierID();
+				String id4 = ((ConsentBean) arg1).getConsentTierID();
+				if(id3!=null && id4!=null)
+				{
+					id1 = Long.parseLong(id3);
+					id2 = Long.parseLong(id4);
+				}
+			}
+			if(arg0 instanceof String && arg1 instanceof String)
+			{
+				id1 = Long.parseLong(getId((String)arg0));
+				id2 = Long.parseLong(getId((String)arg1));
+			}
+//			bug 8905 end
 			if (id1 != null && id2 != null)
 			{
 				return id1.compareTo(id2);
@@ -50,6 +86,29 @@ public class IdComparator implements Comparator
 		}
 		// TODO Auto-generated method stub
 		return 0;
+	}
+//	bug 8905
+	/**
+	 * This method is used to get consent id from key 
+	 * @param key - ConsentBean:counter_consentTierID
+	 * @return id
+	 */
+	private String getId(String arg)
+	{
+		String[] first = ((String)arg).split(":");
+		String id = null;
+		if(first.length > 0)
+		{
+			for(String s : first)
+			{
+				if(s.contains("_"))
+				{
+					String[] idStmt = ((String)s).split("_");
+					id = idStmt[0];
+				}
+			}
+		}
+		return id;
 	}
 
 }
