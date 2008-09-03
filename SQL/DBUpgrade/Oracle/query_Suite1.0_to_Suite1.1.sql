3eb576930315e67641ce520e3abedd98e2735a43
srinath_k <srinath_k>
2008-09-03 16:09:52 +0000

    moved query_Seq creation to new file (bugfix 9310)
    SVN-Revision: 15416

--- SQL/DBUpgrade/Oracle/query_Suite1.0_to_Suite1.1.sql
+++ SQL/DBUpgrade/Oracle/query_Suite1.0_to_Suite1.1.sql
@@ -269,14 +269,3 @@ rename OUTPUT_ATTRIBUTE_SEQ to QUERY_OUTPUT_ATTRIBUTE_SEQ;
 
 -- old sequences 
 drop sequence GRAPH_ENTRY_SEQ;
-
--- shifted query_seq off constraint_seq 
-declare
-    i number :=1;
-    sql_stmt varchar2(50);
-begin
-	select nvl(max(identifier)+1,1) into i from query;
-	sql_stmt :='create sequence QUERY_SEQ start with ' || i ;
-	execute immediate sql_stmt;
-end;
-/
