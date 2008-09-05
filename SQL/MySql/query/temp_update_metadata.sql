ac611523c56adbc164e833111459ac3ab816e68e
deepti_shelar <deepti_shelar>
2008-09-05 13:26:47 +0000

    Fixing bug : 8921 : Separated targets for clob type support and entity group metadata.
    SVN-Revision: 15451

--- SQL/MySql/query/temp_update_metadata.sql
+++ SQL/MySql/query/temp_update_metadata.sql
@@ -1,19 +1,8 @@
 DELETE FROM DYEXTN_STRING_TYPE_INFO WHERE identifier IN (308,321,315);
-
 INSERT INTO DYEXTN_FILE_TYPE_INFO (Identifier) VALUES(308);
-
 INSERT INTO DYEXTN_FILE_TYPE_INFO (Identifier) VALUES(321);
-
 INSERT INTO DYEXTN_FILE_TYPE_INFO (Identifier) VALUES(315);
 
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1);
 
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1208);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1226);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1317);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1436);
 
 
