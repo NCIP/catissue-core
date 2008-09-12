b1877007f5e295199e5d08c4fad1f2580d293cca
baljeet_dhindhwal <baljeet_dhindhwal>
2008-09-12 10:16:31 +0000

    Changes related to 8296 code merge
    SVN-Revision: 15579

--- WEB-INF/src/edu/wustl/catissuecore/flex/dag/CustomFormulaUIBean.java
+++ WEB-INF/src/edu/wustl/catissuecore/flex/dag/CustomFormulaUIBean.java
@@ -1,6 +1,7 @@
 package edu.wustl.catissuecore.flex.dag;
 
 import edu.wustl.common.querysuite.queryobject.ICustomFormula;
+import edu.wustl.common.querysuite.queryobject.IOutputTerm;
 
 
 public class CustomFormulaUIBean
@@ -8,11 +9,46 @@ public class CustomFormulaUIBean
 	private ICustomFormula cf;
 	private CustomFormulaNode twoNode;
 	private SingleNodeCustomFormulaNode singleNode;
-	public CustomFormulaUIBean(ICustomFormula cf,CustomFormulaNode twoNode,SingleNodeCustomFormulaNode singleNode)
+	private boolean isCalculatedResult = false;
+	private IOutputTerm outputTerm ;
+	
+	/**
+	 * @return Returns the isCalculatedResult.
+	 */
+	public boolean isCalculatedResult()
+	{
+		return isCalculatedResult;
+	}
+	
+	/**
+	 * @param isCalculatedResult The isCalculatedResult to set.
+	 */
+	public void setCalculatedResult(boolean isCalculatedResult)
+	{
+		this.isCalculatedResult = isCalculatedResult;
+	}
+	
+	/**
+	 * @return Returns the outputTerm.
+	 */
+	public IOutputTerm getOutputTerm()
+	{
+		return outputTerm;
+	}
+	
+	/**
+	 * @param outputTerm The outputTerm to set.
+	 */
+	public void setOutputTerm(IOutputTerm outputTerm)
+	{
+		this.outputTerm = outputTerm;
+	}
+	public CustomFormulaUIBean(ICustomFormula cf,CustomFormulaNode twoNode,SingleNodeCustomFormulaNode singleNode, IOutputTerm outputTerm)
 	{
 		this.cf =cf;
 		this.singleNode = singleNode;
 		this.twoNode = twoNode;
+		this.outputTerm = outputTerm; 
 	}
 	/**
 	 * @return Returns the cf.
