778be3b1702605ad37b521cec41e4d14f17fc714
nilesh_ghone <nilesh_ghone>
2008-12-02 12:20:23 +0000

    Removing - Folder name changed to 'MsSqlServer'.
    SVN-Revision: 16564

--- SQL/MsSql/query/entity_group_metadata.sql
+++
@@ -1,9 +0,0 @@
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1208);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1226);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1317);
-
-insert into dyextn_tagged_value(T_KEY, T_VALUE, ABSTRACT_METADATA_ID) values ('MetadataEntityGroup', 'MetadataEntityGroup', 1436);
