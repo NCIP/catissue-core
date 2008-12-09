c3ac436ac6f9775fbca1c1c02a04e0cecbd9d422
kiran_pinnamaneni <kiran_pinnamaneni>
2008-12-09 05:59:18 +0000

    Committed for MSSqlServer support
    SVN-Revision: 16641

---
+++ SQL/MsSqlServer/Common/AlterTable.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT catissue_user ON;
+insert into catissue_user (IDENTIFIER, ACTIVITY_STATUS, CSM_USER_ID, EMAIL_ADDRESS,LOGIN_NAME,FIRST_NAME,LAST_NAME) values (1,'Active',1,'admin@admin.com','admin@admin.com','Admin','Admin');
+SET IDENTITY_INSERT catissue_user OFF;
+SET IDENTITY_INSERT catissue_password ON;
+insert into catissue_password (IDENTIFIER,PASSWORD,UPDATE_DATE,USER_ID) values ( '1','6c416f576765696c6e63316f326d3365',NULL,'1');
+SET IDENTITY_INSERT catissue_password OFF;
+ALTER TABLE CATISSUE_PART_MEDICAL_ID ADD constraint mrn_site_unique UNIQUE (MEDICAL_RECORD_NUMBER,SITE_ID);
+--commit;
