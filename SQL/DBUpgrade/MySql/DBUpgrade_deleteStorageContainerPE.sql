0200baf3943e2e7caf759a7a3adfd095f4a329cb
virender_mehta <virender_mehta>
2009-01-07 12:30:13 +0000

    MSR site delete issue solved
    SVN-Revision: 17262

--- SQL/DBUpgrade/MySql/DBUpgrade_deleteStorageContainerPE.sql
+++ SQL/DBUpgrade/MySql/DBUpgrade_deleteStorageContainerPE.sql
@@ -3,6 +3,6 @@ delete
 FROM csm_protection_element
 WHERE object_id regexp '_[[:digit:]]'
     AND object_id NOT regexp 'Site_[[:digit:]]'
-    AND object_id NOT regexp 'CollectionProtocol_[[:digit:]]'
-    AND object_id NOT regexp 'User_[[:digit:]]';
-    
+ 	AND object_id NOT regexp 'CollectionProtocol_[[:digit:]]'
+    AND object_id NOT regexp 'User_[[:digit:]]'
+    AND object_id NOT LIKE 'SITE_%';
--- SQL/DBUpgrade/Oracle/DBUpgrade_deleteStorageContainerPE.sql
+++ SQL/DBUpgrade/Oracle/DBUpgrade_deleteStorageContainerPE.sql
@@ -1,8 +1,9 @@
 /* To remove unnecessary protection elements of StorageContainer */
-delete  
+delete
 FROM csm_protection_element
 WHERE regexp_like(object_id,  '_[[:digit:]]')
    AND NOT regexp_like(object_id,   'Site_[[:digit:]]')
    AND NOT regexp_like(object_id,   'CollectionProtocol_[[:digit:]]')
-   AND NOT regexp_like(object_id,   'User_[[:digit:]]');
+   AND NOT regexp_like(object_id,   'User_[[:digit:]]')
+   AND object_id NOT LIKE 'SITE_%';
   
