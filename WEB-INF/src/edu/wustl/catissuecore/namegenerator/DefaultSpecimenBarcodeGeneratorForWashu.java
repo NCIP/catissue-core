1ffaa16e8f661a1f03d0917c4e75f79261644687
virender_mehta <virender_mehta>
2009-01-22 09:43:02 +0000

    CheckStyle Pmd
    SVN-Revision: 17503

--- WEB-INF/src/edu/wustl/catissuecore/namegenerator/DefaultSpecimenBarcodeGeneratorForWashu.java
+++ WEB-INF/src/edu/wustl/catissuecore/namegenerator/DefaultSpecimenBarcodeGeneratorForWashu.java
@@ -1,3 +1,4 @@
+
 package edu.wustl.catissuecore.namegenerator;
 
 import java.sql.Connection;
@@ -16,6 +17,7 @@ import javax.sql.DataSource;
 import edu.wustl.catissuecore.domain.Specimen;
 import edu.wustl.catissuecore.util.global.Constants;
 import edu.wustl.catissuecore.util.global.Variables;
+
 /**
  * This class  contains the default  implementation for Specimen Barcode generation.
  * @author falguni_sachde
@@ -23,196 +25,210 @@ import edu.wustl.catissuecore.util.global.Variables;
  */
 public class DefaultSpecimenBarcodeGeneratorForWashu implements BarcodeGenerator
 {
+
 	/**
-	 * Current Barcode 
+	 * Current Barcode.
 	 */
 	protected Long currentBarcode;
 	/**
-	 * Datasource Name
+	 * Datasource Name.
 	 */
 	String DATASOURCE_JNDI_NAME = "java:/catissuecore";
+
 	/**
-	 * Default Constructor
+	 * Default Constructor.
 	 */
 	public DefaultSpecimenBarcodeGeneratorForWashu()
 	{
 		super();
 		init();
 	}
-	
+
 	/**
-	 * This is a init() function it is called from the default constructor of Base class.When getInstance of base class
-	 * called then this init function will be called.
-	 * This method will first check the Datatbase Name and then set function name that will convert
-	 * lable from int to String
+	 * This is a init() function it is called from the default constructor
+	 * of Base class.When getInstance of base class called then this init
+	 * function will be called.
+	 * This method will first check the Datatbase Name and then set function
+	 * name that will convert lable from int to String
 	 */
 	protected void init()
-	{		
+	{
 		try
 		{
-			if(Constants.ORACLE_DATABASE.equals(Variables.databaseName))
+			if (Constants.ORACLE_DATABASE.equals(Variables.databaseName))
 			{
-				currentBarcode = getLastAvailableSpecimenBarcode(Constants.ORACLE_MAX_BARCODE_COL);
+				currentBarcode = getLastAvailableSpecimenBarcode
+								(Constants.ORACLE_MAX_BARCODE_COL);
 			}
 			else if (Constants.MSSQLSERVER_DATABASE.equals(Variables.databaseName))
 			{
-				currentBarcode = getLastAvailableSpecimenBarcode(Constants.MSSQLSERVER_MAX_BARCODE_COL);
+				currentBarcode = getLastAvailableSpecimenBarcode
+								(Constants.MSSQLSERVER_MAX_BARCODE_COL);
 			}
 			else
 			{
 				currentBarcode = getLastAvailableSpecimenBarcode(Constants.MYSQL_MAX_BARCODE_COL);
 			}
-		}catch(Exception ex)
+		}
+		catch (Exception ex)
 		{
 			ex.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/**
 	 * This method will retrive unique specimen Barcode.
-	 * @param databaseConstant
+	 * @param databaseConstant database constant
 	 * @return noOfRecords
 	 */
-	private Long getLastAvailableSpecimenBarcode(String databaseConstant)  
+	private Long getLastAvailableSpecimenBarcode(String databaseConstant)
 	{
-		String sql = "select MAX("+databaseConstant+") from CATISSUE_SPECIMEN";
- 		Connection conn = null;
+		String sql = "select MAX(" + databaseConstant + ") from CATISSUE_SPECIMEN";
+		Connection conn = null;
 		Long noOfRecords = new Long("0");
-        try
+		try
 		{
-        	InitialContext ctx = new InitialContext();
-        	DataSource ds = (DataSource)ctx.lookup(DATASOURCE_JNDI_NAME);
-        	conn = ds.getConnection();
-        	ResultSet resultSet= conn.createStatement().executeQuery(sql);
-        	
-        	if(resultSet.next())
-        	{
-        		return new Long (resultSet.getLong(1));
-        	}	        
+			InitialContext ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup(DATASOURCE_JNDI_NAME);
+			conn = ds.getConnection();
+			ResultSet resultSet = conn.createStatement().executeQuery(sql);
+
+			if (resultSet.next())
+			{
+				return new Long(resultSet.getLong(1));
+			}
 		}
-        catch(NamingException e){
-        	e.printStackTrace();
-        }
-        catch(SQLException ex)
-        {
-        	ex.printStackTrace();
-        }
-        finally
-        {
-        	if (conn!=null)
-        	{
-        		try {
+		catch (NamingException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			if (conn != null)
+			{
+				try
+				{
 					conn.close();
-				} catch (SQLException exception) {
+				}
+				catch (SQLException exception)
+				{
 					// TODO Auto-generated catch block
 					exception.printStackTrace();
 				}
-        	}
-        }
-        return noOfRecords;
+			}
+		}
+		return noOfRecords;
 	}
 
-	
-	
 	/**
 	 * Map of objects.
 	 */
 	Map barcodeCountTreeMap = new HashMap();
-	
-	
+
 	/**
-	 * @param parentObject
-	 * @param specimenObject
+	 * @param parentObject Parent Specimen Object
+	 * @param specimenObject specimen object
 	 */
-	 synchronized  void setNextAvailableAliquotSpecimenBarcode(Specimen parentObject,Specimen specimenObject) {
-				
+	synchronized void setNextAvailableAliquotSpecimenBarcode(Specimen parentObject,
+			Specimen specimenObject)
+	{
+
 		String parentSpecimenBarcode = (String) parentObject.getBarcode();
 		long aliquotChildCount = 0;
-		
-		if(barcodeCountTreeMap.containsKey(parentSpecimenBarcode))
+
+		if (barcodeCountTreeMap.containsKey(parentSpecimenBarcode))
 		{
-			 aliquotChildCount= Long.parseLong(barcodeCountTreeMap.get(parentSpecimenBarcode).toString());	
+			aliquotChildCount = Long.parseLong(barcodeCountTreeMap.get(parentSpecimenBarcode)
+					.toString());
 		}
 		else
 		{
-			// biz logic 
-			aliquotChildCount = parentObject.getChildSpecimenCollection().size();	
-			
+			// biz logic
+			aliquotChildCount = parentObject.getChildSpecimenCollection().size();
+
 		}
-		
-		specimenObject.setBarcode( parentSpecimenBarcode + "_" + (++aliquotChildCount) );
-		barcodeCountTreeMap.put(parentSpecimenBarcode,aliquotChildCount);	
-		
+
+		specimenObject.setBarcode(parentSpecimenBarcode + "_" + (++aliquotChildCount));
+		barcodeCountTreeMap.put(parentSpecimenBarcode, aliquotChildCount);
+
 	}
+
 	/**
-	 * @param parentObject
-	 * @param specimenObject
+	 * @param parentObject Parent Specimen Object
+	 * @param specimenObject Specimen object
 	 */
-	synchronized void setNextAvailableDeriveSpecimenBarcode(Specimen parentObject, Specimen specimenObject) {
-		
-		currentBarcode= currentBarcode+1;
-		specimenObject.setBarcode(currentBarcode.toString());
-		barcodeCountTreeMap.put(specimenObject,0);
-	}
-	
-	
-	
-	/* (non-Javadoc)
-	 * @see edu.wustl.catissuecore.namegenerator.BarcodeGenerator#setBarcode(edu.wustl.common.domain.AbstractDomainObject)
-	 */
-	synchronized public void setBarcode(Object obj) 
+	synchronized void setNextAvailableDeriveSpecimenBarcode(Specimen parentObject,
+			Specimen specimenObject)
 	{
-		Specimen objSpecimen = (Specimen)obj;
-		Specimen parentSpecimen = (Specimen)objSpecimen.getParentSpecimen();
-		if(!Constants.COLLECTION_STATUS_COLLECTED.equals(objSpecimen.getCollectionStatus()))
+
+		currentBarcode = currentBarcode + 1;
+		specimenObject.setBarcode(currentBarcode.toString());
+		barcodeCountTreeMap.put(specimenObject, 0);
+	}
+
+	/**
+	 * @param obj Specimen object
+	 */
+	synchronized public void setBarcode(Object obj)
+	{
+		Specimen objSpecimen = (Specimen) obj;
+		Specimen parentSpecimen = (Specimen) objSpecimen.getParentSpecimen();
+		if (!Constants.COLLECTION_STATUS_COLLECTED.equals(objSpecimen.getCollectionStatus()))
 		{
 			return;
 		}
-		if(!barcodeCountTreeMap.containsKey(objSpecimen) &&	objSpecimen.getLineage().equals(Constants.NEW_SPECIMEN))				
+		if (!barcodeCountTreeMap.containsKey(objSpecimen)
+				&& objSpecimen.getLineage().equals(Constants.NEW_SPECIMEN))
 		{
-			currentBarcode= currentBarcode+1;
+			currentBarcode = currentBarcode + 1;
 			objSpecimen.setBarcode(currentBarcode.toString());
-			barcodeCountTreeMap.put(objSpecimen.getBarcode(),0);
+			barcodeCountTreeMap.put(objSpecimen.getBarcode(), 0);
 		}
-		else if(!barcodeCountTreeMap.containsKey(objSpecimen) && objSpecimen.getLineage().equals(Constants.ALIQUOT))				
+		else if (!barcodeCountTreeMap.containsKey(objSpecimen)
+				&& objSpecimen.getLineage().equals(Constants.ALIQUOT))
 		{
-			
-			setNextAvailableAliquotSpecimenBarcode(parentSpecimen,objSpecimen);
+
+			setNextAvailableAliquotSpecimenBarcode(parentSpecimen, objSpecimen);
 		}
-	
-	
-		else if(!barcodeCountTreeMap.containsKey(objSpecimen) && objSpecimen.getLineage().equals(Constants.DERIVED_SPECIMEN))				
+
+		else if (!barcodeCountTreeMap.containsKey(objSpecimen)
+				&& objSpecimen.getLineage().equals(Constants.DERIVED_SPECIMEN))
 		{
-			
-			setNextAvailableDeriveSpecimenBarcode(parentSpecimen,objSpecimen);
+
+			setNextAvailableDeriveSpecimenBarcode(parentSpecimen, objSpecimen);
 		}
-		
-		if(objSpecimen.getChildSpecimenCollection().size()>0)
+
+		if (objSpecimen.getChildSpecimenCollection().size() > 0)
 		{
 			Collection specimenCollection = objSpecimen.getChildSpecimenCollection();
 			Iterator it = specimenCollection.iterator();
-			while(it.hasNext())
+			while (it.hasNext())
 			{
-				Specimen objChildSpecimen = (Specimen)it.next();
+				Specimen objChildSpecimen = (Specimen) it.next();
 				setBarcode(objChildSpecimen);
 			}
-		}		
-		
+		}
+
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.wustl.catissuecore.namegenerator.LabelGenerator#setBarcode(java.util.List)
+	/**
+	 * @param objSpecimenList Specimen object list.
 	 */
-	synchronized public void setBarcode(List objSpecimenList) {
+	synchronized public void setBarcode(List objSpecimenList)
+	{
 
 		List specimenList = objSpecimenList;
-		for (int index=0;index <specimenList.size();index++) 
+		for (int index = 0; index < specimenList.size(); index++)
 		{
-			Specimen objSpecimen = (Specimen)specimenList.get(index);
+			Specimen objSpecimen = (Specimen) specimenList.get(index);
 			setBarcode(objSpecimen);
 		}
-		
+
 	}
 
 }
