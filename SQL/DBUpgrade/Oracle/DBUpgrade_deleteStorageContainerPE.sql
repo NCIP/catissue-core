15c51f70ec631c56468c060ebd89765ed210c3ef
virender_mehta <virender_mehta>
2009-01-29 07:08:26 +0000

    added Alter table
    SVN-Revision: 17572

--- SQL/DBUpgrade/Oracle/DBUpgrade_deleteStorageContainerPE.sql
+++ SQL/DBUpgrade/Oracle/DBUpgrade_deleteStorageContainerPE.sql
@@ -6,4 +6,5 @@ WHERE regexp_like(object_id,  '_[[:digit:]]')
    AND NOT regexp_like(object_id,   'CollectionProtocol_[[:digit:]]')
    AND NOT regexp_like(object_id,   'User_[[:digit:]]')
    AND object_id NOT LIKE 'SITE_%';
-  
+
+ALTER TABLE catissue_disposal_event_param MODIFY REASON varchar(350);
