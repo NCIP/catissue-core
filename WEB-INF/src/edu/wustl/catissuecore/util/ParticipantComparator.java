7f1cec0dc38da2f4e3abbe6731b7ca57bf9cbc6c
shital_savargaonkar <shital_savargaonkar>
2009-02-12 14:21:10 +0000

    Removed PMD issues : pmd issues
    SVN-Revision: 17687

--- WEB-INF/src/edu/wustl/catissuecore/util/ParticipantComparator.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/ParticipantComparator.java
@@ -8,13 +8,15 @@ public class ParticipantComparator implements Comparator
 
 	public int compare(Object obj1, Object obj2)
 	{
-	    StringBuffer sb1 = new StringBuffer(obj1.toString());
+		StringBuffer sb1 = new StringBuffer(obj1.toString());
 	    StringBuffer sb2 = new StringBuffer(obj2.toString());
 	    String compString1 = sb1.substring(sb1.indexOf(":")+1);
 	    String compString2 = sb2.substring(sb2.indexOf(":")+1);
 	    boolean comparisonDone = false;
 	    String ppi1 = sb1.substring(sb1.indexOf("(")+1,sb1.indexOf(")"));
 	    String ppi2 = sb2.substring(sb2.indexOf("(")+1,sb2.indexOf(")"));
+	  	    
+	    
 	    if(sb1.substring(sb1.indexOf(":")+1,sb1.indexOf("(")).equals("N/A ") && 
 	       sb2.substring(sb2.indexOf(":")+1,sb2.indexOf("(")).equals("N/A ")) 
 	    {
@@ -27,14 +29,17 @@ public class ParticipantComparator implements Comparator
 	    	compString1 = ppi1;
 	    	compString2 = ppi2;
 	    }
+	    int returnVal=compString1.toLowerCase().compareTo(compString2.toLowerCase());
+	    
 	    if(!comparisonDone && sb1.substring(sb1.indexOf(":")+1,sb1.indexOf("(")).equals("N/A ") )
 	    {
-	    	return 1;
+	    	returnVal = 1;
 	    }
-	    if(!comparisonDone && sb2.substring(sb2.indexOf(":")+1,sb2.indexOf("(")).equals("N/A "))
+	    else if(!comparisonDone && sb2.substring(sb2.indexOf(":")+1,sb2.indexOf("(")).equals("N/A "))
 	    {
-	    	return -1;
+	    	returnVal = -1;
 	    }
-	    return (compString1.toLowerCase().compareTo(compString2.toLowerCase())) ;
+	   
+	    return returnVal;
 	  }          
 }
