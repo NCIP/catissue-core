857b5bb16e28cfc952fe0a6ee694d6bdae9fce17
kalpana_thakur <kalpana_thakur>
2009-03-23 14:43:13 +0000

    Modified CatissueSuiteTestSuite
    SVN-Revision: 18598

--- CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/CaTissueSuiteTestSuite.java
+++ CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/CaTissueSuiteTestSuite.java
@@ -1,17 +1,16 @@
 package edu.wustl.catissuecore.testcase;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
+import junit.framework.TestSuite;
 /**
  * Add here test cases to run.
- */
+ *//*
 @RunWith(Suite.class)
 @SuiteClasses(
 		{
-	    InitializationTestCase.class,
-		LoginTestCase.class,
+			edu.wustl.catissuecore.testcase.InitializationTestCase.class
+		//LoginTestCase.class,
+		//FirstTimeLoginTestCase.class
+		//DirectDistributionTestCase.class
 		InstitutionTestCases.class,
 		DepartmentTestCases.class,
 		CancerReaserchGroupTestCases.class,
@@ -25,7 +24,7 @@ import org.junit.runners.Suite.SuiteClasses;
 		ParticipantTestCases.class,
 		SpecimenCollectionGroupTestCases.class,
 		SpecimenTestCases.class
-		})
+		})*/
 /**
  * Test suite
  */ 
@@ -38,4 +37,47 @@ public class CaTissueSuiteTestSuite
 	{
 		super();
 	}
+	
+
+	/**
+	 * @param args :
+	 */
+	public static void main(String[] args)
+	{
+		//org.junit.runner.JUnitCore.main("edu.wustl.dao.test.HibernateInsertTestCase");
+
+		junit.awtui.TestRunner.run(CaTissueSuiteTestSuite.class);
+	}
+
+	/**
+	 * @return daoSuite.
+	 */
+	public static junit.framework.Test suite()
+	{
+		TestSuite strutsSuite = new TestSuite("Test struts cases");
+		//daoSuite.addTestSuite(HibernateTestCaseForCatissue.class);
+		
+		strutsSuite.addTestSuite(InitializationTestCase.class);
+		strutsSuite.addTestSuite(LoginTestCase.class);
+		//strutsSuite.addTestSuite(FirstTimeLoginTestCase.class);
+		strutsSuite.addTestSuite(InstitutionTestCases.class);
+		strutsSuite.addTestSuite(DepartmentTestCases.class);
+		strutsSuite.addTestSuite(CancerReaserchGroupTestCases.class);
+		strutsSuite.addTestSuite(SiteTestCases.class);
+		strutsSuite.addTestSuite(BiohazardTestCases.class);
+		strutsSuite.addTestSuite(DistributionProtocolTestCases.class);
+		strutsSuite.addTestSuite(SpecimenArrayTypeTestCases.class);
+		strutsSuite.addTestSuite(StorageTypeTestCases.class);
+		strutsSuite.addTestSuite(StorageContainerTestCases.class);
+		strutsSuite.addTestSuite(CollectionProtocolTestCases.class);
+		strutsSuite.addTestSuite(ParticipantTestCases.class);
+		strutsSuite.addTestSuite(SpecimenCollectionGroupTestCases.class);
+		strutsSuite.addTestSuite(SpecimenTestCases.class);
+		//daoSuite.addTestSuite(HibernateTestCaseForCider.class);
+		//daoSuite.addTestSuite(JDBCTestCasesForCider.class);
+		//return new junit.framework.JUnit4TestAdapter(HibernateInsertTestCase.class);
+		return strutsSuite;
+	}
+
+
 }
