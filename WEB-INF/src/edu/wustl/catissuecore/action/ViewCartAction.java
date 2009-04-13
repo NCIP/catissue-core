ef909f1d7248e1d48c65b1cb345c5ac417521563
kalpana_thakur <kalpana_thakur>
2009-04-13 07:52:23 +0000

    Checked action entries in struts-config.xml Added some classes which were missing
    SVN-Revision: 19428

---
+++ WEB-INF/src/edu/wustl/catissuecore/action/ViewCartAction.java
@@ -0,0 +1,61 @@
+package edu.wustl.catissuecore.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.catissuecore.util.global.Constants;
+import edu.wustl.query.querysuite.QueryShoppingCart;
+import edu.wustl.query.util.global.AQConstants;
+
+/**
+ * @author santhoshkumar_c
+ *
+ */
+public class ViewCartAction extends QueryShoppingCartAction 
+{
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.wustl.common.action.BaseAction#executeAction(org.apache.struts.action.ActionMapping,
+	 *      org.apache.struts.action.ActionForm,
+	 *      javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	
+	@Override
+	protected ActionForward executeAction(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+			HttpServletResponse response) throws Exception
+	{
+		//AdvanceSearchForm searchForm = (AdvanceSearchForm) form;
+		HttpSession session = request.getSession();
+		String target=null;
+
+		QueryShoppingCart cart = (QueryShoppingCart) session
+			.getAttribute(Constants.QUERY_SHOPPING_CART);			
+	
+		// Check if user wants to view the cart.		  
+		request.setAttribute(Constants.EVENT_PARAMETERS_LIST,Constants.EVENT_PARAMETERS);
+		setCartView(request, cart);
+		target = new String(Constants.VIEW);
+		session.removeAttribute(AQConstants.HYPERLINK_COLUMN_MAP);
+		String eventArray[] = Constants.EVENT_PARAMETERS;
+		String newEvenetArray[] = new String[2];
+		int count=0;
+		for(String eventName:eventArray)
+		{
+			if(("Transfer").equals(eventName)||("Disposal").equals(eventName))
+			{
+				newEvenetArray[count]=eventName;
+				count++;
+			}
+		}
+		request.setAttribute("eventArray", newEvenetArray);
+		return mapping.findForward(target);
+	}
+}
