64fb1883280008abaf152dcfc859910d196d973e
himanshu_aseeja <himanshu_aseeja>
2009-04-17 09:22:21 +0000

    shipping tracking merge in catissue
    SVN-Revision: 19739

---
+++ WEB-INF/test/edu/wustl/catissuecore/bizlogic/shippingtracking/TestCreateShipmentBizLogic.java
@@ -0,0 +1,82 @@
+package edu.wustl.catissuecore.bizlogic.shippingtracking;
+import junit.framework.TestCase;
+
+/**
+ *<p>Title: </p>
+ *<p>Description:  </p>
+ *<p>Copyright:TODO</p>
+ *@author 
+ *@version 1.0
+ */
+
+public class TestCreateShipmentBizLogic extends TestCase
+{
+
+    public TestCreateShipmentBizLogic()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public TestCreateShipmentBizLogic(String arg0)
+    {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void testCreateShipment()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithSameSourceAndTargetSites()
+    {
+        
+    }
+    
+    public void testCreateShipmentForProperContactPerson()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithNoContainersOrSpecimens()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithOnlyContainers()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithOnlySpecimens()
+    {
+        
+    }
+    public void testCreateShipmentWithMixContainersAndSpecimens()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithNoBarcode()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithDisabledContainers()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithDisabledSpecimens()
+    {
+        
+    }
+    
+    public void testCreateShipmentWithWrongSpecimenLabel()
+    {
+        
+    }
+    
+    
+}
