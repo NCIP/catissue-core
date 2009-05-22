02fd2379c9e308ab420973091f3c8f82b55b3b6f
janhavi_hasabnis <janhavi_hasabnis>
2009-05-22 05:18:07 +0000

    Added testcases for bugs
    SVN-Revision: 20731

--- CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/SpecimenTestCases.java
+++ CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/SpecimenTestCases.java
@@ -1,7 +1,8 @@
 package edu.wustl.catissuecore.testcase;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 import org.junit.Test;
 
 import edu.wustl.catissuecore.actionForm.NewSpecimenForm;
@@ -10,6 +11,10 @@ import edu.wustl.catissuecore.domain.Biohazard;
 import edu.wustl.catissuecore.domain.Specimen;
 import edu.wustl.catissuecore.domain.SpecimenCharacteristics;
 import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
+import edu.wustl.catissuecore.domain.StorageContainer;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.exception.BizLogicException;
 /**
  * This class contains test cases for Specimen add and checks for label and barcode after its storage position is changed
  * @author Himanshu Aseeja
@@ -122,4 +127,222 @@ public class SpecimenTestCases extends CaTissueSuiteBaseTest
         assertEquals(form.getLabel(),"1234" );
         assertEquals(form.getBarcode(),"1234");
   	}
+	
+	
+	//bug 11174
+	@Test
+	public void testSpecimenEdit()
+	{
+		//Simple Search Action
+		setRequestPathInfo("/SimpleSearch");
+	    addRequestParameter("aliasName", "StorageContainer");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_DataElement_table)", "Specimen");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_DataElement_field)","Specimen.BARCODE.varchar");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_Operator_operator)","Starts With");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_value)","1");
+		addRequestParameter("pageOf","pageOfNewSpecimen");
+		addRequestParameter("operation","search");
+		actionPerform();
+
+		Specimen specimen = (Specimen) TestCaseUtility.getNameObjectMap("Specimen");
+		DefaultBizLogic bizLogic = new DefaultBizLogic();
+		List<Specimen> specimenList = null;
+		try 
+		{
+			specimenList = bizLogic.retrieve("Specimen");
+		}
+		catch (BizLogicException e) 
+		{
+			e.printStackTrace();
+			System.out.println("SpecimenTestCases.testSpecimenEdit(): "+e.getMessage());
+			fail(e.getMessage());
+		}
+		
+		if(specimenList.size() > 1)
+		{
+		    verifyForward("success");
+		    for(Specimen specimenFromList : specimenList)
+		    {
+		    	if(!specimenFromList.getCollectionStatus().equals("Collected") && specimenFromList.getId()!=null && specimenFromList.getLineage().equals("New"))
+		    	{
+		    		TestCaseUtility.setNameObjectMap("specimen",specimenFromList);
+		    		setRequestPathInfo("/NewSpecimenSearch");
+		    		addRequestParameter("id","" + specimenFromList.getId());
+		    		actionPerform();
+		    		verifyForward("pageOfNewSpecimen");
+		    		verifyNoActionErrors();
+		    		//edit operation
+		    		
+		    		specimenFromList.setLabel("spcimen1_" + UniqueKeyGeneratorUtil.getUniqueKey());
+		    		addRequestParameter("label",specimenFromList.getLabel());
+		    	    setRequestPathInfo("/NewSpecimenEdit");
+		    	    addRequestParameter("stContSelection", "2");
+		    	   // addRequestParameter("available", "true");
+		    	    addRequestParameter("collectionStatus", "Collected"); 
+		    	    StorageContainer container = (StorageContainer) TestCaseUtility.getNameObjectMap("StorageContainer");
+		    	    addRequestParameter("storageContainer", container.getId().toString()); 
+		    	    addRequestParameter("positionDimensionOne", "2"); 
+		    	    addRequestParameter("positionDimensionTwo", "2"); 
+		    		addRequestParameter("operation", "edit");
+		    		actionPerform();
+		    		verifyForward("success");
+		    		verifyNoActionErrors();		
+		    		NewSpecimenForm form= (NewSpecimenForm) getActionForm();
+		    	    assertEquals(true,form.isAvailable());
+		    		TestCaseUtility.setNameObjectMap("specimen",specimenFromList);
+		    		break;
+		    	}
+		    }
+		    verifyNoActionErrors();
+		}
+		else if(specimenList.size() == 1)
+		{
+			verifyForwardPath("/SearchObject.do?pageOf=pageOfStorageContainer&operation=search&id=" + specimen.getId());
+			verifyNoActionErrors();
+		}
+		else
+		{
+			verifyForward("failure");
+			//verify action errors
+			String errorNames[] = new String[]{"simpleQuery.noRecordsFound"};
+			verifyActionErrors(errorNames);
+		}			
+	
+	}
+	
+	@Test
+	//bug 11829
+	public void testSpecimenAddToMyList()
+	{
+		/*Simple Search Action*/
+		setRequestPathInfo("/SimpleSearch");
+	    addRequestParameter("aliasName", "Specimen");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_DataElement_table)", "Specimen");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_DataElement_field)","Specimen.BARCODE.varchar");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_Operator_operator)","Starts With");
+		addRequestParameter("value(SimpleConditionsNode:1_Condition_value)","1");
+		addRequestParameter("pageOf","pageOfNewSpecimen");
+		addRequestParameter("operation","search");
+		actionPerform();
+		verifyForward("success");
+		Specimen specimen = (Specimen) TestCaseUtility.getNameObjectMap("Specimen");
+		DefaultBizLogic bizLogic = new DefaultBizLogic();
+		List<Specimen> specimenList = null;
+		try 
+		{
+			specimenList = bizLogic.retrieve("Specimen");
+		}
+		catch (BizLogicException e) 
+		{
+			e.printStackTrace();
+			System.out.println("SpecimenTestCases.testSpecimenEdit(): "+e.getMessage());
+			fail(e.getMessage());
+		}
+		
+		if(specimenList.size() > 1)
+		{
+			verifyForward("success");
+ 	  	    verifyNoActionErrors();
+		}
+		else if(specimenList.size() == 1)
+		{
+			verifyForwardPath("/SearchObject.do?pageOf=pageOfStorageContainer&operation=search&id=" + specimen.getId());
+			verifyNoActionErrors();
+		}
+		else
+		{
+			verifyForward("failure");
+			//verify action errors
+			String errorNames[] = new String[]{"simpleQuery.noRecordsFound"};
+			verifyActionErrors(errorNames);
+		}	
+		
+		setRequestPathInfo("/NewSpecimenSearch");
+		addRequestParameter("id","" + specimen.getId());
+		actionPerform();
+		verifyForward("pageOfNewSpecimen");
+		verifyNoActionErrors();
+		
+		setRequestPathInfo("/AddSpecimenToCart");
+		addRequestParameter("pageOf", "cpChildSubmit");
+		actionPerform();
+		
+		
+		verifyForward("cpChildSubmit");
+		verifyNoActionErrors();
+	    setRequestPathInfo("/AnticipatorySpecimenView");
+		HashMap forwardToHashMap = new HashMap();
+   		forwardToHashMap.put("specimenCollectionGroupId", specimen.getSpecimenCollectionGroup().getId());
+		forwardToHashMap.put("specimenId", specimen.getId());
+		getRequest().setAttribute("forwardToHashMap",forwardToHashMap);
+		addRequestParameter("target", "pageOfMultipleSpWithMenu");
+		actionPerform();
+	    verifyForward("pageOfMultipleSpWithMenu");
+		verifyNoActionErrors();		    
+	
+
+	}
+	@Test
+	public void testSupervisorLogin()
+	  {
+		   	addRequestParameter("loginName","super@super.com");
+	        addRequestParameter("password","Test123");
+	        setRequestPathInfo("/Login.do");
+	        actionPerform();
+	        //verifyForward("/Home.do");
+	        verifyForward("success");
+
+	        SessionDataBean bean = (SessionDataBean)getSession().getAttribute("sessionData");
+	        assertEquals("user name should be equal to loggedinusername","super@super.com",bean.getUserName());
+	        CaTissueSuiteTestUtil.USER_SESSION_DATA_BEAN=bean;
+	        verifyNoActionErrors();
+	  }
+	//bug 11659
+	@Test
+	public void testSpecimenEditBySupervisor()
+	{
+		Specimen specimen = (Specimen) TestCaseUtility.getNameObjectMap("Specimen");
+		DefaultBizLogic bizLogic = new DefaultBizLogic();
+		List<Specimen> specimenList = null;
+		try 
+		{
+			specimenList = bizLogic.retrieve("Specimen");
+		}
+		catch (BizLogicException e) 
+		{
+			e.printStackTrace();
+			System.out.println("SpecimenTestCases.testSpecimenEdit(): "+e.getMessage());
+			fail(e.getMessage());
+		}
+		
+		if(specimenList.size() > 1)
+		{
+			verifyForward("success");
+ 	  	    verifyNoActionErrors();		
+		}
+		else if(specimenList.size() == 1)
+		{
+			verifyForwardPath("/SearchObject.do?pageOf=pageOfStorageContainer&operation=search&id=" + specimen.getId());
+			verifyNoActionErrors();
+		}
+		else
+		{
+			verifyForward("failure");
+			//verify action errors
+			String errorNames[] = new String[]{"simpleQuery.noRecordsFound"};
+			verifyActionErrors(errorNames);
+		}
+		
+		addRequestParameter("label",specimen.getLabel());
+		setRequestPathInfo("/NewSpecimenEdit");
+	    addRequestParameter("tissueSide", "Right");
+		addRequestParameter("operation", "edit");
+		actionPerform();
+		verifyForward("failure");
+		//verify action errors
+		String errormsg[] = new String[] {"access.addedit.object.denied"};
+		verifyActionErrors(errormsg);
+		
+	}	
+	
 }
