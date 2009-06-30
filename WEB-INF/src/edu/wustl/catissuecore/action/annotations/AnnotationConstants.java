5501dac552d496245d99f0d64db7cc148972fecd
renuka_bajpai <renuka_bajpai>
2009-06-30 13:24:10 +0000

    Removed check style error.
    SVN-Revision: 21883

--- WEB-INF/src/edu/wustl/catissuecore/action/annotations/AnnotationConstants.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/annotations/AnnotationConstants.java
@@ -4,9 +4,9 @@
  *<p>Copyright:TODO</p>
  *@author 
  *@version 1.0
- */ 
-package edu.wustl.catissuecore.action.annotations;
+ */
 
+package edu.wustl.catissuecore.action.annotations;
 
 /**
  * @author preeti_munot
@@ -14,65 +14,63 @@ package edu.wustl.catissuecore.action.annotations;
  * To change the template for this generated type comment go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-public class AnnotationConstants
-{
-    public static final String SELECTED_STATIC_ENTITYID = "selectedStaticEntityId";
-    public static final String SELECTED_STATIC_ENTITY_RECORDID = "selectedStaticEntityRecordId";
-    
-    public static final String SELECTED_STATIC_RECORDID = "selectedStaticRecordId";
-    public static final String TYPEID = "typeId";
-    
-    public static final String ENTITY_ID_IN_CONDITION = "entityIdInCondition";
-    public static final String ENTITY_RECORDID_IN_CONDITION = "entityRecordIdInCondition";
-    
-    public static final String SELECTED_ENTITY_MAP_ID = "selectedEntityMapId";
-    public static final String DYNAMIC_EXTN_URL_ATTRIB = "dynamicExtensionsURL";
-    public static final String DYNAMIC_EXTN_DATA_ENTRY_URL_ATTRIB = "dynamicExtensionsDataEntryURL";
-    public static final String CALLBACK_URL_PATH_ANNOTATION_DEFN = "/DefineAnnotations.do";
-    public static final String CALLBACK_URL_PATH_ANNOTATION_DATA_ENTRY = "/LoadAnnotationDataEntryPage.do";
-    
-    public static final String AJAX_OPERATION = "operation";
-    public static final String AJAX_OPERATION_SELECT_GROUP = "selectGroup";
-    public static final String AJAX_OPERATION_SELECTED_GROUPID = "groupId";
-    public static final String EDIT_SELECTED_ANNOTATION = "editSelectedAnnotation";
-    public static final String SELECTED_ANNOTATION="selectedFormId";
-    public static final String OPERATION = "operation";
-    public static final String PAGE_OF_EDIT_ANNOTATION = "pageOfEditAnnotation";
-    public static final String ENTITY_NAME = "entityName";
-    
-    public static final String ENTITY_NAME_PARTICIPANT = "edu.wustl.catissuecore.domain.Participant";
-    public static final String ENTITY_NAME_SPECIMEN = "edu.wustl.catissuecore.domain.Specimen";
-    public static final String ENTITY_NAME_SPECIMEN_COLLN_GROUP = "edu.wustl.catissuecore.domain.SpecimenCollectionGroup";
-  public static final String ENTITY_NAME_COLLECTION_PROTOCOL = "edu.wustl.catissuecore.domain.CollectionProtocol";
-    
-    
-    public static final String PARTICIPANT = "Participant";
-    public static final String SPECIMEN = "Specimen";
-    public static final String SPECIMEN_COLLN_GROUP = "Specimen Collection Group";
-   // public static final String COLLECTION_PROTOCOL_REGISTRATION = "Collection Protocol Registration";
-    
-    
-    public static final String REQST_PARAM_ENTITY_ID = "entityId";
-    public static final String REQST_PARAM_ENTITY_RECORD_ID = "entityRecordId";
-    public static final String COLLECTION_PROTOCOL_ENTITY_ID = "collectionProtocolEntityId";
-    public static final String PARTICIPANT_ENTITY_ID =  "participantEntityId";
-    public static final String SPECIMEN_ENTITY_ID =  "specimenEntityId";
-    public static final String SCG_ENTITY_ID =  "scgEntityId";
-    
-    public static final String REQST_PARAM_CONDITION_ENTITY_ID = "conditionEntityId";
-    public static final String REQST_PARAM_CONDITION_ENTITY_RECORD_ID = "conditionEntityRecordId";
-    
-    public static final String STATUS_ATTACHED = "Attached";
-    public static final String STATUS_DETACHED = "Detached";
-    public static final String STATIC_ENTITY_LIST  = "staticEntityList";
-    
-    public static final String NONE = "None";
-    public static final String HTML_SPACE="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-	public static final String ANNOTATION_LIST_FROM_XML  ="annotationListFromXML";
-	public static final String STATIC_ENTITY_ID="staticEntityId";
-	public static final String STATIC_ENTITY_RECORD_ID="staticEntityRecordId";
-	public static final String NULL="null";
-	public static final String RECORDS_IDs="RecordIds";
+public class AnnotationConstants {
+
+	public static final String SELECTED_STATIC_ENTITYID = "selectedStaticEntityId";
+	public static final String SELECTED_STATIC_ENTITY_RECORDID = "selectedStaticEntityRecordId";
+
+	public static final String SELECTED_STATIC_RECORDID = "selectedStaticRecordId";
+	public static final String TYPEID = "typeId";
+
+	public static final String ENTITY_ID_IN_CONDITION = "entityIdInCondition";
+	public static final String ENTITY_RECORDID_IN_CONDITION = "entityRecordIdInCondition";
+
+	public static final String SELECTED_ENTITY_MAP_ID = "selectedEntityMapId";
+	public static final String DYNAMIC_EXTN_URL_ATTRIB = "dynamicExtensionsURL";
+	public static final String DYNAMIC_EXTN_DATA_ENTRY_URL_ATTRIB = "dynamicExtensionsDataEntryURL";
+	public static final String CALLBACK_URL_PATH_ANNOTATION_DEFN = "/DefineAnnotations.do";
+	public static final String CALLBACK_URL_PATH_ANNOTATION_DATA_ENTRY = "/LoadAnnotationDataEntryPage.do";
+
+	public static final String AJAX_OPERATION = "operation";
+	public static final String AJAX_OPERATION_SELECT_GROUP = "selectGroup";
+	public static final String AJAX_OPERATION_SELECTED_GROUPID = "groupId";
+	public static final String EDIT_SELECTED_ANNOTATION = "editSelectedAnnotation";
+	public static final String SELECTED_ANNOTATION = "selectedFormId";
+	public static final String OPERATION = "operation";
+	public static final String PAGE_OF_EDIT_ANNOTATION = "pageOfEditAnnotation";
+	public static final String ENTITY_NAME = "entityName";
+
+	public static final String ENTITY_NAME_PARTICIPANT = "edu.wustl.catissuecore.domain.Participant";
+	public static final String ENTITY_NAME_SPECIMEN = "edu.wustl.catissuecore.domain.Specimen";
+	public static final String ENTITY_NAME_SPECIMEN_COLLN_GROUP = "edu.wustl.catissuecore.domain.SpecimenCollectionGroup";
+	public static final String ENTITY_NAME_COLLECTION_PROTOCOL = "edu.wustl.catissuecore.domain.CollectionProtocol";
+
+	public static final String PARTICIPANT = "Participant";
+	public static final String SPECIMEN = "Specimen";
+	public static final String SPECIMEN_COLLN_GROUP = "Specimen Collection Group";
+	// public static final String COLLECTION_PROTOCOL_REGISTRATION = "Collection Protocol Registration";
+
+	public static final String REQST_PARAM_ENTITY_ID = "entityId";
+	public static final String REQST_PARAM_ENTITY_RECORD_ID = "entityRecordId";
+	public static final String COLLECTION_PROTOCOL_ENTITY_ID = "collectionProtocolEntityId";
+	public static final String PARTICIPANT_ENTITY_ID = "participantEntityId";
+	public static final String SPECIMEN_ENTITY_ID = "specimenEntityId";
+	public static final String SCG_ENTITY_ID = "scgEntityId";
+
+	public static final String REQST_PARAM_CONDITION_ENTITY_ID = "conditionEntityId";
+	public static final String REQST_PARAM_CONDITION_ENTITY_RECORD_ID = "conditionEntityRecordId";
+
+	public static final String STATUS_ATTACHED = "Attached";
+	public static final String STATUS_DETACHED = "Detached";
+	public static final String STATIC_ENTITY_LIST = "staticEntityList";
+
+	public static final String NONE = "None";
+	public static final String HTML_SPACE = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	public static final String ANNOTATION_LIST_FROM_XML = "annotationListFromXML";
+	public static final String STATIC_ENTITY_ID = "staticEntityId";
+	public static final String STATIC_ENTITY_RECORD_ID = "staticEntityRecordId";
+	public static final String NULL = "null";
+	public static final String RECORDS_IDs = "RecordIds";
 	public static final String FULLY_QUALIFIED_NAME = "edu.wustl.catissuecore.domain.";
 	public static final String EDIT = "Edit";
 }
