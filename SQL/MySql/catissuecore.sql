f3f8395209b1b436edcab0cacd1d9d6719bb99f4
pooja_tavase <pooja_tavase>
2009-11-12 11:21:16 +0000

    Changes to solve bug 14496 : Query table creation scripts in Advanced Query project.
    SVN-Revision: 23413

--- SQL/MySql/catissuecore.sql
+++ SQL/MySql/catissuecore.sql
@@ -1266,4 +1266,10 @@ create table CATISSUE_CLINICAL_DIAGNOSIS (
    CONSTRAINT FK_CD_COLPROT FOREIGN KEY (COLLECTION_PROTOCOL_ID) REFERENCES CATISSUE_COLLECTION_PROTOCOL (IDENTIFIER)
 );
 
+/*Query audit changes.*/
 
+alter table catissue_audit_event_query_log add column QUERY_ID bigint(20) default null; 
+alter table catissue_audit_event_query_log add column TEMP_TABLE_NAME varchar(150) default null;
+alter table catissue_audit_event_query_log add column IF_TEMP_TABLE_DELETED tinyint(1) default false;
+alter table catissue_audit_event_query_log add column ROOT_ENTITY_NAME varchar(150) default null;
+alter table catissue_audit_event_query_log add column COUNT_OF_ROOT_RECORDS bigint(20) default null;
--- SQL/Oracle/catissuecore.sql
+++ SQL/Oracle/catissuecore.sql
@@ -1152,6 +1152,12 @@ create table CATISSUE_CLINICAL_DIAGNOSIS (
 );
 create sequence CATISSUE_CLINICAL_DIAG_SEQ;
 
+/*Query audit changes.*/
+alter table catissue_audit_event_query_log ADD QUERY_ID number(20) default NULL; 
+alter table catissue_audit_event_query_log add TEMP_TABLE_NAME varchar(150) default null;
+alter table catissue_audit_event_query_log add IF_TEMP_TABLE_DELETED number(1) default 0;
+alter table catissue_audit_event_query_log add ROOT_ENTITY_NAME varchar(150) default null;
+alter table catissue_audit_event_query_log add COUNT_OF_ROOT_RECORDS number(20) default null;
 
 commit;
 
