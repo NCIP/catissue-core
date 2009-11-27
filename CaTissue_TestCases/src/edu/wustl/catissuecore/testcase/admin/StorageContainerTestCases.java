fc61702552ca967fb7dd6b013b3f1080409af4c8
nitesh_marwaha <nitesh_marwaha>
2009-11-27 12:16:59 +0000

    test cases added for UpdateContainerCapacity and CallToGetSiteList
    SVN-Revision: 23495

--- CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/admin/StorageContainerTestCases.java
+++ CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/admin/StorageContainerTestCases.java
@@ -5,19 +5,20 @@ import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Test;
+
 import edu.wustl.catissuecore.actionForm.StorageContainerForm;
 import edu.wustl.catissuecore.bizlogic.StorageContainerBizLogic;
-import edu.wustl.catissuecore.bizlogic.StorageTypeBizLogic;
 import edu.wustl.catissuecore.domain.Capacity;
 import edu.wustl.catissuecore.domain.Site;
 import edu.wustl.catissuecore.domain.StorageContainer;
 import edu.wustl.catissuecore.domain.StorageType;
+import edu.wustl.catissuecore.domain.User;
 import edu.wustl.catissuecore.testcase.CaTissueSuiteBaseTest;
 import edu.wustl.catissuecore.testcase.util.CaTissueSuiteTestUtil;
 import edu.wustl.catissuecore.testcase.util.TestCaseUtility;
-import edu.wustl.catissuecore.testcase.util.UniqueKeyGeneratorUtil;
-import edu.wustl.common.bizlogic.DefaultBizLogic;
+import edu.wustl.common.exception.AssignDataException;
 import edu.wustl.common.exception.BizLogicException;
+import edu.wustl.common.util.global.Status;
 
 
 /**
@@ -37,7 +38,7 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 //		storageType.setName("TissueStorageType_1257421218783");
 //		storageType.setId(6L);
 //		TestCaseUtility.setNameObjectMap("StorageType", storageType);
-//		
+//
 //		Site s = new Site();
 //		s.setId(1L);
 //		TestCaseUtility.setNameObjectMap("Site", s);
@@ -53,9 +54,9 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		storageContainerForm.setTypeId(storageType.getId());
 		logger.info("----StorageTypeId : " + storageType.getId());
 		storageContainerForm.setTypeName(storageType.getName());
-		
+
 		Site site = (Site) TestCaseUtility.getNameObjectMap("Site");
-		
+
 		storageContainerForm.setSiteId(site.getId());
 		storageContainerForm.setNoOfContainers(1);
 		storageContainerForm.setOneDimensionCapacity(25);
@@ -65,13 +66,13 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		storageContainerForm.setDefaultTemperature("29");
 		/*addRequestParameter("holdsSpecimenClassTypes", "Cell");
 		addRequestParameter("specimenOrArrayType", "SpecimenArray");*/
-		
+
 		String[] holdsSpecimenClassCollection = new String[4];
 		holdsSpecimenClassCollection[0]="Fluid";
 		holdsSpecimenClassCollection[1]="Tissue";
 		holdsSpecimenClassCollection[2]="Molecular";
 		holdsSpecimenClassCollection[3]="Cell";
-		
+
 		storageContainerForm.setSpecimenOrArrayType("Specimen");
 		storageContainerForm.setHoldsSpecimenClassTypes(holdsSpecimenClassCollection);
 		storageContainerForm.setActivityStatus("Active");
@@ -83,24 +84,24 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		actionPerform();
 		verifyForward("success");
 		verifyNoActionErrors();
-		
+
 		StorageContainerForm form=(StorageContainerForm) getActionForm();
 		StorageContainer storageContainer = new StorageContainer();
-	
+
 //		storageContainer.setSite(site);
-	    Capacity capacity = new Capacity(); 
+	    Capacity capacity = new Capacity();
 	    capacity.setOneDimensionCapacity(form.getOneDimensionCapacity());
 	    capacity.setTwoDimensionCapacity(form.getTwoDimensionCapacity());
 	    storageContainer.setCapacity(capacity);
-	    
+
 	    storageContainer.setId(form.getId());
 	    logger.info("----StorageContainerId : " + storageContainer.getId());
 	    Collection<String> holdsSpecimenClassCollection1 = new HashSet<String>();
 	    String[] specimenClassTypes = form.getHoldsSpecimenClassTypes();
 	    holdsSpecimenClassCollection1.add(specimenClassTypes[0]);
 	    storageContainer.setHoldsSpecimenClassCollection(holdsSpecimenClassCollection1);
-	    
-	    TestCaseUtility.setNameObjectMap("StorageContainer",storageContainer);	    
+
+	    TestCaseUtility.setNameObjectMap("StorageContainer",storageContainer);
 	}
 	//bug 11546
 	/**
@@ -110,14 +111,14 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 	public void testAddFreezerContainer()
 	{
 		StorageType storageType = (StorageType) TestCaseUtility.getNameObjectMap("Freezer_StorageType");
-			
+
 		StorageContainerForm storageContainerForm = new StorageContainerForm();
 		storageContainerForm.setTypeId(storageType.getId());
 		storageContainerForm.setTypeName(storageType.getName());
-		
+
 		Site site = (Site) TestCaseUtility.getNameObjectMap("Site");
 		storageContainerForm.setSiteId(site.getId());
-		
+
 		storageContainerForm.setNoOfContainers(1);
 		storageContainerForm.setOneDimensionCapacity(5);
 		storageContainerForm.setTwoDimensionCapacity(5);
@@ -133,12 +134,12 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		storageContainerForm.setSpecimenOrArrayType("Specimen");
 		storageContainerForm.setHoldsSpecimenClassTypes(holdsSpecimenClassCollection);
 		storageContainerForm.setActivityStatus("Active");
-		storageContainerForm.setOperation("add");	
+		storageContainerForm.setOperation("add");
 		setRequestPathInfo("/StorageContainerAdd");
 		setActionForm(storageContainerForm);
 		actionPerform();
 		verifyForward("success");
-		verifyNoActionErrors();	
+		verifyNoActionErrors();
 	}
 	/**
 	 * Test Storage Container Add invalid parent container.
@@ -149,7 +150,7 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 	{
 		StorageContainer parentStorageContainer = (StorageContainer) TestCaseUtility.getNameObjectMap("StorageContainer");
 		StorageType storageType = (StorageType) TestCaseUtility.getNameObjectMap("StorageType");
-		
+
 		StorageContainerForm storageContainerForm = new StorageContainerForm();
 		storageContainerForm.setTypeId(storageType.getId());
 		storageContainerForm.setTypeName(storageType.getName());
@@ -168,12 +169,12 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		storageContainerForm.setSpecimenOrArrayType("Specimen");
 		storageContainerForm.setHoldsSpecimenClassTypes(holdsSpecimenClassCollection);
 		storageContainerForm.setActivityStatus("Active");
-		
+
 		storageContainerForm.setParentContainerSelected("Container (Manual)");
 		storageContainerForm.setContainerId(parentStorageContainer.getId().toString());
 		storageContainerForm.setPos1("1");
 		storageContainerForm.setPos2("1");
-		storageContainerForm.setOperation("add");	
+		storageContainerForm.setOperation("add");
 		setRequestPathInfo("/StorageContainerAdd");
 		setActionForm(storageContainerForm);
 		actionPerform();
@@ -185,15 +186,15 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 	{
 //		//TODO
 //		fail("Need to write test case");
-		
+
 		StorageContainerBizLogic bizLogic = new StorageContainerBizLogic() ;
-		
+
 		try
 		{
 			bizLogic.insert(null,CaTissueSuiteTestUtil.USER_SESSION_DATA_BEAN) ;
 			assertFalse("StorageType Object is NULL while inserting " +
 					"through BizLogic",true);
-		} 
+		}
 		catch (BizLogicException e)
 		{
 			logger.info("Exception in StorageContainer :" + e.getMessage());
@@ -221,17 +222,17 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		StorageContainer storageContainer = (StorageContainer) TestCaseUtility.getNameObjectMap("StorageContainer");
 		DefaultBizLogic bizLogic = new DefaultBizLogic();
 		List<StorageContainer> storageContainerList = null;
-		try 
+		try
 		{
 			storageContainerList = bizLogic.retrieve("StorageContainer");
 		}
-		catch (BizLogicException e) 
+		catch (BizLogicException e)
 		{
 			e.printStackTrace();
 			System.out.println("StorageContainerTestCases.testStorageContainerEdit(): "+e.getMessage());
 			fail(e.getMessage());
 		}
-		
+
 		if(storageContainerList.size() > 1)
 		{
 		    verifyForward("success");
@@ -249,7 +250,7 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 			String errorNames[] = new String[]{"simpleQuery.noRecordsFound"};
 			verifyActionErrors(errorNames);
 		}
-				
+
 		common search action.Generates StorageContainerForm
 		setRequestPathInfo("/StorageContainerSearch");
 		addRequestParameter("id","" + storageContainer.getId());
@@ -260,21 +261,99 @@ public class StorageContainerTestCases extends CaTissueSuiteBaseTest
 		edit operation
 		storageContainer.setName("container1_" + UniqueKeyGeneratorUtil.getUniqueKey());
 		addRequestParameter("name",storageContainer.getName());
-		
+
 		addRequestParameter("activityStatus","Closed");
 		addRequestParameter("isFull","True");
-		
+
 		setRequestPathInfo("/StorageContainerEdit");
 		addRequestParameter("specimenOrArrayType", "SpecimenArray");
 		addRequestParameter("operation", "edit");
 		actionPerform();
 		verifyForward("success");
 		verifyNoActionErrors();
-		
+
 		StorageContainerForm form = (StorageContainerForm) getActionForm();
 		assertEquals(form.getActivityStatus(), "Closed");
 		assertEquals(form.getIsFull(), "True");
-		
+
 		TestCaseUtility.setNameObjectMap("StorageContainer",storageContainer);
 	}*/
+
+	@Test
+	public void testUpdateContainerCapacity()
+	{
+		StorageContainer storageContainer = (StorageContainer) TestCaseUtility.getNameObjectMap("StorageContainer");
+		StorageContainerForm storageContainerForm = null;
+		//Retrieving Storage container object for edit
+		logger.info("----StorageConatiner ID : " + storageContainer.getId());
+		addRequestParameter("pageOf", "pageOfStorageContainer");
+		addRequestParameter("operation", "search");
+		addRequestParameter("id", storageContainer.getId().toString());
+		setRequestPathInfo("/SearchObject") ;
+		actionPerform();
+		verifyForward("pageOfStorageContainer");
+		verifyNoActionErrors();
+		System.out.println(getActualForward());
+		setRequestPathInfo(getActualForward());
+		actionPerform();
+
+		verifyNoActionErrors();
+		System.out.println(getActualForward());
+		setRequestPathInfo(getActualForward());
+		addRequestParameter("pageOf", "pageOfStorageContainer");
+		addRequestParameter("operation", "edit");
+		actionPerform();
+
+		verifyNoActionErrors();
+		//Action form
+		storageContainerForm=(StorageContainerForm) getActionForm();
+
+		//modifying the container capacity
+		storageContainerForm.setNoOfContainers(3);
+		storageContainerForm.setOneDimensionCapacity(30);
+		storageContainerForm.setTwoDimensionCapacity(30);
+		storageContainerForm.setOneDimensionLabel("row");
+		storageContainerForm.setTwoDimensionLabel("row");
+		storageContainerForm.setDefaultTemperature("28");
+		addRequestParameter("holdsSpecimenClassTypes", "Cell");
+		addRequestParameter("specimenOrArrayType", "SpecimenArray");
+
+		storageContainerForm.setOperation("edit");
+		setActionForm(storageContainerForm);
+		setRequestPathInfo("/StorageContainerEdit");
+		setActionForm(storageContainerForm);
+		actionPerform();
+		verifyForward("success");
+		verifyNoActionErrors();
+
+		verifyActionMessages(new String[]{"object.edit.successOnly"});
+		logger.info("#############Storage Container Updated Successfully##########");
+		try
+		{
+			storageContainer.setAllValues(storageContainerForm);
+		}
+		catch (AssignDataException e)
+		{
+			logger.debug(e.getMessage(),e);
+			fail("failed to assign values");
+		}
+		TestCaseUtility.setNameObjectMap("storageContainer", storageContainer);
+
+	}
+
+	@Test
+	public void testCallToGetSiteList() throws BizLogicException
+	{
+		final String[] siteDisplayField = {"name"};
+		final String valueField = "id";
+		final StorageContainerBizLogic bizLogic = new StorageContainerBizLogic();
+
+		final String[] activityStatusArray = {Status.ACTIVITY_STATUS_DISABLED.toString(),
+				Status.ACTIVITY_STATUS_CLOSED.toString()};
+		final List list = bizLogic.getSiteList(siteDisplayField, valueField, activityStatusArray,
+				1L);
+		assertNotNull(list);
+		logger.info(" ::: Site List Size ::: "+ list.size());
+
+	}
 }
