5eaa2247801a5f286085de481fefb6290139ae02
sagar_baldwa <sagar_baldwa>
2010-01-01 10:58:17 +0000

    Bug 15002 -  Bulk operations UI Issues
    SVN-Revision: 23766

--- SQL/MySql/catissuecore.sql
+++ SQL/MySql/catissuecore.sql
@@ -1301,30 +1301,6 @@ alter table catissue_audit_event_query_log add column IF_TEMP_TABLE_DELETED tiny
 alter table catissue_audit_event_query_log add column ROOT_ENTITY_NAME varchar(150) default null;
 alter table catissue_audit_event_query_log add column COUNT_OF_ROOT_RECORDS bigint(20) default null;
 
-/*Bulk Operations*/
-create table catissue_bulk_operation (
-	IDENTIFIER BIGINT(20) not null auto_increment,
-	OPERATION VARCHAR(100) not null,
-	CSV_TEMPLATE VARCHAR(5000) not null,
-	XML_TEMPALTE VARCHAR(15000) not null,
-	DROPDOWN_NAME VARCHAR(100) not null,
-	PRIMARY KEY  (`IDENTIFIER`), UNIQUE KEY OPERATION (`OPERATION`), UNIQUE KEY DROPDOWN_NAME (`DROPDOWN_NAME`)
-);
-CREATE TABLE JOB_DETAILS (
-	IDENTIFIER bigint(20) NOT NULL auto_increment,
-	JOB_NAME varchar(255) NOT NULL,
-	JOB_STATUS varchar(50) default NULL,
-	TOTAL_RECORDS_COUNT bigint(20) default NULL,
-	FAILED_RECORDS_COUNT bigint(20) default NULL,
-	TIME_TAKEN bigint(20) default NULL,
-	LOG_FILE blob,
-	JOB_STARTED_BY bigint(20) default NULL,
-	START_TIME datetime default NULL,
-	CURRENT_RECORDS_PROCESSED bigint(20) default NULL,
-	LOG_FILE_NAME varchar(50) default NULL,
-	PRIMARY KEY (`IDENTIFIER`)
-);
-
 /**
  * Container Type Req
  */
