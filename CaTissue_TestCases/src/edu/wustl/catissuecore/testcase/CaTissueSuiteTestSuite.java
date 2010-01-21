58eccba72e4445eb5bfc083df7601a79dc2fd200
janhavi_hasabnis <janhavi_hasabnis>
2010-01-21 05:30:11 +0000

    Added testcase for Add To My List.
    SVN-Revision: 23973

--- CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/CaTissueSuiteTestSuite.java
+++ CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/CaTissueSuiteTestSuite.java
@@ -12,6 +12,7 @@ import edu.wustl.catissuecore.testcase.admin.SiteTestCases;
 import edu.wustl.catissuecore.testcase.admin.SpecimenArrayTypeTestCases;
 import edu.wustl.catissuecore.testcase.admin.StorageContainerTestCases;
 import edu.wustl.catissuecore.testcase.admin.StorageTypeTestCases;
+import edu.wustl.catissuecore.testcase.biospecimen.AddToMyListTestCases;
 import edu.wustl.catissuecore.testcase.biospecimen.ConsentsTestCase;
 import edu.wustl.catissuecore.testcase.biospecimen.FlexInterfaceTestCase;
 import edu.wustl.catissuecore.testcase.biospecimen.LabelGeneratorTestCase;
@@ -98,6 +99,7 @@ public class CaTissueSuiteTestSuite
 		strutsSuite.addTestSuite(ParticipantTestCases.class);
 		strutsSuite.addTestSuite(SpecimenCollectionGroupTestCases.class);
 		strutsSuite.addTestSuite(SpecimenTestCases.class);
+		strutsSuite.addTestSuite(AddToMyListTestCases.class);
 	    strutsSuite.addTestSuite(FlexInterfaceTestCase.class);
 		strutsSuite.addTestSuite(SpecimenEventsTestcases.class);
 		strutsSuite.addTestSuite(ConsentsTestCase.class);
