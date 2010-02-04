810191608ba968d3dcbeaa69523049953d4caf60
sagar_baldwa <sagar_baldwa>
2010-02-04 07:46:38 +0000

    Removed uncommented code and added sysout's.
    SVN-Revision: 24087

--- CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/biospecimen/AddToMyListTestCases.java
+++ CaTissue_TestCases/src/edu/wustl/catissuecore/testcase/biospecimen/AddToMyListTestCases.java
@@ -16,46 +16,55 @@ public class AddToMyListTestCases extends CaTissueSuiteBaseTest
 	 */
 	public void testSpecimenAddToMyList()
 	{
-		//Simple Search Action
-		Specimen specimen = (Specimen) TestCaseUtility.getNameObjectMap("Specimen");
-		//Retrieving specimen object for edit
-		logger.info("----specimen ID : " + specimen.getId());
-		addRequestParameter("pageOf", "pageOfNewSpecimen");
-		addRequestParameter("operation", "search");
-		addRequestParameter("id", specimen.getId().toString());
-		setRequestPathInfo("/NewSpecimenSearch") ;
-		actionPerform();
-		verifyForward("pageOfNewSpecimen");
-		verifyNoActionErrors();
-		
-		System.out.println(getActualForward());
-		setRequestPathInfo(getActualForward());
-		addRequestParameter("pageOf", "pageOfNewSpecimen");
-		actionPerform();
-		verifyNoActionErrors();
-		
-		setRequestPathInfo("/NewSpecimenEdit") ;
-		NewSpecimenForm form = (NewSpecimenForm) getActionForm();
-		form.setForwardTo( "addSpecimenToCartForwardtoCpChild" );	
-		addRequestParameter("target", "addSpecimenToCartForwardtoCpChild");
-		addRequestParameter("pageOf", "cpChildSubmit");
-		actionPerform();
+		try
+		{
+			//Simple Search Action
+			Specimen specimen = (Specimen) TestCaseUtility.getNameObjectMap("Specimen");
+			//Retrieving specimen object for edit
+			logger.info("----specimen ID : " + specimen.getId());
+			addRequestParameter("pageOf", "pageOfNewSpecimen");
+			addRequestParameter("operation", "search");
+			addRequestParameter("id", specimen.getId().toString());
+			setRequestPathInfo("/NewSpecimenSearch") ;
+			actionPerform();
+			verifyForward("pageOfNewSpecimen");
+			verifyNoActionErrors();
 			
-		System.out.println(getActualForward());
-		setRequestPathInfo(getActualForward());
-		addRequestParameter("pageOf", "cpChildSubmit");
-		actionPerform();
-		
-		System.out.println(getActualForward());
-		setRequestPathInfo(getActualForward());
-		HashMap forwardToHashMap = new HashMap();
-		forwardToHashMap.put("specimenCollectionGroupId", specimen.getSpecimenCollectionGroup().getId());
-		forwardToHashMap.put("specimenId", specimen.getId());
-		getRequest().setAttribute("forwardToHashMap",forwardToHashMap);
-		addRequestParameter("target", "pageOfMultipleSpWithMenu");
-		actionPerform();
-		//verifyForward("pageOfMultipleSpWithMenu");
-		//verifyNoActionErrors();		
+			System.out.println(getActualForward());
+			setRequestPathInfo(getActualForward());
+			addRequestParameter("pageOf", "pageOfNewSpecimen");
+			actionPerform();
+			verifyNoActionErrors();
+			
+			setRequestPathInfo("/NewSpecimenEdit") ;
+			NewSpecimenForm form = (NewSpecimenForm) getActionForm();
+			form.setForwardTo( "addSpecimenToCartForwardtoCpChild" );	
+			addRequestParameter("target", "addSpecimenToCartForwardtoCpChild");
+			addRequestParameter("pageOf", "cpChildSubmit");
+			actionPerform();
+				
+			System.out.println(getActualForward());
+			setRequestPathInfo(getActualForward());
+			addRequestParameter("pageOf", "cpChildSubmit");
+			actionPerform();
+			System.out.println("Before: testSpecimenAddToMyList ");
+			System.out.println(getActualForward());
+			setRequestPathInfo(getActualForward());
+			HashMap forwardToHashMap = new HashMap();
+			forwardToHashMap.put("specimenCollectionGroupId", specimen.getSpecimenCollectionGroup().getId());
+			forwardToHashMap.put("specimenId", specimen.getId());
+			getRequest().setAttribute("forwardToHashMap",forwardToHashMap);
+			addRequestParameter("target", "pageOfMultipleSpWithMenu");
+			actionPerform();
+			verifyForward("pageOfMultipleSpWithMenu");
+			verifyNoActionErrors();
+			System.out.println("After: testSpecimenAddToMyList Success");
+		}
+		catch (Exception exp)
+		{
+			exp.printStackTrace();
+			System.out.println("Error : testSpecimenAddToMyList: " + exp.getMessage());
+		}
 
 	}
 }
