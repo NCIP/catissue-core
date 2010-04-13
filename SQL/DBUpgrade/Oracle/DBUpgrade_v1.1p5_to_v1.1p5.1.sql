097483e208be7e6bb0823418afef5e16699f5098
nitesh_marwaha <nitesh_marwaha>
2010-04-13 14:02:11 +0000

    change in sql script for label generation
    SVN-Revision: 24361

--- SQL/DBUpgrade/Oracle/DBUpgrade_v1.1p5_to_v1.1p5.1.sql
+++ SQL/DBUpgrade/Oracle/DBUpgrade_v1.1p5_to_v1.1p5.1.sql
@@ -1,2 +1,2 @@
-alter table QUERY_PARAMETERIZED_QUERY add (GENERATE_LABEL number(1,0) default 1);
-alter table QUERY_PARAMETERIZED_QUERY add (LABEL_FORMAT varchar(255) default null);
+alter table CATISSUE_SPECIMEN_PROTOCOL add (GENERATE_LABEL number(1,0) default 1);
+alter table CATISSUE_SPECIMEN_PROTOCOL add (LABEL_FORMAT varchar(255) default null);
