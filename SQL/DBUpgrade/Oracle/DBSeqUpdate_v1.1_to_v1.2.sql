012c51c16a22b50e31ce9d04e149409e5201bc4b
prachi_sharma <prachi_sharma>
2010-07-11 12:59:19 +0000

    * Task/Bug Id: Upgrade scripts. * Code Reviewed By: Sagar On : 11 Jul'10 * Technical Details:  Added a new target for updating the sequences and renamed the mail subject to V1.2 from Suite 1.1.p5. * Unit tested:   NA * Developer Notes:   Added a new target for updating the sequences and renamed the mail subject to V1.2 from Suite 1.1.p5. * Current Build++ NA : NA * PMD - NA    Checkstyle - NA     Findbugs - NA     CPD – NA
    SVN-Revision: 24612

---
+++ SQL/DBUpgrade/Oracle/DBSeqUpdate_v1.1_to_v1.2.sql
@@ -0,0 +1,3 @@
+alter sequence CSM_PROTECTIO_PROTECTION_G_SEQ increment by 20;
+select  CSM_PROTECTIO_PROTECTION_G_SEQ.nextval from dual;
+alter sequence CSM_PROTECTIO_PROTECTION_G_SEQ increment by 1;
