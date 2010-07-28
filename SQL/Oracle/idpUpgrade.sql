eebcfc6cd9c8ff49ac8ae0d0bd3dfb41f8d6bae2
niharika_sharma <niharika_sharma>
2010-07-28 16:00:05 +0000

    * Task/Bug Id: IDP Integration * Code Reviewed By: *  On : * Technical Details:  Updated the sql statement * Unit tested:   Yes * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 24712

--- SQL/Oracle/idpUpgrade.sql
+++ SQL/Oracle/idpUpgrade.sql
@@ -1,7 +1,9 @@
 /** This table is to support WUSTL Key migration */
 CREATE TABLE csm_migrate_user
-(                                                                                       
- LOGIN_NAME varchar2(100 Byte) NOT NULL,                                                                        
- WUSTLKEY varchar2(100 Byte) default NULL UNIQUE,                                                                                 
- primary key (LOGIN_NAME)              
+(
+ LOGIN_NAME varchar2(100 Byte) NOT NULL,
+ TARGET_IDP_NAME varchar2(100 Byte),
+ MIGRATED_LOGIN_NAME varchar2(100 Byte) default NULL UNIQUE,
+ MIGRATION_STATUS varchar2(100 Byte) NOT NULL,
+ primary key (LOGIN_NAME)
 );
