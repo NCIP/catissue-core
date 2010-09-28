a5e40fb9ade7a91826b61b67e5570e7e8a2c4d62
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2010-09-28 14:35:03 +0000

    * Task/Bug Id: <Development task name or bug id> * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  <Technical details for the changes made if any> * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 24748

--- jss/menu_home.js
+++ jss/menu_home.js
@@ -1,6 +1,6 @@
 /*
  * Ext JS Library 2.1
- * 
+ *
  */
 
 Ext.onReady(function(){
@@ -8,10 +8,10 @@ Ext.onReady(function(){
 
     var tb = new Ext.Toolbar();
     tb.render('toolbarLoggedOut');
-    tb.add(new Ext.Toolbar.Button({text: 'Home',link:'RedirectHome.do',handler: handleMenu}));
+    tb.add(new Ext.Toolbar.Button({text: 'Home',link:'CasLogin.do',handler: handleMenu}));
 
     // functions to display feedback
-    
+
 	function handleMenu(item)
 	{
 	document.location.href = item.link;
