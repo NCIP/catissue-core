6a31d1b9264bfc1c5528056780f43c06d6e539cf
sbaldwa <sbaldwa>
2011-02-28 14:30:39 +0000

    * Task/Bug Id: Removed all MsSqlServer scripts/folder from SQL folder. * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  <Technical details for the changes made if any> * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 25311

--- SQL/backup/DBUpGrade/initDB_update_09_Feb.sql
+++
@@ -1,2 +0,0 @@
-update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator Last Name' where RELATIONSHIP_ID=51 and COL_ID=292;
-update CATISSUE_SEARCH_DISPLAY_DATA set DISPLAY_NAME='Principal Investigator First Name' where RELATIONSHIP_ID=51 and COL_ID=293;
