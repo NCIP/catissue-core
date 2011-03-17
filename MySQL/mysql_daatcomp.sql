f21e60b4bf36b235c2bc212e07871fe292bddeb8
Prachi Sharma <prachi_sharma@persistent.co.in>
2011-03-17 15:59:58 +0000

    Removed unwanted tables
    SVN-Revision: 25507

--- MySQL/mysql_daatcomp.sql
+++ MySQL/mysql_daatcomp.sql
@@ -13,12 +13,3 @@ CREATE TABLE `comptablesdatarows` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 create table mysql.common_row_count (count_val int, tab_name varchar(300));
-
-/* create database test and create table asd into the database used for debugging */
-CREATE TABLE test.asd (temp_1 varchar(1000) default NULL);
-
--- comapre Db cmd
-call mysql.Sp_compTables ('catissuev12', 'catissuev112' )
-
-
-
