8e9dcd489c833cb364b536518c07d4bafd496c1e
Srikanth Adiga <srikanth.adiga@krishagni.com>
2011-06-16 18:51:12 +0000

    added the upgarde sql for empi id in participant table
    SVN-Revision: 26425

--- SQL/DBUpgrade/Oracle/DBUpgrade_v1.2.1_to_v1.2.2.sql
+++ SQL/DBUpgrade/Oracle/DBUpgrade_v1.2.1_to_v1.2.2.sql
@@ -16,6 +16,7 @@ alter table catissue_collection_protocol add IS_EMPI_ENABLE  number(1,0);
 update catissue_collection_protocol set IS_EMPI_ENABLE ='0';
 
 alter table CATISSUE_PARTICIPANT add EMPI_ID_STATUS varchar2(50);
+alter table catissue_participant add EMPI_ID varchar2(50);
 
 CREATE TABLE MATCHED_PARTICIPANT_MAPPING
 (
