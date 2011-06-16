acc0d54b50a12ba2e2f12dea075f94025ae01a86
Srikanth Adiga <srikanth.adiga@krishagni.com>
2011-06-16 16:39:59 +0000

    cahnged the name of the table
    SVN-Revision: 26423

--- SQL/DBUpgrade/MySql/DBUpgrade_v1.2.1_to_v1.2.2.sql
+++ SQL/DBUpgrade/MySql/DBUpgrade_v1.2.1_to_v1.2.2.sql
@@ -1,5 +1,5 @@
 create table INTERCEPTOR_ERROR_OBJ (IDENTIFIER bigint(30) NOT NULL,ERROR_TIMESTAMP datetime, OBJECT_TYPE varchar(400),ERROR_CODE varchar(200),OBJECT_ID bigint(30),RECOVERY_DONE boolean,EVENT_CODE bigint(30),NUMBER_OF_TRY int,PROCESSOR_CLASS varchar(400),PRIMARY KEY  (IDENTIFIER));
-create table CATISSUE_SPECIMEN_CIDER_MESSAGE (IDENTIFIER bigint(20) auto_increment primary key ,SPECIMEN_ID bigint(20),SENT_DATE datetime,EVENT_TYPE varchar(100));
+create table CATISSUE_SPECIMEN_MESSAGE_LOG (IDENTIFIER bigint(20) auto_increment primary key ,SPECIMEN_ID bigint(20),SENT_DATE datetime,EVENT_TYPE varchar(100));
 
 
 create table PARTICIPANT_MERGE_MESSAGES(
