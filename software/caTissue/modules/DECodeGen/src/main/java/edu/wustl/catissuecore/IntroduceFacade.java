403f95b25029187dd0cdd92759c8a3913e52dd4d
Ion Olaru <ion.olaru@semanticbits.com>
2011-11-23 21:41:33 +0000

    SVN-Revision: 27738

--- software/caTissue/modules/DECodeGen/src/main/java/edu/wustl/catissuecore/IntroduceFacade.java
+++ software/caTissue/modules/DECodeGen/src/main/java/edu/wustl/catissuecore/IntroduceFacade.java
@@ -3,25 +3,28 @@
  */
 package edu.wustl.catissuecore;
 
+import com.sun.xml.xsom.XSComplexType;
+import com.sun.xml.xsom.XSSchema;
+import com.sun.xml.xsom.XSSchemaSet;
+import com.sun.xml.xsom.parser.XSOMParser;
 import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.common.XMLUtilities;
 import gov.nih.nci.cagrid.introduce.IntroduceConstants;
 import gov.nih.nci.cagrid.introduce.beans.ServiceDescription;
 import gov.nih.nci.cagrid.introduce.beans.namespace.NamespaceType;
 import gov.nih.nci.cagrid.introduce.beans.namespace.SchemaElementType;
 import gov.nih.nci.cagrid.introduce.codegen.SyncTools;
 import gov.nih.nci.cagrid.introduce.common.CommonTools;
+import org.apache.commons.io.FilenameUtils;
+import org.jdom.DefaultJDOMFactory;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.io.FilenameUtils;
-
-import com.sun.xml.xsom.XSComplexType;
-import com.sun.xml.xsom.XSSchema;
-import com.sun.xml.xsom.XSSchemaSet;
-import com.sun.xml.xsom.parser.XSOMParser;
+import java.io.FileOutputStream;
+import java.util.*;
 
 /**
  * Modifies caGrid service project created by Introduce by:
@@ -30,12 +33,14 @@ import com.sun.xml.xsom.parser.XSOMParser;
  * re-creating the skeletons.
  * 
  * @author Denis G. Krylov
+ * @author Ion C. Olaru
  * 
  */
 public final class IntroduceFacade {
 
 	private File serviceDir;
 	private String serviceName;
+	private static String serverWsddFile = "server-config.wsdd";
 
 	public IntroduceFacade(File serviceDir, String serviceName) {
 		this.serviceDir = serviceDir;
@@ -62,17 +67,23 @@ public final class IntroduceFacade {
 		NamespaceType namespace = new NamespaceType();
 		namespace.setLocation("." + File.separator + schemaFile.getName());
 		namespace.setNamespace(schema.getTargetNamespace());
-		namespace.setPackageName(FilenameUtils.getBaseName(schemaFile.getName()) + ".ws");
+
+        String packageName = FilenameUtils.getBaseName(schemaFile.getName()) + ".ws";
+		namespace.setPackageName(packageName);
 
 		Map<String, XSComplexType> typeMap = schema.getComplexTypes();
 		List<SchemaElementType> typeList = new ArrayList<SchemaElementType>();
+
+        Set<String> types = new HashSet<String>();
+
 		for (String typeName : typeMap.keySet()) {
 			SchemaElementType etype = new SchemaElementType();
 			etype.setType(typeName);
 			typeList.add(etype);
+            types.add(typeName);
 		}
-		namespace.setSchemaElement(typeList.toArray(new SchemaElementType[0]));
 
+		namespace.setSchemaElement(typeList.toArray(new SchemaElementType[0]));
 		CommonTools.addNamespace(introService, namespace);
 
 		Utils.serializeDocument(introduceXML.getAbsolutePath(), introService, IntroduceConstants.INTRODUCE_SKELETON_QNAME);
@@ -80,13 +91,61 @@ public final class IntroduceFacade {
 		SyncTools sync = new SyncTools(serviceDir);
 		sync.sync();
 
+        updateServerWSDDFile(types, schema.getTargetNamespace(), packageName);
+
 		// buildStep();
 
 	}
 
+    /**
+     * Updates server-config.xsdd file to contains serializer and deserializer information for new inserted types.
+     * @param types Set of types to be added to the mappings list
+     * @param targetNamespace target namespace to use for the element's namespaces
+     * @param packageName type's package name
+     * @return void
+     * @throws RuntimeException throws RuntimeException if writing to the target file fails for any reason
+     *
+     * */
+    private synchronized void updateServerWSDDFile(Set<String> types, String targetNamespace, String packageName) {
+        String wsddServerFile = serviceDir.getAbsolutePath() + File.separator + serverWsddFile;
+        DefaultJDOMFactory f = new DefaultJDOMFactory();
+
+        try {
+            Element wsddRoot = XMLUtilities.fileNameToDocument(wsddServerFile).getRootElement();
+            for (String s:types) {
+                Element e = new Element("typeMapping");
+                Namespace ns1 = Namespace.getNamespace("ns1", "http://xml.apache.org/axis/wsdd/providers/java");
+                Namespace ns2 = Namespace.getNamespace("ns2", targetNamespace);
+
+                e.addNamespaceDeclaration(ns1);
+                e.addNamespaceDeclaration(ns2);
+                e.setAttribute("serializer", "org.cagrid.data.sdkquery42.encoding.SDK42SerializerFactory");
+                e.setAttribute("deserializer", "org.cagrid.data.sdkquery42.encoding.SDK42DeserializerFactory");
+                e.setAttribute("encodingStyle", "");
+                e.setAttribute("type", "ns1:" + packageName + "." + s);
+                e.setAttribute("qname", "ns2:" + s);
+
+                wsddRoot.getChildren().add(e);
+            }
+
+            XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+            out.output(wsddRoot, new FileOutputStream(wsddServerFile));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(String.format("Can't update <%s> file.", serverWsddFile), e);
+        }
+    }
+
+/**
+ * Program parameters:
+ * ../caTissueStaticDataService/ Catissue_cacore ./src/test/resources/withIdentifier/clinical_annotation.xsd
+ *
+ * */
 	public static void main(String[] args) throws Exception {
         if (args.length < 3) {
             System.out.println("Pass 3 arguments. See Usage.");
+            System.exit(0);
         }
 
         byte i = 0;
