9c236e4d5951d6a3f9e20676eb537fc1257fdf81
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2011-12-26 07:51:27 +0000

    * Task/Bug Id: <Development task name or bug id> * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  Script to update the package name of existing DE models * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 27965

--- software/caTissue/db/Common/de_upgrade_for_single_api.sql
+++ software/caTissue/db/Common/de_upgrade_for_single_api.sql
@@ -1,5 +1,5 @@
-UPDATE dyextn_tagged_value SET t_value=CONCAT('de_entity.',t_value)
+UPDATE dyextn_tagged_value SET t_value=CONCAT('edu.wustl.',t_value)
 WHERE abstract_metadata_id IN (
 SELECT identifier FROM dyextn_abstract_metadata WHERE identifier IN (
 SELECT identifier FROM dyextn_entity_group WHERE is_system_generated = 0)
-) AND t_key LIKE 'PackageName' AND t_value NOT LIKE 'de_entity%';
+) AND t_key LIKE 'PackageName' AND t_value NOT LIKE '%.%';
