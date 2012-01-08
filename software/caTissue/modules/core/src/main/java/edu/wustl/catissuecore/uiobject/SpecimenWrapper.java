4e01a4086d6a204c2aec1a3d71c87d6f98366d19
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2012-01-08 14:47:59 +0000

    * Task/Bug Id: <Development task name or bug id> * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details: Check for empty SPP * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 28037

--- software/caTissue/modules/core/src/main/java/edu/wustl/catissuecore/uiobject/SpecimenWrapper.java
+++ software/caTissue/modules/core/src/main/java/edu/wustl/catissuecore/uiobject/SpecimenWrapper.java
@@ -81,14 +81,17 @@ public class SpecimenWrapper implements ISPPBizlogic
 			SpecimenProcessingProcedureApplication processingSPPApplication, String reasonOfDeviation, User user,
 			Date dateOfEvent, ActionApplicationRecordEntry actionAppRecordEntry) throws BizLogicException
 	{
-		ObjectCloner cloner = new ObjectCloner();
-		SpecimenProcessingProcedureApplication clonedSPPApplication = cloner.clone(processingSPPApplication);
-
-		IBizLogic defaultBizLogic = new CatissueDefaultBizLogic();
-		if(processingSPPApplication.getId() == null)
-			defaultBizLogic.insert(processingSPPApplication);
-		else
-			defaultBizLogic.update(processingSPPApplication,clonedSPPApplication,null);
+		if(processingSPPApplication != null)
+		{
+			ObjectCloner cloner = new ObjectCloner();
+			SpecimenProcessingProcedureApplication clonedSPPApplication = cloner.clone(processingSPPApplication);
+	
+			IBizLogic defaultBizLogic = new CatissueDefaultBizLogic();
+			if(processingSPPApplication.getId() == null)
+				defaultBizLogic.insert(processingSPPApplication);
+			else
+				defaultBizLogic.update(processingSPPApplication,clonedSPPApplication,null);
+		}
 		
 		ActionApplication actionApplication = new ActionApplication();
 		actionApplication.setReasonDeviation(reasonOfDeviation);
