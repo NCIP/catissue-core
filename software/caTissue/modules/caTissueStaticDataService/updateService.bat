ad975a99f7221aea6a8961e44cb66783816a48db
Ion Olaru <ion.olaru@semanticbits.com>
2012-01-23 19:39:49 +0000

    * Task/Bug Id: <Development task name or bug id> * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  <Technical details for the changes made if any> * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 28187

--- software/caTissue/modules/caTissueStaticDataService/updateService.bat
+++ software/caTissue/modules/caTissueStaticDataService/updateService.bat
@@ -1,5 +1,5 @@
-erase /Q de_codegen\artifacts\*
-erase /Q de_codegen\xmi\*
-erase /Q de_codegen\xsd\*
-unzip de_codegen\dynamic_extensions.zip -d de_codegen\
+rem erase /Q de_codegen\artifacts\*
+rem erase /Q de_codegen\xmi\*
+rem erase /Q de_codegen\xsd\*
+rem unzip de_codegen\dynamic_extensions.zip -d de_codegen\
 call ant updateService
