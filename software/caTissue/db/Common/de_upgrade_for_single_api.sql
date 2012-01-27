8bfbe55fb45f4b39a73d8865a352e9b8d0bef076
Pathik Sheth <pathik.sheth@krishagni.com>
2012-01-27 20:59:21 +0000

    handling for the orphan model
    SVN-Revision: 28234

--- software/caTissue/db/Common/de_upgrade_for_single_api.sql
+++ software/caTissue/db/Common/de_upgrade_for_single_api.sql
@@ -4,4 +4,9 @@ SELECT identifier FROM dyextn_abstract_metadata WHERE identifier IN (
 SELECT identifier FROM dyextn_entity_group WHERE is_system_generated = 0)
 ) AND t_key LIKE 'PackageName' AND t_value NOT LIKE '%.%';
 
+update dyextn_entity_group set IS_SYSTEM_GENERATED = 1 where identifier in ( 
+select container.entity_group_id from dyextn_container container  where container.entity_group_id is not null
+and container.abstract_entity_id not in (select target_entity_id from 
+dyextn_association ));
+
 commit;
