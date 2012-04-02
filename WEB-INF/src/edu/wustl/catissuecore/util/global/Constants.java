134c267fe624b753c84388e5f0f53b23a23f4156
Atul Kaushal <atul.kaushal@krishagni.com>
2012-04-02 05:20:56 +0000

    * Task/Bug Id: <Development task name or bug id> * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  <Technical details for the changes made if any> * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 28509

--- WEB-INF/src/edu/wustl/catissuecore/util/global/Constants.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/global/Constants.java
@@ -5855,4 +5855,6 @@ public class Constants
 	public static final String GRID_SETUP_PROP_FILE = "gridsetup.properties";
 	
 	public static final String CP_COORDINATOR_IDS = "coordinatorIds";
+	public static final String LABELLING_SCHEME_ALPHABETS="ALPHABETS";
+	public static final String LABELLING_SCHEME_NUMBERS="NUMBERS";
 }
