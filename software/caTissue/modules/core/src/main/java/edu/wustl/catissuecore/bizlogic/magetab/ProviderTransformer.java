8172bdce50ff3b0f33a42c91390854aea57e286c
Denis Krylov <denis.krylov@semanticbits.com>
2012-04-20 18:51:24 +0000

    PHI BREACH - Participant name listed in Provider fields in SDRF: fixed.
    SVN-Revision: 28583

--- software/caTissue/modules/core/src/main/java/edu/wustl/catissuecore/bizlogic/magetab/ProviderTransformer.java
+++ software/caTissue/modules/core/src/main/java/edu/wustl/catissuecore/bizlogic/magetab/ProviderTransformer.java
@@ -1,52 +1,50 @@
 package edu.wustl.catissuecore.bizlogic.magetab;
 
-
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.sdrf.node.AbstractSDRFNode;
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.sdrf.node.SourceNode;
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.sdrf.node.attribute.ProviderAttribute;
-import edu.wustl.catissuecore.domain.Participant;
+import edu.wustl.catissuecore.domain.CollectionProtocol;
 import edu.wustl.catissuecore.domain.Specimen;
-import edu.wustl.catissuecore.domain.SpecimenCollectionGroup;
 import edu.wustl.common.util.logger.Logger;
 import gov.nih.nci.logging.api.util.StringUtils;
 
 public class ProviderTransformer extends AbstractTransformer {
-	private static transient final Logger logger = Logger
-			.getCommonLogger(ProviderTransformer.class);
+    private static transient final Logger logger = Logger
+            .getCommonLogger(ProviderTransformer.class);
 
-	public ProviderTransformer() {
-		super("Provider", "Provider", "Participant");
-	}
+    public ProviderTransformer() {
+        super("Provider", "Provider", "PI");
+    }
 
-	@Override
-	public void transform(Specimen specimen, AbstractSDRFNode sdrfNode) {
-		SourceNode sourceNode;
-		if (sdrfNode instanceof SourceNode) {
-			sourceNode = (SourceNode) sdrfNode;
-		} else {
-			logger.debug("Got a node which needs has no provider attribute for specimen"
-					+ (StringUtils.isBlank(specimen.getLabel()) ? StringUtils
-							.isBlank(specimen.getBarcode()) ? specimen.getLabel()+"_ID" : specimen
-							.getBarcode() : specimen.getLabel()));
-			return;
-		}
-		SpecimenCollectionGroup scg = specimen.getSpecimenCollectionGroup();
-		Participant participant = specimen.getSpecimenCollectionGroup()
-				.getCollectionProtocolRegistration().getParticipant();
-		if (participant != null) {
-			ProviderAttribute providerAttribute = new ProviderAttribute();
-			providerAttribute.setNodeName(participant.getLastName() + ", "
-					+ participant.getFirstName());
-			providerAttribute.setNodeType("Provider");
-			// providerAttribute.setAttributeValue(site.getName());
-			sourceNode.provider = providerAttribute;
-		}
-	}
-	
-	@Override
-	public boolean isMageTabSpec() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    @Override
+    public void transform(Specimen specimen, AbstractSDRFNode sdrfNode) {
+        SourceNode sourceNode;
+        if (sdrfNode instanceof SourceNode) {
+            sourceNode = (SourceNode) sdrfNode;
+        } else {
+            logger.debug("Got a node which needs has no provider attribute for specimen"
+                    + (StringUtils.isBlank(specimen.getLabel()) ? StringUtils
+                            .isBlank(specimen.getBarcode()) ? specimen
+                            .getLabel() + "_ID" : specimen.getBarcode()
+                            : specimen.getLabel()));
+            return;
+        }
+        CollectionProtocol cp = specimen.getSpecimenCollectionGroup()
+                .getCollectionProtocolRegistration().getCollectionProtocol();
+        if (cp != null && cp.getPrincipalInvestigator() != null) {
+            ProviderAttribute providerAttribute = new ProviderAttribute();
+            providerAttribute.setNodeName(cp.getPrincipalInvestigator()
+                    .getLastName()
+                    + ", "
+                    + cp.getPrincipalInvestigator().getFirstName());
+            providerAttribute.setNodeType("Provider");
+            sourceNode.provider = providerAttribute;
+        }
+    }
+
+    @Override
+    public boolean isMageTabSpec() {
+        return true;
+    }
 
 }
