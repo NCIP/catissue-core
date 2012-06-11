3b7563a939c11f0364ccd4b417db77f47dd9b891
Atul Kaushal <atul.kaushal@krishagni.com>
2012-06-11 12:21:05 +0000

    * Task/Bug Id: Bug fixed 23399. * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  <Technical details for the changes made if any> * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 29145

--- WEB-INF/src/edu/wustl/catissuecore/actionForm/SpecimenArrayAliquotForm.java
+++ WEB-INF/src/edu/wustl/catissuecore/actionForm/SpecimenArrayAliquotForm.java
@@ -170,17 +170,17 @@ public class SpecimenArrayAliquotForm extends AbstractActionForm
 										.getValue("specimenArrayAliquots.label")));
 					}
 				}
-				else if (key.indexOf("_positionDimension") != -1)
+				/*else if (key.indexOf("_positionDimension") != -1)
 				{
 					final String value = (String) this.specimenArrayAliquotMap.get(key);
-					if (value != null && !value.trim().equals("") && !validator.isDouble(value))
+					if (value != null && !value.trim().equals("") s&& !validator.isDouble(value))
 					{
 						errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("errors.item.format",
 								ApplicationProperties
 										.getValue("specimen.positionInStorageContainer")));
 						break;
 					}
-				}
+				}*/
 			}
 		}
 
