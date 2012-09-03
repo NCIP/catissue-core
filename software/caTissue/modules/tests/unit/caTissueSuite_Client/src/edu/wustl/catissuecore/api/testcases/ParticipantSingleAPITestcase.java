e1954c6b0bea1edd7b09b6eea2a2481f244ad7ff
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2012-09-03 06:38:21 +0000

    Single API test case for Participant with Alcohol history
    SVN-Revision: 29382

---
+++ software/caTissue/modules/tests/unit/caTissueSuite_Client/src/edu/wustl/catissuecore/api/testcases/ParticipantSingleAPITestcase.java
@@ -0,0 +1,50 @@
+package edu.wustl.catissuecore.api.testcases;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+import edu.wustl.catissuecore.domain.Participant;
+import edu.wustl.catissuecore.domain.deintegration.ParticipantRecordEntry;
+
+
+public class ParticipantSingleAPITestcase extends AbstractCaCoreApiTestCasesWithRegularAuthentication
+{
+	
+//	public void testSearchPartDE()
+//	{
+//		Participant participant = new Participant();
+//		ParticipantRecordEntry entry = new ParticipantRecordEntry();
+//		AlcoholHealthAnnotation alcoAnnot = new AlcoholHealthAnnotation();
+//		alcoAnnot.setDrinksPerWeek(3L);
+//		alcoAnnot.setParticipantRecordEntry_AlcoholHealthAnnotation(entry);
+//		entry.setParticipant(participant);
+//		entry.setAlcoholHealthAnnotationCollection(new HashSet(Arrays.asList(alcoAnnot)));
+//		participant.setParticipantRecordEntryCollection(new HashSet(Arrays.asList(entry)));
+//		
+//		List<Participant> participants = searchByExample(Participant.class, participant);
+//		for (Participant part : participants) 
+//		{
+//			printObject(part, part.getClass());
+//		}
+//	}
+//	
+//	private void printObject(Object obj, Class klass) throws Exception {
+//		System.out.println("Printing "+ klass.getName());
+//		Method[] methods = klass.getMethods();
+//		for(Method method:methods)
+//		{
+//			if(method.getName().startsWith("get") && !method.getName().equals("getClass"))
+//			{
+//				System.out.print("\t"+method.getName().substring(3)+":");
+//				Object val = method.invoke(obj, (Object[])null);
+//				if(val instanceof java.util.Set)
+//					System.out.println("size="+((Collection)val).size());
+//				else
+//					System.out.println(val);
+//			}
+//		}
+//	}
+	
+}
