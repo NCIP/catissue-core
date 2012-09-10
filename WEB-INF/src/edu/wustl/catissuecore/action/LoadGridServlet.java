edf64b8c89388edc7224c8cfd6d3e91ae951b25e
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2012-09-10 06:48:00 +0000

    conf changes for specimen list grid
    SVN-Revision: 29409

--- WEB-INF/src/edu/wustl/catissuecore/action/LoadGridServlet.java
+++ WEB-INF/src/edu/wustl/catissuecore/action/LoadGridServlet.java
@@ -74,33 +74,10 @@ public class LoadGridServlet extends ConnectorServlet
 			connector.event.attach(gridImplObj);
 			String sql = gridImplObj.getGridQuery(aliasName,getSessionDataBean());
 			String tableColString =gridImplObj.getTableColumnString();
-//			String sql = "Select 0 as CHKBOX, Specimen1.BARCODE  Barcode , Specimen1.LABEL  Label , " +
-//"ExternalIdentifier1.NAME  ExternalIdentifier_Name , ExternalIdentifier1.VALUE  ExternalIdentifier_Value , " +
-//"Specimen1.SPECIMEN_COLLECTION_GROUP_ID  SCG_ID , AbstractSpecimen1.SPECIMEN_CLASS  Class , " +
-//"AbstractSpecimen1.SPECIMEN_TYPE  Type , AbstractSpecimen1.LINEAGE  Lineage , " +
-//"AbstractSpecimen1.PARENT_SPECIMEN_ID  Parent_Specimen_Id , SpecimenCharacteristics1.TISSUE_SIDE  Tissue_Side ," +
-//" SpecimenCharacteristics1.TISSUE_SITE  Tissue_Site , AbstractSpecimen1.PATHOLOGICAL_STATUS  Path_Status , " +
-//"AbstractSpecimen1.INITIAL_QUANTITY  Init_Quantity , Specimen1.AVAILABLE_QUANTITY  Avl_Quantity , " +
-//"Specimen1.AVAILABLE  Available , Specimen1.CREATED_ON_DATE  Created_On , Specimen1.IDENTIFIER  Identifier "+ 
-//" FROM  CATISSUE_SPECIMEN_CHAR SpecimenCharacteristics1 , CATISSUE_ABSTRACT_SPECIMEN AbstractSpecimen1 , " +
-//"CATISSUE_EXTERNAL_IDENTIFIER ExternalIdentifier1 , CATISSUE_SPECIMEN Specimen1 , assigntag atg "+  
-//" WHERE SpecimenCharacteristics1.IDENTIFIER   =  AbstractSpecimen1.SPECIMEN_CHARACTERISTICS_ID  AND " +
-//"Specimen1.IDENTIFIER   =  ExternalIdentifier1.SPECIMEN_ID  AND AbstractSpecimen1.IDENTIFIER   =  " +
-//"Specimen1.IDENTIFIER  AND Specimen1.IDENTIFIER   =  ExternalIdentifier1.SPECIMEN_ID   AND atg.tag_id="+aliasName+" and " +
-//"( ( Specimen1.IDENTIFIER  =atg.obj_id  )   AND UPPER(Specimen1.ACTIVITY_STATUS ) != UPPER('Disabled')  ) "+
-//" ORDER BY Specimen1.IDENTIFIER ,SpecimenCharacteristics1.IDENTIFIER ,ExternalIdentifier1.IDENTIFIER ," +
-//"AbstractSpecimen1.IDENTIFIER "; 
 			connector.dynamic_loading(true);
-			connector.dynamic_loading(100);
-			//assigntag atg where atg.tag_id=1 and sp.identifier = atg.obj_id
-//			String tableColString = "CHKBOX,Barcode,Label,ExternalIdentifier_Name,ExternalIdentifier_Value,SCG_ID,Class,Type,Lineage,Parent_Specimen_Id," +
-//									"Tissue_Side,Tissue_Site,Pathology_Status,Init_Quantity,Avl_Quantity,Available,Created_On,Identifier";
-			//PropertyHandler.getSimpleSearchProperty("Specimen_columns");
-//			sql = "";//PropertyHandler.getSimpleSearchProperty("Specimen_sql");
-//			String tableColString = "Identifier, Name";
+			connector.dynamic_loading(250);
 			connector.render_sql(sql, gridImplObj.getTableColumnString(), tableColString);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		finally
@@ -127,8 +104,6 @@ public class LoadGridServlet extends ConnectorServlet
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,
 			IOException
 	{
-//		setJsonString(req.getParameter("paramJson"));
-//		setGridType(req.getParameter("gridType"));
 		setAliasName(req.getParameter("reqParam"));
 		setPageOf(req.getParameter("pageOf"));
 		setSessionDataBean((SessionDataBean)req.getSession().getAttribute(Constants.SESSION_DATA));
@@ -142,9 +117,6 @@ public class LoadGridServlet extends ConnectorServlet
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException,
 			IOException
 	{
-//		setJsonString(req.getParameter("paramJson"));
-//		setGridType(req.getParameter("gridType"));
-//		setSessionData((SessionDataBean)req.getSession().getAttribute(Constants.SESSION_DATA));
 		super.doGet(req, res);
 
 	}
