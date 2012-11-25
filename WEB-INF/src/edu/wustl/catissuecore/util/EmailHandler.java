19cb0645b65b4b08db61507713e3412c5f4d11f6
Atul Kaushal <atul.kaushal@krishagni.com>
2012-11-25 09:33:56 +0000

    fixed bug #187.
    SVN-Revision: 29560

--- WEB-INF/src/edu/wustl/catissuecore/util/EmailHandler.java
+++ WEB-INF/src/edu/wustl/catissuecore/util/EmailHandler.java
@@ -241,9 +241,9 @@ public class EmailHandler
 	
 			final gov.nih.nci.security.authorization.domainobjects.User csmUser = SecurityManagerFactory.getSecurityManager().getUserById(user.getCsmUserId().toString());
 			
-			body="You asked to reset your password. To do so, please click this link:"
+			body="You have requested to reset your caTissue application password. Please click the below link to reset:"
 				   +"\n\n\t "+CommonServiceLocator.getInstance().getAppURL()+"/ResetPassword.do?operation=edit&pageOf=pageOfResetPassword&resetPasswordToken="+userToken
-				   +"\n\tThis will let you change your password to something new. If you didn't ask for this, don't worry, we'll keep your password safe."
+				   +"\n\tIgnore this email if you have not requested for the password reset and you will be able to login with your old password."
 				   + "\n\n" + ApplicationProperties.getValue("email.catissuecore.team");
 			
 			System.out.println(CommonServiceLocator.getInstance().getAppURL()+"/ResetPassword.do?operation=edit&pageOf=pageOfResetPassword&resetPasswordToken="+userToken);
