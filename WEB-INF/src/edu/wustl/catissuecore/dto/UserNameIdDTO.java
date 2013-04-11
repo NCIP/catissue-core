574dc308ab5e08f8e31767260dc09bc04bacbd56
Rinku Rohra <rinku.rohra@krishagni.com>
2013-04-11 07:13:15 +0000

    SCG Summary page changes
    SVN-Revision: 29994

---
+++ WEB-INF/src/edu/wustl/catissuecore/dto/UserNameIdDTO.java
@@ -0,0 +1,40 @@
+package edu.wustl.catissuecore.dto;
+
+
+public class UserNameIdDTO
+{
+	String firstName;
+	String lastName;
+	Long userId;
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	
+	public Long getUserId()
+	{
+		return userId;
+	}
+	
+	public void setUserId(Long userId)
+	{
+		this.userId = userId;
+	}
+
+}
