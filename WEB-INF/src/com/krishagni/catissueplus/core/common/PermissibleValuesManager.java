8a6d17da60bad6c81635d5e6c1ea1b37e37bac3a
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2014-02-14 10:37:54 +0530

    Added permissible value manager interface

---
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/PermissibleValuesManager.java
@@ -0,0 +1,9 @@
+package com.krishagni.catissueplus.core.common;
+
+import java.util.List;
+
+
+public interface PermissibleValuesManager {
+	public List getPermissibleValueList(String itemName);
+
+}
---
+++ WEB-INF/src/com/krishagni/catissueplus/core/repository/DaoFactory.java
@@ -0,0 +1,8 @@
+package com.krishagni.catissueplus.core.repository;
+
+
+public interface DaoFactory {
+
+	public ParticipantDao getParticipantDao();
+	public CollectionProtocolRegistrationDao getregistrationDao();
+}
