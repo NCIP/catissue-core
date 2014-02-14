5a1efdf78b6479ea463faa5a179e3dcf2b54e078
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2014-02-14 10:20:28 +0530

    Participant Add: Added implementation for Add participant API

--- WEB-INF/src/com/krishagni/catissueplus/core/events/participants/ParticipantDetailsEvent.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/events/participants/ParticipantDetailsEvent.java
@@ -1,6 +1,7 @@
 
 package com.krishagni.catissueplus.core.events.participants;
 
+import com.krishagni.catissueplus.core.events.EventStatus;
 import com.krishagni.catissueplus.core.events.ResponseEvent;
 
 public class ParticipantDetailsEvent extends ResponseEvent {
@@ -13,6 +14,14 @@ public class ParticipantDetailsEvent extends ResponseEvent {
 
 	public void setParticipantDto(ParticipantDetails participantDto) {
 		this.participantDto = participantDto;
+	}
+	
+	public static ParticipantDetailsEvent ok(ParticipantDetails details)
+	{
+		ParticipantDetailsEvent event = new ParticipantDetailsEvent();
+		event.setParticipantDto(details);
+		event.setStatus(EventStatus.OK);
+		return event;
 	}
 
 }
--- WEB-INF/src/com/krishagni/catissueplus/core/events/registration/AllParticipantsSummaryEvent.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/events/registration/AllParticipantsSummaryEvent.java
@@ -3,11 +3,12 @@ package com.krishagni.catissueplus.core.events.registration;
 
 import java.util.List;
 
+import com.krishagni.catissueplus.core.events.EventStatus;
 import com.krishagni.catissueplus.core.events.ResponseEvent;
 import com.krishagni.catissueplus.core.events.participants.ParticipantSummary;
 
 public class AllParticipantsSummaryEvent extends ResponseEvent {
-	
+
 	private List<ParticipantSummary> participantsSummary;
 
 	public List<ParticipantSummary> getParticipantsSummary() {
@@ -16,6 +17,13 @@ public class AllParticipantsSummaryEvent extends ResponseEvent {
 
 	public void setParticipantsSummary(List<ParticipantSummary> participantsSummary) {
 		this.participantsSummary = participantsSummary;
+	}
+
+	public static AllParticipantsSummaryEvent ok(List<ParticipantSummary> participantsSummary) {
+		AllParticipantsSummaryEvent event = new AllParticipantsSummaryEvent();
+		event.setParticipantsSummary(participantsSummary);
+		event.setStatus(EventStatus.OK);
+		return event;
 	}
 
 }
