a51b6b7e478ea95d6501ef4005e14542b01d0498
marwahan <nitesh.marwaha@krishagni.com>
2014-06-16 20:32:40 +0530

    Added missed checkin

---
+++ WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/DistributionProtocol.java
@@ -0,0 +1,109 @@
+
+package com.krishagni.catissueplus.core.administrative.domain;
+
+import java.util.Date;
+
+public class DistributionProtocol {
+
+	private Long id;
+
+	private User principalInvestigator;
+
+	private String title;
+
+	private String shortTitle;
+
+	private String irbId;
+
+	private Date startDate;
+
+	private String descriptionUrl;
+
+	private Long anticipatedSpecimenCount;
+
+	private String activityStatus;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getPrincipalInvestigator() {
+		return principalInvestigator;
+	}
+
+	public void setPrincipalInvestigator(User principalInvestigator) {
+		this.principalInvestigator = principalInvestigator;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getShortTitle() {
+		return shortTitle;
+	}
+
+	public void setShortTitle(String shortTitle) {
+		this.shortTitle = shortTitle;
+	}
+
+	public String getIrbId() {
+		return irbId;
+	}
+
+	public void setIrbId(String irbId) {
+		this.irbId = irbId;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getDescriptionUrl() {
+		return descriptionUrl;
+	}
+
+	public void setDescriptionUrl(String descriptionURL) {
+		this.descriptionUrl = descriptionURL;
+	}
+
+	public Long getAnticipatedSpecimenCount() {
+		return anticipatedSpecimenCount;
+	}
+
+	public void setAnticipatedSpecimenCount(Long anticipatedSpecimenCount) {
+		this.anticipatedSpecimenCount = anticipatedSpecimenCount;
+	}
+
+	public String getActivityStatus() {
+		return activityStatus;
+	}
+
+	public void setActivityStatus(String activityStatus) {
+		this.activityStatus = activityStatus;
+	}
+
+	public void update(DistributionProtocol distributionProtocol) {
+		this.setShortTitle(distributionProtocol.getShortTitle());
+		this.setTitle(distributionProtocol.getTitle());
+		this.setIrbId(distributionProtocol.getIrbId());
+		this.setAnticipatedSpecimenCount(distributionProtocol.getAnticipatedSpecimenCount());
+		this.setPrincipalInvestigator(distributionProtocol.getPrincipalInvestigator());
+		this.setDescriptionUrl(distributionProtocol.getDescriptionUrl());
+		this.setStartDate(distributionProtocol.getStartDate());
+    this.setActivityStatus(distributionProtocol.getActivityStatus());
+	}
+
+}
