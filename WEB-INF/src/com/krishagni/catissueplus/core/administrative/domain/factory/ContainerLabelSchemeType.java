dee40c4cdae7e3c8a0a7f9b29b4c2dbbb8e0465a
ajaysamgir <ajaysamgir@bitbucket.org>
2014-08-12 11:37:54 +0530

    minor code coverage fix

--- WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/ContainerLabelSchemeType.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/ContainerLabelSchemeType.java
@@ -1,12 +1,9 @@
+
 package com.krishagni.catissueplus.core.administrative.domain.factory;
 
-
 public enum ContainerLabelSchemeType {
-	NUMBERS (0, "Numbers"),
-	ALPHABETS_UPPER (1, "ALPHABETS_UPPER"),
-	ALPHABETS_LOWER (2, "ALPHABETS_LOWER"),
-	ROMAN_UPPER (3, "ROMAN_UPPER"),
-	ROMAN_LOWER (4, "ROMAN_LOWER");
+	NUMBERS(0, "Numbers"), ALPHABETS_UPPER(1, "ALPHABETS_UPPER"), ALPHABETS_LOWER(2, "ALPHABETS_LOWER"), ROMAN_UPPER(3,
+			"ROMAN_UPPER"), ROMAN_LOWER(4, "ROMAN_LOWER");
 
 	public String value;
 
@@ -31,12 +28,12 @@ public enum ContainerLabelSchemeType {
 		return enumValue;
 	}
 
-	public static Boolean isValidContainerLabelSchemeType(String labelShemeType) {
+	/*public static Boolean isValidContainerLabelSchemeType(String labelShemeType) {
 		for (ContainerLabelSchemeType type : ContainerLabelSchemeType.values()) {
 			if (type.value().equalsIgnoreCase(labelShemeType)) {
 				return true;
 			}
 		}
 		return false;
-	}
+	}*/
 }
