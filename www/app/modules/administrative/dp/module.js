af92002671b1a6e44d70ac3d788e8c93ae622d55
Vinayak Pawar <vinayakapawar@gmail.com>
2015-01-09 15:56:33 +0530

    Modularised routes

--- www/app/modules/administrative/dp/module.js
+++ www/app/modules/administrative/dp/module.js
@@ -1,7 +1,39 @@
 angular.module('os.administrative.dp', 
-  [
+  [ 
+    'ui.router',
     'os.administrative.dp.list',
     'os.administrative.dp.detail',
     'os.administrative.dp.addedit'
-  ]
-);
+  ])
+
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('dp-list', {
+        url: '/dps',
+        templateUrl: 'modules/administrative/dp/list.html',     
+        controller: 'DpListCtrl',
+        parent: 'signed-in'
+      })
+      .state('dp-new', {
+        url: '/new-dp',
+        templateUrl: 'modules/administrative/dp/addedit.html',
+        controller: 'DpAddEditCtrl',
+        parent: 'signed-in'
+      })
+      .state('dp-detail', {
+        url: '/dps/:dpId',
+        templateUrl: 'modules/administrative/dp/detail.html',
+        resolve: {
+          distributionProtocol: function($stateParams , DistributionProtocol) {
+            return DistributionProtocol.getById($stateParams.dpId);
+          }
+        },
+        controller: 'DpDetailCtrl',
+        parent: 'signed-in'
+      })
+      .state('dp-detail.overview', {
+        url: '/overview',
+        templateUrl: 'modules/administrative/dp/overview.html',
+        parent: 'dp-detail'
+      });
+  });
--- www/app/modules/biospecimen/participant/module.js
+++ www/app/modules/biospecimen/participant/module.js
@@ -1,11 +1,86 @@
 
 angular.module('os.biospecimen.participant', 
-  [
+  [ 
+    'ui.router',
     'os.biospecimen.participant.list',
     'os.biospecimen.participant.detail',
     'os.biospecimen.participant.overview',
     'os.biospecimen.participant.visits',
     'os.biospecimen.participant.addedit',
     'os.biospecimen.participant.addvisit',
-  ]
-);
+  ])
+
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('participant-list', {
+        url: '/participants',
+        templateUrl: 'modules/biospecimen/participant/list.html',
+        controller: 'ParticipantListCtrl',
+        parent: 'cp-detail'
+      })
+      .state('participant-new', {
+        url: '/new-participant',
+        templateProvider: function($stateParams, CpConfigSvc) {
+          var tmpl = CpConfigSvc.getRegParticipantTmpl($stateParams.cpId);
+          return '<div ng-include src="\'' + tmpl + '\'"></div>';
+        },
+        controllerProvider: function($stateParams, CpConfigSvc) {
+          return CpConfigSvc.getRegParticipantCtrl($stateParams.cpId);
+        },
+        parent: 'cp-detail'
+      })
+      .state('participant-detail', {
+        url: '/participants/:cprId',
+        templateUrl: 'modules/biospecimen/participant/detail.html',
+        resolve: {
+          cpr: function($stateParams, CollectionProtocolRegistration) {
+            return CollectionProtocolRegistration.getById($stateParams.cprId);
+          },
+
+          visits: function($stateParams, Visit) {
+            return Visit.listFor($stateParams.cprId, true);
+          }
+        },
+        controller: 'ParticipantDetailCtrl',
+        parent: 'signed-in'
+      })
+      .state('participant-detail.overview', {
+        url: '/overview',
+        templateUrl: 'modules/biospecimen/participant/overview.html',
+        controller: 'ParticipantOverviewCtrl',
+        parent: 'participant-detail'
+      })
+      .state('participant-detail.consents', {
+        url: '/consents',
+        templateUrl: 'modules/biospecimen/participant/consents.html',
+        controller: function() {
+        },
+        parent: 'participant-detail'
+      })
+      .state('participant-detail.visits', {
+        url: '/visits?eventId&visitId',
+        templateUrl: 'modules/biospecimen/participant/visits.html',
+        controller: 'ParticipantVisitsTreeCtrl',
+        resolve: {
+          specimens: function($stateParams, Specimen) {
+            if (!$stateParams.visitId && !$stateParams.eventId) {
+              return [];
+            }
+
+            var visitDetail = {
+              visitId: $stateParams.visitId, 
+              eventId: $stateParams.eventId
+            };
+            return Specimen.listFor($stateParams.cprId, visitDetail);
+          }
+        },
+        parent: 'participant-detail'
+      })
+      .state('participant-detail.annotations', {
+        url: '/annotations',
+        templateUrl: 'modules/biospecimen/participant/annotations.html',
+        controller: function() {
+        },
+        parent: 'participant-detail'
+      });
+  });
