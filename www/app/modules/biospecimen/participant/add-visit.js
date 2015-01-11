f02ec5e2eb726b9a64b9586d8b6260a73165eb94
Vinayak Pawar <vinayakapawar@gmail.com>
2015-01-11 09:50:26 +0530

    1. Auto scroll add visit form into view 2. Pre-populate visit form with event defaults

--- www/app/modules/biospecimen/participant/add-visit.js
+++ www/app/modules/biospecimen/participant/add-visit.js
@@ -1,6 +1,6 @@
 
 angular.module('os.biospecimen.participant.addvisit', ['os.biospecimen.participant.detail'])
-  .controller('AddVisitCtrl', function($scope, $state, Visit, PvManager) {
+  .controller('AddVisitCtrl', function($scope, $state, Visit, PvManager, CollectionProtocolEvent) {
     function loadPvs() {
       $scope.visitStatuses = PvManager.getPvs('visit-status');
       $scope.sites = PvManager.getSites();
@@ -11,15 +11,30 @@ angular.module('os.biospecimen.participant.addvisit', ['os.biospecimen.participa
       };
     };
 
-    function init() {
-      loadPvs();
-
-      $scope.visit = new Visit({
-        id: $scope.visitToAdd.id,
+    function getVisit() {
+      var visit = new Visit({
+        /*id: $scope.visitToAdd.id,*/
         cprId: $scope.cpr.id,
         cpTitle: $scope.cpr.cpTitle,
         eventId: $scope.visit.eventId
       });
+
+      CollectionProtocolEvent.getById($scope.visit.eventId).then(
+        function(cpe) {
+          $scope.visit.clinicalDiagnosis = cpe.clinicalDiagnosis;
+          $scope.visit.clinicalStatus = cpe.clinicalStatus;
+          $scope.visit.site = cpe.defaultSite;
+          $scope.visit.visitDate = new Date($scope.visitToAdd.anticipatedVisitDate);
+          $scope.visit.status = 'Complete';
+        }
+      );
+
+      return visit;
+    }
+
+    function init() {
+      loadPvs();
+      $scope.visit = getVisit();
     }
 
     $scope.addVisit = function() {
