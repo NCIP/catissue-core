8c217b779b115bab10106d2d8802e73ad734cbe4
vgaikwad <vinod.gaikwad@krishagni.com>
2015-02-02 14:19:07 +0530

    Deleted unused files

--- www/app/modules/administrative/models/user.js
+++ www/app/modules/administrative/models/user.js
@@ -3,13 +3,6 @@ angular.module('os.administrative.models.user', ['os.common.models'])
   .factory('User', function(osModel) {
     var User = osModel('users');
 
-    /* use this in permission
-    User.getPermissions = function(userId) {
-      return [
-        {site: 'In Transit', cp: 'CP1', roleName: 'Admin', id:1},
-      ];
-    }*/
-    
     return User;
   });
 
