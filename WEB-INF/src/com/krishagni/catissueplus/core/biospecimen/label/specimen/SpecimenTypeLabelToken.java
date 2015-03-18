dde43cdd95a3c0a9b490e7fdaec16af500a624c9
Vinayak Pawar <vinayakapawar@gmail.com>
2015-03-18 11:15:15 +0530

    Minor fixes related to specimen labels

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/label/specimen/SpecimenTypeLabelToken.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/label/specimen/SpecimenTypeLabelToken.java
@@ -5,10 +5,13 @@ import org.apache.commons.lang.WordUtils;
 import com.krishagni.catissueplus.core.biospecimen.domain.Specimen;
 
 public class SpecimenTypeLabelToken extends AbstractSpecimenLabelToken {
+	
+	public SpecimenTypeLabelToken() {
+		this.name = "SP_TYPE";
+	}
 
 	@Override
 	public String getLabel(Specimen specimen) {
 		return WordUtils.initials(specimen.getSpecimenType());
 	}
-
 }
