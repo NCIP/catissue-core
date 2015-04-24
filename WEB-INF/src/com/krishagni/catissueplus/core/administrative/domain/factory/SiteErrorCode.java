89099ff82a6a772a32bfb24ee9b019a5772df95d
vgaikwad <vinod.gaikwad@krishagni.com>
2015-04-24 18:19:06 +0530

    Removed unused error codes

--- WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/SiteErrorCode.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/SiteErrorCode.java
@@ -18,11 +18,7 @@ public enum SiteErrorCode implements ErrorCode {
 	
 	DUP_CODE,
 	
-	REF_ENTITY_FOUND,
-	
-	RESOURCE_NOT_FOUND,
-	
-	OPERATION_NOT_FOUND;
+	REF_ENTITY_FOUND;
 	
 	public String code() {
 		return "SITE_" + this.name();
