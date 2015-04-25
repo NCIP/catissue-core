133f9b1bd0a14ae0270e5454856f40dc8f3d2155
Vinayak Lonushte <vinayak.lonushte@krishagni.com>
2015-04-25 13:15:10 +0530

    Minor changes

--- www/app/modules/administrative/form/addedit.js
+++ www/app/modules/administrative/form/addedit.js
@@ -2,8 +2,7 @@ angular.module('os.administrative.form.addedit', ['os.administrative.models'])
   .controller('FormAddEditCtrl', function($scope, $state, $sce, $stateParams) {
     
     function init() {
-      $scope.url = "http://localhost:8180/openspecimen/csd_web/pages/csdUI.html#loadCachedForm/";
-     // $scope.url = "/csd_web/pages/csdUI.html#loadCachedForm/";
+      $scope.url = "csd_web/pages/csdUI.html#loadCachedForm/";
       
       if ($stateParams.formId) {
          $scope.url += $stateParams.formId + "/true ?_reqTime=" +  new Date().getTime();
