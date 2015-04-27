c10f451d9e4d5e7aeaac1a267fa77742efb2abe5
Vinayak Lonushte <vinayak.lonushte@krishagni.com>
2015-04-27 13:52:54 +0530

    Merged administrator form module into biospecimen form module.

--- www/app/modules/administrative/models/module.js
+++ www/app/modules/administrative/models/module.js
@@ -7,7 +7,6 @@ angular.module('os.administrative.models',
     'os.administrative.models.dp',
     'os.administrative.models.container',
     'os.administrative.models.role',
-    'os.administrative.models.form',
     'os.administrative.models.setting',
     'os.administrative.models.authdomain'
   ]
