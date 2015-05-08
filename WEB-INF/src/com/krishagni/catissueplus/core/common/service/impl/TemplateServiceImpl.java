1da5c60841b262f4f7fd3e421fd441e628e23fa8
Satish Lakhani <satish.lakhani@krishagni.com>
2015-05-08 18:24:21 +0530

    Gradle build script with existing directory structure

--- WEB-INF/src/com/krishagni/catissueplus/core/common/service/impl/TemplateServiceImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/service/impl/TemplateServiceImpl.java
@@ -3,8 +3,10 @@ package com.krishagni.catissueplus.core.common.service.impl;
 import java.util.Map;
 
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.ui.velocity.VelocityEngineUtils;
 
+import com.krishagni.catissueplus.core.common.errors.OpenSpecimenException;
 import com.krishagni.catissueplus.core.common.service.TemplateService;
 
 public class TemplateServiceImpl implements TemplateService {
@@ -19,6 +21,11 @@ public class TemplateServiceImpl implements TemplateService {
 	}
 
 	public String render(String templateName, Map<String, Object> properties) {
-		return VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, templateName, properties);
+		try {
+			return VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, templateName, properties);
+		} catch (VelocityException ex) {
+			throw OpenSpecimenException.serverError(ex);
+		}
+		
 	}
 }
