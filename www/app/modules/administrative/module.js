31eea2fd0a14df20ee370fc083bcd8775969b019
vgaikwad <vinod.gaikwad@krishagni.com>
2015-05-20 12:47:11 +0530

    Removed order module dependency, which is not exist

--- www/app/modules/administrative/module.js
+++ www/app/modules/administrative/module.js
@@ -6,7 +6,6 @@ angular.module('os.administrative',
     'os.administrative.institute',
     'os.administrative.site',
     'os.administrative.dp',
-    'os.administrative.order',
     'os.administrative.container',
     'os.administrative.role',
     'os.administrative.form'
