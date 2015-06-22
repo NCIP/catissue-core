3eaafd401c072bd81b3b8021cbb7146fe7d57761
Vinayak Pawar <vinayakapawar@gmail.com>
2015-06-22 15:06:32 +0530

    OPSMN-2051: Bulk import jobs pagination UI
    Cause: At any point in time, bulk import jobs dashboard shows
    max 25 jobs. Sometimes it seems users want to refer previous jobs
    as well for debugging etc.
    
    Fix: Implementated pagination in bulk import jobs dashboard view.
    Max 25 jobs are shown per page. Simple next and previous buttons
    are implemented to allow users navigate through jobs list pages.
    Jobs are ordered based on their order of creation. Therefore
    latest jobs always appear in first page.

--- www/app/modules/common/import/list.js
+++ www/app/modules/common/import/list.js
@@ -5,13 +5,26 @@ angular.module('os.common.import.list', ['os.common.import.importjob'])
     function init() {
       $scope.importJobs = [];
       $scope.importDetail = importDetail;
+      $scope.pagingOpts = {
+        totalJobs: 0,
+        currPage: 1,
+        jobsPerPage: 25
+      };
 
-      loadAllJobs();
+
+      $scope.$watch('pagingOpts.currPage', function() {
+        loadJobs($scope.pagingOpts);
+      });
     };
 
-    function loadAllJobs() {
-      ImportJob.query({objectType: importDetail.objectTypes}).then(
+    function loadJobs(pagingOpts) {
+      var startAt = (pagingOpts.currPage - 1) * pagingOpts.jobsPerPage;
+      var maxResults = pagingOpts.jobsPerPage + 1;
+      ImportJob.query({objectType: importDetail.objectTypes, startAt: startAt, maxResults: maxResults}).then(
         function(importJobs) {
+          pagingOpts.totalJobs = (pagingOpts.currPage - 1) * pagingOpts.jobsPerPage + importJobs.length;
+ 
+          importJobs.splice(importJobs.length - 1, 1);
           $scope.importJobs = importJobs;
           angular.forEach(importJobs, function(job) {
             job.outputFileUrl = ImportJob.url() + job.$id() + '/output';
@@ -19,6 +32,6 @@ angular.module('os.common.import.list', ['os.common.import.importjob'])
         }
       );
     };
- 
+
     init();
   });
