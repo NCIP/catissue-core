6e5e297cd8826eb19db69edfa14bedfcd08035dd
Hemantkambale <hemant.kambale@krishagni.com>
2015-07-27 20:05:31 +0530

    Update en_US.js
    updated en_US.js file to change the field names for singhealth.

--- www/app/custom-modules/sgh/i18n/en_US.js
+++ www/app/custom-modules/sgh/i18n/en_US.js
@@ -4,7 +4,43 @@
     "register_participants": "Register Participants",
     "participant_registered" : "Successfully registered {{participantCount}} participants",
     "print_labels": "Print Labels",
-    "bulk_reg_participants": "Bulk Register Participants",
-    "bulk_reg": "Bulk Register"
+    "bulk_pre_printing": "Bulk Pre Printing",
+    "bulk_reg": "Bulk Register",
+    "trid_printed": "Labels Printed Successfully",
+    "trid_count": "TRID Count",
+    "bulk_printing": "Bulk Print Trids for Unplanned Protocols",
+    "buttons":{
+      "print": "Print"
+    }
+  },
+  
+  "participant": {
+   "id": "MPI / MRN / NRIC/FIN",
+   "uid": "NRIC/FIN",
+   "protocol_id": "TRID", 
+   "ppid": "TRID",
+   "ppids": "TRIDs",
+   "uid_short": "NRIC/FIN",
+   "matching_attr": {
+   "uid": "NRIC/FIN"
+   }
+  },
+  
+  "visits": {
+   "name": "TRID"
+  },
+  
+  "cp": {
+    "ppid_fmt": "TRID Format",
+    "visit_name_fmt": "TRID Format"
+  },
+  
+  "srs": {
+   "collector": "Surgeon/Collector",
+   "receiver": "Harvester/Receiver"
+  },
+  
+  "dp": {
+   "title": "Title/TR Code"
   }
 }
