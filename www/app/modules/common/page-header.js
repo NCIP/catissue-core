f0a9c3695d920768cd5ee9938936ceb702ef8ceb
Vinayak Lonushte <vinayak.lonushte@krishagni.com>
2015-08-04 18:02:41 +0530

    Added changes in page-header directive, so that right btn div take width as much as he wants and left header div take remaining space.

--- www/app/modules/common/page-header.js
+++ www/app/modules/common/page-header.js
@@ -8,11 +8,19 @@ angular.module('openspecimen')
           .addClass('os-nav-button')
           .append('<span class="fa fa-bars"></span>');
 
+        var div = angular.element('<div/>')
+          .css('width', '100%')
+          .append(element.find(".os-btns.right"))
+          .append(element.find(":header"));
+
+        element.append(div);
+
         element.addClass('os-page-hdr').prepend(btn).removeAttr('os-page-header');
         element.find(":header")
           .addClass("os-title os-ellipsis")
           .hover(function() {
-            $(this).attr("title", $(this).text());
+            var that = angular.element(this);
+            that.attr("title", that.text());
           });
 
         if (element.find('.os-breadcrumbs').length == 0) {
