bbda890fddc841a9c14aa0e314ddf1197b0ba43f
Satish Lakhani <satish.lakhani@krishagni.com>
2015-09-02 16:58:11 +0530

    Used static data for DP history as backend is pending

--- www/app/modules/administrative/dp/history.js
+++ www/app/modules/administrative/dp/history.js
@@ -1,16 +1,24 @@
 
 angular.module('os.administrative.dp.history', ['os.administrative.models'])
   .controller('DpHistoryCtrl', function($scope, DistributionOrder, dpId) {
-    $scope.orders = [];
-    
-    var opts = {
-      includeStats: true,
-      dpId: dpId
-    };
-
-    DistributionOrder.query(opts).then(
-      function(orders){
-        $scope.orders = orders;
-      }
-    )
+    $scope.orders = [{
+      "name": "Distributed to Prof Tin",
+      "executionDate": "1441174401000",
+      "specimens": {
+                     "specimenType": "DNA",
+                     "tissueSite": "Lung",
+                     "pathologyStatus": "Malignant"
+                   },
+      "specimenCnt": 20
+    },
+    {
+      "name": "Distributed to Prof Tin",
+      "executionDate": "1362002400000",
+      "specimens": {
+                     "specimenType": "RNA",
+                     "tissueSite": "Lung",
+                     "pathologyStatus": "Malignant"
+                   },
+      "specimenCnt": 508
+    }];
   });
