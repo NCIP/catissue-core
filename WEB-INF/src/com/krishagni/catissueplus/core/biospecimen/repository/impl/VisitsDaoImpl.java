0d075aa298fce486cc626dd08ca88e382fae78a0
Vinayak Pawar <vinayakapawar@gmail.com>
2015-09-09 16:18:09 +0530

    Added an API in visits DAO to obtain multiple visits by names.
    The existing API allows to retrieve only one visit by name at a time.
    Using this API requires dependent code to make multiple database calls
    to obtain multiple visits by name. Therefore to avoid multiple DB
    trips, added an API in visits DAO to obtain visits in bulk by names...

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/repository/impl/VisitsDaoImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/repository/impl/VisitsDaoImpl.java
@@ -3,6 +3,7 @@ package com.krishagni.catissueplus.core.biospecimen.repository.impl;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -74,15 +75,19 @@ public class VisitsDaoImpl extends AbstractDao<Visit> implements VisitsDao {
 	}
 	
 	@Override
-	@SuppressWarnings("unchecked")
 	public Visit getByName(String name) {
-		List<Visit> visits = sessionFactory.getCurrentSession()
-				.getNamedQuery(GET_VISIT_BY_NAME)
-				.setString("name", name)
-				.list();
-		
+		List<Visit> visits = getByName(Collections.singleton(name));
 		return !visits.isEmpty() ? visits.iterator().next() : null;
-	}	
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Visit> getByName(Collection<String> names) {
+		return sessionFactory.getCurrentSession()
+				.getNamedQuery(GET_VISIT_BY_NAME)
+				.setParameterList("names", names)
+				.list();
+	}
 	
 	private String getVisitKey(Long visitId, Long cpeId) {
 		String key = "";
