d299a70e686cd20dcbee01a126818f875aacb711
Akshay <akshayhegade@gmail.com>
2015-09-09 17:53:50 +0530

    OPSMN-2414 : New token for sequence in label within a PPI and specific type
    Description :
    - New token: %PPI_SPEC_TYPE_UID%
    - This generates unique number per specimen under a participant per type of specimen.
    - The first specimen of a type will not get any number, the numbering will start from second specimen as 2

---
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/label/specimen/PpidSpecTypeLabelToken.java
@@ -0,0 +1,35 @@
+package com.krishagni.catissueplus.core.biospecimen.label.specimen;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.krishagni.catissueplus.core.biospecimen.domain.Specimen;
+import com.krishagni.catissueplus.core.biospecimen.repository.DaoFactory;
+
+public class PpidSpecTypeLabelToken extends AbstractSpecimenLabelToken {
+
+	@Autowired
+	private DaoFactory daoFactory;
+
+	public PpidSpecTypeLabelToken() {
+		this.name = "PPI_SPEC_TYPE_UID";
+	}
+
+	public DaoFactory getDaoFactory() {
+		return daoFactory;
+	}
+
+	public void setDaoFactory(DaoFactory daoFactory) {
+		this.daoFactory = daoFactory;
+	}
+
+	@Override
+	public java.lang.String getLabel(Specimen specimen) {
+		String ppid = specimen.getVisit().getRegistration().getPpid();
+		String key = ppid + "_" + specimen.getSpecimenType();
+		Long uniqueId = daoFactory.getUniqueIdGenerator().getUniqueId(name, key);
+
+		return uniqueId == 1L ? StringUtils.EMPTY : uniqueId.toString();
+	}
+
+}
