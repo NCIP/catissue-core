05fff1eee940bf35e3876f13dac98bb196f9e364
vgaikwad <vinod.gaikwad@krishagni.com>
2015-09-23 12:41:39 +0530

    Removed comment

--- www/app/modules/administrative/user/display-name.js
+++ www/app/modules/administrative/user/display-name.js
@@ -1,7 +1,6 @@
 angular.module('os.administrative.user.displayname', ['os.administrative.models'])
   .filter('osUserDisplayName', function() {
     return function(user) {
-      // Last name not exist only for 'System' user.
       if (!user.lastName) {
         return user.firstName;
       }
