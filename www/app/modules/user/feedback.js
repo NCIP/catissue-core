7bdb46a3e800daa650b2c4e8a4338048e60bc519
Satish Lakhani <satish.lakhani@krishagni.com>
2015-10-01 18:33:03 +0530

    Minor changes as per suggestions

--- www/app/modules/user/feedback.js
+++ www/app/modules/user/feedback.js
@@ -1,20 +1,29 @@
 angular.module('openspecimen')
-  .controller('FeedbackCtrl', function($scope, $modalInstance, User, Alerts)  {
-    $scope.feedbackDetail = {};
-    $scope.submit = function() {
-      User.sendFeedback($scope.feedback).then(
-        function(resp) {
-          $modalInstance.close('ok');
-          if(resp.status == 'ok') {
-            Alerts.success('Feedback sent successfully');
-          } else {
-            Alerts.error('Something went wrong, please contact system administrator');
-          }
-        }
-      );
-    }
-    
-    $scope.cancel = function() {
-      $modalInstance.dismiss('cancel');
-    }
+  .controller('FeedbackCtrl', function($scope, $modal) {
+    $scope.openFeedbackForm = function() {
+       var modalInstance = $modal.open({
+         templateUrl: 'modules/user/feedback.html',
+         controller: modalCtrl
+       });
+     }
+     
+     var modalCtrl = function($scope, $modalInstance, User, Alerts) {
+       $scope.feedbackDetail = {};
+       $scope.submit = function() {
+         User.sendFeedback($scope.feedback).then(
+           function(resp) {
+             $modalInstance.close('ok');
+             if(resp.status == 'ok') {
+               Alerts.success('Feedback sent successfully');
+             } else {
+               Alerts.error('Something went wrong, please contact system administrator');
+             }
+           }
+         );
+       }
+       
+       $scope.cancel = function() {
+         $modalInstance.dismiss('cancel');
+       }
+     }
   });
--- www/app/modules/user/signed-in.js
+++ www/app/modules/user/signed-in.js
@@ -1,9 +1,8 @@
 angular.module('openspecimen')
-  .controller('SignedInCtrl', function($scope, $rootScope, $state, $modal, currentUser, Setting, Alerts, AuthorizationService) {
+  .controller('SignedInCtrl', function($scope, $rootScope, currentUser, Alerts, AuthorizationService) {
      function init() {
        $scope.alerts = Alerts.messages;
        $rootScope.currentUser = currentUser;
-       $scope.feedback_enabled = $rootScope.global.appProps.feedback_enabled;
      }
 
      $scope.userCreateUpdateOpts = {resource: 'User', operations: ['Create', 'Update']};
@@ -13,11 +12,4 @@ angular.module('openspecimen')
      $scope.scheduledJobReadOpts = {resource: 'ScheduledJob', operations: ['Read']};
 
      init();
-     
-     $scope.openFeedbackForm = function() {
-       var modalInstance = $modal.open({
-         templateUrl: 'modules/user/feedback.html',
-         controller: 'FeedbackCtrl'
-       });
-     }
   })
