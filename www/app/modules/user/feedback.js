3bea75959e4e38d2878d5899d471980edcbbaea0
vinaylonushte <vinayak.lonushte@krishagni>
2015-10-02 12:58:53 +0530

    Refactord feedback.js

--- www/app/modules/user/feedback.js
+++ www/app/modules/user/feedback.js
@@ -1,27 +1,32 @@
 angular.module('openspecimen')
-  .controller('FeedbackCtrl', function($scope, $modal) {
+  .controller('FeedbackCtrl', function($scope, $modal, User, Alerts) {
+
+    function sendFeedback(feedback) {
+      User.sendFeedback(feedback).then(
+        function(resp) {
+          Alerts.success($translate.instant('feedback.success_message'));
+        }
+      );
+    }
+
     $scope.openFeedbackForm = function() {
        var modalInstance = $modal.open({
          templateUrl: 'modules/user/feedback.html',
-         controller: modalCtrl
-       });
-     }
-     
-     var modalCtrl = function($scope, $translate, $modalInstance, User, Alerts) {
-       $scope.feedbackDetail = {};
-       $scope.submit = function() {
-         User.sendFeedback($scope.feedback).then(
-           function(resp) {
-             $modalInstance.close('ok');
-             if(resp.status == 'ok') {
-               Alerts.success($translate.instant('feedback.success_message'));
-             }
+         controller: function ($scope, $modalInstance) {
+           $scope.submit = function() {
+             $modalInstance.close($scope.feedback);
            }
-         );
-       }
        
-       $scope.cancel = function() {
-         $modalInstance.dismiss('cancel');
-       }
+           $scope.cancel = function() {
+             $modalInstance.dismiss('cancel');
+           }
+         }
+       });
+
+       modalInstance.result.then(
+         function(feedback) {
+           sendFeedback(feedback); 
+         }
+       );
      }
   });
