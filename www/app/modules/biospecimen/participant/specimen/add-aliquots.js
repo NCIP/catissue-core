5f9724d4b40a634df3bc433832eb9cb2bf33d2f7
Vinayak Pawar <vinayakapawar@gmail.com>
2015-10-06 13:02:51 -0700

    Fixed comment. Grouped parse time functions.

--- www/app/modules/biospecimen/participant/specimen/add-aliquots.js
+++ www/app/modules/biospecimen/participant/specimen/add-aliquots.js
@@ -3,30 +3,31 @@ angular.module('os.biospecimen.specimen.addaliquots', [])
   .controller('AddAliquotsCtrl', function(
     $scope, $rootScope, $state, $stateParams, specimen, cpr, visit,
     CollectSpecimensSvc, SpecimenUtil) {
+
     function init() {
       $scope.parentSpecimen = specimen;
       $scope.cpr = cpr;
       $scope.visit = visit;
       $scope.aliquotSpec = {createdOn : new Date()};
 
-      console.warn("$rootScope.stateChangeInfo " + $rootScope.stateChangeInfo);
-      // After collecting specimens, control will come to add-aliquot page
-      // because of the collect-specimen implementation,
-      // where, on success control is returning to previous state
+      //
+      // On successful collection of aliquots, direct user to specimen detail view
+      // TODO: where to go should be state input param
+      //
       if ($rootScope.stateChangeInfo.fromState.url.indexOf("collect-specimens") == 1) {
          var params = {specimenId:  $scope.parentSpecimen.id, srId:  $scope.parentSpecimen.reqId};
          $state.go('specimen-detail.overview', params);
       }
     }
 
+    function getState() {
+      return {state: $state.current, params: $stateParams};
+    }
+
     $scope.collectAliquots = function() {
       var specimens = SpecimenUtil.collectAliquots($scope);
       CollectSpecimensSvc.collect(getState(), $scope.visit, specimens, parent);
-    };
-
-    function getState() {
-      return {state: $state.current, params: $stateParams};
-    };
+    }
 
     init();
   });
