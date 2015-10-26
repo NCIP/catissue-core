0e1df4c7b4bf04570fb9e2a7d16b1a40ff905caf
Chetan Khilosiya <chetan.khilosiya@krishagni.com>
2015-10-26 11:16:51 +0530

    OPSMN-2457: Updated the add text box placeholder message.

--- www/app/modules/i18n/en.js
+++ www/app/modules/i18n/en.js
@@ -1300,7 +1300,7 @@
      "sender_comments": "Sender Comments",
      "details": "Order Details",
      "no_specimens_in_list": "No specimens in order list to distribute. Add at least one specimen",
-     "enter_specimen_label": "To add additional specimens to distribution list, enter labels separated by a comma/tab/newline and click add",
+     "enter_specimen_label": "To add specimens to distribution list, enter labels separated by a comma/tab/newline and click add",
      "specimens_not_found_or_no_access": "One or more specimens could not be loaded either because they do not exists or you do not have sufficient rights to access them",
 
      "report_gen_initiated": "Generating distribution order report...",
