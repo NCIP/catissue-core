5615d12f28f523ebbb92b026393247a1d9d8b7ba
vgaikwad <vinod.gaikwad@krishagni.com>
2015-11-03 16:05:30 +0530

    OPSMN-2568 Visit ordering rules are as follows
    1. Sort based on Event Point
    2. If Event Points are same then sort based on their order of creation (i.e Event Id)
    3. If Event Ids are same (this is when multiple visits for same event), then sort based on Visit Date

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/events/VisitSummary.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/events/VisitSummary.java
@@ -153,12 +153,13 @@ public class VisitSummary implements Comparable<VisitSummary> {
 			return result;
 		}
 		
-		Date thisVisit = visitDate != null ? visitDate : anticipatedVisitDate;
-		Date otherVisit = other.visitDate != null ? other.visitDate : other.anticipatedVisitDate;
-		if (thisVisit.equals(otherVisit)) {
-			return eventId.compareTo(other.eventId);
-		} else {
-			return thisVisit.compareTo(otherVisit);
+		result = eventId.compareTo(other.eventId);
+		if (result != 0) {
+			return result;
 		}
+		
+		Date thisVisitDate = visitDate != null ? visitDate : anticipatedVisitDate;
+		Date otherVisitDate = other.visitDate != null ? other.visitDate : other.anticipatedVisitDate;
+		return thisVisitDate.compareTo(otherVisitDate);
 	}	
 }
