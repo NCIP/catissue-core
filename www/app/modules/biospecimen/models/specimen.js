b844b451d83f403b48b9434c397734d6f7d41b9a
vinaylonushte <vinayak.lonushte@krishagni>
2016-02-12 13:40:22 +0530

    OPSMN-2635: Fixed by processing anticipated pooled specimens attrs.

--- www/app/modules/biospecimen/models/specimen.js
+++ www/app/modules/biospecimen/models/specimen.js
@@ -212,11 +212,11 @@ angular.module('os.biospecimen.models.specimen', ['os.common.models', 'os.biospe
       });
 
       if (sr.children) {
-        sr.children = sr.children.map(
-          function(childSr) {
-            return toSpecimenAttrs(childSr);
-          }
-        );
+        sr.children = sr.children.map(toSpecimenAttrs);
+      }
+
+      if (sr.specimensPool) {
+        sr.specimensPool = sr.specimensPool.map(toSpecimenAttrs);
       }
 
       return sr;
