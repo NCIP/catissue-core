0d39e540a899f347d0be7cad10b76dcf73d13a89
Vinayak Pawar <vinayakapawar@gmail.com>
2016-02-16 13:44:06 +0530

    OPSMN-2737: JS utility converter to convert from alphabet/roman to base 10 numbers

--- www/app/modules/common/number-converter.js
+++ www/app/modules/common/number-converter.js
@@ -11,6 +11,20 @@ angular.module('openspecimen')
       return result;
     };
 
+    function fromAlphabet(str, aCharCode) {
+      var len = str.length;
+      var base = 1, result = 0;
+
+      while (len > 0) {
+        len--;
+
+        result += (str.charCodeAt(len) - aCharCode + 1) * base;
+        base *= 26;
+      }
+
+      return result;
+    };
+
     var romanLookupTab = {
       M: 1000, CM: 900,
       D:  500, CD: 400,
@@ -33,27 +47,79 @@ angular.module('openspecimen')
       return upper ? result : result.toLowerCase();
     };
 
+    function fromRoman(str, upper) {
+      str = str.toUpperCase();
+
+      var result = 0;
+      var len = str.length, idx = str.length;
+      while (idx > 0) {
+        --idx;
+        if (idx == len - 1) {
+          var val = romanLookupTab[str.substring(idx, idx + 1)];
+          if (!val) {
+            alert("Invalid roman number: " + str);
+          }
+
+          result += val;
+        } else {
+          var current = romanLookupTab[str.substring(idx, idx + 1)];
+          var ahead   = romanLookupTab[str.substring(idx + 1, idx + 2)];
+          if (!current || !ahead) {
+            alert("Invalid roman number: " + str);
+          }
+
+          if (current < ahead) {
+            result -= current;
+          } else {
+            result += current;
+          }
+        }
+      }
+
+      return result;
+    }
+
     function toNum(num) {
       return num;
     };
 
+    function fromNum(str) {
+      return +str;
+    }
+
     function toUpperAlphabet(num) {
       return toAlphabet(num, 'A'.charCodeAt(0));
+    };
+
+    function fromUpperAlphabet(str) {
+      return fromAlphabet(str, 'A'.charCodeAt(0));
     };
 
     function toLowerAlphabet(num) {
       return toAlphabet(num, 'a'.charCodeAt(0));
     };
 
+    function fromLowerAlphabet(str) {
+      return fromAlphabet(str, 'a'.charCodeAt(0));
+    };
+
     function toUpperRoman(num) {
       return toRoman(num, true);
+    };
+
+    function fromUpperRoman(str) {
+      return fromRoman(str, true);
     };
 
     function toLowerRoman(num) {
       return toRoman(num, false);
     }
 
-    var convertersMap = {
+    function fromLowerRoman(str) {
+      return fromRoman(str, false);
+    }
+
+    var fromNumConvertersMap = {
       'Numbers'             : toNum,
       'Alphabets Upper Case': toUpperAlphabet,
       'Alphabets Lower Case': toLowerAlphabet,
@@ -61,11 +127,25 @@ angular.module('openspecimen')
       'Roman Lower Case'    : toLowerRoman
     };
 
+    var toNumConvertersMap = {
+      'Numbers'             : fromNum,
+      'Alphabets Upper Case': fromUpperAlphabet,
+      'Alphabets Lower Case': fromLowerAlphabet,
+      'Roman Upper Case'    : fromUpperRoman,
+      'Roman Lower Case'    : fromLowerRoman
+    };
+
     function fromNumber(scheme, number) {
-      return convertersMap[scheme](number);
+      return fromNumConvertersMap[scheme](number);
+    }
+
+    function toNumber(scheme, str) {
+      return toNumConvertersMap[scheme](str);
     }
 
     return {
-      fromNumber: fromNumber 
+      fromNumber: fromNumber,
+
+      toNumber: toNumber
     }
   });
