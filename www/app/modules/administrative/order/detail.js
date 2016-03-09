73e5ca99bd068ee4fe1d2cc41e1ba615b52fa104
Vinayak Pawar <vinayakapawar@gmail.com>
2016-03-09 17:38:15 +0530

    OPSMN-2671: Cleanup of HTML and JS code that is no longer required

--- www/app/modules/administrative/order/detail.js
+++ www/app/modules/administrative/order/detail.js
@@ -1,5 +1,5 @@
 angular.module('os.administrative.order.detail', ['os.administrative.models'])
-  .controller('OrderDetailCtrl', function($scope, $state, order, Util, SpecimensHolder, Alerts) {
+  .controller('OrderDetailCtrl', function($scope, order, Util, Alerts) {
   
     function init() {
       $scope.order = order;
@@ -7,59 +7,10 @@ angular.module('os.administrative.order.detail', ['os.administrative.models'])
       if (!!order.distributionProtocol.report && !!order.distributionProtocol.report.id) {
         $scope.rptTmplConfigured = true;
       }
-
-      $scope.selection = {all: false, items: []};
     }
 
     $scope.downloadReport = function() {
       Util.downloadReport(order, 'orders');
-    };
-
-    $scope.toggleAllItemSelect = function() {
-      if ($scope.selection.all) {
-        $scope.selection.items = [].concat($scope.order.orderItems);
-      } else {
-        $scope.selection.items = [];
-      }
-
-      angular.forEach($scope.order.orderItems, function(item) {
-        item.selected = $scope.selection.all;
-      });
-    }
-
-    $scope.toggleItemSelect = function(item) {
-      if (item.selected) {
-        $scope.selection.items.push(item);
-      } else {
-        var idx = $scope.selection.items.indexOf(item);
-        if (idx != -1) {
-          $scope.selection.items.splice(idx, 1);
-        }
-      }
-
-      $scope.selection.all = ($scope.selection.items.length == $scope.order.orderItems.length);
-    }
-
-    $scope.returnSpecimens = function() {
-      if ($scope.selection.items.length == 0) {
-        Alerts.error('orders.no_specimens_to_return');
-        return;
-      }
-
-      var error = false;
-      angular.forEach($scope.selection.items, function(item) {
-        if (item.status == "RETURNED") {
-          Alerts.error('orders.specimen_already_returned', item.specimen);
-          error = true;
-        }
-      });
-
-      if (error) {
-        return;
-      }
-
-      SpecimensHolder.setSpecimens($scope.selection.items);
-      $state.go('return-specimens', {orderId: order.id});
     };
 
     init();
