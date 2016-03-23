019543fc6eac51ef9cba0e2e6f9485aa244ae238
vgaikwad <vinod.gaikwad@krishagni.com>
2016-03-23 12:10:27 +0530

    OPSMN-2890: Enhancement to filter/list containers based on site where they are located.

--- www/app/modules/administrative/container/list.js
+++ www/app/modules/administrative/container/list.js
@@ -1,9 +1,10 @@
 angular.module('os.administrative.container.list', ['os.administrative.models'])
-  .controller('ContainerListCtrl', function($scope, $state, Container, Util) {
+  .controller('ContainerListCtrl', function($scope, $state, Container, Site, Util) {
 
     function init() {
       $scope.containerFilterOpts = {};
       loadContainers();
+      loadSites();
       Util.filter($scope, 'containerFilterOpts', loadContainers);
     }
 
@@ -13,6 +14,13 @@ angular.module('os.administrative.container.list', ['os.administrative.models'])
           $scope.containerList = containers;
         }
       );
+    }
+
+    function loadSites() {
+      $scope.sites = [];
+      Site.listForContainers().then(function(sites) {
+        $scope.sites = sites;
+      });
     }
 
     $scope.showContainerOverview = function(container) {
