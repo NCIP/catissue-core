95d680e47c261b00d73dc4c5d96661beb9b0b92d
Vinayak Pawar <vinayakapawar@gmail.com>
2016-06-09 09:34:40 +0530

    PLUGINS-91: Do not fix created container ID in view model, as ID is used to determine whether to make create or update API call. In absence of ID, the widget will always make create container API call.

--- www/app/modules/administrative/container/creator.js
+++ www/app/modules/administrative/container/creator.js
@@ -20,6 +20,11 @@ angular.module('os.administrative.container')
       scope.createContainer = function() {
         scope.container.$saveOrUpdate().then(
           function(savedContainer) {
+            //
+            // ID is purposely removed so that same widget can be used for
+            // creation of multiple containers
+            //
+            delete savedContainer.id;
             angular.extend(scope.container, savedContainer);
             Alerts.success('container.creation_success', savedContainer);
           }
