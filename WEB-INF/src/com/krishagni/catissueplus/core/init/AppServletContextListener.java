506ef3ba1fdc0bf5777a99f7cf31bda0414122fa
Vinayak Pawar <vinayakapawar@gmail.com>
2016-06-16 17:26:58 +0530

    OPSMN-3125: OpenSpecimen now ships with default logging configuration, which enables logging at INFO level.
    The log files are created in directory: ${OS_DATA_DIR}/logs
    
    OS_DATA_DIR value is same as that specified during install time for storing
    OpenSpecimen data like uploaded files, exported files etc
    
    Logs are rolled over on nightly basis.  Following is logs file format:
    os.log (current / active log file)
    os.20160615.log
    os.20160614.log
    
    However, system admins or developers, who do not like default logging
    configuration can create their own configuration file and can specify the
    path as value of app_log_conf property in build.properties
    
    OpenSpecimen continuously watches (i.e. once in 60 seconds) for changes in
    user specified logging configuration file like change in logging directory
    or logging level. This means users can now change log level without having
    to restart OpenSpecimen.

--- WEB-INF/src/com/krishagni/catissueplus/core/init/AppServletContextListener.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/init/AppServletContextListener.java
@@ -3,6 +3,7 @@ package com.krishagni.catissueplus.core.init;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
@@ -12,7 +13,9 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import com.krishagni.catissueplus.core.common.PluginManager;
 import com.krishagni.catissueplus.core.common.util.ClassPathUtil;
@@ -21,7 +24,13 @@ public class AppServletContextListener implements ServletContextListener {
 
 	private static final Logger logger = Logger.getLogger(AppServletContextListener.class);
 
-	private static final String APP_PROPS = "application.properties";
+	private static final String APP_PROPS       = "application.properties";
+
+	private static final String DATA_DIR_PROP   = "app.data_dir";
+
+	private static final String LOG_CONF_PROP   = "app.log_conf";
+
+	private static final String DEF_LOG_CONF    = "/default-log4j.properties";
 
 	private static final String PLUGIN_DIR_PROP = "plugin.dir";
 
@@ -34,6 +43,8 @@ public class AppServletContextListener implements ServletContextListener {
 			
 			Properties props = new Properties();
 			props.load(in);
+
+			initLogging(props);
 
 			String pluginDir = props.getProperty(PLUGIN_DIR_PROP);
 			if (StringUtils.isNotBlank(pluginDir)) {
@@ -52,6 +63,33 @@ public class AppServletContextListener implements ServletContextListener {
 	public void contextDestroyed(ServletContextEvent sce) {
 		// TODO Auto-generated method stub
 
+	}
+
+	private void initLogging(Properties props)
+	throws Exception {
+		URL url;
+		String logConf = props.getProperty(LOG_CONF_PROP);
+		if (StringUtils.isNotBlank(logConf)) {
+			url = new File(logConf).toURI().toURL();
+		} else {
+			url = this.getClass().getResource(DEF_LOG_CONF);
+		}
+
+		String dataDir = props.getProperty(DATA_DIR_PROP);
+		if (StringUtils.isBlank(dataDir)) {
+			dataDir = ".";
+		}
+
+		String logDir = dataDir + File.separator + "logs";
+		new File(logDir).mkdirs();
+		System.setProperty("os_log_dir", logDir);
+
+		BasicConfigurator.resetConfiguration();
+		PropertyConfigurator.configure(url);
+		logger.info("Initialised logging configuration from following file: " + url);
+		if (url.getProtocol().equals("file")) {
+			PropertyConfigurator.configureAndWatch(url.getFile());
+		}
 	}
 
 	private void loadPluginResources(String pluginDir) 
