a867de821931d1f98f96bde6d072eb91c5d55227
Vinayak Pawar <vinayakapawar@gmail.com>
2016-06-22 13:55:37 +0530

    PLUGINS-103: Enhanced container create widget to show container type dropdown values based on those configured in metadata. If allowed types is not configured, all provisioned/created types are shown.
    e.g:
    
    {
      type: 'storage-container',
      data: {
        allowedTypes: [
          'Rack',
          'Box'
        ]
      }
    }

--- www/app/modules/administrative/container/creator.js
+++ www/app/modules/administrative/container/creator.js
@@ -1,14 +1,34 @@
 angular.module('os.administrative.container')
   .directive('osContainerCreator', function(Site, Container, ContainerType, Alerts) {
+    function selectType(container, types) {
+      if (types.length != 1) {
+        return;
+      }
+
+      container.typeName = types[0].name;
+    }
+
     function linker(scope, element, attrs) {
       scope.container  = new Container(scope.container || {});
        
-      scope.types = []; 
-      ContainerType.query().then(
-        function(types) {
-          scope.types = types;
-        }
-      );
+      var data = {};
+      if (!!attrs.data) {
+        data = JSON.parse(attrs.data);
+      }
+
+      var allowedTypes = data.allowedTypes || [];
+      if (allowedTypes.length == 0) {
+        scope.types = [];
+        ContainerType.query().then(
+          function(types) {
+            scope.types = types;
+            selectType(scope.container, scope.types);
+          }
+        );
+      } else {
+        scope.types = allowedTypes.map(function(type) { return {name: type}; });
+        selectType(scope.container, scope.types);
+      }
 
       scope.sites = [];
       Site.listForContainers('Create').then(
