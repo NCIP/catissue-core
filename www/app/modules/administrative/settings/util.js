af24a2a9d4bffd76a73806c4cefc91b0b775ed8e
Vinayak Pawar <vinayakapawar@gmail.com>
2016-08-26 12:38:59 +0530

    OPSMN-3309: Settings API responses are cached to avoid inundating server with redundant/spurious requests to read mostly static/constant values. However, the problem is failed API responses are also cached. The API could fail for any number of reasons, expired auth token being one such example. As a result, the views depending on these settings never get resolved giving an impression to end user that the app is unresponsive...
    Fix: Cache only successful settings API responses.

--- www/app/modules/administrative/settings/util.js
+++ www/app/modules/administrative/settings/util.js
@@ -3,22 +3,34 @@ angular.module('os.administrative.setting.util', [])
     var settings = {};
 
     function getSetting(module, property) {
-      var setting = settings[module + ":" + property];
+      var key = module + ':' + property;
+
+      var setting = settings[key];
       if (!setting || (new Date().getTime() - setting.time) / (60 * 60 * 1000) >= 1) {
         setting = {
           promise: Setting.getByProp(module, property),
           time: new Date().getTime()
         };
-        settings[module + ":" + property] = setting;
+        settings[key] = setting;
       }
+
+      setting.promise.catch(
+        function() {
+          //
+          // rejected promise, remove it from cache so that it can
+          // be loaded again in subsequent invocations...
+          //
+          delete settings[key];
+        }
+      );
 
       return setting.promise;
     }
 
     function clearSetting(module, property) {
-      var setting = settings[module + ":" + property];
-      if (setting) {
-        delete settings[module + ":" + property];
+      var key = module + ':' + property;
+      if (settings[key]) {
+        delete settings[key];
       }
     }
 
