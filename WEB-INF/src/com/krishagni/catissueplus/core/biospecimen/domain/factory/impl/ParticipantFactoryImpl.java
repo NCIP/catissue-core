9e8406a2ca554ce43fdb6b266fdcd541e7f59392
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2016-11-16 10:59:49 +0530

    OPSMN-3414:Updated participant factory to always set the birth/death date whatever be the inut given by the user. Also added a guard to ensure death date cannot be specified in future

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/domain/factory/impl/ParticipantFactoryImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/domain/factory/impl/ParticipantFactoryImpl.java
@@ -80,8 +80,7 @@ public class ParticipantFactoryImpl implements ParticipantFactory {
 		setEmpi(detail, participant, partial, ose);
 		setName(detail, participant, partial, ose);
 		setVitalStatus(detail, participant, partial, ose);
-		setBirthDate(detail, participant, partial, ose);
-		setDeathDate(detail, participant, partial, ose);
+		setBirthAndDeathDate(detail, participant, partial, ose);
 		setActivityStatus(detail, participant, partial, ose);
 		setSexGenotype(detail, participant, partial, ose);
 		setGender(detail, participant, partial, ose);
@@ -175,42 +174,32 @@ public class ParticipantFactoryImpl implements ParticipantFactory {
 		participant.setVitalStatus(vitalStatus);
 	}
 
-	private void setBirthDate(ParticipantDetail detail, Participant participant, boolean partial, OpenSpecimenException oce) {
-		if (partial && !detail.isAttrModified("birthDate")) {
-			return;
+	private void setBirthAndDeathDate(ParticipantDetail detail, Participant participant, boolean partial, OpenSpecimenException oce) {
+		if (!partial || detail.isAttrModified("birthDate")) {
+			Date birthDate = detail.getBirthDate();
+			if (birthDate != null && birthDate.after(Calendar.getInstance().getTime())) {
+				oce.addError(ParticipantErrorCode.INVALID_BIRTH_DATE);
+				return;
+			}
+
+			participant.setBirthDate(birthDate);
 		}
-				
-		Date birthDate = detail.getBirthDate();
-		
-		if (birthDate == null) {
-			return;
+
+		if (!partial || detail.isAttrModified("deathDate")) {
+			Date deathDate = detail.getDeathDate();
+
+			if (deathDate != null && deathDate.after(Calendar.getInstance().getTime())) {
+				oce.addError(ParticipantErrorCode.INVALID_DEATH_DATE);
+			}
+
+			// TODO: how do we set vital status to dead now?
+			participant.setDeathDate(deathDate);
 		}
-		
-		if (birthDate.after(Calendar.getInstance().getTime())) {
-			oce.addError(ParticipantErrorCode.INVALID_BIRTH_DATE);
-			return;
-		}
-		
-		participant.setBirthDate(birthDate);
-	}
-	
-	private void setDeathDate(ParticipantDetail detail, Participant participant, boolean partial, OpenSpecimenException oce) {
-		if (partial && !detail.isAttrModified("deathDate")) {
-			return;
-		}
-				
-		Date deathDate = detail.getDeathDate();		
-		if (deathDate == null) {
-			return;
-		}
-		
-		if (participant.getBirthDate() != null && deathDate.before(participant.getBirthDate())) {
+
+		if (participant.getBirthDate() != null && participant.getDeathDate() != null &&
+			participant.getBirthDate().after(participant.getDeathDate())) {
 			oce.addError(ParticipantErrorCode.INVALID_DEATH_DATE);
 		}
-		
-		// TODO: how do we set vital status to dead now?
-		
-		participant.setDeathDate(deathDate);
 	}
 
 	private void setActivityStatus(ParticipantDetail detail, Participant participant, boolean partial, OpenSpecimenException oce) {
