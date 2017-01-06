e16d968788cb050f8a8a98861c0f4785f3f601e3
Vinayak Pawar <vinayakapawar@gmail.com>
2017-01-06 13:53:57 +0530

    OPSMN-3514: Displayed consent signature/witness/comments section in UI irrespective of whether the CP has consent tiers configured for it or not.  This ensures users can do exactly same operations as they would via bulk import i.e. set only consent date or consent signature or consent witness or combination of these.

--- www/app/modules/biospecimen/participant/consents.js
+++ www/app/modules/biospecimen/participant/consents.js
@@ -8,8 +8,6 @@ angular.module('os.biospecimen.participant.consents', [])
       $scope.consent = consent;
       $scope.uploadMode = false;
       $scope.editMode = false;
-      $scope.consentExists = $scope.consent.consentTierResponses.length > 0;
-
       loadPvs();
     }
 
