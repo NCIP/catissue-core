d1d6051c54e50ad4d9ba89a4aacfcdca2704420a
Vinayak Pawar <vinayakapawar@gmail.com>
2017-02-06 11:42:46 +0530

    OPSMN-3640: Auto bulk import jobs that consisted of records/objects spanning multiple rows of input CSV file were failing as the job's CSV type was always SINGLE_ROW_PER_OBJ. This meant auto bulk import of consents, shipping, distribution order never worked!
    This bug was caused because of incorrect check that didn't consider input file's
    extension while tokenising its filename.
    
    Fixed bug by dropping extension from filename before using it for tokenising to
    determine - object, operation type, and CSV type.

--- WEB-INF/src/com/krishagni/catissueplus/core/importer/services/impl/ImportRecordsTask.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/importer/services/impl/ImportRecordsTask.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -141,7 +142,7 @@ public class ImportRecordsTask implements ScheduledTask {
 	}
 
 	private ImportDetail getImportDetail(File file, String fileId) {
-		String filename = file.getName();
+		String filename = FilenameUtils.getBaseName(file.getName());
 		String[] tokens = filename.split("_");
 
 		int i = 0;
